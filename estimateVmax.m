d68c216a64e03c7846a4eccd9ace6277a010af7e
shyamsrinivasan <shyamsweth@gmail.com>
2015-08-25 00:29:49 -0400

    Home addition

--- estimateVmax.m
+++ estimateVmax.m
@@ -8,6 +8,16 @@ mu_g = 0.1;
 %initial mu calculated
 mu_c = biomass_flux(model,met,[],flux);
 %unknown fluxes
-while  
+while abs(mu_c-mu_g)<=1e-4
+    %g6p - always first
+    vglc = strcmpi(model.rxns,'exGLC');
+    g6p = strcmpi(model.mets,'g6p[c]');
+    vg6p = model.S(g6p,:)>0;
+    [~,vcontr] = ConvenienceKinetics(model,pmeter,MC,vg6p);
+    Vmax(vg6p) = flux(vglc)/vcontr;
+    %rxnlist
+    rxnlist = {'
+    
+end
 
 %everything else
