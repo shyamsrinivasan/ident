96b02ae203f2fa24bc4da3d2c0d72e612bcd51b9
shyamsrinivasan <shyamsweth@gmail.com>
2016-02-19 14:41:27 -0500

    Lab Update 19Feb

--- Untitled.m
+++ Untitled.m
@@ -1,27 +1,6 @@
-%
-S = model.S;
-Keq = model.Keq;
-Vss = model.Vss;
+o2c = 10; %mM
+o2e = 0:0.01:1000; %mM
+vo2t = (469.*o2e)./(1+o2e/ko2e+o2c/ko2c);
 
-%remove reactions with zero fluxes
-S(:,abs(Vss)<1e-6) = [];
-Keq(abs(Vss)<1e-6) = [];
-Vss(Vss<1e-6) = [];
-
-%nonzero reactions for thermodynamic analysis
-[nmet,nrxn] = size(S);
-
-%Ax <=b 
-A = S';
-vsign = repmat(sign(Vss),1,nmet);
-A = vsign.*A;
-
-x = zeros(nmet,1);
-b = vsign.*log(Keq);
-lb = zeros(nmet,1);
-lb(lb==0) = log(1e-6);
-ub = zeros(nmet,1);
-ub(ub==0) = log(1000);
-cprod = sparse(1,nmet);
-
-[x,xobj,flag] = cplexlp(cprod(:),A,b,[],[],lb,ub);
+hold on
+plot(o2e,vo2t,'r-');
