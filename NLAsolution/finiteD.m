3453bf19b88ef9e8aae6ee609a4309313cbe8b70
shyamsrinivasan <shyamsweth@gmail.com>
2016-04-25 17:00:03 -0400

    changes to finiteD - working FWD Difference

--- NLAsolution/finiteD.m
+++ NLAsolution/finiteD.m
@@ -20,6 +20,7 @@ function FD_C
 % nothing yet to see here
 
 function J = FD_F(Jpatt,x,fh)
+eps = 1e-3;
 % rows function evaluation
 % columns state variable vector
 nr = size(Jpatt,1);
@@ -29,18 +30,20 @@ nr = size(Jpatt,1);
 rws = sortrows([rw cl],1);
 % work on rws
 Jsp = zeros(size(rws,1),1);
+cnt = 1;
 for irow = 1:size(Jpatt,1)
     fm = fh(irow,x);
-    cls = rws(rws(:,1)==irow,2);
+    cls = rws(rws(:,1)==irow,2);    
     for jcls = 1:length(cls)
         xmod = x;
-        xmod(cls(jcls)) = xmod(cls(jcl))+eps;
+        xmod(cls(jcls)) = xmod(cls(jcls))+eps;
+        fprintf('%d -- %d\n',irow,cls(jcls));
         fj = fh(irow,xmod);
         Jsp(cnt) = (fj-fm)/eps;
         cnt = cnt + 1;
     end
 end
-J = sparse(rws(1),rws(2),Jsp,nr,nr);
+J = sparse(rws(:,1),rws(:,2),Jsp,nr,nr);
 
 % Jij = (f(x+e,p)-f(x,p))/e
 % Jij = (fi([x1 x2 ... xj+e ... xn]) - fi([x1 x2 ... xj ... xn]))/e
