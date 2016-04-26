3398ad7230e6a99de9d5a6d13663cfa83aef8cc7
shyamsrinivasan <shyamsweth@gmail.com>
2016-04-26 10:03:10 -0400

    Supports plotting of flux envelopes for any 2 pair of fluxes

--- FluxEnvelope.m
+++ FluxEnvelope.m
@@ -1,37 +1,48 @@
+% draw production envelope between any 2 fluxes 'flux1'(x-axis - typically 
+% biomass) and 'flux2'(y-axis - typically target)
+% flux1 and flux2 can be a cell array of strings or just double indices 
 
-function [hsubfig,prxnid,flag] = FluxEnvelope(model,flux1id,flux2id,ess_rxn,varname)
-if nargin < 5
-    rxnid = 1:model.nt_rxn;    
-else   
-    rxnid = cellfun(@(x)strcmpi(x,varname),model.rxns,'UniformOutput',false);
-    rxnid = cell2mat(cellfun(@(x)any(x),rxnid,'UniformOutput',false));
-    rxnid = find(rxnid);    
-end
-if nargin < 4
+function [hsubfig,prxnid,flag] = FluxEnvelope(model,fluxid,ess_rxn)
+if nargin < 3
     ess_rxn = {};
 end
-if nargin < 3
-    error('flenlope:flux2id','Need atleast 2 fluxes 2 draw an envelope');
+if nargin < 2
+    error('flenlope:flux2id','Specify fluxes to draw an envelope');
+else
+    if size(fluxid,2)<2
+        error('flenlope:flux2id','Need atleast 2 fluxes to draw an envelope');
+    end
 end
-
-if iscell(flux1id)
-    flux1id = find(strcmpi(model.rxns,flux1id{1}));
-end
-if iscell(flux2id)
-    flux2id = find(strcmpi(model.rxns,flux2id{1}));
-end
-
-nrxn = length(rxnid);
+if iscell(fluxid)
+    flux1id = fluxid(:,1);    
+    flux1id = cellfun(@(x)strcmpi(x,flux1id),model.rxns,'UniformOutput',false);
+    flux1id = cell2mat(cellfun(@(x)any(x),flux1id,'UniformOutput',false));
+    flux1id = find(flux1id);
     
+    flux2id = fluxid(:,2);
+    flux2id = cellfun(@(x)strcmpi(x,flux2id),model.rxns,'UniformOutput',false);
+    flux2id = cell2mat(cellfun(@(x)any(x),flux2id,'UniformOutput',false));
+    flux2id = find(flux2id);
+    
+    if length(flux2id)<length(flux1id)
+        flux2id = repmat(flux2id,length(flux1id),1);
+    end
+    if length(flux1id)<length(flux2id)
+        flux1id = repmat(flux1id,length(flux2id),1);
+    end
+elseif ~iscell(fluxid)
+    flux1id = fluxid(:,1);
+    flux2id = fluxid(:,2);
+end
+
+% number of different envelopes (x(flux1)-y(flux2) combinations)
+nrxn = size(fluxid,1);  
+nt_rxn = size(model.S,2);
 flag = 1;
-% v0 = vLP;
-% load('C:\Users\shyam\Documents\Courses\CHE 1125 Project\Kinetic Model\N2m.mat');
-
 npts = 100;
-% rxnid = setdiff(1:model.nt_rxn,[model.bmrxn model.Vupind]);
 
-vLPmxAll = zeros(model.nt_rxn,npts,nrxn);
-vLPmnAll = zeros(model.nt_rxn,npts,nrxn);
+vLPmxAll = zeros(nt_rxn,npts,nrxn);
+vLPmnAll = zeros(nt_rxn,npts,nrxn);
 Maxtarget = zeros(1,npts,nrxn);
 Mintarget = zeros(1,npts,nrxn);
 flval = zeros(npts,nrxn);
@@ -39,60 +50,50 @@ flval = zeros(npts,nrxn);
 % fix flux bounds as in FBA for uptake fluxes
 [model,bounds] = changebounds(model,ess_rxn);
 
-% Find maximum/minimum allowable flux 1 (x-axis for envelope)
-[LP1max,LP1min] = solveLP(model,bounds,ess_rxn,flux1id);
-if LP1max.flag > 0 && LP1min.flag > 0
-    fprintf('\nMaximum Allowable %s flux = %2.3g h-1\n',model.rxns{flux1id},-LP1max.obj);    
-    fprintf('Minimum Allowable %s flux = %2.3g h-1\n',model.rxns{flux1id},LP1min.obj);
-end
-% if model.gmax > -gMax
-%     model.gmax = -gMax;
-% end
-
-% Find maximum/minimum allowable flux 2 (y-axis for envelope)
-[LP2max,LP2min] = solveLP(model,bounds,ess_rxn,flux2id);
-if LP2max.flag > 0 && LP2min.flag > 0
-    fprintf('\nMaximum allowable %s flux = %2.3g h-1\n',model.rxns{flux2id},-LP2max.obj);    
-    fprintf('Minimum allowable %s flux = %2.3g h-1\n',model.rxns{flux2id},LP2min.obj);
-end
+for irxn = 1:nrxn
     
-for ifl = 1:nrxn
-    %Uptake Flux
-%     bounds.Vuptake = model.Vuptake;
-%     bounds.vl = zeros(model.nt_rxn,1);
-%     % bounds.vl(bounds.vl==0) = -1;
-% %     bounds.vl(logical(model.rev)) = -100;
-%     bounds.vl(bounds.vl==0) = -100;   
-%     bounds.vu = zeros(model.nt_rxn,1);          
-%     %Corresponding flux bounds
-%     bounds.vu(bounds.vu==0) = 100;
-    % Determine Max and Min for flux to be constrained with =
-    [LPmax,LPmin] = solveLP(model,bounds,ess_rxn,rxnid(ifl));
-    if LPmax.flag > 0 && LPmin.flag > 0
-        flval(:,ifl) = linspace(LPmin.obj,-LPmax.obj,npts);
-    else
-        flval(:,ifl) = [];
+    % Find maximum/minimum allowable flux 1 (x-axis for envelope)
+    [LP1max,LP1min] = solveLP(model,bounds,ess_rxn,flux1id(irxn));
+    if LP1max.flag > 0 && LP1min.flag > 0
+        fprintf('\nMaximum Allowable %s flux = %2.3g h-1\n',...
+            model.rxns{flux1id(irxn)},-LP1max.obj);    
+        fprintf('Minimum Allowable %s flux = %2.3g h-1\n',...
+            model.rxns{flux1id(irxn)},LP1min.obj);
     end
-    for iv = 1:size(flval,1)
-        bounds.vl(rxnid(ifl)) = flval(iv,ifl);
-        bounds.vu(rxnid(ifl)) = flval(iv,ifl);        
+    
+    % Find maximum/minimum allowable flux 2 (y-axis for envelope)
+    [LP2max,LP2min] = solveLP(model,bounds,ess_rxn,flux2id(irxn));
+    if LP2max.flag > 0 && LP2min.flag > 0
+        fprintf('\nMaximum allowable %s flux = %2.3g h-1\n',...
+            model.rxns{flux2id(irxn)},-LP2max.obj);    
+        fprintf('Minimum allowable %s flux = %2.3g h-1\n',...
+            model.rxns{flux2id(irxn)},LP2min.obj);
+    end
+    
+    % Determine Max and Min for flux to be constrained with =
+    if LP1max.flag > 0 && LP1min.flag > 0
+        flval(:,irxn) = linspace(LP1min.obj,-LP1max.obj,npts);
+    else
+        flval(:,irxn) = [];
+    end
+    
+    % fix flux1 at each of flval(ivalue) and max/min flux2
+    for iv = 1:npts
+        bounds.vl(flux1id(irxn)) = flval(iv,irxn);
+        bounds.vu(flux1id(irxn)) = flval(iv,irxn);        
 
-        [LPmax,LPmin] = solveLP(model,bounds,ess_rxn,flux2id);
+        [LPmax,LPmin] = solveLP(model,bounds,ess_rxn,flux2id(irxn));
         if ~isempty(LPmax.v) && LPmax.flag > 0
-            vLPmxAll(:,iv,ifl) = LPmax.v(1:model.nt_rxn);
-            Maxtarget(1,iv,ifl) = -LPmax.obj;            
+            vLPmxAll(:,iv,irxn) = LPmax.v(1:nt_rxn);
+            Maxtarget(1,iv,irxn) = -LPmax.obj;            
         end
         if ~isempty(LPmin.v) && LPmin.flag > 0
-            vLPmnAll(:,iv,ifl) = LPmin.v(1:model.nt_rxn);
-            Mintarget(1,iv,ifl) = LPmin.obj;
+            vLPmnAll(:,iv,irxn) = LPmin.v(1:nt_rxn);
+            Mintarget(1,iv,irxn) = LPmin.obj;
         end
-        vLPmax = [];
-        vLPmin = [];
-        fmax = [];
-        fmin = [];
     end
 end
-
+    
 %Infeasible Problem
 if ~any(Maxtarget)
     hsubfig = [];
@@ -108,46 +109,39 @@ else
     nplots = nrxn+1;
 end
 nrows = nplots/2;
-if length(varname)>1
+if nrxn > 1
     ncol = 2;
 else
     ncol=1;
 end
 % fig_name = texlabel(['Flux Envelope \mu = ' num2str(model.gmax) 'h^{-1}']);
-fig_name = sprintf('Flux Envelope %g',model.gmax);
+fig_name = sprintf('Flux Envelope');
 if isempty(findobj('type','figure','Name',fig_name))
     hfig = figure('Name',fig_name); 
     figure(hfig);
 end
-hsubfig = zeros(model.nt_rxn,1);
-ifl = 1;%1,2,3...,nplots
-%trxid = rxid(ifl);%1,2,3,...,nt_rxn
+hsubfig = zeros(nt_rxn,1);
+ifl = 1; % 1,2,3...,nplots
+
 while ifl <= nrxn
 % for ifl = 2:(nrxn)
-    if hsubfig(rxnid(ifl)) ~= 0
+    if hsubfig(flux1id(ifl)) ~= 0
 %     if hsubfig(ifl) ~= 0
-        hca = findobj(hsubfig(rxnid(ifl)),'type','axes');
+        hca = findobj(hsubfig(flux1id(ifl)),'type','axes');
 %         hca = findobj(hsubfig(ifl),'type','axes');
         set(hfig,'CurrentAxes',hca);  
-    else%subplot is unassigned
-        hsubfig(rxnid(ifl)) = subplot(nrows,ncol,ifl);
+    else % subplot is unassigned
+        hsubfig(flux1id(ifl)) = subplot(nrows,ncol,ifl);
 %         hsubfig(ifl) = subplot(nrows,2,ifl-1);   
         hca = gca;
-        %Make sure more plots can be added at end of loop
+        % Make sure more plots can be added at end of loop
         set(hca,'NextPlot','add');     
-        set(hsubfig(rxnid(ifl)),'NextPlot','add');
+        set(hsubfig(flux1id(ifl)),'NextPlot','add');
     end    
-    ylabel = sprintf('Flux %s \n mmole/mmole uptake',model.rxns{prxnid});
-%     if ifl == 11
-%         hline = plot([-flval fliplr(-flval)]./model.Vuptake,...
-%                  [-Maxtarget(1,:,ifl) fliplr(Mintarget(1,:,ifl))]./model.Vuptake);
-%         xlabel = sprintf('Flux %s',model.rxns{5});
-%     else
-        hline = plot([flval(:,ifl)' fliplr(flval(:,ifl)')],...
-                 [-Maxtarget(1,:,ifl) fliplr(Mintarget(1,:,ifl))]);
-        xlabel = sprintf('Flux %s \n mmole/mmole uptake',model.rxns{rxnid(ifl)});
-%         xlabel = sprintf('Flux %s',model.rxns{ifl});
-%     end
+    ylabel = sprintf('Flux %s \n mmole/mmole uptake',model.rxns{flux2id(ifl)});
+    hline = plot([flval(:,ifl)' fliplr(flval(:,ifl)')],...
+             [-Maxtarget(1,:,ifl) fliplr(Mintarget(1,:,ifl))]);
+    xlabel = sprintf('Flux %s \n mmole/mmole uptake',model.rxns{flux1id(ifl)});
     set(hline,'LineWidt',2,...
               'Color',[0 0 0]);
     set(get(gca,'YLabel'),'String',ylabel);
@@ -157,11 +151,11 @@ while ifl <= nrxn
 end
 
 %Annotation TextBox
-set(hfig,'CurrentAxes',hsubfig(rxnid(1)));
-h = annotation('textbox',[.3 .9 .1 .1],...
-               'String',{['$\mu$ = ' num2str(model.gmax) '$h^{-1}$'],...
-                         ['Vuptake = ' num2str(model.Vuptake) ' mmole/gDCW.h']},...
-                'Color',[0 0 0],...
-                'BackgroundColor',[1 1 1]);
+% set(hfig,'CurrentAxes',hsubfig(flux1id(1)));
+% h = annotation('textbox',[.3 .9 .1 .1],...
+%                'String',{['$\mu$ = ' num2str(model.gmax) '$h^{-1}$'],...
+%                          ['Vuptake = ' num2str(model.Vuptake) ' mmole/gDCW.h']},...
+%                 'Color',[0 0 0],...
+%                 'BackgroundColor',[1 1 1]);
 
 return
