439256d4f34efce057b44e24bd6f9976c0fce3ec
shyamsrinivasan <shyamsweth@gmail.com>
2016-06-02 17:01:25 -0400

    Changes corresponding to addition of solveODEonly.m

--- KineticModel/Kotte2014/solveEquilibriumODE.m
+++ KineticModel/Kotte2014/solveEquilibriumODE.m
@@ -2,49 +2,18 @@
 % script for running ODEs and MATCONT for multiple sets of parameters
 % regardless of the source of parameter samples
 % should be run from Kotte2014_Script
+[allxdyn,allxeq,allfdyn,allfeq] =...
+ solveODEonly(npts,M,model,allpvec,opts,tspan,...
+              allxdyn,allxeq,allfdyn,allfeq);
 
 for ipt = 1:npts
-    fprintf('\nIteration #%d Equilibrium Integration...',ipt);
-    % change in pvec
-    pvec = allpvec(ipt,:);
-%     pvec(idp) = allpvec(ipt,:);
-    
-    if ipt == 1001
-        dbstop at 34
-    end
-    % new equilibrium solution
-    givenModel = @(t,x)KotteODE(t,x,model,pvec);
-    [tout,yout] = ode45(givenModel,tspan,M,opts);
-    allxdyn(:,:,ipt) = yout';
-    allxeq(:,ipt) = yout(end,:)';       
-    
-    gfun = @(x)Kotte_givenNLAE(x,model,pvec);
-    options = optimoptions('fsolve','Display','iter',...
-                                    'TolFun',1e-12,'TolX',1e-12,...
-                                    'MaxFunEvals',10000,...
-                                    'MaxIter',5000);
-%     [xf,fval,exitflag,output,jacobian] = fsolve(gfun,M,options);
-    xeq = yout(end,:)';
-     
-%     allxf(:,ipt) = xf;
-%     allflag(1,ipt) = exitflag;
-%     xeq = xf;
-    
-    % calculation of fluxes for allxeq
-    allfeq(:,ipt) = Kotte_givenFlux([allxeq(:,ipt);model.PM],pvec,model); 
-    for itout = 1:length(tout)
-        allfdyn(:,itout,ipt) = Kotte_givenFlux([allxdyn(:,itout,ipt);model.PM],pvec,model); 
-    end
-    plotKotteVariables(tout,yout,1);
-%     plotKotteVariables(tout,allfdyn(:,:,ipt)',2);
-    fprintf('Complete\n');
-    
-    % continuation from initial equilibrium - initialization
     fprintf('Iteration #%d Equilibrium Continuation...\n',ipt);
+    
+    ap = 9;
+    xeq = allxeq(:,ipt);
+    pvec = allpvec(ipt,:);
+    
     % run MATCONT
-%     clear sys x0 v0 ap opt x1 v1 s1 h1 f1
-    ap = 9; % index for parameter to be continued on     
-%     runMATCONT
     [data,y,p] = execMATCONT(xeq,pvec,ap,fluxg,model);
     
     % save MATCONT results
@@ -57,6 +26,15 @@ for ipt = 1:npts
         
     fprintf('Equilibrium Continuation Complete\n');
 end
+
+% using fsolve to determine equilibrium points    
+%     gfun = @(x)Kotte_givenNLAE(x,model,pvec);
+%     options = optimoptions('fsolve','Display','iter',...
+%                                     'TolFun',1e-12,'TolX',1e-12,...
+%                                     'MaxFunEvals',10000,...
+%                                     'MaxIter',5000);
+%     [xf,fval,exitflag,output,jacobian] = fsolve(gfun,M,options);
+
 
 % check which solutions have mss
 mssid = [];
