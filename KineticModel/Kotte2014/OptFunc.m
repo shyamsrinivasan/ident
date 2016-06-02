da34cc062373555d0728a19bb8fb951ede21bd40
shyamsrinivasan <shyamsweth@gmail.com>
2016-06-02 15:49:14 -0400

    function recording all details of optimization problem

--- KineticModel/Kotte2014/OptFunc.m
+++ KineticModel/Kotte2014/OptFunc.m
@@ -1,13 +1,13 @@
-function out = OptFunc(x,model,pvec)
+function out = OptFunc(model,pvec)
 out{1} = @(x)objval(x,model,pvec);
 out{2} = @(x)objgrad(x,model,pvec);
 out{3} = @(x)nlconstr(x,model,pvec);
 out{4} = @(x)nlconstrjac(x,model,pvec);
-out{5} = @()nlconstrjacsparsity(model);
+out{5} = @()nlconstrjacsparsity(model,pvec);
 
 function z = objval(x,model,pvec)
 mc = x(1:3);
-pvec = cons([pvec x(4:end)],x);
+pvec = cons([pvec x(4:end)'],x);
 if ~isempty(model)
     PM = cons(model.PM,mc);
     allmc = [mc;PM];
@@ -20,29 +20,39 @@ z = zeros(1,1);
 z = cons(z,allmc);
 mss_pepout = 1.9;
 mss_pepout = cons(mss_pepout,allmc);
-z(1) = flux(pepout) - mss_pepout;
+z(1) = (flux(pepout) - mss_pepout)^2;
 
-function grad = objgrad(x,model)
+function grad = objgrad(x,model,pvec)
 xADobj = deriv(x,eye(length(x)));
-ADfun = @(x)objval(x,model);
+ADfun = @(x)objval(x,model,pvec);
 xADres = ADfun(xADobj);
 grad = getydot(xADres);
 
-function A = nlconstr(x,model)
+function A = nlconstr(x,model,pvec)
 mc = x(1:3);
-pvec = x(4:end);
+pvec = cons([pvec x(4:end)'],x);
 A = Kotte_givenNLAE(mc,model,pvec);
 
-function jac = nlconstrjac(x,model)
+function jac = nlconstrjac(x,model,pvec)
+% method 1 - jacobian using ADMAT direct way
 xADobj = deriv(x,eye(length(x)));
-ADfun = @(x)nlconstr(x,model);
+ADfun = @(x)nlconstr(x,model,pvec);
 xADres = ADfun(xADobj);
-jac = getydot(xADres); 
+jac = sparse(getydot(xADres)); 
 
-function jspr = nlconstrsparsity(model)
-Jpatt = modelSparsity(model);
-Jmc = Jpatt(1:3,1:3);
-% Jpvec = sparse([
+% method 2 - jacobian using ADMAT sparse jacobians
+% Extra.fh = @(x)nlconstr(x,model,pvec);
+% jpatt = getjpi('ADMATjaceg',3,6,Extra,'f');
+% [~,jac] = evalj('ADMATjaceg',x,Extra,3,jpatt);
+
+
+function jstr = nlconstrjacsparsity(model,pvec)
+Extra.fh = @(x)nlconstr(x,model,pvec);
+[~,jstr] = getjpi('ADMATjaceg',3,6,Extra,'f');
+% Jpatt = modelSparsity(model);
+% Jmc = Jpatt(1:3,1:3);
+% Jpvec = sparse([1 0 1;0 1 1;0 0 0]);
+% jspr = [Jmc Jpvec];
 
 
 
