abd2126f1be057cf114bd6411ad0f7712a016aa5
shyamsrinivasan <shyamsweth@gmail.com>
2016-06-10 00:29:53 -0400

    changes to allow when ub is lower than lb
    (cherry picked from commit ebc098c7eaf33a0b4956d7c7a9682ad8008af6a4)

--- KineticModel/Kotte2014/sampleEKP.m
+++ KineticModel/Kotte2014/sampleEKP.m
@@ -3,7 +3,12 @@ function allsmplepar = sampleEKP(pvec,lb,ub,idp,npts)
 nps = length(idp);
 Nxeq = zeros(nps,npts);
 for ip = 1:nps
-    Nxeq(ip,:) = ivalueperturbation(lb(ip),ub(ip),npts);
+    if lb(ip)<ub(ip)
+        Nxeq(ip,:) = ivalueperturbation(lb(ip),ub(ip),npts);
+    else
+        % ub becomes lb and lb becomes ub
+        Nxeq(ip,:) = ivalueperturbation(ub(ip),lb(ip),npts);
+    end
 end
 
 allsmplepar = repmat(pvec,npts,1);
