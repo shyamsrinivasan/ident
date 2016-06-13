9acd180a88b5996daf949449eaac84dde287aa0a
shyamsrinivasan <shyamsweth@gmail.com>
2016-06-13 14:01:18 -0400

    Changes to obtain a list of specified colors
    (cherry picked from commit f82e95a6bf42e879df473dd3f74faaa030fb1a50)

--- KineticModel/chooseColors.m
+++ KineticModel/chooseColors.m
@@ -8,7 +8,13 @@ load('C:\Users\shyam\Documents\Courses\CHE1125Project\mat_files\Colors.mat');
 if ~yc    
     Clrnd = floor(1 + (76-1)*rand(ncolor,1));
 else
-    Clrnd = repmat(find(strcmpi(Colors,color)),ncolor,1);
+    if length(color)>1
+        Clrnd = cellfun(@(x)strcmpi(Colors,x),color,'UniformOutput',false);
+        Clrnd = cellfun(@(x)find(x),Clrnd,'UniformOutput',false);
+        Clrnd = cell2mat(Clrnd);
+    else
+        Clrnd = repmat(find(strcmpi(Colors,color)),ncolor,1);
+    end
 end
 ColorSpec = cell(length(Clrnd),1);
 for j = 1:length(Clrnd)
