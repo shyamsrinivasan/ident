c59771213989dd31b85e1acb7f9db1c41b2ec0a9
shyamsrinivasan <shyamsweth@gmail.com>
2016-07-08 16:43:08 -0400

    changes to plot 2d and 3d figures - cannot do vs parameters
    (cherry picked from commit c9c219da8ddc61123979c75604ef94467580b91d)

--- KineticModel/bifurcationPlot.m
+++ KineticModel/bifurcationPlot.m
@@ -1,4 +1,5 @@
-function bifurcationPlot(y,p,s1,f1,idx,ipx,hfig)
+function bifurcationPlot(y,s1,f1,idx,hfig)
+% idx - [x-axis y-axis z-axis] index values
 if nargin<7
     hfig = figure;
 end
@@ -15,14 +16,7 @@ if size(s1,1)>2
 end
 evalRe = real(f1);
 
-for kndex = 1:length(xindex)
-    if (kndex+1)<=length(xindex)
-        pval = p(ipx,xindex(kndex):xindex(kndex+1));
-        yval = y(idx,xindex(kndex):xindex(kndex+1));        
-    else
-        pval = p(ipx,xindex(kndex):end);
-        yval = y(idx,xindex(kndex):end);
-    end
+for kndex = 1:length(xindex)    
     % plot data from initial value to first bifurcation
     if xindex(kndex)+1<=size(y,2)
         if any(evalRe(:,xindex(kndex))<0) && any(evalRe(:,xindex(kndex)+1)>=0)
@@ -43,27 +37,152 @@ for kndex = 1:length(xindex)
             LineP.Color = 'k';
         end
     end
+    LineP.LineWidth = 3;
+    LPval = y(:,xindex(2:end-1));
     
-    % plot data
-    if ~isempty(hfig)
-        figure(hfig);
-        set(gca,'NextPlot','add');
-        plot(pval,yval,'LineStyle',LineP.LineStyle,'Color',LineP.Color,'LineWidth',3);
-        hold on
-    else
-        plot(pval,yval,'LineStyle',LineP.LineStyle,'Color',LineP.Color,'LineWidth',3);
-        hold on
+    if length(idx)<=2
+        % plot2D
+        if (kndex+1)<=length(xindex)
+            xval = y(idx(1),xindex(kndex):xindex(kndex+1));
+            yval = y(idx(2),xindex(kndex):xindex(kndex+1));
+        else
+            xval = y(idx(1),xindex(kndex):end);
+            yval = y(idx(2),xindex(kndex):end);
+        end
+        % plot data
+        hfig = plot2Dbifurcation(xval,yval,idx(1),idx(2),LPval,hfig,LineP);
+    elseif length(idx)<=3
+        % plot3D 
+        if (kndex+1)<=length(xindex)
+            xval = y(idx(1),xindex(kndex):xindex(kndex+1));
+            yval = y(idx(2),xindex(kndex):xindex(kndex+1));
+            zval = y(idx(3),xindex(kndex):xindex(kndex+1));
+        else
+            xval = y(idx(1),xindex(kndex):end);
+            yval = y(idx(2),xindex(kndex):end);
+            zval = y(idx(3),xindex(kndex):end);
+        end
+        % plot data
+        hfig = plot3Dbifurcation(xval,yval,zval,idx(1),idx(2),idx(3),...
+                                 LPval,hfig,LineP);
     end
 end
-
-% plot bifurcation points
-xindex = xindex(2:end-1);
-for ip = 1:length(xindex)
-    line(p(ipx,xindex(ip)),y(idx,xindex(ip)),'LineStyle','none',...
-                             'Marker','o','MarkerEdgeColor','r',...
-                             'MarkerFaceColor','r','MarkerSize',6);
 end
 
+function hfig = plot2Dbifurcation(xval,yval,xid,yid,LPval,hfig,LineP)
+if ~isempty(hfig)
+    figure(hfig);
+    set(gca,'NextPlot','add');
+    hl = plot(xval,yval);
+else
+    hl = plot(xval,yval);
+    hfig = gcf;
+end
+
+if ~isempty(LineP)
+    set(hl,LineP);
+else
+    set(hl,'LineWidth',3);
+end
+
+[xlabel,ylabel] = getaxislabels(2,2,[xid yid]);
+
+% plot bifurcation points
+line(LPval(xid,:),LPval(yid,:),'LineStyle','none',...
+                             'Marker','o','MarkerEdgeColor','r',...
+                             'MarkerFaceColor','r','MarkerSize',6);
+setproperties(2,gca,xlabel,ylabel);
+end
+
+function hfig = plot3Dbifurcation(xval,yval,zval,xid,yid,zid,LPval,hfig,LineP)
+if ~isempty(hfig)
+    figure(hfig);    
+    hl = plot3(xval,yval,zval);
+    set(get(hl,'Parent'),'NextPlot','add');
+else
+    hl = plot3(xval,yval,zval);
+    hfig = gcf;
+end
+
+if ~isempty(LineP)
+    set(hl,LineP);
+else
+    set(hl,'LineWidth',3);
+end
+
+[xlabel,ylabel,zlabel] = getaxislabels(3,2,[xid yid zid]);
+plot3(LPval(xid,:),LPval(yid,:),LPval(zid,:),'LineStyle','none',...
+                             'Marker','o','MarkerEdgeColor','r',...
+                             'MarkerFaceColor','r','MarkerSize',6);
+setproperties(3,gca,xlabel,ylabel,zlabel);  
+end
+
+function [xlabel,ylabel,zlabel] = getaxislabels(plotype,datatype,idx)
+if nargin <3
+    idx = [];
+end
+
+fluxlist = {'ACpts mmole/h','ENZC mmole/h','ECbiomass(FDP) mmole/h',...
+           'GLUX mmole/h','PEPout mmole/h'};
+cnclist = {'PEP','FDP','ENZ'};  
+parlist = {'kEcat','KEacetate','KFbpFBP','vFbpmax','Lfbp','KFbpPEP',...
+           'vEXmax','KEXPEP','vemax','KeFBP','ne','acetate','d','kPEPout'};
+
+if plotype == 3    
+    if datatype == 1
+        [xlabel,ylabel,zlabel] = deal(fluxlist(idx));
+%         zlabel = fluxlist(idx);
+%         ylabel = parlist(idp(2));
+    elseif datatype == 2
+        xlabel = cnclist(idx(1));
+        ylabel = cnclist(idx(2));
+        zlabel = cnclist(idx(3));
+    end
+elseif plotype ==2
+    if datatype == 1
+        [xlabel,ylabel] = deal(fluxlist(idx));
+%         zlabel = fluxlist(idx);
+%         ylabel = parlist(idp(2));
+    elseif datatype == 2
+        xlabel = cnclist(idx(1));
+        ylabel = cnclist(idx(2));        
+    elseif datatype == 3
+        xlabel = parlist(idx(1));
+        ylabel = cnclist(idx(2));
+    end
+    zlabel = {};
+end
+end
+
+function setproperties(plotype,hsfig,xlabel,ylabel,zlabel)
+if nargin < 5
+    zlabel = {};
+end
+% set axis properties
+set(get(gca,'YLabel'),'String',ylabel);  
+set(get(gca,'YLabel'),'FontName','Arial');   
+set(get(gca,'YLabel'),'FontSize',22); 
+set(get(gca,'XLabel'),'String',xlabel);  
+set(get(gca,'XLabel'),'FontName','Arial');   
+set(get(gca,'XLabel'),'FontSize',22);
+if plotype == 3
+    set(get(gca,'ZLabel'),'String',zlabel);  
+    set(get(gca,'ZLabel'),'FontName','Arial');   
+    set(get(gca,'ZLabel'),'FontSize',22);
+    axesP.ZColor = [.1 .1 .1];
+end
+axesP.FontName  = 'Arial';
+axesP.FontSize = 22;
+axesP.LineWidth = 1.5;
+axesP.TickLength = [0.01 0.01];
+axesP.XColor = [.1 .1 .1];
+axesP.YColor = [.1 .1 .1];
+if plotype == 3
+    set(gca,axesP);
+else
+    set(hsfig,axesP);
+end
+end
 % plot data from initial value to first bifurcation
 % pval = p(ipx,1:xindex(1));
 % yval = y(idx,1:xindex(1));
