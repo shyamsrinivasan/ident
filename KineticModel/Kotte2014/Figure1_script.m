fa1149a7984423adef49e93dae0f9aae90f12324
shyamsrinivasan <shyamsweth@gmail.com>
2016-07-11 04:03:22 -0400

    Add separatrix and saddle point calculation
    (cherry picked from commit 0c04e0374e678c88a46c804aa52d639155523c8d)

--- KineticModel/Kotte2014/Figure1_script.m
+++ KineticModel/Kotte2014/Figure1_script.m
@@ -12,7 +12,7 @@ cnfname = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\Kine
 [mc,FBAmodel,met] = readCNCfromFile(cnfname,FBAmodel);
 
 % run FBA
-Vup_struct.ACt2r = 1;
+Vup_struct.ACt2r = 2;
 Vup_struct.ENZ1ex = 1;
 FBAmodel = FBAfluxes(FBAmodel,'fba',{'ACt2r','ENZ1ex'},Vup_struct,...
                     [find(strcmpi(FBAmodel.rxns,'FDex'))...
@@ -20,8 +20,7 @@ FBAmodel = FBAfluxes(FBAmodel,'fba',{'ACt2r','ENZ1ex'},Vup_struct,...
                  
 % flux envelope
 [hfig,hsubfig,prxnid,flag] = FluxEnvelope(FBAmodel,...
-                        {'bmt2r','PEPt2r';...                        
-                        'bmt2r','ACpts'},...
+                        {'bmt2r','PEPt2r'},...
                         {'ACt2r','ENZ1ex'});   
                     
 % remove metabolites held constant from consideration in the model
@@ -69,6 +68,7 @@ allpvec = pvec;
 
 % run equilibrium solution followed by MATCONT
 opts = odeset('RelTol',1e-12,'AbsTol',1e-10);
+ac = find(strcmpi(model.mets,'ac[e]'));
 allxeq = zeros(length(M),npts);
 allxdyn = zeros(length(M),length(tspan),npts);
 allxf = zeros(length(M),npts);
@@ -76,14 +76,32 @@ allfeq = zeros(length(fluxg),npts);
 allfdyn = zeros(length(fluxg),length(tspan),npts);
 solveEquilibriumODE     
 
-fid = [3 5;3 1];
+% get saddle node
+[saddle,saddlepar] = getsaddlenode(data.s1,data.x1,5e-3);
+pvec(12) = saddlepar;
+model.PM(ac-length(saddle)) = saddlepar;
+
+% perturb saddle to get steady states
+eps = 1e-4;
+tspanf = 0:0.1:2000;
+pival = saddle+eps*[1;1;1];
+[~,xeq1,~,feq1] = solveODEonly(1,pival,model,pvec,opts,tspanf);
+nival = saddle-eps*[1;1;1];
+[~,xeq2,~,feq2] = solveODEonly(1,nival,model,pvec,opts,tspanf);
+fss = [feq1 feq2];
+% fss(:,1) = fss(:,1)/fss(1,1);
+% fss(:,2) = fss(:,2)/fss(1,2);
+
+fid = [3 5];
 if isempty(fss)
     % plot initial and final values from continuation only
     plotPointsonFluxEnvelope(hfig,hsubfig,fid,[ifval ffval]);
 elseif size(fss,2)>=2
     % plot all the ultiple steady states
-    plotPointsonFluxEnvelope(3,old_hsubfig,fid,fss)    
+    plotPointsonFluxEnvelope(hfig,hsubfig,fid,fss)    
 elseif size(fss,2)>=1
     % plot initial , unstabel and final conitunation values
     plotPointsonFluxEnvelope(hfig,hsubfig,fid,[ifval fss ffval]);
 end
+
+NumericalSeparatrix(model,pvec,opts,[],[],ap,data.s1,data.x1,5e-3);
