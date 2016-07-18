77c10ea571347c423b622de62cd9ea092e9e5de9
shyamsrinivasan <shyamsweth@gmail.com>
2016-07-18 19:41:43 -0400

    add parameters for bifurcationPlot

--- KineticModel/Kotte2014/getKotteaxislabels.m
+++ KineticModel/Kotte2014/getKotteaxislabels.m
@@ -6,6 +6,7 @@ end
 fluxlist = {'ACpts a.u','ENZC a.u','ECbiomass(FDP) a.u',...
            'GLUX a.u','PEPout a.u'};
 cnclist = {'PEP a.u','FDP a.u','ENZ a.u'};  
+parlist = {'acetate a.u'};
 % parlist = {'Parameter 1, kEcat','','','Parameter 2, vFbpmax','','',...
 %            'Parameter 3, vEXmax','','','','','','','Parameter 4, kPEPout'};
 
@@ -21,12 +22,19 @@ if plotype == 3
     end
 elseif plotype == 2
     if datatype == 1
-        [xlabel,ylabel] = deal(fluxlist(idx));
+        xlabel = fluxlist(idx(1));
+        ylabel = fluxlist(idx(2));
 %         zlabel = fluxlist(idx);
 %         ylabel = parlist(idp(2));
     elseif datatype == 2
         xlabel = cnclist(idx(1));
-        ylabel = cnclist(idx(2));        
+        ylabel = cnclist(idx(2));
+    elseif datatype == 3
+        xlabel = parlist(idx(1)-length(cnclist));
+        ylabel = cnclist(idx(2));
+    elseif datatype == 4
+        xlabel = parlist(idx(1)-length(fluxlist));
+        ylabel = fluxlist(idx(2));    
     end
     zlabel = {};
 end
