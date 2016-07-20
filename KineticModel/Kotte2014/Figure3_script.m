c6e662c4870bf2d016a05a0a9873a053bfe026b1
shyamsrinivasan <shyamsweth@gmail.com>
2016-07-20 15:16:41 -0400

    complete script for parameter varaition figure

---
+++ KineticModel/Kotte2014/Figure3_script.m
@@ -0,0 +1,121 @@
+% Figure 3
+% calculate changes in steady state due to parameter pertrubations
+% build stoichioemtrc matrices
+addpath(genpath('C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel'));
+rxfname = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel\Kotte2014\Kotte2014.txt';
+cnfname = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel\Kotte2014\Kotte2014C.txt';
+
+% create model structure
+[FBAmodel,parameter,variable,nrxn,nmetab] = modelgen(rxfname);
+
+% obtain conentrations from file
+[mc,FBAmodel,met] = readCNCfromFile(cnfname,FBAmodel);
+
+% run FBA
+Vup_struct.ACt2r = 1;
+Vup_struct.ENZ1ex = 1;
+FBAmodel = FBAfluxes(FBAmodel,'fba',{'ACt2r','ENZ1ex'},Vup_struct,...
+                    [find(strcmpi(FBAmodel.rxns,'FDex'))...
+                     find(strcmpi(FBAmodel.rxns,'PEPex'))]);
+                 
+% remove metabolites held constant from consideration in the model
+% integration phase
+[model,pvec,newmc,cnstmet] =...
+remove_eMets(FBAmodel,parameter,mc,[FBAmodel.Vind FBAmodel.Vex],...
+{'enz1[c]','enz1[e]','enz[e]','ac[e]','bm[c]','bm[e]','pep[e]'});
+
+% only initialize for varmets   
+nvar = length(model.mets)-length(find(cnstmet));
+M = newmc(1:nvar);
+PM = newmc(nvar+1:end);
+model.PM = PM;       
+
+% call to parameter sampling script for analysis of mss
+% parameters
+clear pvec
+kEcat = 1;
+KEacetate = 0.1;    % or 0.02
+KFbpFBP = 0.1;
+vFbpmax = 1;
+Lfbp = 4e6;
+KFbpPEP = 0.1;
+vEXmax = 1;
+KEXPEP = 0.3;
+vemax = 1.1;        % for bifurcation analysis: 0.7:0.1:1.3
+KeFBP = 0.45;       % or 0.45
+ne = 2;             % or 2
+acetate = 0.1;      % a.u acetate
+d = 0.25;           % or 0.25 or 0.35
+kPEPout = 0.2;
+pvec = [KEacetate,KFbpFBP,Lfbp,KFbpPEP,...
+        KEXPEP,vemax,KeFBP,ne,acetate,d,...
+        kPEPout,kEcat,vFbpmax,vEXmax];
+    
+% sample parameters indicated by indices in idp
+cmb = [.05 1 1;1 .05 1;1 1 .05;.05 .05 .05;...
+       .25 1 1;1 .25 1;1 1 .25;.25 .25 .25;...
+       .5 1 1;1 .5 1;1 1 .5;.5 .5 .5;...
+       2 1 1;1 2 1;1 1 2;2 2 2;...
+       4 1 1;1 4 1;1 1 4;4 4 4];
+idp = [12 13 14];
+type = 'together';
+npts = size(cmb,1);
+
+% systems check
+givenModel = @(t,x)KotteODE(t,x,model,pvec);
+fluxg = Kotte_givenFlux([M;model.PM],pvec,model);
+dMdtg = givenModel(0,M);
+
+tspan = 0:0.1:2000;
+
+opts = odeset('RelTol',1e-12,'AbsTol',1e-10);
+if strcmpi(type,'together')
+    alliidpvec = zeros(npts,length(pvec),size(idp,1));
+    alliidxeq = zeros(length(M),npts,size(idp,1));
+    alliidxdyn = zeros(length(M),length(tspan),npts,size(idp,1));
+    alliidfeq = zeros(length(fluxg),npts,size(idp,1));
+    alliidfdyn = zeros(length(fluxg),length(tspan),npts,size(idp,1));
+else    
+    alliidpvec = zeros(npts,length(pvec),length(idp));
+    alliidxeq = zeros(length(M),npts,length(idp));
+    alliidxdyn = zeros(length(M),length(tspan),npts,length(idp));
+    alliidfeq = zeros(length(fluxg),npts,length(idp));
+    alliidfdyn = zeros(length(fluxg),length(tspan),npts,length(idp));
+end
+
+% set parameters from cmb at idp position(s)
+allpvec = repmat(pvec,npts,1);
+allpvec(:,idp) = cmb;
+
+for iid = 1:1 % length(idp)
+    % reset pvec
+    pvec = [KEacetate,KFbpFBP,Lfbp,KFbpPEP,...
+            KEXPEP,vemax,KeFBP,ne,acetate,d,...
+            kPEPout,kEcat,vFbpmax,vEXmax];
+    
+    plb = 0;
+    pub = 1;    
+    fprintf('Parameter Combination #%d\n',iid);
+    
+    % run equilibrium solution followed by MATCONT
+    allxeq = zeros(length(M),npts);
+    allxdyn = zeros(length(M),length(tspan),npts);    
+    allfeq = zeros(length(fluxg),npts);
+    allfdyn = zeros(length(fluxg),length(tspan),npts);
+    solveEquilibriumODE
+    
+    % save solution
+    alliidpvec(:,:,iid) = allpvec;
+    alliidxeq(:,:,iid) = allxeq;
+    alliidxdyn(:,:,:,iid) = allxdyn;
+    alliidfeq(:,:,iid) = allfeq;
+    alliidfdyn(:,:,:,iid) = allfdyn;
+    
+    siid.(['iid' num2str(iid)]) = s;
+    allmssid.(['iid' num2str(iid)]) = mssid;
+    allnss.(['iid' num2str(iid)]) = nss;
+end
+
+% get figure for para,eter perturbation through initial value perturbation
+runDynamicRep
+
