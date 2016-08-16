dc106f4635a68945fb92307cc72b705ba7ed6e16
shyamsrinivasan <shyamsweth@gmail.com>
2016-08-16 18:09:14 -0400

    added sampling of metabolites

--- KineticModel/script_ensm.m
+++ KineticModel/script_ensm.m
@@ -2,10 +2,12 @@
 % testing with Kotte model of glucoeneogenesis
 addpath(genpath('C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel'));
 rxfname = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel\gtoy1.txt';
-
-%create model structure
+cnfname = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel\gtoy1C.txt';
+% create model structure
 [FBAmodel,parameter,variable,nrxn,nmetab] = modelgen(rxfname);
 
+% obtain conentrations from file
+[mc,FBAmodel,met] = readCNCfromFile(cnfname,FBAmodel);
 % FBA or pFBA solution - optional
 % fix flux uptakes for FBA solution
 Vup_struct.exAC = 20;%mmol/gDCW.h
@@ -17,9 +19,15 @@ ess_rxn = {'exH','exPI','exAC'};
 FBAmodel.bmrxn = 14;
 FBAmodel = FBAfluxes(FBAmodel,'fba',ess_rxn,Vup_struct);
 
+rxn_add = {};
 % Metabolite conecntrations 
 % extracellular metabolites in M moles/L
-met.pi_e = 4e-2;
+% met.pi_e = 4e-2;
 
 % sample initial metabolite concentrations for estimating kinetic parameters
-[mc,parameter,smp] = parallel_sampling(FBAmodel,parameter,'setupMetLP',met);
+[mc,parameter,smp] = parallel_sampling(FBAmodel,parameter,'setupMetLP_gtoy',met,mc,rxn_add);
+if isempty(mc)
+    % multiple saples are being supplied
+    mc = smp{1,1};
+    parameter = smp{1,2};
+end
