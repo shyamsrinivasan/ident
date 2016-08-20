ede36483b143d247bffc718cd28e6313b408ea3f
shyamsrinivasan <shyamsweth@gmail.com>
2016-08-19 20:00:49 -0400

    stable ss for bistable cases for diff acetate and kpep values

--- KineticModel/Kotte2014/Figure7_script.m
+++ KineticModel/Kotte2014/Figure7_script.m
@@ -1,13 +1,17 @@
 % data from Figure 5
-load('C:\Users\shyam\Documents\Courses\CHE1125Project\Results\KotteModel\kPEPoutVariation_Aug17.mat');
+load('C:\Users\shyam\Documents\Courses\CHE1125Project\Results\KotteModel\kPEPoutVariation_Aug19.mat');
 
-% needed variables: alliidpvec,alliidxeq,alliidfeq,tout,ap,idp;
+% needed variables: alliidpvec,alliidxeq,alliidfeq,tout,ap,idp,(allacxeq,allacfeq,acetate);
 npts = size(alliidpvec,1);
 nvar = size(alliidxeq,1);
+nflux = size(alliidfeq,1);
 ndp = size(alliidpvec,3);
 tout = tspan;
-% umber of bifurcation points
+tspanf = 0:0.1:2000;
+
+% umber of continuation points
 nbifpts = 6000;
+ac = find(strcmpi(model.mets,'ac[e]'));
 
 for iid = 1:ndp
     if isfield(allnss,sprintf('iid%d',iid))
@@ -20,22 +24,121 @@ for iid = 1:ndp
             hf1 = [];
             allmsspts = union(allmsspts,msspts(sslps==ss(iss)));
             allacetate = zeros(length(allmsspts),nbifpts);
+            bistablex = zeros(2*nvar,npts,length(acetate));
+            bistablef = zeros(2*nflux,npts,length(acetate));
+            % unistablex = zeros(nvar,npts,length(acetate));
+            % unistablef = zeros(nflux,npts,length(acetate));
+            saddleac = zeros(npts,length(acetate));
             for ipt = 1:npts
+                pvec = alliidpvec(ipt,:,iid);
                 addanot.text = ['R' num2str(ipt)];
                 % if point capable of mss
-                if ismember(ipt,allmsspts)   
-%                     s1 =...
-%                     siid.(['iid' num2str(iid)]).(['pt' num2str(ipt)]).s1;
-%                     x1 =...
-%                     siid.(['iid' num2str(iid)]).(['pt' num2str(ipt)]).x1;
-%                     f1 =...
-%                     siid.(['iid' num2str(iid)]).(['pt' num2str(ipt)]).f1;
-%                     index =...
-%                     cat(1,siid.(['iid' num2str(iid)]).(['pt' num2str(ipt)]).s1.index);
+                if ismember(ipt,allmsspts)  
+                    % find two steady states for different acetate
+                    % concentrations
+                    s1 =...
+                    siid.(['iid' num2str(iid)]).(['pt' num2str(ipt)]).s1;
+                    x1 =...
+                    siid.(['iid' num2str(iid)]).(['pt' num2str(ipt)]).x1;
+                    for iac = 1:length(acetate)
+                        % calculate saddle for each acetate concentration
+                        eps = 1e-4;
+                        saddle = [];
+                        saddlepar = [];
+                        while isempty(saddlepar)
+                            [saddle,saddlepar,status] = geteqcontpt(s1,x1,acetate(iac),eps);
+                            eps = eps*10;
+                        end
+                        if status % obtained good saddle node
+                            pvec(ap) = saddlepar;
+                            saddleac(ipt,iac) = saddlepar;
+                            model.PM(ac-length(saddle)) = saddlepar;
+                            % perturb saddle to get steady states
+                            eps = 1e-4;                            
+                            pival = saddle+eps*[1;1;1];
+                            [~,xeq1,~,feq1] =...
+                            solveODEonly(1,pival,model,pvec,opts,tspanf);
+                            nival = saddle-eps*[1;1;1];
+                            [~,xeq2,~,feq2] =...
+                            solveODEonly(1,nival,model,pvec,opts,tspanf);
+                            bistablex(1:nvar,ipt,iac) = xeq1;
+                            bistablex(nvar+1:end,ipt,iac) = xeq2;
+                            bistablef(1:nflux,ipt,iac) = feq1;
+                            bistablef(nflux+1:end,ipt,iac) = feq2;
+                        elseif ~status 
+                            % saddle parameter out of bifurcation bounds
+                            % get the one possible steady state
+                            pvec(ap) = acetate(iac);
+                            saddleac(ipt,iac) = acetate(iac);
+                            model.PM(ac-length(saddle)) = acetate(iac);
+                            [~,xeq1,~,feq1] =...
+                            solveODEonly(1,M,model,pvec,opts,tspanf);
+                            bistablex(1:nvar,ipt,iac) = xeq1;
+                            bistablex(nvar+1:end,ipt,iac) = xeq1;
+                            bistablef(1:nflux,ipt,iac) = feq1;
+                            bistablef(nflux+1:end,ipt,iac) = feq1;
+                        end
+                    end
                     allacetate(ipt,:) =...
                     siid.(['iid' num2str(iid)]).(['pt' num2str(ipt)]).x1(end,:);
-%                     hf1 = bifurcationPlot(x1,s1,f1,[4,1],ap,hf1,addanot);
+                % else if point not capable of mss
+                elseif ~ismember(ipt,allmsspts)
+                    % find the monostable steady states for different
+                    % acetate concentrations
+                    for iac = 1:length(acetate)
+                        % take data from existing information in workspace
+                        bistablex(1:nvar,ipt,iac) = allacxeq(:,ipt,iac);
+                        bistablex(nvar+1:end,ipt,iac) = allacxeq(:,ipt,iac);
+                        bistablef(1:nflux,ipt,iac) = allacfeq(:,ipt,iac);
+                        bistablef(nflux+1:end,ipt,iac) = allacfeq(:,ipt,iac);
+                    end
                 end
+                % if point capable of mss
+%                 if ismember(ipt,allmsspts)   
+%                     % calculate high and low states for each point 
+%                     % for diferent acetate concentrations in acetate                    
+%                     
+%                     % calculate saddle for each acetate concentration
+%                     eps = 1e-4;
+%                     for iac = 1:length(acetate)
+%                         saddle = [];
+%                         saddlepar = [];
+%                         while isempty(saddlepar)
+%                             [saddle,saddlepar,status] = geteqcontpt(s1,x1,acetate(iac),eps);
+%                             eps = eps*10;
+%                         end
+%                         if ~isempty(saddle)
+%                             % if acetate(iac) is out of bounds - only high
+%                             % steady state or low steady state
+%                             if ~status
+%                                 pvec(ap) = acetate(iac);
+%                                 saddleac(ipt) = acetate(iac);
+%                                 model.PM(ac-length(saddle)) = acetate(iac);
+%                                 [~,xeq1,~,feq1] =...
+%                                 solveODEonly(1,saddle,model,pvec,opts,tspanf);
+%                                 bistablex(1:nvar,ipt,iac) = xeq1;
+%                                 bistablef(1:nflux,ipt,iac) = feq1;
+%                             % if acetate(iac) within bounds - both high and
+%                             % low steady states from saddle
+%                             elseif status
+%                             end
+%                             
+%                             
+%                             bistablex(1:nvar,ipt,iac) = xeq1;
+%                             bistablex(nvar+1:end,ipt,iac) = xeq2;
+%                             bistablef(1:nflux,ipt,iac) = feq1;
+%                             bistablef(nflux+1:end,ipt,iac) = feq2;
+%                         end
+%                     end             
+%                     allacetate(ipt,:) =...
+%                     siid.(['iid' num2str(iid)]).(['pt' num2str(ipt)]).x1(end,:);
+%                 elseif ~ismember(ipt,allmsspts)
+%                     for iac = 1:length(acetate)
+%                         saddleac(ipt) = acetate(iac);
+%                         unistablex(:,ipt,iac) = allacxeq(:,ipt,iac);
+%                         unistablef(:,ipt,iac) = allacfeq(:,ipt,iac);
+%                     end
+%                 end
             end  
             endacetate = min(max(allacetate,[],2));
             % find indices for every row in allacetate close to endacetate
@@ -63,6 +166,14 @@ for iid = 1:ndp
     %                 selectpts = x1(
                 end
             end
+            figure
+            plot(minac,kpepval,'Color','r','LineWidth',2);
+            hold on
+            plot(maxac,kpepval,'Color','r','LineWidth',2);
+            plot([max(minac) max(maxac)],[max(kpepval) max(kpepval)],...
+                 'Color','r','LineWidth',2);
+            xlabel('acetate a.u');
+            ylabel('kPEPout a.u');
         end
     end
 end
