161d0dcc5b7cd832bb5e1244f7cfb92ca33b71d4
shyamsrinivasan <shyamsweth@gmail.com>
2016-08-30 18:29:09 -0400

    lines to adjust strating point of continuation and add saddle calculation

--- KineticModel/Kotte2014/enzymecont.m
+++ KineticModel/Kotte2014/enzymecont.m
@@ -1,8 +1,12 @@
 % enzyme parameter continuation
-
-% data from perturbation continuation on acetate
+% data from perturbation and continuation on acetate for 24 perturbations
+% only - data in main text
+% load('C:\Users\shyam\Documents\Courses\CHE1125Project\Results\KotteModel\VmaxVariationAllPerturbations_July29.mat');
+load('C:\Users\shyam\Documents\Courses\CHE1125Project\Results\KotteModel\VmaxVariation_IvalPerturbation_Aug30.mat');
+% data from perturbation continuation on acetate large number of
+% perturbations for finer detail - data in SI
 % Figure 3
-load('C:\Users\shyam\Documents\Courses\CHE1125Project\Results\KotteModel\VmaxVariationAll_Aug02.mat');
+% load('C:\Users\shyam\Documents\Courses\CHE1125Project\Results\KotteModel\VmaxVariationAll_Aug02.mat');
 
 % get original ss and continuation without perturbations
 clear pvec
@@ -25,29 +29,44 @@ pvec = [KEacetate,KFbpFBP,Lfbp,KFbpPEP,...
         kPEPout,kEcat,vFbpmax,vEXmax];
 
 % systems check
-givenModel = @(t,x)KotteODE(t,x,model,pvec);
 fluxg = Kotte_givenFlux([M;model.PM],pvec,model);
-dMdtg = givenModel(0,M);    
-    
+tspan = 0:0.1:2000;    
 opts = odeset('RelTol',1e-12,'AbsTol',1e-10);
-colorSpec = chooseColors(4,{'Green','Purple','Red','Orange'});
 ac = find(strcmpi(model.mets,'ac[e]'));
 npts = 1;
 
+% continuation on acetate -  to get saddle original saddle node
+ap = 9;
 allxeq = zeros(length(M),npts);
 allxdyn = zeros(length(M),length(tspan),npts);
 allxf = zeros(length(M),npts);
 allfeq = zeros(length(fluxg),npts);
 allfdyn = zeros(length(fluxg),length(tspan),npts);
 allpvec = pvec;
-% continuation on acetate
-ap = 9;
 solveEquilibriumODE 
+
+% get saddle node
+[orig_saddle,orig_saddlepar] = getsaddlenode(data.s1,data.x1,5e-3);
+pvec(ap) = orig_saddlepar;
+model.PM(ac-length(orig_saddle)) = orig_saddlepar;   
+allpvec = pvec;
+ap1 = ap;
+clear ap
 % conitnuation on enzyme parameters 12, 13 and 14 for default values of [1
 % 1 1]'
 for ip = 1:length(idp)
     ap = idp(ip);
-    solveEquilibriumODE 
+    if ap~=14
+        allpvec(ap) = 0.01;
+    else
+        allpvec(ap) = 5;
+    end
+    solveEquilibriumODE
+    pvec = [KEacetate,KFbpFBP,Lfbp,KFbpPEP,...
+            KEXPEP,vemax,KeFBP,ne,acetate,d,...
+            kPEPout,kEcat,vFbpmax,vEXmax];
+    pvec(ap1) = orig_saddlepar;
+    allpvec = pvec;
 end
 % continuation on enzyme parameters 12, 13 and 14 for perturbed values in
 % cmb
@@ -56,14 +75,20 @@ end
 hf1 = [];
 hf2 = [];
 hf3 = [];
-ipt = 3;
+ipt = 4;
 while ipt<size(cmb,1)
 % for ipt = 1:size(cmb,1)
     xeq = alliidxeq(:,ipt);
     pvec = alliidpvec(ipt,:);
+    pvec(ap1) = orig_saddlepar;
     addanot.text = ['E' num2str(ipt)];
     for ip = 1:length(idp)
-        ap = idp(ip);        
+        ap = idp(ip);   
+        if ap~=14
+            pvec(ap) = 0.01;
+        else
+            pvec(ap) = 5;
+        end
         % run MATCONT
         [data,y,p] = execMATCONT(xeq,pvec,ap,fluxg,model);
         if ~isempty(data) && size(data.s1,1)>2
