4dbfa7df43b01608ef99a3bbe76b0834df569324
shyamsrinivasan <shyamsweth@gmail.com>
2016-09-01 17:22:10 -0400

    change allosteric activation and vectorize rxn loop

--- CKinetics.m
+++ CKinetics.m
@@ -4,6 +4,7 @@ allmc = mc;
 S = model.S;
 SI = model.SI;
 nrxn = model.nt_rxn;
+rev = model.rev;
 K = pvec.K;
 KIact = pvec.KIact;
 KIihb = pvec.KIihb;
@@ -46,7 +47,7 @@ for ic = 1:nc
         Sb = -S(sbid,Vind(irxn));
         Sp = S(prid,Vind(irxn));
 
-        if model.rev(Vind(irxn))          
+        if rev(Vind(irxn))          
             if all(mc(sbid)>0) && all(mc(prid)>0)
               nr_flux = kfwd(Vind(irxn))*prod((mc(sbid)./K(sbid,Vind(irxn))).^Sb) -...
                         kbkw(Vind(irxn))*prod((mc(prid)./K(prid,Vind(irxn))).^Sp);
@@ -76,7 +77,7 @@ for ic = 1:nc
                 dr_sb = 1;
                 dr_pr = 1;
             end
-        elseif ~model.rev(Vind(irxn))                           
+        elseif ~rev(Vind(irxn))                           
             if all(mc(sbid)>0)
                 nr_flux =...
                 kfwd(Vind(irxn))*prod((mc(sbid)./K(sbid,Vind(irxn))).^Sb);
@@ -102,15 +103,21 @@ for ic = 1:nc
             if any(SI(:,Vind(irxn))>0)
                 acid = SI(:,Vind(irxn))>0;
                 sac = SI(acid,Vind(irxn));
-                nr_flux = nr_flux*...
-                          prod(1 + (mc(acid)./KIact(acid,Vind(irxn))).^sac);                      
+                acflx = prod((0.5+(1-0.5).*mc(acid)./KIact(acid,Vind(irxn))/...
+                        (1+mc(acid)./KIact(acid,Vind(irxn)))).^sac);
+                nr_flux = acflx*nr_flux;
+%                 nr_flux = nr_flux*...
+%                           prod(1 + (mc(acid)./KIact(acid,Vind(irxn))).^sac);                      
             end
             % inhibition
             if any(SI(:,Vind(irxn))<0)
                 ihid = SI(:,Vind(irxn))<0;
                 sih = SI(ihid,Vind(irxn));
-                nr_flux =...
-                nr_flux*prod(1./(1+(mc(ihid)./KIihb(ihid,Vind(irxn))).^sih));                
+                ibflx = prod((0.5+(1-0.5).*1/...
+                        (1+mc(acid)./KIact(acid,Vind(irxn)))).^sac);
+                nr_flux = ibflx*nr_flux;
+%                 nr_flux =...
+%                 nr_flux*prod(1./(1+(mc(ihid)./KIihb(ihid,Vind(irxn))).^sih));                
             end
         end  
 
@@ -123,5 +130,105 @@ for ic = 1:nc
         flux(Vind(irxn),ic) = Vmax(Vind(irxn))*vflux(Vind(irxn),ic);
     end
 end
+
+% vectorized version of CKinetics for all rxns in Vind - under testing
+for ic = 1:nc
+    mc = allmc(:,ic);
+    vecmc = repmat(mc,1,length(Vind));
+    acflx = zeros(model.nt_rxn,1);
+    ibflx = zeros(model.nt_rxn,1);
+    acdr = zeros(model.nt_rxn,1);
+    ibdr = zeros(model.nt_rxn,1);
+    alls = S(:,Vind);allp = S(:,Vind);
+    alls(S(:,Vind)>0) = 0;
+    allp(S(:,Vind)<0) = 0;
+    allK = K(:,Vind);
+    sratio = vecmc(logical(alls))./allK(logical(alls));
+    pratio = vecmc(logical(allp))./allK(logical(allp));
+    fwdflx = kfwd(Vind).*prod(sratio.^-alls(logical(alls)),2);
+    revflx = kbkw(Vind).*prod(pratio.^allp(logical(allp)),2);    
+    
+    % set reverse flux for zero products = 0
+    [~,rxn] = find(vecmc(logical(alls))==0);
+    if ~isempty(rxn)
+        revflx(rxn) = 0;
+    end
+    
+    % set forwrd flux for zero substrate = 0
+    [~,rxn] = find(vecmc(logical(allp))==0);
+    if ~isempty(rxn)
+        fwdflx(rxn) = 0;
+    end
+    
+    % set reverse flux for irreversible reactions = 0
+    revflx(~rev(Vind)) = 0;
+    
+    % numerator of kinetics
+    nrflx = fwdflx-revflx;
+    
+    % denominator of kinetics
+    drflx = 1+...
+            prod(sratio.^-alls(logical(alls)),2)+...
+            prod(pratio.^allp(logical(allp)),2);
+    
+    % allosteric activation  
+    % reaction that have an activation component
+    [~,rxn] = find(SI(:,Vind)>0);
+    Vact = Vind(rxn);
+    allacs = SI(:,Vind);
+    allacs(SI(:,Vind)<0) = 0;
+    allKIa = KIact(:,Vind);
+    acratio = vecmc(logical(allacs))./allKIa(logical(allacs));
+    if ~isempty(acratio)
+        acflx(Vact) = prod((0.5+(1-0.5)*acratio/(1+acratio)).^allacs(logical(allacs)),2);
+    else
+        acflx(Vact) = 1;
+    end
+    nrflx(rxn) = acflx(Vact).*nrflx(rxn);
+    
+    % allosteric inhibition
+    [~,rxn] = find(SI(:,Vind)<0);
+    Vihb = Vind(rxn);
+    allibs = SI(:,Vind);
+    allibs(SI(:,Vind)>0) = 0;
+    allKIi = KIihb(:,Vind);
+    ibratio = vecmc(logical(allibs))./allKIi(logical(allibs));
+    if ~isempty(ibratio)
+        ibflx(Vihb) = prod((0.5+(1-0.5)*ibratio/(1+ibratio)).^-allibs(logical(allibs)),2);
+    else
+        ibflx(Vihb) = 1;
+    end
+    nrflx(rxn) = ibflx(Vihb).*nrflx(rxn);
+    
+    % other activation
+%     spacratio = vecmc(SI(:,Vind)>0,Vind)./KIact(SI(:,Vind)>0,Vind);
+%     if ~isempty(spacratio)
+%         acdr(Vind) = sum((1/spacratio).^SI(SI(:,Vind)>0,Vind));
+%     end
+%     drflx = drflx+acdr(Vind);
+%     
+%     % other inhibition
+%     spibratio = vecmc(SI(:,Vind)<0,Vind)./KIihb(SI(:,Vind)<0,Vind);
+%     if ~isempty(spibratio)
+%         ibdr(Vind) = sum(spibratio.^(-SI(SI(:,Vind)<0,Vind)));
+%     end
+%     drflx = drflx+ibdr(Vind);
+    
+    % scale flux
+    vflux(Vind,ic) = scale_flux(nrflx./drflx);
+    
+    % scale all ---> A (no substrate) reactions to zero
+%     [met,rxn] = find(S(S(:,Vind)<0,:)==0);
+%     vflux(rxn,ic) = 0;
+    
+    % scale all A ---> (no product) reactions to zero 
+%     [met,rxn] = find(S(S(:,Vind)>0,:)==0);
+%     vflux(rxn,ic) = 0;
+    
+    % multiply with Vmax or enzyme concentration
+    flux(Vind,ic) = Vmax(Vind,ic).*vflux(Vind,ic);    
+end
+
+
 flux = flux(Vind,:);
 vflux = vflux(Vind,:);
