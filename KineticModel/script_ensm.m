13ebbf6ab754fb428f94390f2584e394d34295e2
shyamsrinivasan <shyamsweth@gmail.com>
2016-09-01 17:25:17 -0400

    no major changes to calls

--- KineticModel/script_ensm.m
+++ KineticModel/script_ensm.m
@@ -3,8 +3,8 @@
 addpath(genpath('C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel'));
 % rxfname = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel\gtoy1.txt';
 % cnfname = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel\gtoy1C.txt';
-rxfname = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel\Kotte2014\Kotte2014.txt';
-cnfname = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel\Kotte2014\Kotte2014C.txt';
+rxfname = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel\gtoy2.txt';
+cnfname = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel\gtoy2C.txt';
 % create model structure
 [FBAmodel,parameter,variable,nrxn,nmetab] = modelgen(rxfname);
 
@@ -19,12 +19,13 @@ Vup_struct.ENZ1ex = 1;
 
 % designate reactions for which uptake should not be zero in FBA
 % ess_rxn = {'exH','exPI','exAC'};
-ess_rxn = {'ACt2r','ENZ1ex'};
+ess_rxn = {'ACt2r'};
 
 % assign initial fluxes and calculate FBA fluxes for direction
 % FBAmodel.bmrxn = 14;
+FBAmodel.bmrxn = 3;
 FBAmodel = FBAfluxes(FBAmodel,'fba',ess_rxn,Vup_struct,...
-                     find(strcmpi(FBAmodel.rxns,'EC_Biomass')));
+                     find(strcmpi(FBAmodel.rxns,'bmex')));
 
 rxn_add = {};
 % Metabolite conecntrations 
@@ -42,14 +43,14 @@ end
 % get parameter estimates - estimate kinetic parameters in an ensemble
 rxn_add = {};
 rxn_excep = {};
-FBAmodel.bmrxn = [];
+% FBAmodel.bmrxn = [];
 ensb = parallel_ensemble(FBAmodel,mc,parameter,rxn_add,rxn_excep);
 
 % serially solve ODE of model to steady state
 FBAmodel.rxn_add = rxn_add;
 FBAmodel.rxn_excep = rxn_excep;
 if ensb{1,2}.feasible    
-    sol = IntegrateModel(FBAmodel,ensb,ensb{1,1});
+    sol = IntegrateModel(FBAmodel,ensb,ensb{1,1},ess_rxn,Vup_struct);
 else
     error('No feasible model found');
 end
