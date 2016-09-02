963d8183d7422412bb853dba11407bc89ef8dc3b
shyamsrinivasan <shyamsweth@gmail.com>
2016-09-02 15:52:04 -0400

    changes to incorporate toyNLAE call

--- KineticModel/ToyODEmodel.m
+++ KineticModel/ToyODEmodel.m
@@ -1,26 +1,28 @@
 function [dXdt] = ToyODEmodel(t,mc,data,model,pvec)
 
-nvar = length(mc);
-dXdt = zeros(nvar,1);
-Pimc = model.Pimc;
+dXdt = toyNLAE(mc,model,pvec);
 
-%% Fluxes
-flux = iflux(model,pvec,[mc.*model.imc;Pimc]);
-
-%% Metabolites
-% Cytosolic
-dXdt(1:nvar) = (1./model.imc).*(model.S(1:nvar,:)*(flux*3600));
-
-% h[c] is assume constant
-% hc = strcmpi('h[c]',model.mets);
-% dXdt(hc) = 0;
-
-% change pi[c]
-% pic = strcmpi('pi[c]',model.mets);
-% dXdt(pic) = dXdt(pic)+0.001;
-
-%Extracellular
-% dXdt(nin_m+1:nt_m) = 0;%
+% nvar = length(mc);
+% dXdt = zeros(nvar,1);
+% Pimc = model.Pimc;
+% 
+% %% Fluxes
+% flux = iflux(model,pvec,[mc.*model.imc;Pimc]);
+% 
+% %% Metabolites
+% % Cytosolic
+% dXdt(1:nvar) = (1./model.imc).*(model.S(1:nvar,:)*(flux*3600));
+% 
+% % h[c] is assume constant
+% % hc = strcmpi('h[c]',model.mets);
+% % dXdt(hc) = 0;
+% 
+% % change pi[c]
+% % pic = strcmpi('pi[c]',model.mets);
+% % dXdt(pic) = dXdt(pic)+0.001;
+% 
+% %Extracellular
+% % dXdt(nin_m+1:nt_m) = 0;%
 
 %% staus check for CVODE in SUNDIALS TB
 % if any(mc<0)
@@ -29,6 +31,5 @@ dXdt(1:nvar) = (1./model.imc).*(model.S(1:nvar,:)*(flux*3600));
     flag = 0;
 % end
 newdata = data;
-newdata.flux = flux;
 newdata.Y = mc;
 newdata.t = t;
