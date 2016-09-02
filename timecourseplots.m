1a5d360ba390c9ad808ea91f3082049c2e2e447d
shyamsrinivasan <shyamsweth@gmail.com>
2016-09-02 12:40:02 -0400

    syntax follows Kotte model plots with subfigures

--- timecourseplots.m
+++ timecourseplots.m
@@ -1,91 +1,95 @@
-function timecourseplots(time,y,type,model,name)
-if nargin<5
-    name = {};
+function [hfig,ha] = timecourseplots(xdata,ally,type,name,model,hfig,ha,LP,addanot)
+if nargin<9
+    addanot = [];
 end
+if nargin<8
+    LP = [];
+end
+if nargin<7
+    ha = [];
+end
+if nargin<6
+    hfig = [];
+end
+
+global Line annot
+Line = LP;
+annot = addanot;
+
 switch(type)
-    case 'c'
-        metCurves(name,y)
-    case 'f'
-        fluxCurves(name,y)
-    case 'both'
-        metCurves(name,y)
-        fluxCurves(name,y)
+    case 1        
+        % get id for metabolites in cell array name
+        if iscell(name)
+            name = cellfun(@(x)strcmpi(model.mets,x),name,'UniformOutput',false);
+            name = cellfun(@(x)find(x),name,'UniformOutput',false);
+            yid = cell2mat(name);
+        else
+            yid = name;
+        end    
+        % get ydata and plot info
+        ydata = ally(yid,:);
+        [hfig,ha,hlval] = plot2Dcourse(xdata,ydata,hfig,ha,LP);
+        drawnow
+    case 2
+        % get id for fluxes in cell array name
+        if iscell(name)
+            name = cellfun(@(x)strcmpi(model.rxns,x),name,'UniformOutput',false);
+            name = cellfun(@(x)find(x),name,'UniformOutput',false);
+            yid = cell2mat(name);
+        else
+            yid = name;
+        end  
+        % get ydata and plot info
+        ydata = ally(yid,:);
+        [hfig,ha,hlval] = plot2Dcourse(xdata,ydata,hfig,ha,LP); 
+        drawnow
     otherwise
         fprintf('No suitable plot functions found\n');
 end
-
-function metCurves(name,y)
-    if isempty(name)
-        name = model.mets(1:model.nint_metab);
-        fprintf('Printing all Metabolites in a single plot\n');
-    else
-        name = name.mets;
-    end
-    nplots = length(name);
-    if nplots == model.nint_metab
-        hfig = figure('Name','metabolites');   
-        figure(hfig);
-        plot(gca,time,y(1:model.nint_metab,:));
-%         plotData(time,y(1:model.nint_metab,:),hfig);
-    else
-        hst = zeros(nplots,1);
-        for in = 1:nplots
-            tfm = strcmpi(model.mets,name{in});
-            if any(tfm)
-                % create new figure
-                if isempty(findobj('type','figure','Name','metabolites'))
-                    hfig = figure('Name','metabolites');
-                else
-                    hfig = findobj('type','figure','Name','metabolites');
-                end
-                if rem(nplots,2)==0
-                    hst(in) = subplot(nplots/2,2,in);
-                else
-                    hst(in) = subplot((nplots+1)/2,2,in);
-                end
-                LineP.DisplayName = model.mets{tfm};
-                LineP.YLabel = model.mets{tfm};
-                plotData(time,y(tfm,:),[hfig,hst(in)],LineP);                
-            end
-        end
-    end
 end
 
-function fluxCurves(name,y)
-    if isempty(name)
-        name = model.rxns(model.Vind);
-        fprintf('Printing all fluxes in a single plot\n');
-    else
-        name = name.rxns;
-    end
-    nplots = length(name);
-    if nplots == length(model.Vind)
-        hfig = figure('Name','fluxes');   
-        figure(hfig);
-        plot(gca,time,y);
-%         plotData(time,y(model.Vind,:),[hfig,hst]);
-    else
-        hst = zeros(nplots,1);
-        for in = 1:nplots
-            tff = strcmpi(model.rxns,name{in});
-            if any(tff)
-                % create new figure
-                if isempty(findobj('type','figure','Name','fluxes'))
-                    hfig = figure('Name','fluxes');
-                else
-                    hfig = findobj('type','figure','Name','fluxes');
-                end
-                if rem(nplots,2)==0
-                    hst(in) = subplot(nplots/2,2,in);
-                else
-                    hst(in) = subplot((nplots+1)/2,2,in);
-                end
-                LineP.DisplayName = model.rxns{tff};
-                LineP.YLabel = model.rxns{tff};
-                plotData(time,y(tff,:),[hfig,hst(in)],LineP);    
-            end
-        end
-    end
+function [hfig,ha,hlval] = plot2Dcourse(xdata,ydata,hfig,ha)
+global Line annot
+if ~isempty(hfig)
+    set(0,'CurrentFigure',hfig);
+else
+    hfig = figure('Name','Met Courses'); 
+    set(0,'CurrentFigure',hfig);
 end
 
+ndata = size(ydata,1);
+if ndata>1    
+    if rem(ndata,2)==0
+        nrows = ndata/2;
+    else
+        nrows = (ndata+1)/2;
+    end    
+else
+    nrows = 1;
+end
+ncols = 2;
+nplots = nrows*ncols;
+if isempty(ha)|length(ha)~=nplots
+    ha = zeros(nplots,1);
+end
+
+for ip = 1:nplots
+    if ip<=ndata
+        if ha(ip)   % if subplot is already assigned
+            set(hfig,'CurrentAxes',ha(ip));
+            hlval = line(xdata,ydata(ip,:));
+        else        % if subplot is emtpy and unassigned
+            ha(ip) = subplot(nrows,ncols,ip);
+            set(hfig,'CurrentAxes',ha(ip));
+            hlval = line(xdata,ydata(ip,:));
+        end
+        if ~isempty(Line)
+            set(hlval,Line);
+        end
+        % set axis labels
+        % set axis properties
+    else
+        break
+    end
+end
 end
