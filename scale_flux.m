1453ed443d07b543bf946efd76ddaa0a0a3c705a
shyamsrinivasan <shyamsweth@gmail.com>
2016-09-03 11:48:02 -0400

    change positive check from logical to relational operators

--- scale_flux.m
+++ scale_flux.m
@@ -1,7 +1,7 @@
 function flux = scale_flux(flux)
 for irxn = 1:length(flux)
     vf = flux(irxn);
-    if vf
+    if vf>0 | vf<0
         if abs(vf)<=1e-10
             flux(irxn) = 0;
         end    
