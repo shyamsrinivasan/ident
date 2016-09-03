05670cfd49176af810851112317ea8398cd218c6
shyamsrinivasan <shyamsweth@gmail.com>
2016-09-03 11:42:02 -0400

    remove isnan check for vf

--- scale_flux.m
+++ scale_flux.m
@@ -1,7 +1,7 @@
 function flux = scale_flux(flux)
 for irxn = 1:length(flux)
     vf = flux(irxn);
-    if ~isnan(vf) && vf
+    if vf
         if abs(vf)<=1e-10
             flux(irxn) = 0;
         end    
