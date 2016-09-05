d4550e4435169e68d8d4fd2a451ca1cd33da5865
shyamsrinivasan <shyamsweth@gmail.com>
2016-09-05 16:38:09 -0400

    comment out use of ADMAT - see issue #18

--- KineticModel/getjacobian.m
+++ KineticModel/getjacobian.m
@@ -1,36 +1,41 @@
-function [J,lambda,w] = getjacobian(x,pvec,model)
+function [Jxact,lambda,w] = getjacobian(x,pvec,model)
 % get analytical jacobian based on modular rate law
 nrxn = model.nt_rxn;
-nmet = length(x);
+nmet = model.nt_metab;
 DVX = zeros(nmet,nrxn);
 
 Vind = model.Vind;
 Vex = model.Vex;
 
-DVX(:,Vind) = CKjacobian(model,pvec,x,Vind);
-DVX(:,Vex) = TKjacobian(model,pvec,x,Vex);
+DVX(:,Vind) = CKjacobian(model,pvec,[x;model.PM],Vind);
+DVX(:,Vex) = TKjacobian(model,pvec,[x;model.PM],Vex);
 
 S = model.S;
-J = sparse(zeros(nmet,nmet));
+Jxact = sparse(zeros(nmet,nmet));
 
 % complete jacobian - build in column loops rowwise
 % J = nmet x nmet
 for imet = 1:nmet
-    J(:,imet) = S*DVX(imet,:)';
+    Jxact(:,imet) = S*DVX(imet,:)';
 end
 
 % since extracellular concentrations are sometime zero the jacobian for
 % internal metabolites alone is also given separately
 nint = model.nint_metab;
-Jint = J(1:nint,1:nint);
+Jint = Jxact(1:nint,1:nint);
 
-% use ADMAT to calculate jacobians
-admatfun = @(x)toyNLAE(x,model,pvec);
-xADMATobj = deriv(x,eye(size(x,1)));
-xADMATres = admatfun(xADMATobj);
-F = getval(xADMATres);
-J = getydot(xADMATres); 
+% use ADMAT to calculate jacobians - see issue #18 on github
+% admatfun = @(x)toyNLAE(x,model,pvec);
+% xADMATobj = deriv(x,eye(size(x,1)));
+% xADMATres = admatfun(xADMATobj);
+% F = getval(xADMATres);
+% J = getydot(xADMATres); 
 
 % eigen value and eigen vector
-[w,lambda] = eig(J);
+[w,lambda] = eig(full(Jxact));
 lambda = diag(lambda);
+
+[w,lambda] = eig(full(Jint));
+lambda = diag(lambda);
+
+Jxact = Jint;
