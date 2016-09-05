88f1a31e43edfb3f5055d7a008187b61cc3f24ae
shyamsrinivasan <shyamsweth@gmail.com>
2016-09-05 16:41:03 -0400

    change field name Pimc to PM in model

--- IntegrateModel.m
+++ IntegrateModel.m
@@ -34,7 +34,8 @@ end
 % integration phase
 [model,pvec,mc] = addremoveMets(model,{'h2o[c]','h2o[e]'},pvec,mc);
 [newmodel,newpvec,newmc,cnstmet] = remove_eMets(model,pvec,mc,[model.Vind model.Vex],...
-                           {'glc[e]','o2[e]','h[e]','h[c]','pi[e]','pyr[e]'});
+                           {'ac[e]','bm[e]','pep[e]'});
+% metid = {'glc[e]','o2[e]','h[e]','h[c]','pi[e]','pyr[e]'}                       
 
 % preinitialize Vind and Vex to reduce integration overhead in iflux.m                       
 newmodel.Vind = addToVind(newmodel,newmodel.Vind,newmodel.rxn_add,newmodel.rxn_excep);
@@ -53,7 +54,7 @@ imc(imc==0) = 1;
 % noramlize concentration vector to intial state
 Nimc = newmc(1:nvar);%imc./imc;
 % Nimc(4) = 20;
-Pimc = newmc(nvar+1:end);
+PM = newmc(nvar+1:end);
 Nimc(imc==0) = 0;
 
 % intorduce perturbation in initial conditions
@@ -68,9 +69,9 @@ Nimc(imc==0) = 0;
 
 newmodel.imc = imc;
 newmodel.imc(newmodel.imc==0) = 1;
-newmodel.Pimc = Pimc;
+newmodel.PM = PM;
 % calculate initial flux
-flux = iflux(newmodel,newpvec,[Nimc.*imc;Pimc]);
+flux = iflux(newmodel,newpvec,[Nimc.*imc;PM]);
 
 % ecoli model
 % dXdt = ODEmodel(0,Nimc,[],model,pvec);
