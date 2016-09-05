aedf320a2a6cdc7fafc2531d99e8b0448360f251
shyamsrinivasan <shyamsweth@gmail.com>
2016-09-05 16:40:15 -0400

    remove cons (again)

--- TKinetics.m
+++ TKinetics.m
@@ -10,6 +10,9 @@ Vmax = pvec.Vmax;
 vflux = zeros(nrxn,1);
 flux = zeros(nrxn,1);
 
+% vflux = cons(vflux,M);
+% flux = cons(flux,M);
+
 vecmc = repmat(M,1,nrxn);
 
 he = find(strcmpi(model.mets,'h[e]'));
