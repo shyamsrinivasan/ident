1857dcb3a7666e8f497030d2793ed474b7ebf76b
shyamsrinivasan <shyamsweth@gmail.com>
2016-09-07 18:55:32 -0400

    add division by concentrations for non 0 concentrations - J element is 0 otherwise

--- KineticModel/getjacobian.m
+++ KineticModel/getjacobian.m
@@ -7,22 +7,30 @@ DVX = zeros(nmet,nrxn);
 Vind = model.Vind;
 Vex = model.Vex;
 
-DVX(:,Vind) = CKjacobian(model,pvec,[x;model.PM],Vind);
-DVX(:,Vex) = TKjacobian(model,pvec,[x;model.PM],Vex);
+allmet = [x;model.PM];
 
+DVX(:,Vind) = CKjacobian(model,pvec,allmet,Vind);
+DVX(:,Vex) = TKjacobian(model,pvec,allmet,Vex);
+
+DVX(allmet==0,:) = 0;
+DVX(allmet~=0,:) = DVX(allmet~=0,:)./repmat(allmet(allmet~=0),1,nrxn);
 S = model.S;
-Jxact = sparse(zeros(nmet,nmet));
 
 % complete jacobian - build in column loops rowwise
 % J = nmet x nmet
-for imet = 1:nmet
-    Jxact(:,imet) = S*DVX(imet,:)';
-end
+Jxact = sparse(S*DVX');
+% for imet = 1:nmet
+%     Jxact(:,imet) = S*DVX(imet,:)';
+% end
 
 % since extracellular concentrations are sometime zero the jacobian for
 % internal metabolites alone is also given separately
-nint = model.nint_metab;
-Jint = Jxact(1:nint,1:nint);
+% nint = model.nint_metab;
+% Jint = Jxact(1:nint,1:nint);
+
+% get jacobian using finite difference
+% rhsfun = @(irow,mc)Svrow(irow,x,model,pvec);
+% Jnum = model_Jacobian(model,allmet,rhsfun);
 
 % use ADMAT to calculate jacobians - see issue #18 on github
 % admatfun = @(x)toyNLAE(x,model,pvec);
@@ -34,8 +42,10 @@ Jint = Jxact(1:nint,1:nint);
 % eigen value and eigen vector
 [w,lambda] = eig(full(Jxact));
 lambda = diag(lambda);
+% 
+% [w,lambda] = eig(full(Jint));
+% lambda = diag(lambda);
 
-[w,lambda] = eig(full(Jint));
-lambda = diag(lambda);
-
-Jxact = Jint;
+% Jxact = Jint;
+% lambda = [];
+% w = [];
