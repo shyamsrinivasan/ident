ac9d01e667d7331023a71eb15b4f0f902273408b
shyamsrinivasan <shyamsweth@gmail.com>
2016-09-12 10:36:52 -0400

    perform perturbations on initial xeq

--- KineticModel/ode/perturbation.m
+++ KineticModel/ode/perturbation.m
@@ -1,4 +1,4 @@
-function [outsol,outss,allxss,allfss] = perturbation(xeq,model,pvec,solverP,npts,type)
+function [alloutsol,allpxeq,allpfeq] = perturbation(xeq,model,pvec,solverP,npts,type)
 if nargin<6
     type = 'rnd';
 end
@@ -6,20 +6,28 @@ if nargin<5
     npts = 1;
 end
 
-nmodels = size(pvec,2);
+nmodels = size(pvec,2);nvar = size(xeq,1);
 
 if nmodels>10
     parfor im = 1:nmodels
     end
 else
+    allpxeq = zeros(nvar,nmodels);
+    allpfeq = zeros(length(model.rxns),nmodels);
+    alloutsol(nmodels) = struct();
     for im = 1:nmodels
         % get npts perturbed points of xeq of type
         Nxeq = eqperturbation(xeq(:,im),npts,type);
         
         % solve each model strating from Nxeq
-        [outsol,outss,allxeq,allfeq,allJac,alllambda] =...
-        solveAllpvec(model,pvec,Nxeq,solverP);
+        [outsol,outss,xss,fss] =...
+        solveAllpvec(model,pvec(im),Nxeq,solverP);
     
-        % parse results for each perturbation for each models        
+        % parse results for each perturbation for each models 
+        alloutsol(im).t = outsol.t;
+        alloutsol(im).y = outsol.y;
+        alloutsol(im).flux = outsol.flux;
+        allpxeq(:,im) = xss;
+        allpfeq(:,im) = fss;
     end
 end
