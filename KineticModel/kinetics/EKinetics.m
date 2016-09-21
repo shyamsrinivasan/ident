fcc6b6fc6214efd929aa56a383126e49b1b0947f
shyamsrinivasan <shyamsweth@gmail.com>
2016-09-21 18:24:13 -0400

    condition for empty substrate index

--- KineticModel/kinetics/EKinetics.m
+++ KineticModel/kinetics/EKinetics.m
@@ -1,4 +1,4 @@
-function flux = EKinetics(model,pvec,M,VFex)
+function [flux,vflux] = EKinetics(model,pvec,M,VFex)
 [~,nc] = size(M);
 S = model.S;
 nrxn = model.nt_rxn;
@@ -21,17 +21,19 @@ end
 for irxn = 1:nrxn
     if ismember(irxn,VFex)
         alls = S(:,irxn);alls(S(:,irxn)>0) = 0;alls(remid,:) = 0;
-        sratio = M(logical(alls),:);
-        thetas = sratio;
-        fwdflx = 0.1*thetas;
-        revflx = 0;
-        if any(Vuptake(irxn))
-            revflx = Vuptake(irxn);
-        end        
-        nrflx = fwdflx-revflx;
-        drflx = 1;
-        vflux(irxn,:) = scale_flux(nrflx./drflx);
-        flux(irxn,:) = Vmax(irxn).*vflux(irxn,:);
+        if any(alls)
+            sratio = M(logical(alls),:);
+            thetas = sratio;
+            fwdflx = 0.1*thetas;
+            revflx = 0;
+            if any(Vuptake(irxn))
+                revflx = Vuptake(irxn);
+            end        
+            nrflx = fwdflx-revflx;
+            drflx = 1;
+            vflux(irxn,:) = scale_flux(nrflx./drflx);
+            flux(irxn,:) = Vmax(irxn).*vflux(irxn,:);
+        end
     end
 end
 flux = flux(VFex);
