9cc5844498da0f0b21dc880e5463aac7dd2d3618
shyamsrinivasan <shyamsweth@gmail.com>
2016-11-05 10:52:17 -0400

    get branches from eq pts for kotte model

---
+++ KineticModel/Kotte2014/kotte_branches.m
@@ -0,0 +1,27 @@
+function fixed_points = kotte_branches(npoints,range,contdir,eqpts,ap,opts)
+
+% set parameters
+global lambda c
+c = 3;
+
+neqpts = size(eqpts,1);
+
+% initialize parameter
+delpar = (range(2) - range(1))/(npoints-1);
+
+% direction of continuation
+if contdir == 1
+    pararray = range(1):delpar:range(2);
+elseif contdir == -1
+    pararray = range(2):-delpar:range(1);
+end
+
+arraycont = zeros(neqpts,3*npoints);
+arraycont(:,1:3) = arraycont;
+
+conttype = zeros(neqpts,npoints);
+
+% continuation of fixed points
+for i = 1:npoints
+    fixed_points = 
+end
