6dfd10012f7867276acf2d2b4d6e3892ff929bb7
shyamsrinivasan <shyamsweth@gmail.com>
2016-11-06 14:48:43 -0500

    change var name lambda to eigvals_all

--- KineticModel/Kotte2014/KotteStabilityInfo.m
+++ KineticModel/Kotte2014/KotteStabilityInfo.m
@@ -1,4 +1,4 @@
-function [type,lambda] = KotteStabilityInfo(eqpts,model,pvec)
+function [type,eigvals_all] = KotteStabilityInfo(eqpts,model,pvec)
 % type
 % 1 : saddle 2D stable manifold
 % 2 : saddle 1D stable manifold
@@ -6,15 +6,14 @@ function [type,lambda] = KotteStabilityInfo(eqpts,model,pvec)
 % 4 : source 3D unstable manifold
 
 % eigen values
-lambda = zeros(size(eqpts,2),size(eqpts,1));
+eigvals_all = zeros(size(eqpts,2),size(eqpts,1));
 for ipts = 1:size(eqpts,1)
-    [~,lambda(:,ipts)] = getKotteJacobian(eqpts(ipts,:)',pvec,model);
+    [~,eigvals_all(:,ipts)] = getKotteJacobian(eqpts(ipts,:)',pvec,model);
 end
-[neig,nvec] = size(lambda);
-
+[neig,nvec] = size(eigvals_all);
 type = zeros(nvec,1);
 
-eigval = real(lambda);
+eigval = real(eigvals_all);
 
 eigval(eigval>0) = 0;
 eigval(eigval<0) = 1;
@@ -22,18 +21,15 @@ eigval(eigval<0) = 1;
 sumall = sum(eigval);
 
 unstable = find(~all(eigval));
-nunstable = length(unstable);
-
+% nunstable = length(unstable);
 stable = find(all(eigval));
-nstable = length(stable);
-
+% nstable = length(stable);
 nnegeig_unstable = sumall(unstable);
 nnegeig_stable = sumall(stable);
 
 type(unstable(nnegeig_unstable==2)) = 1;
 type(unstable(nnegeig_unstable==1)) = 2; 
-type(unstable(nnegeig_unstable==0)) = 4; 
-
+type(unstable(nnegeig_unstable==0)) = 4;
 type(stable(nnegeig_stable==neig)) = 3;
 
 
