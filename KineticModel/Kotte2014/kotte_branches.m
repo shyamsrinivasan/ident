d1adf1ec293e9c3d19571804d995772ecda9f074
shyamsrinivasan <shyamsweth@gmail.com>
2016-11-06 14:50:32 -0500

    more output from continuation call

--- KineticModel/Kotte2014/kotte_branches.m
+++ KineticModel/Kotte2014/kotte_branches.m
@@ -1,11 +1,5 @@
 function fixed_points = kotte_branches(npoints,range,contdir,eqpts,model,pvec,ap,opts)
 
-% set parameters
-global c
-c = 3;
-
-neqpts = size(eqpts,1);
-
 % initialize parameter
 delpar = (range(2) - range(1))/(npoints-1);
 
@@ -16,13 +10,10 @@ elseif contdir == -1
     pararray = range(2):-delpar:range(1);
 end
 
-arraycont = zeros(neqpts,3*npoints);
-% arraycont(:,1:3) = arraycont;
-
-conttype = zeros(neqpts,npoints);
-
 % continuation of fixed points
-contpt = continuation(model,pvec,pararray,ap,eqpts,opts);
+[contpt,conteig,contpttype] = continuation(model,pvec,pararray,ap,eqpts,opts);
+
+fixed_points = contpt;
 for i = 1:npoints
 %     fixed_points = 
 end
