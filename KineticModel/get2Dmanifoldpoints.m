5301dd35455d0aaa22094ef92248f21bd04cbbe3
shyamsrinivasan <shyamsweth@gmail.com>
2016-11-07 19:11:58 -0500

    dynamic assignment of allxdynr

--- KineticModel/get2Dmanifoldpoints.m
+++ KineticModel/get2Dmanifoldpoints.m
@@ -1,10 +1,12 @@
 function allxdynr = get2Dmanifoldpoints(points,model,pvec,tspanr,opts)
 
 npoints = size(points,1);
-nvar = size(points,2);
-npts = length(tspanr)-2;
-allxdynr = zeros(nvar,(npts-2)*npoints);
+% nvar = size(points,2);
+% npts = length(tspanr)-2;
+% allxdynr = zeros(nvar,(npts-2)*npoints);
+allxdynr = [];
 for ip = 1:npoints
     xdynr = solveODEonly(1,points(ip,:)',model,pvec,opts,tspanr); 
-    allxdynr(:,(ip-1)*npts+1:ip*npts) = xdynr(:,3:end);
+%     allxdynr(:,(ip-1)*npts+1:ip*npts) = xdynr(:,3:end);
+    allxdynr = [allxdynr xdynr];
 end
