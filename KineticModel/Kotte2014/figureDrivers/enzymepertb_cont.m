7a5a7ea0813c52916e929ba606bb3131ced787bf
shyamsrinivasan <shyamsweth@gmail.com>
2016-11-21 18:25:37 -0500

    remove unnecessary content

--- KineticModel/Kotte2014/figureDrivers/enzymepertb_cont.m
+++ KineticModel/Kotte2014/figureDrivers/enzymepertb_cont.m
@@ -176,86 +176,6 @@ allpvec = repmat(pvec,npts,1);
 allpvec(:,idp) = cmb;
 allpvec(:,ap) = pvec(ap);
 
-% save allpvec for ap
-allpvecofap = allpvec(:,ap);
-
-orig2pvec = allpvec;
-for id = 1:length(idp)
-    id2xeq = zeros(length(M),npts);    
-    id2feq = zeros(length(fluxg),npts);    
-    ap = idp(id);
-    
-    % fix parameter at a lower value than 1
-    if ap~=14
-        allpvec(:,ap) = 0.01;
-    else
-        allpvec(:,ap) = 5;
-    end
-    
-    % get equilibrium point
-    [~,id2xeq,~,id2feq] =...
-    solveODEonly(npts,M,model,allpvec,opts,tspan,...
-              [],id2xeq,[],id2feq);
-          
-    % continue on parameter
-    [s,mssid,nss] = setupMATCONT(id2xeq,allpvec,ap,model,fluxg,npts,900);
-    allpvec = orig2pvec;
-end
-
-
-
-
-%%
-load('C:\Users\shyam\Documents\Courses\CHE1125Project\Results\KotteModel\VmaxVariation_IvalPerturbation_Aug30.mat');
-
-clear pvec
-kEcat = 1;
-KEacetate = 0.1;    % or 0.02
-KFbpFBP = 0.1;
-vFbpmax = 1;
-Lfbp = 4e6;
-KFbpPEP = 0.1;
-vEXmax = 1;
-KEXPEP = 0.3;
-vemax = 1.1;        % for bifurcation analysis: 0.7:0.1:1.3
-KeFBP = 0.45;       % or 0.45
-ne = 2;             % or 2
-acetate = 0.1;      % a.u acetate
-d = 0.25;           % or 0.25 or 0.35
-kPEPout = 0.2;
-pvec = [KEacetate,KFbpFBP,Lfbp,KFbpPEP,...
-        KEXPEP,vemax,KeFBP,ne,acetate,d,...
-        kPEPout,kEcat,vFbpmax,vEXmax];
-
-% systems check
-givenModel = @(t,x)KotteODE(t,x,model,pvec);
-fluxg = Kotte_givenFlux([M;model.PM],pvec,model);
-dMdtg = givenModel(0,M);    
-    
-opts = odeset('RelTol',1e-12,'AbsTol',1e-10);
-colorSpec = chooseColors(5,{'Green','Purple','Red','Navy','HotPink'});
-ac = find(strcmpi(model.mets,'ac[e]'));
-npts = 1;
-
-allxeq = zeros(length(M),npts);
-allxdyn = zeros(length(M),length(tspan),npts);
-allfeq = zeros(length(fluxg),npts);
-allfdyn = zeros(length(fluxg),length(tspan),npts);
-allpvec = pvec;
-
-% continuation on acetate
-ap = 9;
-solveEquilibriumODE 
-ap1 = ap;
-clear ap
-
-% conitnuation on enzyme parameters 12, 13 and 14 for default values of [1
-% 1 1]'
-model.PM(ac-length(orig_saddle)) = orig_saddlepar;
-for ip = 1:length(idp)
-    ap = idp(ip);
-    solveEquilibriumODE 
-end
 % continuation on enzyme parameters 12, 13 and 14 for perturbed values in
 % cmb
 % change ipt from 1 through 4 to cycle through different types of perturbations
@@ -264,10 +184,15 @@ hf1 = [];
 hf2 = [];
 hf3 = [];
 ipt = 4;
+orig2pvec = allpvec;
+npts = 1;
 while ipt<size(cmb,1)
 % for ipt = 1:size(cmb,1)
-    xeq = alliidxeq(:,ipt);
-    pvec = alliidpvec(ipt,:);
+    % xeq = alliidxeq(:,ipt);
+%     pvec = alliidpvec(ipt,:);
+    pvec = allpvec(ipt,:);
+    id2xeq = zeros(length(M),npts);    
+    id2feq = zeros(length(fluxg),npts);
     addanot.text = ['E' num2str(ipt)];
     for ip = 1:length(idp)
         ap = idp(ip);   
@@ -276,8 +201,12 @@ while ipt<size(cmb,1)
         else
             pvec(ap) = 5;
         end
+        % get equilibrium point
+        [~,id2xeq,~,id2feq] =...
+        solveODEonly(npts,M,model,pvec,opts,tspan,...
+              [],id2xeq,[],id2feq);
         % run MATCONT
-        [data,y,p] = execMATCONT(xeq,pvec,ap,fluxg,model);
+        [data,y,p] = execMATCONT(id2xeq,pvec,ap,fluxg,model);
         if ~isempty(data) && size(data.s1,1)>2
             if ap == 12
                 hf1 = bifurcationPlot(data.x1,data.s1,data.f1,[4,1],ap,hf1,addanot);
@@ -290,8 +219,35 @@ while ipt<size(cmb,1)
         % save MATCONT results
         s.(['pt' num2str(ipt)]) = data;
     end
+    pvec = allpvec(ipt,:);
     ipt = ipt+4;
 end
+
+
+% orig2pvec = allpvec;
+% for id = 1:length(idp)
+%     id2xeq = zeros(length(M),npts);    
+%     id2feq = zeros(length(fluxg),npts);    
+%     ap = idp(id);
+%     
+%     % fix parameter at a lower value than 1
+%     if ap~=14
+%         allpvec(:,ap) = 0.01;
+%     else
+%         allpvec(:,ap) = 5;
+%     end
+%     
+%     % get equilibrium point
+%     [~,id2xeq,~,id2feq] =...
+%     solveODEonly(npts,M,model,allpvec,opts,tspan,...
+%               [],id2xeq,[],id2feq);
+%           
+%     % continue on parameter
+%     [s,mssid,nss] = setupMATCONT(id2xeq,allpvec,ap,model,fluxg,npts,900);
+%     allpvec = orig2pvec;
+% end
+
+
                     
 
 
