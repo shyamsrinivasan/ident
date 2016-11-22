ef7ea0ce829267811607a85cdeac7fddc2444c9b
shyamsrinivasan <shyamsweth@gmail.com>
2016-11-22 11:12:21 -0500

    get boundaries of bistability & remove variables

--- KineticModel/Kotte2014/figureDrivers/enzymepertb_cont.m
+++ KineticModel/Kotte2014/figureDrivers/enzymepertb_cont.m
@@ -66,9 +66,7 @@ idp = [12 13 14];
 pmin = zeros(length(idp),npts); % min
 pmax = zeros(length(idp),npts); % max
 origpvec1 = pvec;
-for id = 1:length(idp)
-    id1xeq = zeros(length(M),npts);    
-    id1feq = zeros(length(fluxg),npts);        
+for id = 1:length(idp)    
     % xbounds = zeros(nvar,npts);
     ap = idp(id);
     
@@ -80,12 +78,11 @@ for id = 1:length(idp)
     end
     
     % find equilibrium solution and continue
-    [~,id1xeq,~,id1feq] =...
-    solveODEonly(npts,M,model,pvec,opts,tspan,...
-              [],id1xeq,[],id1feq);
+    [~,id1xeq] =...
+    solveODEonly(npts,M,model,pvec,opts,tspan);
           
     % continute using MATCONT
-    [s,mssid,nss] = setupMATCONT(id1xeq,pvec,ap,model,fluxg,npts,900);
+    [s,mssid,nss] = setupMATCONT(id1xeq,pvec,ap,model,fluxg,npts,1500);
     
     % identify boundaries of bistable enzyme parameters
     for ipt = 1:npts
@@ -93,15 +90,13 @@ for id = 1:length(idp)
             index = cat(1,s.(['pt' num2str(ipt)]).s1.index);
             x1 = s.(['pt' num2str(ipt)]).x1;
             pcont = x1(nvar+1:end,index);
+            pmin(id,ipt) = min(pcont(:,2:end-1));
+            pmax(id,ipt) = max(pcont(:,2:end-1));
         end
-    end
-    
+    end    
     % reset parameters 
     pvec = origpvec1;
 end
-
-
-
 
 %% continue on acetate for original/nominal parameter values
 ap = 9;
@@ -132,9 +127,9 @@ xss = [xeq1 xeq2];
 %% continuation on enzyme parameters at saddle node for nominal enzyme 
 origpvec = pvec; % pvec backup
 npts = 1;
-for id = 1:length(idp)
-    id2xeq = zeros(length(M),npts);    
-    id2feq = zeros(length(fluxg),npts);    
+pminsaddle = zeros(length(idp),npts); % min
+pmaxsaddle = zeros(length(idp),npts); % max
+for id = 1:length(idp)    
     ap = idp(id);
     
     % fix parameter at a lower value than 1
@@ -145,18 +140,110 @@ for id = 1:length(idp)
     end
     
     % find equilibrium solution and continue
-    [~,id2xeq,~,id2feq] =...
-    solveODEonly(npts,M,model,pvec,opts,tspan,...
-              [],id2xeq,[],id2feq);
+    [~,id2xeq] =...
+    solveODEonly(npts,M,model,pvec,opts,tspan);
           
     % continute using MATCONT
-    [s,mssid,nss] = setupMATCONT(id2xeq,pvec,ap,model,fluxg,npts,900);
+    [s,mssid,nss] = setupMATCONT(id2xeq,pvec,ap,model,fluxg,npts,1500);
     pvec = origpvec;
+    
+    % determine bistable parameter boundaries
+    for ipt = 1:npts
+        if ismember(ipt,mssid)
+            index = cat(1,s.(['pt' num2str(ipt)]).s1.index);
+            x1 = s.(['pt' num2str(ipt)]).x1;
+            pcont = x1(nvar+1:end,index);
+            pminsaddle(id,ipt) = min(pcont(:,2:end-1));
+            pmaxsaddle(id,ipt) = max(pcont(:,2:end-1));
+        end
+    end   
 end
 % reset ap
 ap = 9;
 %% continuation on enzyme parameters at saddle node for enzyme perturbations
 % sample parameters indicated by indices in idp
+% cmb = [.05 1 1;1 .05 1;1 1 .05;.05 .05 .05;...
+%        .125 1 1;1 .125 1;1 1 .125;.125 .125 .125;...
+%        .25 1 1;1 .25 1;1 1 .25;.25 .25 .25;...
+%        .5 1 1;1 .5 1;1 1 .5;.5 .5 .5;...
+%        2 1 1;1 2 1;1 1 2;2 2 2;...
+%        4 1 1;1 4 1;1 1 4;4 4 4];
+% 
+% type = 'together';
+% npts = size(cmb,1);
+% 
+% if strcmpi(type,'together')
+%     alliidpvec = zeros(npts,length(pvec),size(idp,1));
+%     alliidxeq = zeros(length(M),npts,size(idp,1));
+%     alliidxdyn = zeros(length(M),length(tspan),npts,size(idp,1));
+%     alliidfeq = zeros(length(fluxg),npts,size(idp,1));
+%     alliidfdyn = zeros(length(fluxg),length(tspan),npts,size(idp,1));
+% else    
+%     alliidpvec = zeros(npts,length(pvec),length(idp));
+%     alliidxeq = zeros(length(M),npts,length(idp));
+%     alliidxdyn = zeros(length(M),length(tspan),npts,length(idp));
+%     alliidfeq = zeros(length(fluxg),npts,length(idp));
+%     alliidfdyn = zeros(length(fluxg),length(tspan),npts,length(idp));
+% end
+% 
+% % set acetate conentration
+% pvec = [KEacetate,KFbpFBP,Lfbp,KFbpPEP,...
+%         KEXPEP,vemax,KeFBP,ne,acetate,d,...
+%         kPEPout,kEcat,vFbpmax,vEXmax];
+% pvec(ap) = orig_saddlepar;
+% model.PM(ac-length(orig_saddle)) = orig_saddlepar;
+% 
+% % set parameters from cmb at idp position(s)
+% allpvec = repmat(pvec,npts,1);
+% allpvec(:,idp) = cmb;
+% allpvec(:,ap) = pvec(ap);
+% 
+% % continuation on enzyme parameters 12, 13 and 14 for perturbed values in
+% % cmb
+% % change ipt from 1 through 4 to cycle through different types of perturbations
+% % see Table 1 in Methods for the types fo perturbations
+% ipt = 4;
+% hf1 = [];
+% hf2 = [];
+% hf3 = [];
+% orig2pvec = allpvec;
+% npts = 1;
+% while ipt<size(cmb,1)
+%     pvec = allpvec(ipt,:);
+%     id2xeq = zeros(length(M),npts);    
+%     id2feq = zeros(length(fluxg),npts);
+%     addanot.text = ['E' num2str(ipt)];
+%     for ip = 1:length(idp)
+%         ap = idp(ip);   
+%         if ap~=14
+%             pvec(ap) = 0.01;
+%         else
+%             pvec(ap) = 5;
+%         end
+%         % get equilibrium point
+%         [~,id2xeq,~,id2feq] =...
+%         solveODEonly(npts,M,model,pvec,opts,tspan,...
+%               [],id2xeq,[],id2feq);
+%         % run MATCONT
+%         [data,y,p] = execMATCONT(id2xeq,pvec,ap,fluxg,model);
+%         if ~isempty(data) && size(data.s1,1)>2
+%             if ap == 12
+%                 hf1 = bifurcationPlot(data.x1,data.s1,data.f1,[4,1],ap,hf1,addanot);
+%             elseif ap == 13
+%                 hf2 = bifurcationPlot(data.x1,data.s1,data.f1,[4,1],ap,hf2,addanot);
+%             elseif ap == 14
+%                 hf3 = bifurcationPlot(data.x1,data.s1,data.f1,[4,1],ap,hf3,addanot);
+%             end            
+%         end
+%         % save MATCONT results
+%         s.(['pt' num2str(ipt)]) = data;
+%         pvec = allpvec(ipt,:);
+%     end    
+%     ipt = ipt+4;
+% end
+
+%% continuation on enzyme parameters for perturbed values in cmb at saddle 
+% node (same as above cell)
 cmb = [.05 1 1;1 .05 1;1 1 .05;.05 .05 .05;...
        .125 1 1;1 .125 1;1 1 .125;.125 .125 .125;...
        .25 1 1;1 .25 1;1 1 .25;.25 .25 .25;...
@@ -166,20 +253,6 @@ cmb = [.05 1 1;1 .05 1;1 1 .05;.05 .05 .05;...
 
 type = 'together';
 npts = size(cmb,1);
-
-if strcmpi(type,'together')
-    alliidpvec = zeros(npts,length(pvec),size(idp,1));
-    alliidxeq = zeros(length(M),npts,size(idp,1));
-    alliidxdyn = zeros(length(M),length(tspan),npts,size(idp,1));
-    alliidfeq = zeros(length(fluxg),npts,size(idp,1));
-    alliidfdyn = zeros(length(fluxg),length(tspan),npts,size(idp,1));
-else    
-    alliidpvec = zeros(npts,length(pvec),length(idp));
-    alliidxeq = zeros(length(M),npts,length(idp));
-    alliidxdyn = zeros(length(M),length(tspan),npts,length(idp));
-    alliidfeq = zeros(length(fluxg),npts,length(idp));
-    alliidfdyn = zeros(length(fluxg),length(tspan),npts,length(idp));
-end
 
 % set acetate conentration
 pvec = [KEacetate,KFbpFBP,Lfbp,KFbpPEP,...
@@ -193,79 +266,51 @@ allpvec = repmat(pvec,npts,1);
 allpvec(:,idp) = cmb;
 allpvec(:,ap) = pvec(ap);
 
-% continuation on enzyme parameters 12, 13 and 14 for perturbed values in
-% cmb
-% change ipt from 1 through 4 to cycle through different types of perturbations
-% see Table 1 in Methods for the types fo perturbations
-ipt = 4;
-hf1 = [];
-hf2 = [];
-hf3 = [];
 orig2pvec = allpvec;
-npts = 1;
-while ipt<size(cmb,1)
-% for ipt = 1:size(cmb,1)
-    % xeq = alliidxeq(:,ipt);
-%     pvec = alliidpvec(ipt,:);
-    pvec = allpvec(ipt,:);
-    id2xeq = zeros(length(M),npts);    
-    id2feq = zeros(length(fluxg),npts);
-    addanot.text = ['E' num2str(ipt)];
-    for ip = 1:length(idp)
-        ap = idp(ip);   
-        if ap~=14
-            pvec(ap) = 0.01;
-        else
-            pvec(ap) = 5;
-        end
-        % get equilibrium point
-        [~,id2xeq,~,id2feq] =...
-        solveODEonly(npts,M,model,pvec,opts,tspan,...
-              [],id2xeq,[],id2feq);
-        % run MATCONT
-        [data,y,p] = execMATCONT(id2xeq,pvec,ap,fluxg,model);
-        if ~isempty(data) && size(data.s1,1)>2
-            if ap == 12
-                hf1 = bifurcationPlot(data.x1,data.s1,data.f1,[4,1],ap,hf1,addanot);
-            elseif ap == 13
-                hf2 = bifurcationPlot(data.x1,data.s1,data.f1,[4,1],ap,hf2,addanot);
-            elseif ap == 14
-                hf3 = bifurcationPlot(data.x1,data.s1,data.f1,[4,1],ap,hf3,addanot);
-            end            
-        end
-        % save MATCONT results
-        s.(['pt' num2str(ipt)]) = data;
-        pvec = allpvec(ipt,:);
-    end    
-    ipt = ipt+4;
+for ip = 1:length(idp)
+    ap = idp(ip);    
+    
+    % fix parameter at a lower value than 1
+    if ap~=14
+        allpvec(:,ap) = 0.01;
+    else
+        allpvec(:,ap) = 5;
+    end
+    
+    % find equilibirum point for all npts points 
+    [~,id3xeq] = solveODEonly(npts,M,model,allpvec,opts,tspan);
+
+    % continue using MATCONT for all npts points
+    [s,mssid,nss] = setupMATCONT(id3xeq,allpvec,ap,model,fluxg,npts,900); 
+    
+    % store continuation data
+    siid.(['iid' num2str(ip)]) = s;
+    allmssid.(['iid' num2str(ip)]) = mssid;
+    allnss.(['iid' num2str(ip)]) = nss;
+    
+     % reset parameter
+    allpvec = orig2pvec;
 end
 
-
-% orig2pvec = allpvec;
-% for id = 1:length(idp)
-%     id2xeq = zeros(length(M),npts);    
-%     id2feq = zeros(length(fluxg),npts);    
-%     ap = idp(id);
-%     
-%     % fix parameter at a lower value than 1
-%     if ap~=14
-%         allpvec(:,ap) = 0.01;
-%     else
-%         allpvec(:,ap) = 5;
-%     end
-%     
-%     % get equilibrium point
-%     [~,id2xeq,~,id2feq] =...
-%     solveODEonly(npts,M,model,allpvec,opts,tspan,...
-%               [],id2xeq,[],id2feq);
-%           
-%     % continue on parameter
-%     [s,mssid,nss] = setupMATCONT(id2xeq,allpvec,ap,model,fluxg,npts,900);
-%     allpvec = orig2pvec;
-% end
-
-
-                    
+%% identify bistable bounds
+for ip = 1:length(fieldnames(siid))
+    mssid = allmssid.(['iid' num2str(ip)]);    
+    pbounds = zeros(2,length(mssid));
+    xbounds = zeros(nvar,2*length(mssid));
+    mssipt = 1;
+    for ipt = 1:npts
+        if ismember(ipt,mssid)
+            index = cat(1,siid.(['iid' num2str(ip)]).(['pt' num2str(ipt)]).s1.index);
+            x1 = siid.(['iid' num2str(ip)]).(['pt' num2str(ipt)]).x1;
+            pcont = x1(nvar+1:end,index);
+            pbounds(1,mssipt) = min(pcont(:,2:end-1));
+            pbounds(2,mssipt) = max(pcont(:,2:end-1));
+            mssipt = mssipt + 1;
+        end
+    end
+    pbistable.(['iid' num2str(ip)]) = pbounds;
+end
+              
 
 
 
