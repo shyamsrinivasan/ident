7b8dc08611103b98322a8b5fde0680de291cfddd
shyamsrinivasan <shyamsweth@gmail.com>
2016-11-28 13:58:30 -0500

    sample around a point within ball of radius Rs

---
+++ KineticModel/Kotte2014/SphereSampling.m
@@ -0,0 +1,24 @@
+% sample npts points from a ball of radius Rs around any point p
+function sampledpts = SphereSampling(origin,Rs,npts)
+if nargin<3
+    npts = 100;
+end
+if nargin<2
+    % ball of unit radius in 3d cartesian space
+    Rs = 1;
+end
+if nargin<1
+    origin = [0;0;0];
+end
+
+% get npts uniformly distributed points
+U = random('Uniform',0,1,[3 npts]);
+% get nptsx3 standard normal points
+x = random('Normal',0,1,[3 npts]);
+% calculate direction vector
+d = x/sqrt(x(1,:)^2+x(2,:)^2+x(3,:)^2);
+% calculate distance from origin
+lambda = Rs*U.^(1/3);
+
+% translation points to new origin in origin
+sampledpts = repmat(origin,1,npts)+lambda.*d;
