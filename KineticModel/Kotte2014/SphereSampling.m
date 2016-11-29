0730db1a1becb21435bba064aee99dc592f050e7
shyamsrinivasan <shyamsweth@gmail.com>
2016-11-28 19:46:13 -0500

    correct sampling dimensions

--- KineticModel/Kotte2014/SphereSampling.m
+++ KineticModel/Kotte2014/SphereSampling.m
@@ -11,12 +11,13 @@ if nargin<1
     origin = [0;0;0];
 end
 
+nvar = 3;
 % get npts uniformly distributed points
-U = random('Uniform',0,1,[3 npts]);
+U = random('Uniform',0,1,[nvar npts]);
 % get nptsx3 standard normal points
-x = random('Normal',0,1,[3 npts]);
+x = random('Normal',0,1,[nvar npts]);
 % calculate direction vector
-d = x/sqrt(x(1,:)^2+x(2,:)^2+x(3,:)^2);
+d = x./repmat(sqrt(x(1,:).^2+x(2,:).^2+x(3,:).^2),nvar,1);
 % calculate distance from origin
 lambda = Rs*U.^(1/3);
 
