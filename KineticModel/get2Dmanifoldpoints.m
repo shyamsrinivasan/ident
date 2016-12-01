da4ddc0acecb84cf3ecc660cd58dbbe23adc25d4
shyamsrinivasan <shyamsweth@gmail.com>
2016-12-01 17:29:28 -0500

    provide separated axis variables in 3D

--- KineticModel/get2Dmanifoldpoints.m
+++ KineticModel/get2Dmanifoldpoints.m
@@ -1,4 +1,4 @@
-function allxdynr = get2Dmanifoldpoints(points,model,pvec,tspanr,opts)
+function varargout = get2Dmanifoldpoints(points,model,pvec,tspanr,opts)
 
 npoints = size(points,1);
 % nvar = size(points,2);
@@ -8,5 +8,25 @@ allxdynr = [];
 for ip = 1:npoints
     xdynr = solveODEonly(1,points(ip,:)',model,pvec,opts,tspanr); 
 %     allxdynr(:,(ip-1)*npts+1:ip*npts) = xdynr(:,3:end);
-    allxdynr = [allxdynr xdynr];
+    allxdynr = [allxdynr xdynr(:,3:end)];
 end
+
+x = allxdynr(1,:);
+y = allxdynr(2,:);
+z = allxdynr(3,:);
+
+chop_pos = 5;
+
+r = find(x>chop_pos|x<0|y>chop_val|y<0|z>chop_val|z<0);
+x(r) = [];
+y(r) = [];
+z(r) = [];
+
+if nargout<=1
+    varargout{1} = allxdynr;
+elseif nargout>1
+    varargout{1} = x;
+    varargout{2} = y;
+    varargout{3} = z;
+end
+
