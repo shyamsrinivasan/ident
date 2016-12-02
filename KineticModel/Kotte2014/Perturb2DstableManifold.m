9cbb8ad16b108b9e72848cd61196bdabe3684d82
shyamsrinivasan <shyamsweth@gmail.com>
2016-12-02 17:23:26 -0500

    perturbation to 2D manifold surface

---
+++ KineticModel/Kotte2014/Perturb2DstableManifold.m
@@ -0,0 +1,10 @@
+% perturbation of points on the 2D manifold surface
+function [] = Perturb2DstableManifold(2Dmanpts,saddle,model,pvec,npts)
+% 2Dmanpts - points on the 2D manifold represented as triangles from
+% delaunay triangulation
+
+% choose points in random that are away from each other
+
+% simulate from said points in forward time 
+
+% check of final ss is the saddle node
