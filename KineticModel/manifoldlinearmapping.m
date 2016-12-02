e0bc21bbb255b060fb5bc96d3c4aa91a66fcb967
shyamsrinivasan <shyamsweth@gmail.com>
2016-12-02 13:07:26 -0500

    changes to way of calculating linear maps

--- KineticModel/manifoldlinearmapping.m
+++ KineticModel/manifoldlinearmapping.m
@@ -8,20 +8,14 @@ function xnew = manifoldlinearmapping(x1,x2,W1,W2)
 % perform linear mapping of unit circle onto plane in R3 spanned by W1,W2
 % [W1 W2]; %transformation matrix
 
-% x1_new = zeros(1,npoints+1);
-% x2_new = zeros(1,npoints+1);
-% x3_new = zeros(1,npoints+1);
+T = [W1 W2];
+x1new = T(1,:)*[x1;x2];
+x2new = T(2,:)*[x1;x2];
+x3new = T(3,:)*[x1;x2];
 
-xnew = [W1 W2]*[x1;x2];
-
-
-% for ii=1:npoints+1
-%     x_mat = [x1(ii); x2(ii)];
-%     x1_new(ii) = T(1,:)*x_mat;
-%     x2_new(ii) = T(2,:)*x_mat;
-%     x3_new(ii) = T(3,:)*x_mat;
-% end
+xnew = [x1new;x2new;x3new];
 
 
-% [x1new,x2_new,x3_new] =...
-% manifoldprojection(x1,x2,eigvec_stable(:,1),eigvec_stable(:,2),npoints);
+% xnew = [W1 W2]*[x1;x2];
+
+
