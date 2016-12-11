c46d86523d3d376cb9daac1e71a72d7f7e0e6250
shyamsrinivasan <shyamsweth@gmail.com>
2016-12-10 22:09:02 -0500

    working version - pass only variables

--- KineticModel/bvpshooting/BVPshooting_algebraic.m
+++ KineticModel/bvpshooting/BVPshooting_algebraic.m
@@ -1,17 +1,17 @@
-function fx = BVPshooting_algebraic(delyi,delyf,xic,yiknwn,yfknwn)
+function fx = BVPshooting_algebraic(delyi,delyf,xic,yiunkwn,yfknwn)
 % x - n x n-r matrix of solutions from n-r reverse integration runs on the
 %             adjoint equation
 % 
 % generate the n-r algebraic equations to be solved to get n-r unknown
 % initial conditions
 % xr+1*delyr+1 + ... + xn*delyn
-r = length(yiknwn);
+% r = length(yiknwn);
 nvar = size(delyi,1);
-yiunkwn = setdiff(1:nvar,yiknwn);
+% yiunkwn = setdiff(1:nvar,yiknwn);
 
-fx = zeros(nvar-r,1);
-for m = 1:nvar-r
-    fx(m) = sum(xic(yiunkwn,m).*delyi(yiunkwn))-delyf(yfknwn(m));
+fx = zeros(nvar,1);
+for m = 1:nvar
+    fx(m) = sum(xic(yiunkwn,m).*delyi)-delyf(yfknwn(m));
 end
 
 
