f3b02896bb1dde12816a7c6f1cb37df66744b1cb
shyamsrinivasan <shyamsweth@gmail.com>
2016-12-12 18:07:07 -0500

    remove commented sections

--- KineticModel/bvpshooting/shooting.m
+++ KineticModel/bvpshooting/shooting.m
@@ -51,50 +51,23 @@ delyi = getvaldiff(yinit,yinit);
 
 % integrate system with guessed/new intial conditions
 opts = odeset('RelTol',1e-12,'AbsTol',1e-10);
-[tf,ydyn] = ode45(@HoltODE,0:0.1:3.5,yinit,opts);
+[~,ydyn] = ode45(@HoltODE,0:0.1:3.5,yinit,opts);
 yf = ydyn(end,:)';
 
 % check difference in final values
 delyf = getvaldiff(yterm,yf); % assuming yf is obtained at tf
 
+[yi,yf] =...
+execshooting(@HoltODE,yinit,yterm,ti,tf,yiunkwn,yfknwn,delyi,delyf,yf,eps);
+
 % start of while loop for BVP shooting
-eps = 1e-6;
-while abs(delyf(yfknwn)) <= repmat(eps,length(yfknwn),1)
-    % integrate adjoint equation in reverse time from tf to t0 for m =
-    % 1,...,n-r
-    fprintf('Solving adjoint Equation...\n');
-    xic = zeros(nvar,nvar-r);
-    for m = 1:nvar-r
-        getAdj = @(t,x)adjointEquation(t,x,yf);
-        xf = zeros(nvar,1);
-        xf(yfknwn(m)) = 1;
-        [~,xint] = ode45(getAdj,0:-0.1:-3.5,xf,opts);
-        xic(:,m) = xint(end,:)';
-    end
-
-    % solve n-r algebraic equations given by 
-    fprintf('Solving algebraic Equations...\n');
-    delyvar = delyi(yiunkwn);
-    getAgeq = @(delyi)BVPshooting_algebraic(delyi,delyf,xic,yiunkwn,yfknwn);
-    fx = getAgeq(delyvar);
-    options = optimoptions('fsolve','Display','iter',...
-                           'TolFun',1e-10,...
-                           'TolX',1e-10,...
-                           'MaxFunEvals',1000000,...
-                           'MaxIter',50000);
-    [new_delyi,fval,exitflag,output,jacobian] = fsolve(getAgeq,delyvar,options);
-    delyi(yiunkwn) = new_delyi(:);
-    yinit = yinit + delyi;
-    
-    % integrate system with guessed/new intial conditions
-    fprintf('Integrated system to find final value...\n');
-    opts = odeset('RelTol',1e-12,'AbsTol',1e-10);
-    [tf,ydyn] = ode45(@HoltODE,0:0.1:3.5,yinit,opts);
-    yf = ydyn(end,:)';
-
-    % check difference in final values
-    delyf = getvaldiff(yterm,yf); % assuming yf is obtained at tf
-end
+% eps = 1e-4;
+% ti = 0;
+% tf = 3.5;
+% while abs(delyf(yfknwn)) > repmat(eps,length(yfknwn),1)
+%     % run shooting iteration
+%     delyf = itershooting(@HoltODE,yinit,yterm,ti,tf,yiunkwn,yfknwn,delyi,delyf,yf,opts);
+% end
 
 
 
