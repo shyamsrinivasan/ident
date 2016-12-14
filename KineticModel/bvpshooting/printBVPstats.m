e13cfd9be4321b5d8f8c5c97ed4d846b794c998b
shyamsrinivasan <shyamsweth@gmail.com>
2016-12-14 15:47:57 -0500

    add time elapsed and success/failute tags

--- KineticModel/bvpshooting/printBVPstats.m
+++ KineticModel/bvpshooting/printBVPstats.m
@@ -1,4 +1,4 @@
-function printBVPstats(iter,delyf,t0,tf)
+function printBVPstats(iter,delyf,t0,tf,delt,scfl)
 if nargin<1
     call=1;
 else
@@ -7,8 +7,9 @@ end
 
 if call == 1
     fprintf('\n\tTwo point BVP solution stats\t\t\n');
-    fprintf('--------------------------------------\n');
-    fprintf('iter \t\t max(delyf) \t\t t0 \t\t tf \n');
+    fprintf('-------------------------------------------------\n');
+    fprintf('iter \t\t max(delyf) \t\t t0 \t\t\t tf \t\t Time \t\t S/F\n');
 elseif call == 2
-    fprintf('%d \t\t\t %5.3g \t\t\t %3.2f \t\t\t %3.2f \n',iter,max(delyf),t0,tf);
+    fprintf('%d \t\t\t %5.3g \t\t\t %3.2f \t\t %3.2f \t\t %3.2f \t\t %s \n',...
+            iter,max(delyf),t0,tf,delt,scfl);
 end
