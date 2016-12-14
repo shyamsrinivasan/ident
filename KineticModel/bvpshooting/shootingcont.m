b6375431ef4b39b8e3f9f9b3327a064d396a12bf
shyamsrinivasan <shyamsweth@gmail.com>
2016-12-14 15:48:55 -0500

    change tau and ival

--- KineticModel/bvpshooting/shootingcont.m
+++ KineticModel/bvpshooting/shootingcont.m
@@ -23,12 +23,12 @@ yfunkwn = find(~yfknwn);
 yfknwn = find(yfknwn);
 
 % choose unknown initial conditions
-yinit(yiunkwn) = [-1.000000000;0.600000000];
+yinit(yiunkwn) = [-.978197694;0.646786696];
 delyi = getvaldiff(yinit,yinit);
 
 % cotinuation parameters
 F = 1.0; 
-tau = 0.5;
+tau = 0.2;
 ti = 0;
 tf = 3.5;
 tterm = 11.0;
@@ -39,22 +39,30 @@ opts = odeset('RelTol',1e-12,'AbsTol',1e-10);
 % [yi,yf,delyi,delyf] =...
 % itershooting(@HoltODE,yinit,yterm,ti,tf,yiunkwn,yfknwn,delyi,[],[],opts);
 yi = yinit;
-[~,ydyn] = ode45(@HoltODE,ti:0.1:tf,yi,opts);
-yf = ydyn(end,:)';
+[tf,yf] = integrateshooting(@HoltODE,ti,tf,yi,opts);
+% [~,ydyn] = ode45(@HoltODE,ti:0.1:tf,yi,opts);
+% yf = ydyn(end,:)';
 delyf = getvaldiff(yterm,yf);
 
+allyi = yi;
+allti = ti;
+alltf = tf;
 while tf<tterm
     flag = 1;
     while flag        
         % solve 2 point bvp over (t0,t1) using goodman lance method
-        [yi,yf,delyi,delyf,flag] =...
-        execshooting(@HoltODE,yi,yf,ti,tf,yiunkwn,yfknwn,delyi,delyf,yf,eps);    
+        [yi,yf,tf,delyi,~,flag] =...
+        execshooting(@HoltODE,yi,yterm,ti,tf,yiunkwn,yfknwn,delyi,delyf,yf,eps);    
         if flag
+            % store yi,ti,tf values
+            allyi = [allyi yi];
+            allti = [allti;ti];
+            alltf = [alltf;tf];
             tf = tf + tau;
         end     
-        [~,ydyn] = ode45(@HoltODE,ti:0.1:tf,yi,opts);
-        ysimf = ydyn(end,:)';
-        delyf = getvaldiff(yf,ysimf);
+        yfold = yf;
+        [~,yf] = integrateshooting(@HoltODE,ti,tf,yi,opts);        
+        delyf = getvaldiff(yterm,yf);
 %         [yi,yf,delyi,delyf] =...
 %         itershooting(@HoltODE,yi,yf,ti,tf,yiunkwn,yfknwn,delyi,newdelyf,ysimf,opts);
     end
