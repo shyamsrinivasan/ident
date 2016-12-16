f59b0fa1c6f5cb04392214d72b39220581c35853
shyamsrinivasan <shyamsweth@gmail.com>
2016-12-16 16:49:22 -0500

    new run script for re-written code

--- KineticModel/bvpshooting/shootingcont.m
+++ KineticModel/bvpshooting/shootingcont.m
@@ -36,11 +36,43 @@ tf = 3.5;
 tterm = 11.0;
 eps = 1e-4;
 
+yi = yinit;
+opts = odeset('RelTol',1e-13,'AbsTol',1e-14);
 % integrate till time t1 till which there are no numerical problems
-opts = odeset('RelTol',1e-18,'AbsTol',1e-16);
+status = 1;
+while status
+    [tftest,~,flag1] = integrateshooting(@HoltODE,ti,tf,yi,opts);
+    if flag1<0
+        tf = (tf+tfnew)/2;
+    elseif flag1>0
+        status = 0;
+    end
+end
+
+status = 1;
+
+while status
+    [yic,yfc,tfnew,delyic,delyfc,flag] = execshooting(@HoltODE,ti,tf,yi,[],yterm,delyi,[],yiunkwn,yfknwn,eps,opts);
+
+    if flag<0
+        tf = tf-tau/2;
+        yi = yinit;
+        delyi = getvaldiff(yinit,yinit);
+        continue
+    elseif flag>0
+        tf = tf+tau;
+        yi = yic(:,end);
+        delyi = delyic(:,end);
+    end
+
+end
+
+
+
+
 % [yi,yf,delyi,delyf] =...
 % itershooting(@HoltODE,yinit,yterm,ti,tf,yiunkwn,yfknwn,delyi,[],[],opts);
-yi = yinit;
+
 [tf,yf] = integrateshooting(@HoltODE,ti,tf,yi,opts);
 % [~,ydyn] = ode45(@HoltODE,ti:0.1:tf,yi,opts);
 % yf = ydyn(end,:)';
@@ -51,34 +83,34 @@ allyf = yf;
 allti = ti;
 alltf = tf;
 saveyi = [];
-while tf<tterm
-    flag = 1;
-    while flag        
-        % solve 2 point bvp over (t0,t1) using goodman lance method
-        oldtf = tf;
-        [yi,yf,tf,delyi,~,flag] =...
-        execshooting(@HoltODE,yi,yterm,ti,tf,yiunkwn,yfknwn,delyi,delyf,yf,eps);    
-        if flag
-            % store yi,ti,tf values
-            allyi = [allyi yi];
-            allyf = [allyf yf];
-            allti = [allti;ti];
-            alltf = [alltf;tf];
-            tf = tf + tau;
-        else
-            % tf = tf-tau/2;
-            saveyi = [allyi yi];
-            yi = allyi(:,end-1);
-            yfold = yf;
-            [~,yf] = integrateshooting(@HoltODE,ti,tf(end),yi,opts);        
-            delyf = getvaldiff(yterm,yf);
-        end     
-        
-%         [yi,yf,delyi,delyf] =...
-%         itershooting(@HoltODE,yi,yf,ti,tf,yiunkwn,yfknwn,delyi,newdelyf,ysimf,opts);
-    end
-    tau = tau/2;
-end
+% while tf<tterm
+%     flag = 1;
+%     while flag        
+%         % solve 2 point bvp over (t0,t1) using goodman lance method
+%         oldtf = tf;
+%         [yi,yf,tf,delyi,~,flag] =...
+%         execshooting(@HoltODE,yi,yterm,ti,tf,yiunkwn,yfknwn,delyi,delyf,yf,eps);    
+%         if flag
+%             % store yi,ti,tf values
+%             allyi = [allyi yi];
+%             allyf = [allyf yf];
+%             allti = [allti;ti];
+%             alltf = [alltf;tf];
+%             tf = tf + tau;
+%         else
+%             % tf = tf-tau/2;
+%             saveyi = [allyi yi];
+%             yi = allyi(:,end-1);
+%             yfold = yf;
+%             [~,yf] = integrateshooting(@HoltODE,ti,tf(end),yi,opts);        
+%             delyf = getvaldiff(yterm,yf);
+%         end     
+%         
+% %         [yi,yf,delyi,delyf] =...
+% %         itershooting(@HoltODE,yi,yf,ti,tf,yiunkwn,yfknwn,delyi,newdelyf,ysimf,opts);
+%     end
+%     tau = tau/2;
+% end
 
 
 
