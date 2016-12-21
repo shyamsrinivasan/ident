c90dda131d72f3ba23b0d1e95c9d2d5fa1d69239
shyamsrinivasan <shyamsweth@gmail.com>
2016-12-20 22:20:50 -0500

    accept chop vals array

--- KineticModel/Kotte2014/chopvals.m
+++ KineticModel/Kotte2014/chopvals.m
@@ -3,7 +3,7 @@ function [x,y,z,r] = chopvals(x,y,z,chop_pos)
 % remove points past threshold on 3-D plane
 % chop_pos = 5;
 
-r = find(x>chop_pos|x<0|y>chop_pos|y<0|z>chop_pos|z<0);
+r = find(x>chop_pos(1)|x<0|y>chop_pos(2)|y<0|z>chop_pos(3)|z<0);
 x(r) = [];
 y(r) = [];
 z(r) = [];
