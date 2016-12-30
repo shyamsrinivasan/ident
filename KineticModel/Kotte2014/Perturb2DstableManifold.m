76b4bf3aeb4066df0606fa45cd275dc184b80505
shyamsrinivasan <shyamsweth@gmail.com>
2016-12-29 19:29:14 -0500

    add figure and take in plot indices and handles

--- KineticModel/Kotte2014/Perturb2DstableManifold.m
+++ KineticModel/Kotte2014/Perturb2DstableManifold.m
@@ -1,7 +1,11 @@
 % perturbation of points on the 2D manifold surface
-function [] = Perturb2DstableManifold(man2Dpts,saddle,model,pvec,opts,tspanf,hfig,npts)
+function [] = Perturb2DstableManifold(man2Dpts,saddle,model,pvec,opts,tspanf,hfig,id,npts)
 % 2Dmanpts - points on the 2D manifold represented as triangles from
 % delaunay triangulation or from the known trajectories
+kid = man2Dpts(:,1)<0|man2Dpts(:,1)>5|man2Dpts(:,2)<0|...
+      man2Dpts(:,2)>5|man2Dpts(:,3)<0|man2Dpts(:,3)>5;
+  
+man2Dpts(kid,:) = [];
 nmanpts = size(man2Dpts,1);
 
 % choose points in random that are away from each other
@@ -9,8 +13,8 @@ rndid = randi(nmanpts,npts,1);
 startval = man2Dpts(rndid,:);
 
 % perturb said points
-posival = startval + repmat(1e-2*[0 0 1],npts,1);
-negival = startval - repmat(1e-2*[0 0 1],npts,1);
+posival = startval + repmat(1e-3*[1 1 1],npts,1);
+negival = startval - repmat(1e-3*[1 1 1],npts,1);
 
 % simulate from said points in forward time
 for ival = 1:npts
@@ -18,14 +22,37 @@ for ival = 1:npts
     [xdynf2,xeq2] = solveODEonly(1,negival(ival,:)',model,pvec,opts,tspanf);    
     
     % xdynr1 & xdynr2 plots
-    FIGodetrajectories(xdynf1,posival(ival,:)',xeq1,2,[1 2 3],hfig);
+    FIGodetrajectories(xdynf1,posival(ival,:)',xeq1,2,id,hfig);
     if xeq1(1)<xeq1(2)
-        FIGmssEqIvalPerturbations(posival(ival,:)',xeq1,2,[1 2 3],hfig);
+        FIGmssEqIvalPerturbations(posival(ival,:)',xeq1,2,id,hfig);
     end
-    FIGodetrajectories(xdynf2,negival(ival,:)',xeq2,2,[1 2 3],hfig);
+    FIGodetrajectories(xdynf2,negival(ival,:)',xeq2,2,id,hfig);
     if xeq2(1)<xeq2(2)
-        FIGmssEqIvalPerturbations(negival(ival,:)',xeq2,2,[1 2 3],hfig);
+        FIGmssEqIvalPerturbations(negival(ival,:)',xeq2,2,id,hfig);
     end
 end
 
+gcf
+if length(id)==3
+    axis([0 2.5 0 1.6 0 1.6]);
+    view([116 22]);
+elseif length(id) == 2   
+    switch id(1)
+        case 1
+            xlim = [0 2.5];
+        case 2
+            xlim = [0 1.6];
+        case 3
+            xlim = [0 1.6];
+    end
+    switch id(2)
+        case 1
+            ylim = [0 2.5];
+        case 2
+            ylim = [0 1.6];
+        case 3
+            ylim = [0 1.6];
+    end
+    axis([xlim ylim]);    
+end
 % check of final ss is the saddle node
