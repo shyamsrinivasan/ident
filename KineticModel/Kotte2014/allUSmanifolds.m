2fa22426fe8d8ca37fe0c92a10ba098c11e68b38
shyamsrinivasan <shyamsweth@gmail.com>
2016-12-30 20:27:10 -0500

    plot all Wus for all saddle nodes as a surface

--- KineticModel/Kotte2014/allUSmanifolds.m
+++ KineticModel/Kotte2014/allUSmanifolds.m
@@ -81,54 +81,51 @@ allfdyn = zeros(length(fluxg),length(tspan),npts);
 ap = 9;
 solveEquilibriumODE    
 
-% get all parameter (saddles) values between the 2 limit points
-allsaddles = extractsaddles(data.s1,data.x1);
-
-% get eigval and eig vec for all saddle points
-alleig = zeros(3,size(allsaddles,2));
-allw = zeros(3,3,size(allsaddles,2));
-for ipts = 1:size(data.x1,2)
-    pvec(ap) = allsaddles(end,ipts);
-    model.PM(ac-length(saddle)) = allsaddles(end,ipts);
-    [~,alleig(:,ipts),w] = getKotteJacobian(allsaddles(1:3,ipts),pvec,model);
-    allw(:,:,ipts) = w;
-end
-
-
 % get saddle node
 [saddle,saddlepar] = getsaddlenode(data.s1,data.x1,5e-3);
-pvec(ap) = saddlepar;
-model.PM(ac-length(saddle)) = saddlepar;
+% get all parameter (saddles) values between the 2 limit points
+allsaddles = extractsaddles(data.s1,data.x1);
+nsadpts = size(allsaddles,2);
 
-% calculate eig val for all points on continuation curve data.x1
-alleig = zeros(3,size(data.x1,2));
-for ipts = 1:size(data.x1,2)
-    pvec(ap) = data.x1(end,ipts);
-    model.PM(ac-length(saddle)) = data.x1(end,ipts);
-    [~,alleig(:,ipts)] = KotteStabilityInfo(data.x1(1:3,ipts)',model,pvec);      
+% get eigval and eig vec for all saddle points
+alleig = zeros(nvar,nsadpts);
+allw = zeros(nvar,nvar,nsadpts);
+tspanr = 0:-.1:-30;
+tspanf = 0:0.1:2000;
+eps = 1e-4;
+allWus = zeros(nvar,length(tspanr),nsadpts);
+
+for ipts = 1:nsadpts
+    pvec(ap) = allsaddles(end,ipts);
+    model.PM(ac-length(saddle)) = allsaddles(end,ipts);
+    
+    % get eig val and eig vector
+    [~,alleig(:,ipts),w] = getKotteJacobian(allsaddles(1:3,ipts),pvec,model);
+    allw(:,:,ipts) = w;
+    
+    % calculate unstable manifold for all saddle points
+    [xWus,xeq] = calc1DWus(allsaddles(1:3,ipts),w,alleig(:,ipts),model,pvec,opts,tspanr,tspanf,eps);
+    
+    % collect reverse trajectory 
+    allWus(:,:,ipts) = xWus;
 end
 
-% perturb saddle to get steady states
-pvec(ap) = saddlepar;
-model.PM(ac-length(saddle)) = saddlepar;
-eps = 1e-4;
-tspanf = 0:0.1:2000;
-pival = saddle+eps*[1;1;1];
-[~,xeq1] = solveODEonly(1,pival,model,pvec,opts,tspanf);
+%% unstable manifold in 3D
+% figure
+allx = [];ally = [];allz = [];
+for jpt = 1:nsadpts    
+    [~,nzid,~] = find(allWus(:,:,jpt)~=0,1,'last');
+    relWus = allWus(:,1:nzid,jpt);
+    kid = relWus(1,:)<0|relWus(1,:)>5|relWus(2,:)<0|relWus(2,:)>5|relWus(3,:)<0|relWus(3,:)>5;
+    relWus(:,kid) = [];
+    allx = [allx relWus(1,:)];
+    ally = [ally relWus(2,:)];
+    allz = [allz relWus(3,:)];
+%     hold on
+%     plot3(relWus(1,:),relWus(2,:),relWus(3,:),'LineStyle','none','Marker','.','Color','r');    
+end
+hfig = Manifold2DPlot(allx,ally,allz);
 
-nival = saddle-eps*[1;1;1];
-[~,xeq2] = solveODEonly(1,nival,model,pvec,opts,tspanf);
 
-[~,eigval,w] = getKotteJacobian(saddle,pvec,model);
-
-% all manifolds in 2D
-% tspanr = [0,-30;0 -12];
-% NumericalSeparatrix(model,pvec,opts,ap,data.s1,data.x1,[xeq1 xeq2],'all',tspanr,2,5e-3);
-
-% unstable manifold in 3D
-tspanr = [0:-.1:-30]; % 0 -12];
-[hfig,xmafold] =...
-NumericalSeparatrix(model,pvec,opts,ap,data.s1,data.x1,[xeq1 xeq2],...
-                    'unstable',tspanr,3,5e-3);
-perturbManifolds(xmafold(1:3,:)',saddle,model,pvec,opts,tspanf,...
-                        hfig,[1 2 3],10); 
+% perturbManifolds(xmafold(1:3,:)',saddle,model,pvec,opts,tspanf,...
+%                         hfig,[1 2 3],10); 
