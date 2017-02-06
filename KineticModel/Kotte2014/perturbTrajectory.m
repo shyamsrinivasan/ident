497362ecab9f3a5aa1c8b58caf32cab873eeca55
shyamsrinivasan <shyamsweth@gmail.com>
2017-02-06 18:39:37 -0500

    create random initial values

--- KineticModel/Kotte2014/perturbTrajectory.m
+++ KineticModel/Kotte2014/perturbTrajectory.m
@@ -1,5 +1,6 @@
-function perturbTrajectory(traj,model,pvec,opts,tspanf,hfig,id,npts)
-if nargin<8
+% choose points in random that are away from each other
+function newivals = perturbTrajectory(traj,npts)
+if nargin<2
     npts = 5000;
 end
 
@@ -13,28 +14,36 @@ newnpts = ptspinterval*ninterval;
 newivals = zeros(newnpts,nvar);
 ipts = 0;
 oldmin = 0;
+skip = 0;
 for jinterval = 1:ninterval
     if ~oldmin
         minx = min([traj(:,jinterval) traj(:,jinterval+1)],[],2);
     end
     maxx = max([traj(:,jinterval) traj(:,jinterval+1)],[],2);
-    if any((maxx-minx)<1e-5)
+    if any(abs(maxx-minx)>1e-4)
+        newminx = min([minx maxx],[],2);
+        newmaxx = max([minx maxx],[],2);        
+    else
         oldmin = 1;
+%         skip = skip+1;
+%         ipts = ipts+ptspinterval;
         continue
     end
     % get numbers from a uniform distribution
-    pd1 = makedist('Uniform','lower',minx(1),'upper',maxx(1));
-    pd2 = makedist('Uniform','lower',minx(2),'upper',maxx(2));
-    pd3 = makedist('Uniform','lower',minx(3),'upper',maxx(3));
+    pd1 = makedist('Uniform','lower',newminx(1),'upper',newmaxx(1));
+    pd2 = makedist('Uniform','lower',newminx(2),'upper',newmaxx(2));
+    pd3 = makedist('Uniform','lower',newminx(3),'upper',newmaxx(3));
     r1 = random(pd1,ptspinterval,1);
     r2 = random(pd2,ptspinterval,1);
     r3 = random(pd3,ptspinterval,1);
 %     if ipts == 
 %         newivals(1:ptspinterval,:) = [r1 r2 r3];
-    if ipts>1 && ipts<newnpts
+    if ipts<newnpts
         newivals(ipts+1:ipts+ptspinterval,:) = [r1 r2 r3];
-    end
-    ipts = ipts+ptspinterval;
+        ipts = ipts+ptspinterval;
+    end    
 end
 
-% choose points in random that are away from each other
+newivals = newivals(1:ipts,:);
+
+
