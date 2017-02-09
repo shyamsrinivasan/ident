abf612a42b0704276cfbcd488bd580b2f189bd7d
shyamsrinivasan <shyamsweth@gmail.com>
2017-02-09 13:04:49 -0500

    add algebraic equation solving through fsolve

--- KineticModel/Kotte2014/getPequilibrium.m
+++ KineticModel/Kotte2014/getPequilibrium.m
@@ -1,21 +1,32 @@
 % get final equilibrium values for initial values perturbed from 
 % unstable manifold
-function getPequilibrium(ivals,model,pvec,opts,tspanf,hfig,id)
+function getPequilibrium(ivals,model,pvec,options,opts,tspanf,hfig,id)
 
 [npts,nvar] = size(ivals);
 
-posp = ivals + repmat(1e-4*[1 1 1],npts,1);
-negp = ivals - repmat(1e-4*[1 1 1],npts,1);
+posp = ivals + repmat(1e-3*[1 1 1],npts,1);
+negp = ivals - repmat(1e-3*[1 1 1],npts,1);
+
+allxeq = zeros(2*nvar,npts);
+allfeq = zeros(2*5,npts);
 
 for jval = 1:npts
-    [~,xeq1] = solveODEonly(1,posp(jval,:)',model,pvec,opts,tspanf);
-    [~,xeq2] = solveODEonly(1,negp(jval,:)',model,pvec,opts,tspanf); 
+    
+%     [xae1,feq1] = solveAEonly(1,posp(jval,:)',model,pvec,options);
+%     [xae2,feq2] = solveAEonly(1,negp(jval,:)',model,pvec,options);      
+    [~,xde1,~,feq1] = solveODEonly(1,posp(jval,:)',model,pvec,opts,tspanf);
+    [~,xde2,~,feq2] = solveODEonly(1,negp(jval,:)',model,pvec,opts,tspanf); 
+    
+    allxeq(1:nvar,jval) = xde1;
+    allxeq(nvar+1:end,jval) = xde2;
+    allfeq(1:5,jval) = feq1;
+    allfeq(5+1:end,jval) = feq2;
     
     % equilibrium point plots
-    if xeq1(1)<xeq1(2)
+    if xde1(1)<xde1(2)
         FIGmssEqIvalPerturbations(posp(jval,:)',xeq1,2,id,hfig);
     end
-    if xeq2(1)<xeq2(2)
+    if xde2(1)<xde2(2)
         FIGmssEqIvalPerturbations(negp(jval,:)',xeq2,2,id,hfig);
     end
 end
