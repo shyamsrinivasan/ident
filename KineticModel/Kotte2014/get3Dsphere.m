65729a3aa40643c477ff42904ef19a0c1a971529
shyamsrinivasan <shyamsweth@gmail.com>
2017-02-26 15:22:44 -0500

    remove points less than zero

--- KineticModel/Kotte2014/get3Dsphere.m
+++ KineticModel/Kotte2014/get3Dsphere.m
@@ -1,5 +1,6 @@
 function newivals = get3Dsphere(center,r,npts)
 % get points around the centre within a sphere at a maximum distance r from it
+% keep only positive points
 
 theta = random('unif',0,2*pi,npts,1);
 phi = random('unif',0,pi,npts,1);
@@ -10,3 +11,6 @@ x2 = r.*sin(theta).*sin(phi);
 x3 = r.*cos(phi);
 
 newivals = repmat(center,npts,1) + [x1 x2 x3];
+
+id = newivals(:,1)<0|newivals(:,2)<0|newivals(:,3)<0;
+newivals(id,:) = [];
