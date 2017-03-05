1b21cf708f5404cecd0386edd0a7a849a8841e9c
shyamsrinivasan <shyamsweth@gmail.com>
2017-03-05 03:02:46 -0500

    changes to get all stable steady states

--- KineticModel/Kotte2014/continuation.m
+++ KineticModel/Kotte2014/continuation.m
@@ -1,4 +1,7 @@
 function [contpt,varargout] = continuation(model,pvec,lambda,ap,iguess,opts)
+if nargin<6
+    opts = optimoptions(@fsolve,'TolX',1e-6,'TolFun',1e-6,'Display','iter');
+end
 
 % npts = size(iguess,1);
 % contpt = zeros(npts,size(iguess,2));
@@ -20,38 +23,54 @@ function [contpt,varargout] = continuation(model,pvec,lambda,ap,iguess,opts)
 % end
 
 ac = find(strcmpi(model.mets,'ac[e]'));
-nvar = size(iguess,2);
-npts = size(iguess,1);
+nvar = size(iguess,1);
+% npts = size(iguess,1);
 npar = length(lambda);
-alleqsol = zeros(npts,npar*nvar);
-eqpttype = zeros(npts,npar);
-alleigval = zeros(nvar,npar*npts);
+alleqsol = zeros(nvar,2*npar);
+alleqflux = zeros(5,2*npar);
+% eqpttype = zeros(npts,npar);
+alleigval = zeros(nvar,2*npar);
 % continuation algorithm
 % set parameter value loop
 for ip = 1:npar 
     model.PM(ac-nvar) = lambda(ip);
     pvec(ap) = lambda(ip);
-    sysfun = @(x)Kotte_givenNLAE(x,model,pvec);    
-    % re-initialize neweqpt
-    neweqpt= zeros(npts,nvar);
-    for jval = 1:npts
-        neweqpt(jval,:) = fsolve(sysfun,iguess(jval,:)',opts);
-    end
+    [~,neweqpt,~,eqflux] = solveODEonly(1,iguess,model,pvec,opts,1:2000);
+
     % store neweqpt
-    alleqsol(:,(ip-1)*nvar+1:ip*nvar) = neweqpt;
+    alleqsol(:,ip) = neweqpt;
+    alleqflux(:,ip) = eqflux;
     
     % get stability info
-    [type,eigval] = KotteStabilityInfo(neweqpt,model,pvec);
-    eqpttype(:,ip) = type;
-    alleigval(:,(ip-1)*npts+1:ip*npts) = eigval;
+    [~,eigval] = KotteStabilityInfo(neweqpt',model,pvec);
+    alleigval(:,ip) = eigval;
     
     % set initial value loop (from different solution branches ?)
-    iguess = neweqpt;
+    iguess = alleqsol(:,ip);
 end
-% last points only
-contpt = alleqsol(:,nvar*npar-(nvar-1):nvar*npar);   
-varargout{1} = alleigval(:,nvar*npar-(nvar-1):nvar*npar);
-varargout{2} = eqpttype(:,end);
+
+ipt = npar;
+for ip = npar:-1:1
+    ipt = ipt+1;
+    model.PM(ac-nvar) = lambda(ip);
+    pvec(ap) = lambda(ip);
+    [~,neweqpt,~,eqflux] = solveODEonly(1,iguess,model,pvec,opts,1:2000);
+    
+    % store neweqpt
+    alleqsol(:,ipt) = neweqpt;
+    alleqflux(:,ipt) = eqflux;
+    
+    % get stability info
+    [~,eigval] = KotteStabilityInfo(neweqpt',model,pvec);
+    alleigval(:,ipt) = eigval;
+    
+    % set initial value loop (from different solution branches ?)
+    iguess = alleqsol(:,ipt);
+end
+contpt = alleqsol; % (:,nvar*npar-(nvar-1):nvar*npar);   
+varargout{1} = alleqflux;
+varargout{2} = alleigval; % (:,nvar*npar-(nvar-1):nvar*npar);
+% varargout{2} = eqpttype(:,end);
 % all value
-varargout{3} = alleigval;
-varargout{4} = eqpttype;
+% varargout{3} = alleigval;
+% varargout{4} = eqpttype;
