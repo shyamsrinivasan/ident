038ff0ea785de46d9553e7ab10d358664336687d
Shyam Srinivasan <shyamsweth@gmail.com>
2017-04-02 16:31:16 -0400

    pass options as input
    (cherry picked from commit d5cd4c6364e3dc7493127381748246afab8d0478)

--- KineticModel/Kotte2014/continuation/LPcontinuation.m
+++ KineticModel/Kotte2014/continuation/LPcontinuation.m
@@ -1,4 +1,4 @@
-function data = LPcontinuation(x0,ap,funame,pvec)
+function data = LPcontinuation(x0,ap,funame,pvec,opt)
 
 global sys
 sys.gui.pausespecial=1;  %Pause at special points 
@@ -7,12 +7,6 @@ sys.gui.pauseeachpoint=0; %Pause at each point
 
 % limit point conitnuation (2 free variables in ap)
 [x0,v0]=init_LP_LP(funame,x0,pvec',ap);
-opt=contset;
-opt=contset(opt,'MaxNumPoints',300);
-opt=contset(opt,'MinStepSize',0.00001);
-opt=contset(opt,'MaxStepSize',0.01);
-opt=contset(opt,'Singularities',1);
-opt = contset(opt,'Eigenvalues',1);
 [x,v,s,h,f]=cont(@limitpoint,x0,v0,opt);
 
 % separation of variable and parameter vectors
