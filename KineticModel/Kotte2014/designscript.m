b9a01a3b8a93ede4457bdd2160efaf3dbac69e82
shyamsrinivasan <shyamsweth@gmail.com>
2017-04-03 18:44:00 -0400

    script for design using repressilator

---
+++ KineticModel/Kotte2014/designscript.m
@@ -0,0 +1,27 @@
+% inverse bifurcation from Lu, et al., 2006
+% initial parameter sets
+alpha = 10^2.5;
+beta = 1;
+delta = 1; % 1e-3;
+h = 5; % 1.5;
+pvec = [alpha,beta,delta,h];
+pi = [alpha,beta];
+ps = [delta,h];
+
+% parameter bounds    
+psl = [1e-4,0];
+psu = [1e-1,2];
+
+% solve ode system to get initial equilibrium points
+ival = [10;10;10;10;10;10];
+tspan = 0:0.1:100;
+odefun = @(t,x)repressilatorODE(t,x,[],pvec');
+opts = odeset('RelTol',1e-12,'AbsTol',1e-10);
+[tout,yout] = ode15s(odefun,tspan,ival,opts);
+figure
+plot(tout,yout);
+
+% run MATCONT
+ap = 2;
+[data,y,p] = execMATCONT(@repressilator,[],yout(end,:)',pvec',ap,[],[],300);
+
