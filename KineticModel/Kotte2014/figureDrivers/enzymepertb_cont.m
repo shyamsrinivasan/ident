ba44ff3b0c9978dff09160e52a36a89d50651cb2
shyamsrinivasan <shyamsweth@gmail.com>
2017-04-11 11:03:54 -0400

    use runKotte and changes for setupMATCONT

--- KineticModel/Kotte2014/figureDrivers/enzymepertb_cont.m
+++ KineticModel/Kotte2014/figureDrivers/enzymepertb_cont.m
@@ -2,70 +2,27 @@
 % this code tests whether the ability of a model to be bistable is governed
 % by its bifucation w.r.t enzyme parameter
 % values
-% build stoichioemtrc matrices
-addpath(genpath('C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel'));
-rxfname = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel\Kotte2014\Kotte2014.txt';
-cnfname = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel\Kotte2014\Kotte2014C.txt';
+runKotte
+% get saddle node to get both stable steady states and get a bistable
+% acetate concentration for perturbation
+[orig_saddle,orig_saddlepar] = getsaddlenode(data.s1,data.x1,5e-3);
+pvec(ap) = orig_saddlepar;
+model.PM(ac-length(orig_saddle)) = orig_saddlepar;
 
-% create model structure
-[FBAmodel,parameter,variable,nrxn,nmetab] = modelgen(rxfname);
-
-% obtain conentrations from file
-[mc,FBAmodel,met] = readCNCfromFile(cnfname,FBAmodel);
-
-% run FBA
-Vup_struct.ACt2r = 1;
-Vup_struct.ENZ1ex = 1;
-FBAmodel = FBAfluxes(FBAmodel,'fba',{'ACt2r','ENZ1ex'},Vup_struct,...
-                    [find(strcmpi(FBAmodel.rxns,'FDex'))...
-                     find(strcmpi(FBAmodel.rxns,'PEPex'))]);
-                 
-% remove metabolites held constant from consideration in the model
-% integration phase
-[model,pvec,newmc,cnstmet] =...
-remove_eMets(FBAmodel,parameter,mc,[FBAmodel.Vind FBAmodel.Vex],...
-{'enz1[c]','enz1[e]','enz[e]','ac[e]','bm[c]','bm[e]','pep[e]'});
-
-% only initialize for varmets   
-nvar = length(model.mets)-length(find(cnstmet));
-M = newmc(1:nvar);
-PM = newmc(nvar+1:end);
-model.PM = PM;       
-
-% call to parameter sampling script for analysis of mss
-% parameters
-clear pvec
-kEcat = 1;
-KEacetate = 0.1;    % or 0.02
-KFbpFBP = 0.1;
-vFbpmax = 1;
-Lfbp = 4e6;
-KFbpPEP = 0.1;
-vEXmax = 1;
-KEXPEP = 0.3;
-vemax = 1.1;        % for bifurcation analysis: 0.7:0.1:1.3
-KeFBP = 0.45;       % or 0.45
-ne = 2;             % or 2
-acetate = 0.1;      % a.u acetate
-d = 0.25;           % or 0.25 or 0.35
-kPEPout = 0.2;
-pvec = [KEacetate,KFbpFBP,Lfbp,KFbpPEP,...
-        KEXPEP,vemax,KeFBP,ne,acetate,d,...
-        kPEPout,kEcat,vFbpmax,vEXmax];
-
-% systems check
-givenModel = @(t,x)KotteODE(t,x,model,pvec);
-fluxg = Kotte_givenFlux([M;model.PM],pvec,model);
-tspan = 0:0.1:2000;
-opts = odeset('RelTol',1e-12,'AbsTol',1e-10);
-ac = find(strcmpi(model.mets,'ac[e]'));
-npts = 1;
-
+% perturb saddle to get steady states
+eps = 1e-4;
+tspanf = 0:0.1:2000;
+pival = orig_saddle+eps*[1;1;1];
+[~,xeq1,~,feq1] = solveODEonly(1,pival,model,pvec,opts,tspanf);
+nival = orig_saddle-eps*[1;1;1];
+[~,xeq2,~,feq2] = solveODEonly(1,nival,model,pvec,opts,tspanf);
+xss = [xeq1 xeq2];
 %% continuation on enzyme parameters at original parameters (acetate & enzyme)
 idp = [12 13 14];
 pmin = zeros(length(idp),npts); % min
 pmax = zeros(length(idp),npts); % max
 origpvec1 = pvec;
+figure
 for id = 1:length(idp)    
     % xbounds = zeros(nvar,npts);
     ap = idp(id);
@@ -82,7 +39,9 @@ for id = 1:length(idp)
     solveODEonly(npts,M,model,pvec,opts,tspan);
           
     % continute using MATCONT
-    [s,mssid,nss] = setupMATCONT(id1xeq,pvec,ap,model,fluxg,npts,1500,1);
+    [s,mssid,nss] = setupMATCONT(@KotteMATCONT,@Kottecont_fluxcalc,...
+                                @getKotteaxislabels,id1xeq,pvec,ap,model,...
+                                fluxg,npts,1500,1);
     
     % identify boundaries of bistable enzyme parameters
     for ipt = 1:npts
@@ -97,33 +56,6 @@ for id = 1:length(idp)
     % reset parameters 
     pvec = origpvec1;
 end
-
-%% continue on acetate for original/nominal parameter values
-ap = 9;
-allpvec = pvec;
-
-% find equilibrium solution and run equilibrium continuation
-allxeq = zeros(length(M),npts);
-allxdyn = zeros(length(M),length(tspan),npts);
-allfeq = zeros(length(fluxg),npts);
-allfdyn = zeros(length(fluxg),length(tspan),npts);
-solveEquilibriumODE
-
-% get saddle node to get both stable steady states and get a bistable
-% acetate concentration for perturbation
-[orig_saddle,orig_saddlepar] = getsaddlenode(data.s1,data.x1,5e-3);
-pvec(ap) = orig_saddlepar;
-model.PM(ac-length(orig_saddle)) = orig_saddlepar;
-
-% perturb saddle to get steady states
-eps = 1e-4;
-tspanf = 0:0.1:2000;
-pival = orig_saddle+eps*[1;1;1];
-[~,xeq1,~,feq1] = solveODEonly(1,pival,model,pvec,opts,tspanf);
-nival = orig_saddle-eps*[1;1;1];
-[~,xeq2,~,feq2] = solveODEonly(1,nival,model,pvec,opts,tspanf);
-xss = [xeq1 xeq2];
-
 %% continuation on enzyme parameters at saddle node for nominal enzyme 
 origpvec = pvec; % pvec backup
 npts = 1;
