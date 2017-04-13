37f4d784fc23657d7b0e12e155c49ce89fef6c2f
shyamsrinivasan <shyamsweth@gmail.com>
2017-04-13 12:15:18 -0400

    figures from collected data

--- KineticModel/Kotte2014/figureDrivers/regionVolume.m
+++ KineticModel/Kotte2014/figureDrivers/regionVolume.m
@@ -22,7 +22,7 @@ xss = [xeq1 xeq2];
 tspanr = 0:-.1:-30;
 tspanf = 0:0.1:2000;
 eps = 1e-4;
-[~,eig,w] = getKotteJacobian(orig_saddle,pvec,model);
+[~,eig,w] = getKotteJacobian(@Kotte_givenNLAE,orig_saddle,pvec,model);
 [xWus,xeq] = calc1DWus(orig_saddle,w,eig,model,pvec,opts,tspanr,tspanf,eps);
 % chop xWus
 [~,nzid,~] = find(xWus~=0,1,'last');
@@ -33,19 +33,30 @@ relWus = real(xWus(:,1:nzid));
 % expanding region of perturbation
 % radius where no initial point produces the 2nd steady state = rAss1 =
 % [1.22 1.22 1.22]'
-rndivals = get3Dsphere(xeq1',1.22,50000);
+rndivals = get3Dsphere(xeq1',1.24,50000);
 save('regionVoluemSamplevals');
 % integrate
 options = [];
-% hfig = figure;
 % [ppival,npival,allxeq,ssid,allfeq] =...
-% getPequilibrium(rndivals,model,pvec,options,opts,tspanf);
-% plotrndivals(rndivals,ssid,allxeq,[1 2 3],2,hfig,[]);
-% plotrndivals(rndivals,ssid,allxeq,[1 2],2,hfig,[]);
-% plotrndivals(rndivals,ssid,allxeq,[2 3],2,hfig,[]);
-% plotrndivals(rndivals,ssid,allxeq,[2 3],2,hfig,[]);
-
-
+% getPequilibrium(rndivals(1:10000),model,pvec,options,opts,tspanf);
+% run sim*_regionVolume.m for * = 1,2,3,4,5
+%% plot volume of region of attraction
+% hfig = figure;
+% hold on
+% load('C:\Users\shyam\Documents\Courses\CHE1125Project\mat_files\KotteModel\run2\regionVolume_run1_Mar21_10K.mat');
+% plotrndivals(rndivals(1:10000,:),ssid,allxeq,[1 2 3],2,hfig,[]);
+% clearvars -except hfig
+% load('C:\Users\shyam\Documents\Courses\CHE1125Project\mat_files\KotteModel\run2\regionVolume_run3_Mar23_10K.mat');
+% plotrndivals(rndivals(10001:20000,:),ssid,allxeq,[1 2 3],2,hfig,[]);
+% clearvars -except hfig
+% load('C:\Users\shyam\Documents\Courses\CHE1125Project\mat_files\KotteModel\run2\regionVolume_run4_Mar23_10K.mat');
+% plotrndivals(rndivals(20001:30000,:),ssid,allxeq,[1 2 3],2,hfig,[]);
+% clearvars -except hfig
+% load('C:\Users\shyam\Documents\Courses\CHE1125Project\mat_files\KotteModel\run2\regionVolume_run5_Mar23_10K.mat');
+% plotrndivals(rndivals(30001:40000,:),ssid,allxeq,[1 2 3],2,hfig,[]);
+% load('C:\Users\shyam\Documents\Courses\CHE1125Project\mat_files\KotteModel\run2\regionVolume_run2_Mar21_7K.mat');
+% plotrndivals(rndivals(40001:end,:),ssid,allxeq,[1 2 3],2,hfig,[]);
+% clearvars -except hfig
 %% get random initial values for all 3 variables and calculate steady state
 % rndivals = randomivals([0 5;0 5;0 5],10000);
 % % integrate
