c3dae83156c3888dc1943240369d3c50e6efacf0
shyamsrinivasan <shyamsweth@gmail.com>
2017-04-18 18:56:30 -0400

    changes to radius for xeq2
    (cherry picked from commit 4f2392a9ee6b0068b832c22893d12dbbab482880)

--- KineticModel/Kotte2014/sim5_regionVolume.m
+++ KineticModel/Kotte2014/sim5_regionVolume.m
@@ -3,4 +3,4 @@ load('regionVolumeSamplevals.mat');
 % integrate
 options = [];
 [ppival,npival,allxeq,ssid,allfeq] =...
-getPequilibrium(rndivals(40001:end,:),model,pvec,options,opts,tspanf);
+getPequilibrium(rndivals(30001:end,:),model,pvec,options,opts,tspanf);
