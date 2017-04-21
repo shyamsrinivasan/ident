4e9d5f04ed1426fd4ba9236be10ad25bff796f38
shyamsrinivasan <shyamsweth@gmail.com>
2017-04-20 22:02:58 -0400

    correct initial vals for ODE sim

--- KineticModel/Kotte2014/design/script_twobusAE.m
+++ KineticModel/Kotte2014/design/script_twobusAE.m
@@ -1,11 +1,26 @@
 % run twobusAE to find equilibrium points and get fold bifurcation manifold
-x0 = [-0.1;-1];
-p = [.1;.1];
+x0 = [-0.18;.1];
+p = [.5;.3];
 
 opts = odeset('AbsTol',1e-8,'RelTol',1e-10);
 odefun = @(t,x)twobusODE(t,x,p);
-[t,y] = ode45(odefun,0:.1:1,x0);
+[t,y] = ode45(odefun,0:.1:10,x0,opts);
 figure
 plot(t,y);
+
+% get stability info
+jac1 = twobus_jacobian(x0,p);
+[v1,eigval1,w1] = eig(jac1);
+eigval1 = diag(eigval1);
+
+jac2 = twobus_jacobian(y(end,:),p);
+[v2,eigval2,w2] = eig(jac2);
+eigval2 = diag(eigval2);
+
+% get steady state using fsolve - gives a different equilibrium solution
+% aefun = @(x)twobusAE(x,p);
+% clear opts
+% opts = optimoptions(@fsolve,'Display','iter','TolFun',1e-10,'TolX',1e-8);
+% [x,fval,flag] = fsolve(aefun,x0,opts);
 
 
