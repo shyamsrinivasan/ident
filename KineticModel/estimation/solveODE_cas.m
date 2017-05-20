a9cb4b8ab8b488a0190ded5cb5ba7954165ebcb0
shyamsrinivasan <shyamsweth@gmail.com>
2017-05-19 23:45:29 -0400

    ode wrapper for casadi solver

--- KineticModel/estimation/solveODE_cas.m
+++ KineticModel/estimation/solveODE_cas.m
@@ -1,4 +1,5 @@
-function solveODE_cas(opts)
+% ode solver wrapper for problems defined using casadi
+function solveODE_cas(fh,opts)
 
 if isfield(opts,'tspan')
     tspan = opts.tspan;
@@ -10,7 +11,16 @@ if isfield(opts,'x0');
     x0 = opts.x0;
 end
 if isfield(opts,'p')
-    p = opts.p;
+    odep = opts.odep;
 end
 
-[tout
+% get expressions for function(s) and its jacobian
+[~,~,~,fx,x,p] = fh();
+
+% define casdi problem structure
+problem = struct('x',x,'ode',fx,'p',p);
+options = struct('output_t0',1,'print_stats',1,'grid',tspan);
+Fint = casadi.integrator('Fint','cvodes',problem,options);
+sol = Fint('x0',x0,'p',odep);
+yout = full(sol.xf);
+
