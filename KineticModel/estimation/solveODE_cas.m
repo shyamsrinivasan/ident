4fde77c8e73d654dadcfb299ee563570444161c7
shyamsrinivasan <shyamsweth@gmail.com>
2017-05-22 23:56:53 -0400

    ode solver wrapper for casadi

--- KineticModel/estimation/solveODE_cas.m
+++ KineticModel/estimation/solveODE_cas.m
@@ -1,5 +1,11 @@
 % ode solver wrapper for problems defined using casadi
-function solveODE_cas(fh,opts)
+function [yout,fout,yss,fss] = solveODE_cas(fh,opts,flxh)
+if nargin>2
+    givenflux = 1;
+else
+    givenflux = 0;    
+end
+
 
 if isfield(opts,'tspan')
     tspan = opts.tspan;
@@ -7,10 +13,16 @@ end
 if isfield(opts,'solver_opts')
     solver_opts =  opts.solver_opts;
 end
+% if isfield(solver_opts,'abstol')
+%     abstol = solver_opts.abstol;
+% end
+% if isfield(solver_opts,'reltol')
+%     reltol = solver_opts.reltol;
+% end
 if isfield(opts,'x0');
     x0 = opts.x0;
 end
-if isfield(opts,'p')
+if isfield(opts,'odep')
     odep = opts.odep;
 end
 
@@ -18,9 +30,30 @@ end
 [~,~,~,fx,x,p] = fh();
 
 % define casdi problem structure
-problem = struct('x',x,'ode',fx,'p',p);
-options = struct('output_t0',1,'print_stats',1,'grid',tspan);
-Fint = casadi.integrator('Fint','cvodes',problem,options);
+neq = size(fx,1);
+dx = casadi.SX(neq,1);
+for i = 1:neq
+    dx(i) = fx{i};
+end
+problem = struct('x',x,'ode',dx,'p',p);
+solver_opts.output_t0 = 1;
+solver_opts.print_stats = 1;
+solver_opts.grid = tspan;
+% options = struct('output_t0',1,'print_stats',1,'grid',tspan);
+Fint = casadi.integrator('Fint','cvodes',problem,solver_opts);
 sol = Fint('x0',x0,'p',odep);
 yout = full(sol.xf);
+
+ntime = size(yout,2);
+fout = zeros(5,ntime);
+if givenflux
+    [~,FXflx] = flxh(x,p);
+    for i = 1:ntime
+        fout(:,i) = full(FXflx(yout(:,i),odep));
+    end  
+end
+  
+
+yss = yout(:,end);
+fss = fout(:,end);
 
