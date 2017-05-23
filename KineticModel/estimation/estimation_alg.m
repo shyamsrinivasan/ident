bc5f02bae6ceb7e95134c139e19b841fb91eaf51
shyamsrinivasan <shyamsweth@gmail.com>
2017-05-23 16:54:40 -0400

    move components to separate scripts for calling

--- KineticModel/estimation/estimation_alg.m
+++ KineticModel/estimation/estimation_alg.m
@@ -11,61 +11,26 @@
 % experimental data is generated through addition of noise to actual kotte model
 % vmodel will use convenience kinetics for estimating fluxes
 
-% load original kotte model
-load('C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel\Kotte2014\model\kotte_model.mat');
-p = pvec;
-ival = M;
-clear pvec
-
-% solve using casadi
+%% no noise deterministic model - uses casadi to solve
 tspan = 0:0.1:500;
-% odep = p;
-% solver_opts = struct('abstol',1e-3,'reltol',1e-3);
-% opts = struct('tspan',tspan,'x0',ival,'solver_opts',solver_opts,'odep',odep);
-% [xdyn,fdyn,xss1,fss1] = solveODE_cas(@kotte_CAS,opts,@kotte_flux_CAS);
+run_nonoise
 
-odep = struct('p',p,'model',model);
+% perturb model from ss
+% perturb enzyme 2 (flux(4),vEX)
+odep(14) = .5; % 2;
+perturb_nonoise
 
-% systems check
-noisy_model = @(t,x)simnoisyODE_kotte(t,x,odep);
-noisy_flux_test = noisyflux_kotte([M;model.PM],odep);
+%% noisy stochastic model - uses ode23 to solve
+% tspan = 0:0.1:500;
+run_withnoise
 
-solver_opts = odeset('RelTol',1e-3,'AbsTol',1e-3);
-opts = struct('tspan',tspan,'x0',ival,'solver_opts',solver_opts,'odep',odep);
-
-% simulate noisy system from random initial condition - this is better for
-% simulating the noisy model
-[xdyn,fdyn,xss1,fss1] = solve_ode(@simnoisyODE_kotte,opts,@flux_kotte);
-
-figure
-subplot(211);
-plot(tspan,xdyn);
-ylabel('concentrations a.u.');
-legend('pep','fdp','E');
-subplot(212)
-plot(tspan,fdyn);
-ylabel('fluxes a.u.');
-legend('J','E(FDP)','vFbP','vEX','vPEPout');
-
-% perturb system from steady state and generate noisy data
+% perturb noisy model from ss
 % perturb enzyme 2 (flux(4),vEX)
 odep.p(14) = .5; % 2;
-odep.p(15) = .02;
-odep.p(16) = .3;
-opts.ival = xss1(1:3);
-opts.odep = odep;
-[x2dyn,f2dyn,xss2,fss2] = solve_ode(@simnoisyODE_kotte,opts,@flux_kotte);
-figure
-subplot(211);
-plot(tspan,xdyn);
-ylabel('concentrations a.u.');
-legend('pep','fdp','E');
-subplot(212)
-plot(tspan,fdyn);
-ylabel('fluxes a.u.');
-legend('J','E(FDP)','vFbP','vEX','vPEPout');
+perturb_noise
 
-% casadi test
+
+%% casadi test
 % [FX,FX_flx,fx_sym] = kotte_CAS();
 
 % generate model data from convinience kinetics
