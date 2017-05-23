ae2f326e95183766a71dc82e3649885828257c5f
shyamsrinivasan <shyamsweth@gmail.com>
2017-05-23 19:04:41 -0400

    added obj and contr for NLP

--- KineticModel/estimation/estimation_obj.m
+++ KineticModel/estimation/estimation_obj.m
@@ -1,8 +1,16 @@
 function obj = estimation_obj(x,pstruct)
+% vmodel - flux from convinience kientics model
+% vnoisy - flux from 
 
 if isfield(pstruct,'vnoisy')
     vnoisy = pstruct.vnoisy;
 end
+if isfield(pstruct,'flxh')
+    flxh = pstruct.flxh;
+end
+if isfield(pstruct,'c')
+    c = pstruct.c;
+end
 
-vmodel = convkin_kotte(x,pstruct);
+vmodel = flxh(c,x);
 obj = norm(vmodel-vnoisy);
