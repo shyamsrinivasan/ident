18931bc61f43edbb69de63579dfea2d64d8e53b4
shyamsrinivasan <shyamsweth@gmail.com>
2017-05-22 23:55:27 -0400

    noisy model for casadi

--- KineticModel/estimation/noisy_model/kotte_noisyflux_CAS.m
+++ KineticModel/estimation/noisy_model/kotte_noisyflux_CAS.m
@@ -1,4 +1,6 @@
-function flux = kotte_CAS(x,p,noise)
+function [flux,FXflx] = kotte_noisyflux_CAS(x,p)
+
+flux = cell(5,1);
 
 % parameters
 KEacetate = p(1);    % or 0.02
@@ -15,21 +17,29 @@ kEcat = p(12);
 vFbpmax = p(13);    
 vEXmax = p(14);   
 
+noise = casadi.SX(rand(5,1)*2);
+
 % metabolic fluxes
 % J(E, acetate)
-flux(1,:) = kEcat.*x(3).*acetate./(acetate+KEacetate);
+flux{1} = kEcat.*x(3).*acetate./(acetate+KEacetate) + noise(1);
 
 % enzyme production fluxes
 % E(FBP) for J (%FBP ---| Cra and Cra ---> E)
-flux(2,:) = vemax.*(1-1./(1+(KeFBP./x(2)).^ne));
+flux{2} = vemax.*(1-1./(1+(KeFBP./x(2)).^ne)) +noise(2);
 
 % vFbp(PEP,FBP)
 ratio = 1+x(2)./KFbpFBP;
-flux(3,:) = vFbpmax.*(ratio-1).*(ratio).^3./...
-            (ratio.^4+Lfbp.*(1+x(1)./KFbpPEP).^(-4));
+flux{3} = vFbpmax.*(ratio-1).*(ratio).^3./...
+            (ratio.^4+Lfbp.*(1+x(1)./KFbpPEP).^(-4)) + noise(3);
 
 % vEX(PEP)
-flux(4,:) = vEXmax.*x(1)./(x(1)+KEXPEP);
+flux{4} = vEXmax.*x(1)./(x(1)+KEXPEP) + noise(4);
 
 % vPEPout
-flux(5,:) = kPEPout*x(1);
+flux{5} = kPEPout*x(1) + noise(5);
+
+FXflx = casadi.Function('FXflx',{x,p},{[flux{1};...
+                                        flux{2};...
+                                        flux{3};...
+                                        flux{4};...
+                                        flux{5}]});
