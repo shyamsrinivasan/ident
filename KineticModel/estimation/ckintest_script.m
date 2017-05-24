93f86d103df67d25094838a06e89c0b4cea221d2
shyamsrinivasan <shyamsweth@gmail.com>
2017-05-24 15:58:21 -0400

    test script for convinience kientics of kotte model

--- KineticModel/estimation/ckintest_script.m
+++ KineticModel/estimation/ckintest_script.m
@@ -6,29 +6,24 @@ p = pvec;
 ival = M;
 clear pvec
 
-p(15) = .02;
-p(16) = .3;
-p(17) = 0;
-
-odep = struct('p',p,'model',model);
-
-% systems check
-convkin_model = @(t,x)conkinODE_kotte(t,x,odep);
-convkin_flux_test = convkin_kotte([M;model.PM],odep);
-
-tspan = 0:0.1:60000;
-solver_opts = odeset('RelTol',1e-3,'AbsTol',1e-3);
-
-% simulate system from random initial condition
-opts = struct('tspan',tspan,'x0',ival,'solver_opts',solver_opts,'odep',odep);
-[xdyn,fdyn,xss1,fss1] = solve_ode(@conkinODE_kotte,opts,@convkin_kotte);
+% [FX,~,~,fx_sym,~,~,FX_flx] = kotte_conkin_CAS();
+clear tspan odep solver_opts opts
+tspan = 0:0.1:2000;
+p(14) = 1; % 2;V2max
+p(15) = .1; % K1pep
+p(16) = .3; % K2fdp
+p(17) = 0; % rhoA - regulation
+odep = p;
+solver_opts = struct('abstol',1e-3,'reltol',1e-3);
+opts = struct('tspan',tspan,'x0',ival(1:3),'solver_opts',solver_opts,'odep',odep);
+[x3dyn,f3dyn,xss3,fss3] = solveODE_cas(@kotte_conkin_CAS,opts,@kotte_convkinflux_CAS);
 figure
 subplot(211);
-plot(tspan,xdyn);
+plot(tspan,x3dyn);
 ylabel('concentrations a.u.');
 legend('pep','fdp','E');
 subplot(212)
-plot(tspan,fdyn);
+plot(tspan,f3dyn);
 ylabel('fluxes a.u.');
 legend('J','E(FDP)','vFbP','vEX','vPEPout');
 
