a60d13ec3bf94679220e13d82f7baaf135dd94f5
shyamsrinivasan <shyamsweth@gmail.com>
2017-05-24 11:52:10 -0400

    use acetate as concentration variable and not parameter

--- KineticModel/estimation/noiseless_model/kotte_CAS.m
+++ KineticModel/estimation/noiseless_model/kotte_CAS.m
@@ -1,12 +1,12 @@
 function [FX,DFX,D2FX,fx_sym,x,p,FX_flux] = kotte_CAS()
 
-x = casadi.SX.sym('x',3,1);
-p = casadi.SX.sym('p',14,1);
+x = casadi.SX.sym('x',4,1);
+p = casadi.SX.sym('p',13,1);
 
-fx_sym = cell(3,1);
+fx_sym = cell(4,1);
 
 % parameters
-d = p(10);
+d = p(9);
 
 flux = kotte_flux_CAS(x,p);
 % PEP
@@ -16,10 +16,15 @@ fx_sym{2} = flux{4} - flux{3};
 % enzymes
 % E
 fx_sym{3} = flux{2} - d*x(3);
+% acetate
+fx_sym{4} = 0;
+
 
 FX = casadi.Function('FX',{x,p},{[fx_sym{1};...
                                   fx_sym{2};...
-                                  fx_sym{3}]});
+                                  fx_sym{3};...
+                                  fx_sym{4}]});
+                              
 FX_flux = casadi.Function('FX_flux',{x,p},{[flux{1};...
                                             flux{2};...
                                             flux{3};...
