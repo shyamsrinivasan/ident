d210f044ec166c67a9cfe978bd668e62287da446
shyamsrinivasan <shyamsweth@gmail.com>
2017-05-24 19:23:11 -0400

    parameter opt for flux 3

---
+++ KineticModel/estimation/objectives/obj_flux3_CAS.m
@@ -0,0 +1,42 @@
+function [FX,DFX,D2FX,DFp,fx_sym,x,p] = obj_flux3_CAS
+% get function expression for objective function of optimization problem
+% and its gradient
+
+% x - parameters that are being optimized for
+% p - parameters and concentrations that are being held constant
+
+% concentrations
+pep = casadi.SX.sym('pep',1);
+fdp = casadi.SX.sym('fdp',1);
+enz = casadi.SX.sym('enz',1);
+ac = casadi.SX.sym('ac',1);
+
+% parameters
+K3pep = casadi.SX.sym('K3pep',1);
+V3max = casadi.SX.sym('V3max',1);
+K3fdp = casadi.SX.sym('K3fdp',1);
+rhoA = casadi.SX.sym('rhoA',1);
+
+% given fluxes
+gflux = casadi.SX.sym('gflux',1);
+
+% flux = k1cat.*enz.*ac./(ac+K1ac); % original model
+acratio = pep./K3pep;
+acflx = (rhoA + (1-rhoA).*acratio./(1+acratio)).^4;
+flux = V3max.*acflx.*(fdp./K3fdp)./(1+fdp./K3fdp); % conv kin
+
+x = [K3pep;V3max;K3fdp;rhoA];
+p = [pep;fdp;enz;ac;gflux];
+
+fx_sym = norm(flux-gflux);
+FX = casadi.Function('FX',{x,p},{fx_sym});
+
+% gradient
+dfx = jacobian(fx_sym,x);
+DFX = casadi.Function('DFX',{x,p},{dfx});
+
+% parameter sensitivity
+dfp = jacobian(fx_sym,p);
+DFp = casadi.Function('DFp',{x,p},{dfp});
+
+D2FX = [];
---
+++ KineticModel/estimation/optim_scripts/flux3.m
@@ -0,0 +1,43 @@
+% optimization of flux parameters in kotte model for a CK formulation
+% no noise deterministic model - uses casadi to solve - get initial ss
+tspan = 0:0.1:300;
+[xdyn,fdyn,xss1,fss1,opts] = run_nonoise(tspan);
+
+% perturb model from ss
+% perturb enzyme 2 (flux(4),vEX)
+opts.x0 = xss1;
+opts.odep(12) = .5; % 2;
+[x2dyn,f2dyn,xss2,fss2] = perturb_nonoise(opts);
+
+plist = {'K1ac','K3fdp','L3fdp','K3pep','K2pep','vemax','KeFDP','ne',...
+        'd','V4max','k1cat','V3max','V2max','K1pep','K2fdp','rhoA'};
+
+%% flux 3 
+p_id = cellfun(@(x)strcmpi(plist,x),{'K3fdp','K3pep','V3max'},'UniformOutput',false);
+p_id = cellfun(@(x)find(x),p_id);
+p = opts.odep(p_id)';
+
+p = [p;0]; % add rhoA to list of parameters
+f3 = fss2(3); % add steayd state experimental flux
+optim_p = [xss2;f3]; % concentrations & fluxes (expt) are parameters
+lb = [1e-6;1e-6;1e-3;0];
+ub = [20;20;2000;1];
+[x_opt,fval,~,~,opts] = runoptim_flux(opts,@obj_flux3_CAS,lb,ub,p,optim_p);
+
+% check flux using conkin rate law
+pconv = [.1;.3;0]; % extra parameters for CK 'K1pep','K2fdp','rhoA'
+odep = [opts.odep';pconv];
+opt_pid = [p_id,16];
+odep(opt_pid) = x_opt;
+solver_opts = struct('abstol',1e-6,'reltol',1e-6);
+opts = struct('tspan',tspan,'x0',xss1,'solver_opts',solver_opts,'odep',odep);
+[x4dyn,f4dyn,xss4,fss4] = solveODE_cas(@kotte_conkin_CAS,opts,@kotte_convkinflux_noCAS);
+figure
+subplot(211);
+plot(tspan,x4dyn);
+ylabel('concentrations a.u.');
+legend('pep','fdp','E');
+subplot(212)
+plot(tspan,f4dyn);
+ylabel('fluxes a.u.');
+legend('J','E(FDP)','vFbP','vEX','vPEPout');
