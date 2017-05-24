2809448dde40d46f20bee674fabb02284216bc12
shyamsrinivasan <shyamsweth@gmail.com>
2017-05-24 15:43:34 -0400

    change var names

--- KineticModel/estimation/ckin_model/kotte_convkinflux_CAS.m
+++ KineticModel/estimation/ckin_model/kotte_convkinflux_CAS.m
@@ -3,40 +3,40 @@ function [flux,FXflx] = kotte_convkinflux_CAS(x,p)
 flux = cell(5,1);
 
 % parameters
-KEacetate = p(1);    % or 0.02
-KFbpFBP = p(2);
-Lfbp = p(3);
-KFbpPEP = p(4);
-KEXPEP = p(5);
+K1ac = p(1);    % or 0.02
+K3fdp = p(2);
+% L3fdp = p(3);
+K3pep = p(4);
+K2pep = p(5);
 vemax = p(6);        % for bifurcation analysis: 0.7:0.1:1.3
-KeFBP = p(7);        % or 0.45
+KeFDP = p(7);        % or 0.45
 ne = p(8);             % or 2
-acetate = p(9);
-kPEPout = p(11);
-kEcat = p(12);   
-vFbpmax = p(13);    
-vEXmax = p(14);
+ac = p(9);
+V4max = p(11);
+k1cat = p(12);   
+V3max = p(13);    
+V2max = p(14);
 K1pep = p(15);  % .02
-KEXfdp = p(16); % .3
+K2fdp = p(16); % .3
 rhoA = p(17); % 0.5
 
 % convinience kientics for acetate uptake and conversion to pep
-flux{1} = kEcat.*x(3).*(acetate./KEacetate)/...
-            (1 + acetate./KEacetate + x(1)./K1pep);
+flux{1} = k1cat.*x(3).*(ac./K1ac)/...
+            (1 + ac./K1ac + x(1)./K1pep);
 
 % hill kinetics for enzyme production        
-flux{2} = vemax.*(1-1./(1+(KeFBP./x(2)).^ne));
+flux{2} = vemax.*(1-1./(1+(KeFDP./x(2)).^ne));
 
 % convinience kinetics w/ allostery for vFbp 
-acratio = x(1)./KFbpPEP;
+acratio = x(1)./K3pep;
 acflx = (rhoA + (1-rhoA).*acratio./(1+acratio)).^4;
-flux{3} = vFbpmax.*acflx.*(x(2)./KFbpFBP)./(1+x(2)./KFbpFBP);
+flux{3} = V3max.*acflx.*(x(2)./K3fdp)./(1+x(2)./K3fdp);
 
 % convinience kinetics for vEX(pep)
-flux{4} = vEXmax.*(x(1)./KEXPEP)/(1+x(1)./KEXPEP+x(2)./KEXfdp);
+flux{4} = V2max.*(x(1)./K2pep)/(1+x(1)./K2pep+x(2)./K2fdp);
 
 % linear kinetics for vPEPout
-flux{5} = kPEPout*x(1);
+flux{5} = V4max*x(1);
 
 FXflx = casadi.Function('FXflx',{x,p},{[flux{1};...
                                         flux{2};...
