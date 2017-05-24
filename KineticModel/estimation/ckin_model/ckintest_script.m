d45160335f231ebe835fbae4ef7928ac22f3b27a
shyamsrinivasan <shyamsweth@gmail.com>
2017-05-24 18:04:36 -0400

    reduce tolerance and add opts as output

--- KineticModel/estimation/ckin_model/ckintest_script.m
+++ KineticModel/estimation/ckin_model/ckintest_script.m
@@ -1,4 +1,4 @@
-function [x3dyn,f3dyn,xss3,fss3] = ckintest_script(tspan)
+function [x3dyn,f3dyn,xss3,fss3,opts] = ckintest_script(tspan)
 % convinience kinetic test script
 % test for kotte network
 % load original kotte model
@@ -9,13 +9,13 @@ p(9) = []; % remove acetate
 clear pvec
 
 % [FX,~,~,fx_sym,~,~,FX_flx] = kotte_conkin_CAS();
-clear tspan odep solver_opts opts
+clear odep solver_opts opts
 p(13) = 1; % 2;V2max
 p(14) = .1; % K1pep
 p(15) = .3; % K2fdp
 p(16) = 0; % rhoA - regulation
 odep = p;
-solver_opts = struct('abstol',1e-3,'reltol',1e-3);
+solver_opts = struct('abstol',1e-6,'reltol',1e-6);
 opts = struct('tspan',tspan,'x0',ival,'solver_opts',solver_opts,'odep',odep);
 [x3dyn,f3dyn,xss3,fss3] = solveODE_cas(@kotte_conkin_CAS,opts,@kotte_convkinflux_noCAS);
 figure
