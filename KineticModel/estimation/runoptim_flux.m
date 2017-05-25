a93032d001176656873a8f8f59f9d3d8bfc78664
shyamsrinivasan <shyamsweth@gmail.com>
2017-05-25 11:52:35 -0400

    change solver options

--- KineticModel/estimation/runoptim_flux.m
+++ KineticModel/estimation/runoptim_flux.m
@@ -6,11 +6,12 @@ obj = @(x)full(FX(x,optim_p));
 grad = @(x)full(gradF(x,optim_p));
 
 % setup opti problem
-optim_opts = optiset('maxiter',5000,'maxfeval',5000,'display','iter');
+optim_opts = optiset('solver','NLOPT','maxiter',5000,'maxfeval',5000,'display','iter');
 optim_prob = opti('obj',obj,'grad',grad,'bounds',lb,ub,'options',optim_opts);
 [xval,fval,exitflag,info] = solve(optim_prob,x0); 
 
-if exitflag == 1 && strcmpi(info.Status,'Success')
+if exitflag == 1 &&...
+   (strcmpi(info.Status,'Success')||strcmpi(info.Status,'Optimal'))
     x_opt = xval;
 else
     x_opt = [];
