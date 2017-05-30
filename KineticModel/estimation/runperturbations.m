f345a60f6743a036529df910c6aae5e3a6322c68
shyamsrinivasan <shyamsweth@gmail.com>
2017-05-29 20:17:52 -0400

    restore parameters from backup

--- KineticModel/estimation/runperturbations.m
+++ KineticModel/estimation/runperturbations.m
@@ -3,11 +3,12 @@ function [xss_pval,fss_pval] = runperturbations(fh,pid,pval,opts)
 np = length(pval);
 xss_pval = zeros(length(opts.x0),np);
 fss_pval = zeros(5,np);
-% odep_bkp = opts.odep;
+odep_bkp = opts.odep;
 
 for ival = 1:np
     opts.odep(pid) = pval(ival);
     [~,~,xss,fss] = fh(opts);
     xss_pval(:,ival) = xss;
     fss_pval(:,ival) = fss;
+    opts.odep = odep_bkp;
 end
