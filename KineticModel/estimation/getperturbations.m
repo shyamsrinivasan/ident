5b34b0459deca673d0619f2c4e5299e6d80b41a1
shyamsrinivasan <shyamsweth@gmail.com>
2017-06-01 13:06:24 -0400

    get all perturbations in a loop

---
+++ KineticModel/estimation/getperturbations.m
@@ -0,0 +1,27 @@
+function sol = getperturbations(ptopts,fh,opts,sol)
+npt = size(ptopts,2);
+if nargin<4
+    sol = struct([]);
+    istart = 1;
+%     iend = npt;
+else
+    istart = size(sol,2)+1;
+%     iend = size(sol,2)+npt;
+end
+
+for i = 1:npt
+    if isfield(ptopts,'exp_pid')
+        exp_pid = ptopts(i).exp_pid;
+    end
+    if isfield(ptopts,'exp_pval')
+        exp_pval = ptopts(i).exp_pval;
+    end
+    [xss,fss] = runperturbations(fh,exp_pid,exp_pval,opts);
+    sol(istart).xss = xss;
+    sol(istart).fss = fss;
+    sol(istart).exp_pid = exp_pid;
+    sol(istart).exp_pval = exp_pval;
+    
+    istart = istart+1;
+end
+
