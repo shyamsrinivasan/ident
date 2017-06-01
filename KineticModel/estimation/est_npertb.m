0fbcf06de9606972d465b40af9dc59ae0a93a373
shyamsrinivasan <shyamsweth@gmail.com>
2017-06-01 14:32:07 -0400

    use runoptim as wrapper around optimize_p

--- KineticModel/estimation/est_npertb.m
+++ KineticModel/estimation/est_npertb.m
@@ -11,57 +11,44 @@ tspan = 0:0.1:300;
 ival_bkp = opts.x0;
 odep_bkp = opts.odep;
 
-%% perturbation to flux 1
+%% perturbation to all fluxes 
 opts.x0 = xss1;
 opts.tspan = 0:.1:300;
 opts.odep = odep_bkp;
-exp_pid = 11; % 'k1cat'
-exp_pval = [.1;.5;1.0;1.5;2];
-[xss_1,fss_1] = runperturbations(@perturb_nonoise,exp_pid,exp_pval,opts);
+
+% flux 1, 2 and 3 % k1cat, 'V2max', 'V3max'
+ptopts = struct('exp_pid',{11,13,12},...
+                'exp_pval',{[.1;.5;1.0;1.5;2],...
+                            [.1;.5;1.0;1.5;2],...
+                            [.1;.5;1.0;1.5;2]}); 
+sol = getperturbations(ptopts,@perturb_nonoise,opts);
 close all
 
-%% perturbation to flux 2
-% perutrb model from ss
-opts.x0 = xss1;
-opts.tspan = 0:.1:300;
-opts.odep = odep_bkp;
-exp_pid = 13; % 'V2max'
-exp_pval = [.1;.5;1.0;1.5;2];
-[xss_2,fss_2] = runperturbations(@perturb_nonoise,exp_pid,exp_pval,opts);
-close all
-
-%% perturbation to flux 3
-% restore from backup
-opts.x0 = xss1;
-opts.tspan = 0:.1:300;
-opts.odep = odep_bkp;
-exp_pid = 12; % 'V3max'
-exp_pval = [.1;.5;1.0;1.5;2];
-[xss_3,fss_3] = runperturbations(@perturb_nonoise,exp_pid,exp_pval,opts);
-close all
-
-%% perturbation to flux 4
-opts.x0 = xss1;
+% flux 4 V4max
 opts.tspan = 0:.1:10000;
-opts.odep = odep_bkp;
-exp_pid = 10; % 'V4max'
-exp_pval = [0;.1;.3;.5;.7;.9;1];
-[xss_4,fss_4] = runperturbations(@perturb_nonoise,exp_pid,exp_pval,opts);
+ptopts = struct('exp_pid',10,'exp_pval',[0;.1;.3;.5;.7;.9;1]);
+sol = getperturbations(ptopts,@perturb_nonoise,opts,sol);
 close all
 
-%% use only 1 perturbation set flux to get parameters
-x_opt_1 = optimize_p(opts,xss_1,fss_1,plist,odep_bkp);
+%% use perturbations to get kinetic parameters
+optimopts = struct('xss',{sol(1).xss,cat(2,sol(1:2).xss),...
+                          cat(2,sol(1:3).xss),cat(2,sol(1:4).xss)},...
+                   'fss',{sol(1).fss,cat(2,sol(1:2).fss),...
+                          cat(2,sol(1:3).fss),cat(2,sol(1:4).fss)});
+x_opt = runoptimp(opts,plist,odep_bkp,optimopts);               
+% x_opt_1 = optimize_p(opts,sol(1).xss,sol(1).fss,plist,odep_bkp);
 
 %% use 2 perturbation sets 
-x_opt_2 = optimize_p(opts,[xss_1,xss_2],[fss_1,fss_2],plist,odep_bkp);
+% x_opt_2 =...
+% optimize_p(opts,cat(2,sol(1:2).xss),cat(2,sol(1:2).fss),plist,odep_bkp);
 
 %% use 3 perturbation sets
-x_opt_3 =...
-optimize_p(opts,[xss_1,xss_2,xss_3],[fss_1,fss_2,fss_3],plist,odep_bkp);
+% x_opt_3 =...
+% optimize_p(opts,cat(2,sol(1:3).xss),cat(2,sol(1:3).fss),plist,odep_bkp);
 
 %% use 4 perturbation sets
-x_opt_4 =...
-optimize_p(opts,[xss_1,xss_2,xss_3,xss_4],[fss_1,fss_2,fss_3,fss_4],plist,odep_bkp);
+% x_opt_4 =...
+% optimize_p(opts,cat(2,sol(1:4).xss),cat(2,sol(1:4).fss),plist,odep_bkp);
 
 %% rerun all perturbations with new parameters from x_opt_1
 % check if new parameters give the same perturbed flux value
