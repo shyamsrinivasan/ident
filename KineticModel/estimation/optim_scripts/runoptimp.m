675cf86ddcfedbdc2212f91e511dcce48aefed71
shyamsrinivasan <shyamsweth@gmail.com>
2017-06-04 14:26:10 -0400

    add optim fun handle as input arg

--- KineticModel/estimation/optim_scripts/runoptimp.m
+++ KineticModel/estimation/optim_scripts/runoptimp.m
@@ -1,4 +1,4 @@
-function x_opt = runoptimp(opts,plist,odep_bkp,optimstruct)
+function x_opt = runoptimp(opts,plist,odep_bkp,optimstruct,optimfh)
 
 noptim = size(optimstruct,2);
 x_opt = cell(noptim,1);
@@ -8,5 +8,5 @@ for j = 1:noptim
         xss = optimstruct(j).xss;
         fss = optimstruct(j).fss;
     end
-    x_opt{j} = optimize_p(opts,xss,fss,plist,odep_bkp);
+    x_opt{j} = optimfh(opts,xss,fss,plist,odep_bkp);
 end
