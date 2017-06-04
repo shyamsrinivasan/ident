40f52d84486f867258b6671a55c3c88fc507bd07
shyamsrinivasan <shyamsweth@gmail.com>
2017-06-04 15:42:41 -0400

    function for nl constraints

--- KineticModel/estimation/optim_scripts/nlconstoptim_flux.m
+++ KineticModel/estimation/optim_scripts/nlconstoptim_flux.m
@@ -1,14 +1,32 @@
 function [x_opt,fval,xss4,fss4,opts] =...
-         nlconstoptim_flux(opts,objCASh,lb,ub,x0,optim_p,multi)
+         nlconstoptim_flux(opts,objCASh,lb,ub,x0,optim_p,multi,constrfh)
 
 if nargin<7
     multi = 0;
 end
-np = size(optim_p,2);
+if nargin<8
+    costrfh = [];
+end
+% np = size(optim_p,2);
 
-[FX,gradF] = objCASh(np);
-obj = @(x)full(FX(x,optim_p));
-grad = @(x)full(gradF(x,optim_p));
+% objective vector
+e = sparse(1,1,-1,1,5); % [-1 0 0 0 0];
+
+% constraint function
+if ~isempty(costrfh)
+    nlcons = @(x)constrfh(x,optim_p);
+    givenconstr = 1;
+else
+    givenconstr = 0;
+end
+% constraint rhs
+nlrhs
+% constraint type : (-1 <, 0 =, 1 >)
+nle
+
+% FX = objCASh(np);
+% obj = @(x)full(FX(x,optim_p));
+% grad = @(x)full(gradF(x,optim_p));
 
 % setup opti problem
 solver_opts = nloptset('algorithm','GN_DIRECT');
@@ -21,7 +39,12 @@ optim_opts = optiset('solver','NLOPT','maxiter',5000,...
 % optim_opts = optiset('solver','IPOPT','maxiter',5000,...
 %                                       'maxfeval',5000,...                                      
 %                                       'display','final');
-optim_prob = opti('obj',obj,'grad',grad,'bounds',lb,ub,'options',optim_opts);
+if givenconstr
+    optim_prob = opti('obj',e,'nlmix',nlcons,nlrhs,nle,'bounds',lb,ub,'options',optim_opts);
+else
+    optim_prob =...
+    opti('obj',obj,'bounds',lb,ub,'options',optim_opts);
+end
 if multi
     [xval,fval,exitflag,info] = multisolve(optim_prob,[],[100 10]);   
 else
@@ -41,4 +64,5 @@ end
 
 xss4 = [];
 fss4 = [];
+
 
