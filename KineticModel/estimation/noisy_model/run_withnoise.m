2bb7c3ffcef93ebffa33f6c20bed43eea82a82af
shyamsrinivasan <shyamsweth@gmail.com>
2017-06-04 14:25:28 -0400

    changed to fun to resemble no noise counterpart

--- KineticModel/estimation/noisy_model/run_withnoise.m
+++ KineticModel/estimation/noisy_model/run_withnoise.m
@@ -1,3 +1,4 @@
+function [xdyn,fdyn,xss1,fss1,opts] = run_withnoise(tspan)
 %% noisy stochastic model
 % kinetic modeling with noisy metabolomics data 
 % test using kotte model 
@@ -13,28 +14,43 @@
 % vmodel will use convenience kinetics for estimating fluxes
 
 %% load original kotte model
-load('C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel\Kotte2014\model\kotte_model.mat');
-p = pvec;
-ival = M;
-clear pvec odep solver_opts opts
+if ~exist('C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel\Kotte2014\model\kotte_model.mat')
+    status = 2;
+    fprintf('\nLinux System\n');
+else 
+    status = 1;
+    fprintf('\nWindows System\n');
+end
+if status == 1
+    load('C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel\Kotte2014\model\kotte_model.mat');
+elseif status == 2
+    load('/home/shyam/Documents/Courses/CHE1125Project/IntegratedModels/KineticModel/Kotte2014/model/kotte_model.mat');    
+end
+
+pconv = [.1,.3,0]; % extra parameters for CK 'K1pep','K2fdp','rhoA'
+p = [pvec,pconv];
+p(9) = [];
+ival = [M;pvec(9)];
+clear pvec
 
 %% solve noisy model using ode45
-odep = struct('p',p,'model',model);
-
-% systems check
-noisy_model = @(t,x)simnoisyODE_kotte(t,x,odep);
-noisy_flux_test = noisyflux_kotte([M;model.PM],odep);
+% odep = struct('p',p,'model',model);
+odep = p;
 solver_opts = odeset('RelTol',1e-3,'AbsTol',1e-3);
 opts = struct('tspan',tspan,'x0',ival,'solver_opts',solver_opts,'odep',odep);
 
+% % systems check
+% noisy_model = @(t,x)simnoisyODE_kotte(t,x,odep);
+% noisy_flux_test = noisyflux_kotte([M;model.PM],odep);
+
 % simulate noisy system from random initial condition - this is better for
 % simulating the noisy model
-[xdyn,fdyn,xss1,fss1] = solve_ode(@simnoisyODE_kotte,opts,@flux_kotte);
+[xdyn,fdyn,xss1,fss1] = solve_ode(@simnoisyODE_kotte,opts,@kotte_flux_noCAS);
 figure
 subplot(211);
 plot(tspan,xdyn);
 ylabel('concentrations a.u.');
-legend('pep','fdp','E');
+legend('pep','fdp','E','acetate');
 subplot(212)
 plot(tspan,fdyn);
 ylabel('fluxes a.u.');
