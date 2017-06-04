405790727c1a242514a509b2514ac91ed16e6af9
shyamsrinivasan <shyamsweth@gmail.com>
2017-06-04 14:41:52 -0400

    changes to support constraints

--- KineticModel/estimation/runoptim_flux.m
+++ KineticModel/estimation/runoptim_flux.m
@@ -4,11 +4,20 @@ function [x_opt,fval,xss4,fss4,opts] =...
 if nargin<7
     multi = 0;
 end
+if nargin<8
+    costrfh = [];
+end
 np = size(optim_p,2);
 
-[FX,gradF] = objCASh(np);
+FX = objCASh(np);
 obj = @(x)full(FX(x,optim_p));
-grad = @(x)full(gradF(x,optim_p));
+% grad = @(x)full(gradF(x,optim_p));
+if ~isempty(costrfh)
+    constr = @(x)constrfh();
+    givenconstr = 1;
+else
+    givenconstr = 0;
+end
 
 % setup opti problem
 solver_opts = nloptset('algorithm','GN_DIRECT');
@@ -21,7 +30,12 @@ optim_opts = optiset('solver','NLOPT','maxiter',5000,...
 % optim_opts = optiset('solver','IPOPT','maxiter',5000,...
 %                                       'maxfeval',5000,...                                      
 %                                       'display','final');
-optim_prob = opti('obj',obj,'grad',grad,'bounds',lb,ub,'options',optim_opts);
+if givenconstr
+    optim_prob = opti('obj',obj,'bounds',lb,ub,'options',optim_opts);
+else
+    optim_prob =...
+    opti('obj',obj,'bounds',lb,ub,'options',optim_opts);
+end
 if multi
     [xval,fval,exitflag,info] = multisolve(optim_prob,[],[100 10]);   
 else
