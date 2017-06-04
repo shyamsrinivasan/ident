17339b4cd5c4db0a8e156683a52db181e65ba9f6
shyamsrinivasan <shyamsweth@gmail.com>
2017-06-04 15:38:48 -0400

    wrapper to call opti solver for noisy data

---
+++ KineticModel/estimation/optim_scripts/nlconstoptim_flux.m
@@ -0,0 +1,44 @@
+function [x_opt,fval,xss4,fss4,opts] =...
+         nlconstoptim_flux(opts,objCASh,lb,ub,x0,optim_p,multi)
+
+if nargin<7
+    multi = 0;
+end
+np = size(optim_p,2);
+
+[FX,gradF] = objCASh(np);
+obj = @(x)full(FX(x,optim_p));
+grad = @(x)full(gradF(x,optim_p));
+
+% setup opti problem
+solver_opts = nloptset('algorithm','GN_DIRECT');
+optim_opts = optiset('solver','NLOPT','maxiter',5000,...
+                                      'maxfeval',5000,...
+                                      'tolrfun',1e-8,...
+                                      'tolafun',1e-10,...
+                                      'solverOpts',solver_opts,...
+                                      'display','final');
+% optim_opts = optiset('solver','IPOPT','maxiter',5000,...
+%                                       'maxfeval',5000,...                                      
+%                                       'display','final');
+optim_prob = opti('obj',obj,'grad',grad,'bounds',lb,ub,'options',optim_opts);
+if multi
+    [xval,fval,exitflag,info] = multisolve(optim_prob,[],[100 10]);   
+else
+    [xval,fval,exitflag,info] = solve(optim_prob,x0); 
+end
+
+if exitflag == 1 &&...
+   (strcmpi(info.Status,'Success')||...
+    strcmpi(info.Status,'Optimal')||...
+    strcmpi(info.Status,'Converged / Target Reached'))
+
+    x_opt = xval;
+else
+    x_opt = [];
+    fval = []; 
+end
+
+xss4 = [];
+fss4 = [];
+
