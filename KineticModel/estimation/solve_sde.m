34053a3cc86977ec0eed2a7dbb9b8b153b695502
shyamsrinivasan <shyamsweth@gmail.com>
2017-06-07 17:05:33 -0400

    wrapper to solve SDE using SDE toolkit

---
+++ KineticModel/estimation/solve_sde.m
@@ -0,0 +1,43 @@
+function [yout,flux,yss,fss] = solve_sde(fh,gh,opts,flxh)
+if nargin<3
+    flxh = [];
+end
+
+if isfield(opts,'tspan')
+    tspan = opts.tspan;
+end
+if isfield(opts,'solver_opts')
+    solver_opts =  opts.solver_opts;
+else
+    solver_opts = [];
+end
+if isfield(opts,'x0');
+    x0 = opts.x0;
+end
+if isfield(opts,'odep')
+    odep = opts.odep;
+else
+    odep = [];
+end
+if ~isempty(odep)
+    newfh = @(t,x)fh(t,x,odep);
+else
+    newfh = fh;
+end
+tstart = tic;
+yout = sde_euler(newfh,gh,tspan,x0,solver_opts);
+fprintf('\nTime to solve ode :%4.3f\n',toc(tstart));
+yout = yout';
+
+yss = yout(:,end);
+if ~isempty(flxh)
+    flux = flxh(yout,odep);
+else
+    flux = [];
+end
+
+if ~isempty(flux)
+    fss = flux(:,end);
+else
+    fss = [];
+end
