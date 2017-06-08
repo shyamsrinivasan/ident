a0f325f2982eff60263f7ffa6552aa460d3ec851
shyamsrinivasan <shyamsweth@gmail.com>
2017-06-08 16:17:13 -0400

    code cleanup

--- KineticModel/estimation/optim_scripts/nlconstoptim_flux.m
+++ KineticModel/estimation/optim_scripts/nlconstoptim_flux.m
@@ -1,13 +1,24 @@
 function [x_opt,fval,xss4,fss4,opts] =...
-         nlconstoptim_flux(opts,objCASh,lb,ub,x0,optim_p,multi,constrfh)
-
-if nargin<7
-    multi = 0;
+         nlconstoptim_flux(opts,objCASh,lb,ub,x0,optim_p,ss_val,multi,constrfh)
+if nargin<9
+    givenconstr = 0;
+else
+    givenconstr = 1;
 end
 if nargin<8
-    constrfh = [];
+    multi = 0;
 end
-% np = size(optim_p,2);
+if isfield(opts,'solver')
+    solver = opts.solver;
+else
+    solver = 'nlopt';
+end
+if isfield(opts,'opt_alg')
+    opt_alg = opts.opt_alg;
+else
+    opt_alg = [];
+end
+np = size(ss_val,2); % # perturbations
 m = 3;
 nvar = 6;
 var_id = 6;
@@ -21,24 +32,25 @@ if ~isempty(objCASh)
 end
 
 % constraint function
-if ~isempty(constrfh)
+if givenconstr
     % nlcons = @(x)constrfh(x,optim_p);
-    [FXcons,DFXcons] = constrfh();
-    nlconFX = @(x)full(FXcons(x,optim_p));
-    nlconDFX = @(x)full(DFXcons(x,optim_p));
-    givenconstr = 1;
-else
-    givenconstr = 0;
+    [FXcons,DFXcons] = constrfh(np);
+    nlconFX = @(x)full(FXcons(x,optim_p,ss_val));
+    nlconDFX = @(x)full(DFXcons(x,optim_p,ss_val));    
 end
 
 % constraint rhs
-nlrhs = zeros(m+2,1);
-nlrhs(1:2) = 0.1;
+nlrhs = zeros(2*m+1,1);
+% flux norm
+nlrhs(1) = 0.1;     
+% concentration norm
+nlrhs(2:1+m) = 0.1;
 % nlrhs(3:6) = 1e-6; % precision level for Sv <= 0
-% constraint type : (-1 <=, 0 =, 1 >=)
-nle = zeros(m+2,1); % -ones(m+2,1);
-nle(1:2) = -1;
 
+% constraint type : (-1 <=, 0 =, 1 >=)
+nle = zeros(2*m+1,1); % -ones(m+2,1);
+nle(1:2) = -1;
+nle(2:1+m) = -1;
 % test if x0 satisfies constraints 
 nlconsval = nlconFX(x0);
 % == constraints
@@ -65,22 +77,32 @@ end
 %     any(gecons_iid(nle==1)<0)    
 %     error('Initial point is infeasible - constraint(s) are violated');
 % end
-        
-
 
 % setup opti problem
-% solver_opts = nloptset('algorithm','AUGLAG','subalgorithm','LN_COBYLA'); 
-solver_opts = nloptset('algorithm','GN_ISRES'); 
-optim_opts = optiset('solver','NLOPT','maxiter',5000,...
-                                      'maxfeval',500000,...
-                                      'tolrfun',1e-6,...
-                                      'tolafun',1e-6,...
-                                      'solverOpts',solver_opts,...
-                                      'display','final');
-% solver_opts
-% optim_opts = optiset('solver','IPOPT','maxiter',5000,...
-%                                       'maxfeval',5000,...                                      
-%                                       'display','final');
+optim_opts = optiset('solver',solver,...
+                      'maxiter',5000,...
+                      'maxfeval',500000,...
+                      'tolrfun',1e-3,...
+                      'tolafun',1e-3,...
+                      'display','final');                               
+switch(solver)
+    case 'nlopt'        
+        switch(opt_alg)            
+            case 'GN_ISRES'
+                solver_opts = nloptset('algorithm','GN_ISRES'); 
+            otherwise
+                solver_opts =...
+                nloptset('algorithm','AUGLAG','subalgorithm','LN_COBYLA'); 
+        end
+        optim_opts = optiset(optim_opts,'solverOpts',solver_opts);           
+    case 'ipopt'
+         
+    case 'scip'
+        
+    otherwise
+        error('Nonexistent solver in OPTI');
+end
+
 if givenconstr
     optim_prob =...
     opti('obj',obj,'nlmix',nlconFX,nlrhs,nle,'nljac',nlconDFX,...
@@ -93,7 +115,8 @@ if multi
     [xval,fval,exitflag,info] = multisolve(optim_prob,[],[100 10]);   
 else
     [xval,fval,exitflag,info] = solve(optim_prob,x0); 
-end
+end     
+
 
 if exitflag == 1 &&...
    (strcmpi(info.Status,'Success')||...
