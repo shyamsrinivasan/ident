45b752604c13af9a4272aaac06752912407cb6bc
shyamsrinivasan <shyamsweth@gmail.com>
2017-06-08 16:17:47 -0400

    add multiple perturbations to one enzyme

--- KineticModel/estimation/est_noise.m
+++ KineticModel/estimation/est_noise.m
@@ -16,7 +16,7 @@ opts.tspan = 0:.1:600;
 opts.odep = odep_bkp;
 % flux 1, 2 and 3 % k1cat, 'V2max', 'V3max'
 ptopts = struct('exp_pid',{11},...
-                'exp_pval',{2}); 
+                'exp_pval',{[.1;.5;1.0;1.5;2]}); 
 sol = getperturbations(ptopts,@perturb_noisy,opts);
 
 % run perturbation without noise to get feasible initial solution
@@ -37,9 +37,11 @@ soli = getperturbations(ptopts,@perturb_nonoise,optsi);
 %% use single perturbation sets to get parameters
 optimopts = struct('xss',{sol(1).xss},...
                    'fss',{sol(1).fss});
-opts.init_xss = soli(1).xss;               
+opts.init_xss = soli(1).xss(:,1);   
+opts.solver = 'nlopt';
+% opts.opt_alg = 'GN_ISRES';
 odep_opt = odep_bkp;
-odep_opt(11) = 2;
+% odep_opt(11) = 2;
 opt_sol = runoptimp(opts,plist,odep_opt,optimopts,@optimize_p_noisy);     
 
 
