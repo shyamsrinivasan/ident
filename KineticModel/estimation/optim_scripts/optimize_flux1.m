0dd4c5ec9a9a34bf2cae2303ae2c414d1bf477d3
shyamsrinivasan <shyamsweth@gmail.com>
2017-06-13 17:20:28 -0400

    functional representation for flux 1 parameter opt

---
+++ KineticModel/estimation/optim_scripts/optimize_flux1.m
@@ -0,0 +1,18 @@
+function optsol = optimize_flux1(opts,xss,fss,plist,odep_bkp)
+
+opts.odep = odep_bkp;
+optsol = struct([]); 
+
+ts1 = tic;
+
+% 'K1ac','k1cat'
+fprintf('Optimizing with a single set of perturbations\n');
+fprintf('\nOptimizing parameters for flux 1.....\n');
+[x_opt_4_1,opt_id_4_1,~,fval_4_1] = flux1_k_noisy(opts,xss,fss,plist);
+optsol(1).x_opt = x_opt_4_1;
+optsol(1).opt_id = opt_id_4_1;
+optsol(1).fval = fval_4_1;
+optsol(1).time = toc(ts1);
+fprintf('Time to estimate flux 1 parameters %4.3g\n',toc(ts1));
+
+fprintf('\nTotal time for parameter estimation %4.3g\n',toc(ts1));
