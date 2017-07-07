3b8c94b5612a35669691ada0e2eb08e743563037
shyamsrinivasan <shyamsweth@gmail.com>
2017-07-06 20:53:22 -0400

    quadratic obj to minimize distance between exp and model flux

---
+++ KineticModel/estimation/objectives/objnoisy.m
@@ -0,0 +1,12 @@
+% objective to minimize the L2-norm of fluxes (quadratic?)
+function fx = objnoisy(x,p,data)
+
+% assign parameter values from variable vector
+% p(data.p_id) = x(data.nc+1:end);
+
+% calculate flux
+vest = getmodelflux(x(1:data.nc),p);
+vest(1) = x(5).*x(3).*p(17)./(p(17)+x(4));
+
+% calc objective
+fx = sqrt(sum((vest(1)-data.vexp(1,:)).^2));
