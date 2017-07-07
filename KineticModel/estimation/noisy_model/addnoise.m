55d96ec8f9a4c0349979f183277f3922f2fc9ec3
shyamsrinivasan <shyamsweth@gmail.com>
2017-07-07 14:25:19 -0400

    fun to add percentage noise to concentrations and calc equivalent flux

---
+++ KineticModel/estimation/noisy_model/addnoise.m
@@ -0,0 +1,25 @@
+% add noise to input data and calculate corresponding noisy flux
+function [noisy_xss,noisy_fss] = addnoise(x,p)
+
+nvar = size(x,1);
+nflx = size(kotte_flux_noCAS(x,p(1,:)),1);
+npar = size(p,1);
+nsmp = size(x,2);
+
+if nsmp>1    
+    pd = makedist('Uniform','lower',-.5,'upper',.5);    
+else
+    pd = makedist('Uniform','lower',-.5,'upper',.5);    
+end
+met_noise = random(pd,nvar,nsmp);
+noisy_xss = x.*(1+met_noise);
+
+if npar>1
+    noisy_fss = zeros(nflx,npar);
+    for i = 1:npar
+        noisy_fss(:,i) = kotte_flux_noCAS(noisy_xss(:,i),p(i,:));
+    end
+else
+    noisy_fss = kotte_flux_noCAS(noisy_xss,p);
+end
+
