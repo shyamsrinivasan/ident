c6c60edd89d5989b32b47eee4d9478ba3ae4b545
shyamsrinivasan <shyamsweth@gmail.com>
2017-07-15 16:34:39 -0400

    setup optimization problem for any given flux

--- KineticModel/estimation/optim_scripts/setup_optim_prob.m
+++ KineticModel/estimation/optim_scripts/setup_optim_prob.m
@@ -1,11 +1,98 @@
-function optsol = setup_optim_prob(optimdata)
 % setup optimization problem for each flux in optimdata input
+function [prob,newdata] = setup_optim_prob(optimdata)
+
 if isfield(optimdata,'flxid')
     flxid = optimdata.flxid;
 end
 
-% setup problem 
+% all parameters available
+rxn_plist = {{'K1ac', 'k1cat'},{},{'K3fdp','K3pep','V3max'},{'K2pep','V2max'},...
+              {'V4max'},{}};
+relevant_list = rxn_plist{flxid};       
+% all parameters available (ordered)
+plist = {'K1ac','K3fdp','L3fdp','K3pep','K2pep','vemax','KeFDP','ne',...
+        'd','V4max','k1cat','V3max','V2max','K1pep','K2fdp','rhoA','acetate'};        
+% find relevant parametyers in ordered list for id
+if ~isempty(relevant_list)
+    p_id = cellfun(@(x)strcmpi(x,plist),relevant_list,'UniformOutput',false);
+    p_id = cellfun(@(x)find(x),p_id);
+else    
+    error('No parameters to estimate');
+end
+if isfield(optimdata,'nc')
+    nc = optimdata.nc;
+end
+if isfield(optimdata,'nf')
+    nf = optimdata.nf;
+end
 
+% determine nvar
+nvar = nc+nf+length(p_id);
+newdata = optimdata;
+newdata.nvar = nvar;
+newdata.p_id = p_id;
+
+% setup bounds
+if isfield(optimdata,'lb')
+    lb = optimdata.lb;
+else
+    lb = zeros(nvar,1);
+end
+if isfield(optimdata,'ub')
+    ub = optimdata.ub;
+else
+    ub = zeros(nvar,1);
+end
+if isfield(optimdata,'wt_xss')
+    exp_xss  = optimdata.wt_xss;
+end
+if isfield(optimdata,'wt_fss')
+    exp_fss = optimdata.wt_fss;
+end
+if isfield(optimdata,'eps')
+    eps = optimdata.eps;
+end
+% set bounds - concentration
+lb(1:nc) = exp_xss.*(1-eps);
+ub(1:nc) = exp_xss.*(1+eps);
+% set bounds - parameter
+lb(nc+1:nc+length(p_id)) = .1*ones(length(p_id),1); 
+ub(nc+1:optimdata.nc+length(p_id)) = 10*ones(length(p_id),1);
+% set bounds - flux
+lb(nvar-nf+1:nvar) = exp_fss(flxid)*(1-eps);
+ub(nvar-nf+1:nvar) = exp_fss(flxid)*(1+eps);
+
+% setup objectives and cons
+if isfield(optimdata,'obj')
+    objhs = optimdata.obj;
+end
+if isfield(optimdata,'nlcons')
+    conshs = optimdata.nlcons;
+end
+if isfield(optimdata,'nlrhs')
+    rhsval = optimdata.nlrhs;
+end
+if isfield(optimdata,'nle')
+    nles = optimdata.nle;
+end
+
+% choose obj and cons
+fhobj = str2func(objhs{flxid});
+obj = @(x)fhobj(x,optimdata.odep,optimdata);
+newdata.obj = obj;
+
+fhcons = str2func(conshs{flxid});
+nlcons = @(x)fhcons(x,optimdata.odep,optimdata);
+newdata.nlcons = nlcons;
+
+nlrhs = rhsval{flxid};
+newdata.nlrhs = nlrhs;
+nle = nles{flxid};
+newdata.nle = nle;
+
+% setup problem structure
+prob =...
+struct('obj',obj,'nlcons',nlcons,'nlrhs',nlrhs,'nle',nle,'lb',lb,'ub',ub);
 
 
 
