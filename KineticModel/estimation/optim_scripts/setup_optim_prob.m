b7eccf0bea07ffa172d291e7556f6307f2717305
shyamsrinivasan <shyamsweth@gmail.com>
2017-07-14 22:46:31 -0400

    setup problem for each input flux

---
+++ KineticModel/estimation/optim_scripts/setup_optim_prob.m
@@ -0,0 +1,12 @@
+function optsol = setup_optim_prob(optimdata)
+% setup optimization problem for each flux in optimdata input
+if isfield(optimdata,'flxid')
+    flxid = optimdata.flxid;
+end
+
+% setup problem 
+
+
+
+
+    
