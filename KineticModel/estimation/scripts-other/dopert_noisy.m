e85021e597fd6a6498e53fb28f605c5d09cb4306
shyamsrinivasan <shyamsweth@gmail.com>
2017-07-27 14:43:23 -0400

    include wt (no pertub) data in perturbation structure

--- KineticModel/estimation/scripts-other/dopert_noisy.m
+++ KineticModel/estimation/scripts-other/dopert_noisy.m
@@ -1,7 +1,9 @@
-function [exp_sol,noisy_sol] = dopert_noisy(opts,noisy_xss,odep_bkp,pt_sol_id)
+function [exp_sol,noisy_sol] =...
+        dopert_noisy(opts,noisy_xss,noisy_fss,odep_bkp,pt_sol_id)
 % generate perturbation data and add noise
 % perturb system from noisy initial conditions
-opts.x0 = noisy_xss(:,1);
+id = 1;
+opts.x0 = noisy_xss(:,id);
 opts.tspan = 0:.1:300;
 opts.odep = odep_bkp;
 pt_val = struct('exp_pid',{11,12,13},...
@@ -21,10 +23,10 @@ for j = 1:length(pt_sol_id)
 end
 
 % combine all perturbation flux data into a single vector
-exp_sol.xss = cat(2,noisy_sol.xss);
-exp_sol.fss = cat(2,noisy_sol.fss);
-exp_sol.exp_pid = cat(2,noisy_sol.exp_pid);
-exp_sol.exp_pval = cat(2,noisy_sol.exp_pval);
-exp_sol.odep = cat(1,noisy_sol.odep);
+exp_sol.xss = [noisy_xss(:,1) cat(2,noisy_sol.xss)];
+exp_sol.fss = [noisy_fss(:,1) cat(2,noisy_sol.fss)];
+exp_sol.exp_pid = [0 cat(2,noisy_sol.exp_pid)];
+exp_sol.exp_pval = [0 cat(2,noisy_sol.exp_pval)];
+exp_sol.odep = [odep_bkp;cat(1,noisy_sol.odep)];
 
 
