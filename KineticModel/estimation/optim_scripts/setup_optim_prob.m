499dbf3ebb2e6b13238ddb7b28cc3668e17a717a
shyamsrinivasan <shyamsweth@gmail.com>
2017-07-29 19:02:33 -0400

    set bounds separately for kcat and Km

--- KineticModel/estimation/optim_scripts/setup_optim_prob.m
+++ KineticModel/estimation/optim_scripts/setup_optim_prob.m
@@ -64,6 +64,12 @@ ub(1:nc*npert) = xss_exp_v.*(1+eps);
 % set bounds - parameter
 lb(nc*npert+1:nc*npert+length(p_id)) = .05*ones(length(p_id),1); 
 ub(nc*npert+1:nc*npert+length(p_id)) = 5*ones(length(p_id),1);
+% set separate bounds for Km
+lb(nc*npert+1:nc*npert+1) = .05*ones(1,1); 
+ub(nc*npert+1:nc*npert+1) = 1*ones(1,1);
+% set separate bounds for kcat
+lb(nc*npert+2:nc*npert+length(p_id)) = .1*ones(1,1); 
+ub(nc*npert+2:nc*npert+length(p_id)) = 3*ones(1,1);
 % set bounds - flux
 lb(nvar-nf*npert+1:nvar) = vss_exp_v*(1-eps);
 ub(nvar-nf*npert+1:nvar) = vss_exp_v*(1+eps);
