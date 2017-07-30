fbdfcebb21899094ffdc240008e55976cbda19ed
shyamsrinivasan <shyamsweth@gmail.com>
2017-07-29 22:56:52 -0400

    parse and store conc,flux,par in struct array

---
+++ KineticModel/estimation/parse_optsol.m
@@ -0,0 +1,18 @@
+% parse given solution vector into concentrations, fluxes and parameters
+function new_optsol = parse_optsol(optsol,data)
+
+nval = size(optsol,2);
+new_optsol = optsol;
+if nval>1
+    xval = cat(2,optsol.xval);
+    [xconc,xpar,xflx] = parsesolvec(xval,data);
+else
+    [xconc,xpar,xflx] = parsesolvec(optsol.xval,data);
+end
+
+% reshape and add to structure array
+for ival = 1:nval
+    new_optsol(ival).xconc = reshape(xconc(:,ival),[data.nc,data.npert]);
+    new_optsol(ival).xpar = xpar(:,ival);
+    new_optsol(ival).xflx = xflx(:,ival)';
+end
