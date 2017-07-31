be16dacbb0c31f29c8095977bc2d9a6d90804f64
shyamsrinivasan <shyamsweth@gmail.com>
2017-07-31 03:31:12 -0400

    plot 2-d or 3-d scatters in parameter space

---
+++ KineticModel/estimation/compare_pars.m
@@ -0,0 +1,19 @@
+% function to plot 2-d and 3-d diagrams comapring parameters
+function compare_pars(est_data)
+
+figure
+par = est_data.xpar;
+[npar,~] = size(par);
+
+if npar<=2 % 2-d plot
+    line(par(1,:),par(2,:),'LineStyle','none',...
+                            'Marker','.',...
+                            'MarkerSize',10);
+elseif npar<=3 % 3-d plot
+    line(par(1,:),par(2,:),par(3,:),'LineStyle','none',...
+                                    'Marker','.',...
+                                    'MarkerSize',10);
+end
+
+
+
