1de696ca9ba0cb141a90c7d29c107ee1c440befa
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-04 10:59:48 -0400

    parameter estimation for non-noisy model

---
+++ KineticModel/estimation/noiseless_model/dopert_nonoise.m
@@ -0,0 +1,33 @@
+function [exp_sol,no_noise_sol] =...
+        dopert_nonoise(opts,xss,fss,odep_bkp,pt_sol_id)
+    
+% generate perturbation data 
+% perturb system from deterministic (non-noisy) initial conditions
+id = 1;
+opts.x0 = xss(:,id);
+opts.tspan = 0:.1:300;
+opts.odep = odep_bkp;
+pt_val = struct('exp_pid',{11,12,13},...
+                'exp_pval',{[2],...
+                            [.2],...
+                            [.5]}); 
+sol = getperturbations(pt_val,@perturb_nonoise,opts);
+
+no_noise_sol = sol(pt_sol_id);
+j = length(pt_sol_id);
+% include wt (unperturbed) data
+no_noise_sol(j+1).xss = xss(:,id);
+no_noise_sol(j+1).fss = fss(:,id);
+no_noise_sol(j+1).exp_pid = 0;
+no_noise_sol(j+1).exp_pval = 0;
+no_noise_sol(j+1).odep = odep_bkp;
+
+% combine all perturbation flux data into a single vector
+exp_sol.xss = cat(2,no_noise_sol.xss);
+exp_sol.fss = cat(2,no_noise_sol.fss);
+exp_sol.exp_pid = cat(2,no_noise_sol.exp_pid);
+exp_sol.exp_pval = cat(2,no_noise_sol.exp_pval);
+exp_sol.odep = cat(1,no_noise_sol.odep);
+
+    
+    
---
+++ KineticModel/estimation/scripts-other/gen_nonoise.m
@@ -0,0 +1,70 @@
+% script to generate non-noisy data to estimated steady state
+% fluxes and concentrations in kotte model
+
+% generate experimental data - get initial ss
+tspan = 0:0.1:100;
+[xdyn,fdyn,xss,fss,opts] = run_nonoise(tspan);
+
+% backup parameters and initial conditions
+ival_bkp = opts.x0;
+odep_bkp = opts.odep;
+
+% perturb system from non-noisy initial conditions
+pt_sol_id = [1 2 3];
+[exp_sol,no_noise_sol] = dopert_nonoise(opts,xss,fss,odep_bkp,pt_sol_id);
+close all
+
+% get only data from one steady state
+pss = ones(1,numel(exp_sol.exp_pval));
+% pss(1) = 0;
+% pss(exp_sol.xss(1,:)>exp_sol.xss(2,:)) = 0;    
+
+% options structure for solving problem
+optimdata = struct('nc',3,'nflx',6,'nf',1,'flxid',1,'eps',.2,...
+                    'vexp',exp_sol.fss(:,logical(pss)),...
+                    'xexp',exp_sol.xss(:,logical(pss)),...
+                    'odep',odep_bkp,...
+                    'wt_xss',xss(:,1),'wt_fss',fss(:,1));
+                
+% define all objectiv efun handles
+allobjh = {'objnoisy',...
+           [],...
+           'objnoisy',...
+           'objnoisy',...
+           'objnoisy',...
+           []};     
+       
+% define all constraint fun handles
+allconsh = {'consnoisyf1',...
+            [],...
+            'consnoisyf3',...
+            'consnoisyf2',...
+            'consnoisyf4',...
+            []};
+% define all rhs for nl cons
+allnlrhs = {[],[],0,0,0,[]};
+% define cons type for nl cons
+allnle = {[],[],0,0,0,[]};       
+
+% problem defn
+setup_opts = optimdata;
+setup_opts.obj = allobjh;
+setup_opts.nlcons = allconsh;
+setup_opts.nlrhs = allnlrhs;
+setup_opts.nle = allnle;
+
+[prob,optimdata] = setup_optim_prob(setup_opts);
+
+% initial values for consrained nl(or quadratic?) optimization
+x0 = getrandomivals(optimdata,.1,1000);
+solveropt = struct('solver','ipopt','multi',0);
+optsol = choose_nlconsopt(prob,x0,optimdata,solveropt);
+
+% combine results for comparison plot
+est_data = combine_results(optsol,opts,no_noise_sol,optimdata,pss,pss);
+
+% compare fluxes and concentrations
+compare_vals(est_data,no_noise_sol,optimdata,opts,pss);
+
+% compare parameters in parameter space
+compare_pars(est_data);
