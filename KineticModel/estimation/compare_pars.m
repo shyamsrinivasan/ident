0303283a412a34be0932fa2a5264c743f4e71d13
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-06 14:37:11 -0400

    add bar graph comparison of parameters

--- KineticModel/estimation/compare_pars.m
+++ KineticModel/estimation/compare_pars.m
@@ -1,23 +1,35 @@
 % function to plot 2-d and 3-d diagrams comapring parameters
-function compare_pars(est_data)
+function compare_pars(est_data,plotype)
+if nargin<2
+    plotype=1;
+end
 if isempty(est_data)
     fprintf('No optimal solution found\n');
     return
 end
 
 figure
-par = est_data.xpar;
-[npar,~] = size(par);
+xpar = est_data.xpar;
+[npar,~] = size(xpar);
+par = est_data.par;
+perr = est_data.perr;
 
-if npar<=2 % 2-d plot
-    line(par(1,:),par(2,:),'LineStyle','none',...
-                            'Marker','.',...
-                            'MarkerSize',10);
-elseif npar<=3 % 3-d plot
-    line(par(1,:),par(2,:),par(3,:),'LineStyle','none',...
-                                    'Marker','.',...
-                                    'MarkerSize',10);
+if plotype==1 % plot a bar graph of parameter value averages and error bars
+    set(gca,'NextPlot','add');
+    bh = bar(par);
+    xerr_pos = cat(1,bh.XData)+cat(1,bh.XOffset);
+    yerr_pos = par;
+    erh = errorbar(xerr_pos,yerr_pos,perr,'LineStyle','none');
+elseif plotype==2
+    if npar<=2 % 2-d plot
+        line(xpar(1,:),xpar(2,:),'LineStyle','none',...
+                                'Marker','.',...
+                                'MarkerSize',10);
+    elseif npar<=3 % 3-d plot
+        line(xpar(1,:),xpar(2,:),xpar(3,:),'LineStyle','none',...
+                                        'Marker','.',...
+                                        'MarkerSize',10);
+    end
 end
-
 
 
