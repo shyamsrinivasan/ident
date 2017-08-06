e940a6919c8b936fcc1e7637dfe643216e44907c
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-06 01:33:41 -0400

    add noise to objective function

--- KineticModel/estimation/objectives/obj_typeb.m
+++ KineticModel/estimation/objectives/obj_typeb.m
@@ -6,15 +6,16 @@ nc = data.nc;
 nf = data.nf;
 npert = data.npert;
 nvar = data.nvar;
+np = data.np;
 w1 = data.flux_wt;
 w2 = data.conc_wt;
 
 % flux norm
-flux_norm = sqrt(sum((x(nvar-nf*npert+1:nvar)-data.vexp).^2));
+flux_norm = sqrt(sum((x(nc*npert+np+1:nc*npert+np+nf*npert)-data.vexp).^2));
 
 % concentration norm
 conc_norm = sqrt(sum((x(1:nc*npert)-data.xexp).^2));
 
 % calc objective
-fx = w1.*flux_norm + w2.*conc_norm;
+fx = w1.*flux_norm + w2.*conc_norm + x(nvar-1).^4 + x(nvar).^4;
 
