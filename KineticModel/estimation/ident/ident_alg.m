ec73a8d8f7af8cde8261038c92caa8af0f04f1a9
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-10 17:35:40 -0400

    working till problem setup and creation

--- KineticModel/estimation/ident/ident_alg.m
+++ KineticModel/estimation/ident/ident_alg.m
@@ -14,23 +14,32 @@ nsmp = 10;
 
 % perturb system from noisy initial conditions
 pt_sol_id = [1 2 3];
-[exp_sol,noisy_sol] = dopert_noisy(opts,noisy_xss,noisy_fss,odep_bkp,pt_sol_id);
+[exp_sol,noisy_sol] =...
+dopert_noisy_dyn(opts,noisy_xss,noisy_fss,odep_bkp,pt_sol_id);
 close all
 
-ident_data = struct('nvar',3);
+xexp_var = ones(size(noisy_sol(1).xdyn,1),size(noisy_sol(1).xdyn,2));
+optim_opts = struct('pname','K1ac','nc',3,'nf',6,...
+                    'odep',odep_bkp,...
+                    'tspan',0:.1:300,...
+                    'x0',noisy_xss(:,1),...
+                    'xexp_dyn',noisy_sol(1).xdyn,...
+                    'xexp_var',xexp_var,...
+                    'objf','ident_obj',...
+                    'gradobjfh','ident_gradobj',...
+                    'boundfh','boundsallp',...
+                    'modelf','kotte_output',...
+                    'modelsensf','kotte_sensoutput');
+
+[prob,data] = setup_ident_prob(optim_opts);
+
+% set initial parameter vales for optimization
+p0 = opts.odep(data.varid);
+solveropt = struct('solver','ipopt','multi',0);
 
 % 1. start by solving optimal estimation problem for fixed parameter i
-modelfh = @(p)kotte_output(p,ident_data);
-modelsensfh = @(p)kotte_sensoutput(p,data);
-funh = struct('modelfh',modelfh,'modelsensfh',modelsensfh);
-
-objfh = @(p)ident_obj(p,data,funh);
-gradobjfh = @(p)ident_gradobj(p,data,funh);
-
-prob = struct('obj',objfh,'gradobj',gradobjfh,'lb',lb,'ub',ub);
-% optsol = nlconsopt(prob,data);
-
-[xopt] = nlsqopt(prob,data);
+[xopt] = nlsqopt(prob,p0,solveropt);
+                
 % 2. take inc/dec. steps in parameter i
 % 3. re-optimize parameters for new parameter i
 % 4. repeat steps until threshold is crossed
