cf406b6d31b17c12cb4323bfb354469c1bf1d165
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-09 22:24:50 -0400

    solve ode model+sens equations to generate time course

---
+++ KineticModel/estimation/ident/kotte_senseoutput.m
@@ -0,0 +1,13 @@
+% function to provide time course for x and dx/dp
+% p - parameter vector
+% data - other required info structure
+function yout = kotte_senseoutput(p,data)
+if isfield(data,'x0')
+    x0 = data.x0;
+end
+
+fh = @()kotteCASwSENS(3,17,@kotte_CAS); % @kotte_CAS
+
+solver_opts = struct('abstol',1e-6,'reltol',1e-6);
+opts = struct('tspan',data.tspan,'x0',x0,'solver_opts',solver_opts,'odep',p);
+yout = solveODE_cas(fh,opts);
