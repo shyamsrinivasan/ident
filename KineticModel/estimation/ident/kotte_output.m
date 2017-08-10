cc95090e252d94326d6832a2af420e010a16939d
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-09 22:23:50 -0400

    solve ode model to generate time course model data

--- KineticModel/estimation/ident/kotte_output.m
+++ KineticModel/estimation/ident/kotte_output.m
@@ -2,10 +2,11 @@
 % x - states
 % p - parameter vector
 % data - other required info structure
-function yout = kotte_output(y,p,data)
+function yout = kotte_output(p,data)
+if isfield(data,'x0')
+    x0 = data.x0;
+end
 
 solver_opts = struct('abstol',1e-6,'reltol',1e-6);
-opts = struct('tspan',data.tspan,'x0',y,'solver_opts',solver_opts,'odep',p);
-yout = solveODE_cas(@kotte_CAS,opts,@kotte_flux_noCAS);
-
-
+opts = struct('tspan',data.tspan,'x0',x0,'solver_opts',solver_opts,'odep',p);
+yout = solveODE_cas(@kotte_CAS,opts);
