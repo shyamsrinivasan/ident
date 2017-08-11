75ae1927b5de5725693b05cbcdde26b2e00a7285
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-11 11:51:56 -0400

    test with idx incorporated

--- KineticModel/estimation/ident/sens_test.m
+++ KineticModel/estimation/ident/sens_test.m
@@ -16,16 +16,17 @@ end
 pconv = [.1,.3,0,pvec(9)]; % extra parameters for CK 'K1pep','K2fdp','rhoA' and 'acetate'
 p = [pvec,pconv];
 p(9) = [];
-ival = [M;zeros(3*17,1)];
+ival = [M;zeros(3*16,1)];
 clear pvec
 
-[FXaug,FX] = kotteCASwSENS(3,17);
+[FXaug,FX] = kotteCASwSENS(3,17,1);
 
 % solve deterministic (no noise) model using casadi(cvodes)
+tspan = 0:.1:300;
 odep = p;
-fh = @()kotteCASwSENS(3,17); % @kotte_CAS
+fh = @()kotteCASwSENS(3,17,1); % @kotte_CAS
 solver_opts = struct('abstol',1e-6,'reltol',1e-6);
-opts = struct('tspan',0:.1:300,'x0',ival,'solver_opts',solver_opts,'odep',odep);
+opts = struct('tspan',tspan,'x0',ival,'solver_opts',solver_opts,'odep',odep);
 [xdyn,fdyn,xss1,fss1] = solveODE_cas(fh,opts);
 figure
 subplot(211);
