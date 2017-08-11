c89cffb9fc98246f505b1613ae4e63ae8272b198
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-11 09:08:39 -0400

    supply appropriate ivals for integration

--- KineticModel/estimation/ident/kotte_senseoutput.m
+++ KineticModel/estimation/ident/kotte_senseoutput.m
@@ -7,7 +7,8 @@ if isfield(data,'x0')
 end
 
 fh = @()kotteCASwSENS(3,17); % @kotte_CAS
+ival = [x0;zeros(3*17,1)];
 
 solver_opts = struct('abstol',1e-6,'reltol',1e-6);
-opts = struct('tspan',data.tspan,'x0',x0,'solver_opts',solver_opts,'odep',p);
+opts = struct('tspan',data.tspan,'x0',ival,'solver_opts',solver_opts,'odep',p);
 yout = solveODE_cas(fh,opts);
