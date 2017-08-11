b44239912f87402a07047f952d02b462281ddaf9
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-11 11:51:21 -0400

    add p index to remove identifiability parameter

--- KineticModel/estimation/ident/kotteCASwSENS.m
+++ KineticModel/estimation/ident/kotteCASwSENS.m
@@ -1,4 +1,4 @@
-function [FXaug,FX,D2FX,fx_aug,aug_x,p] = kotteCASwSENS(nvar,np)
+function [FXaug,FX,D2FX,fx_aug,aug_x,p] = kotteCASwSENS(nvar,np,idx)
 
 x = casadi.SX.sym('x',nvar,1);
 p = casadi.SX.sym('p',np,1);
@@ -22,9 +22,14 @@ dfp = jacobian([fx_sys{:}],p);
 for ip = 1:np
     fx_sens{ip} = dfx*sens_var(:,ip) + dfp(:,ip);
 end
-fx_sens = [fx_sens{:}];
-fx_sens_vec = reshape(fx_sens,[nvar*np,1]);
-sens_var_vec = reshape(sens_var,[nvar*np,1]);
+if ~isempty(idx)
+    pid = setdiff(1:np,idx);    
+else
+    pid = 1:np;    
+end
+fx_sens = [fx_sens{pid}]; % remove column corresponding to idx parameter
+fx_sens_vec = reshape(fx_sens,[nvar*length(pid),1]);
+sens_var_vec = reshape(sens_var(:,pid),[nvar*length(pid),1]);
 
 aug_x = [x;sens_var_vec];
 fx_aug = [[fx_sys{:}]';fx_sens_vec(:)];
