5052f0bd1f43638bdbd5c483c38ce5c52ac9d955
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-12 18:10:49 -0400

    working version of 1st step of ident optimization

--- KineticModel/estimation/ident/syside_test2.m
+++ KineticModel/estimation/ident/syside_test2.m
@@ -12,16 +12,20 @@ pt_sol_id = [1 2 3];
 close all
 
 % new sysid_test with functions
-[xdyn_fun,ode,x,p] = RK4integrator_cas(@kotte_CAS);
+[xdyn_fun,ode,x,p,ident_c,p_useless,acetate] = RK4integrator_cas(@kotteCASident,1);
 
 xinit = no_noise_sol(4).xss;
 xexp_data = no_noise_sol(1).xdyn(:,2:end);
 
-x_sym = xdyn_fun(no_noise_sol(4).xss,repmat(p,1,3000));
+x_sym =...
+xdyn_fun(no_noise_sol(4).xss,repmat(p,1,3000),.1,repmat(opts.odep(14:16)',1,3000),.1);
 x_error = (xexp_data-x_sym);
 obj = .5*dot(x_error,x_error);
 
 % setup nlp
 nlp = struct('x',p,'f',obj);
 solver = casadi.nlpsol('solver','ipopt',nlp);
-sol_opt = solver('x0',opts.odep(2:end)');
+p0 = opts.odep(2:13)';
+p0(6) = .1;
+sol_opt = solver('x0',opts.odep(2:13)');
+
