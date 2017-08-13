5286fc9f50d8a0f90f8db03764f9502fa4465095
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-13 00:56:42 -0400

    solve nlsqopt prob with ipopt

---
+++ KineticModel/estimation/ident/solve_nlsqopt.m
@@ -0,0 +1,30 @@
+% function to solve nlsq problem using casadi built in ipopt solver
+function optsol = solve_nlsqopt(prob,x0)
+if isfield(prob,'obj')
+    obj = prob.obj;
+end
+if isfield(prob,'x')
+    x = prob.x;
+end    
+if isfield(prob,'lb')
+    lb = prob.lb;
+else
+    lb = zeros(length(x0),1);
+end
+if isfield(prob,'ub')
+    ub = prob.ub;
+else
+    ub = zeros(length(x0),1);
+    ub(ub==0) = Inf;
+end
+
+% setup nlp
+nlp = struct('x',x,'f',obj);
+solver = casadi.nlpsol('solver','ipopt',nlp);
+% p0 = opts.odep(2:13)';
+% p0(6) = .1;
+sol_opt = solver('x0',x0,'lbx',lb,'ubx',ub);
+
+optsol.xval = full(sol_opt.x);
+optsol.f = full(sol_opt.f);
+optsol.lambda = full(sol_opt.lam_x);
