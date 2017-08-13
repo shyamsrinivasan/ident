c2567eccabee8d185da5f391beece35d04c337bb
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-12 20:13:17 -0400

    use options data structure

--- KineticModel/estimation/ident/RK4integrator_cas.m
+++ KineticModel/estimation/ident/RK4integrator_cas.m
@@ -1,13 +1,29 @@
 % integrate kotte model w/ custom RK4 integrator using mapaccum function of
 % CasADi
 function [xdyn_fun,ode,x,p_all,ident_c,p_useless,acetate] =...
-        RK4integrator_cas(casfh,ident_idx)
+        RK4integrator_cas(casfh,data)
+
+if isfield(data,'pname')
+    pname = data.pname;
+end
+if isfield(data,'tspan')
+    tspan = data.tspan;
+end
+npts = length(tspan)-1;
+
+plist = {'K1ac','K3fdp','L3fdp','K3pep','K2pep','vemax','KeFDP','ne',...
+        'd','V4max','k1cat','V3max','V2max','K1pep','K2fdp','rhoA','acetate'}; 
+if ~isempty(pname)
+    ident_idx = find(strcmpi(plist,pname));
+else
+    ident_idx = [];
+end    
 
 [ode,~,~,~,x,p_all,ident_c,p_useless,acetate] = casfh(ident_idx);
 % [ode,~,~,fx,x,p] = kotte_CAS();
 
 % RK4 integrator
-dt = .1;
+dt = tspan(end)/npts;
 k1 = ode(x,p_all,ident_c,p_useless,acetate);
 k2 = ode(x+dt/2.0*k1,p_all,ident_c,p_useless,acetate);
 k3 = ode(x+dt/2.0*k2,p_all,ident_c,p_useless,acetate);
@@ -22,7 +38,7 @@ xstate_onepoint =...
 casadi.Function('xstate_onepoint',{x,p_all,ident_c,p_useless,acetate},{xstate});
 xstate_onepoint = xstate_onepoint.expand();
 
-xdyn_fun = xstate_onepoint.mapaccum('all_samples',3000);
+xdyn_fun = xstate_onepoint.mapaccum('all_samples',npts);
 
 % xdyn = all_samples(opts.x0,repmat(opts.odep(2:end)',1,3000));
 
