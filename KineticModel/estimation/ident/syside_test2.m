c2bd2610d103522a4c4eeb5d3f735e3fbaea7083
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-15 00:39:43 -0400

    inc. # perturbations

--- KineticModel/estimation/ident/syside_test2.m
+++ KineticModel/estimation/ident/syside_test2.m
@@ -12,7 +12,7 @@ pt_sol_id = [1 2 3];
 [exp_sol,no_noise_sol] = dopert_nonoise_dyn(opts,xss,fss,odep_bkp,pt_sol_id);
 close all
 
-optim_opts = struct('pname','K1ac','nc',3,'nf',6,...
+optim_opts = struct('pname','k1cat','nc',3,'nf',6,...
                     'casmodelfun',@kotteCASident,...
                     'integratorfun','RK4integrator_cas',...
                     'odep',odep_bkp,...
@@ -30,7 +30,7 @@ thetai_fixed_value = .1;
 theta_step = 0;
 
 % loop all the abopve statements for complete identifiability algforithm
-maxiter = 100;
+maxiter = 200;
 chiPLE = zeros(1,maxiter);
 xPLE = zeros(12,maxiter);
 thetai_inc = zeros(1,maxiter);
@@ -41,8 +41,8 @@ obj_step = zeros(1,maxiter);
 p0 = opts.odep(2:13)';
 iter = 1;
 
-figure
-hold on
+% figure
+% hold on
 while iter<maxiter || chiPLE(iter)<delta_alpha
     
     [optsol,thetai_fixed_value,theta_step,obj_new] =...
@@ -59,7 +59,7 @@ while iter<maxiter || chiPLE(iter)<delta_alpha
     thetai_step(iter) = theta_step;
     
     % figure for PLE
-    line(thetai_inc,chiPLE(iter),'LineStyle','none','Marker','.','MarkerSize',10);
+%     line(thetai_inc,chiPLE(iter),'LineStyle','none','Marker','.','MarkerSize',10);
     
     iter = iter+1;
 end
