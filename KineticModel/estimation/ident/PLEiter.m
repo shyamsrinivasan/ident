cc4ca121505031fed94c3724851de47b21fa4748
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-15 00:10:10 -0400

    no major change

--- KineticModel/estimation/ident/PLEiter.m
+++ KineticModel/estimation/ident/PLEiter.m
@@ -1,4 +1,4 @@
-function [optsol,thetai_fixed_value,theta_step,iter_theta_step] =...
+function [optsol,thetai_fixed_value,theta_step,obj_new,iter_theta_step] =...
         PLEiter(thetai_fixed_value,theta_step,xval,p_val,PLE_threshold,setup_opts)
     
 % new theta
@@ -15,7 +15,7 @@ optsol = solve_nlsqopt(prob_cas,xval);
 % thetai_step(iter) = theta_step;
 
 % adpative step in thetai
-[theta_step,iter_theta_step] =...
+[theta_step,obj_new,iter_theta_step] =...
 adaptive_step(optsol.fval,optsol.xval,prob_cas,p_val,thetai_fixed_value,PLE_threshold);
     
     
