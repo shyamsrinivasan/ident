a9481ee74a0c662d68441b8b1253ef6a9add2825
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-16 10:46:09 -0400

    remove commented lines

--- KineticModel/estimation/ident/PLEiter.m
+++ KineticModel/estimation/ident/PLEiter.m
@@ -8,12 +8,6 @@ thetai_fixed_value = thetai_fixed_value + theta_step;
 prob_cas = identopt_setup(setup_opts,thetai_fixed_value);
 optsol = solve_nlsqopt(prob_cas,xval);
 
-% store obj values
-% chiPLE(iter) = optsol.fval;
-% xPLE(:,iter) = optsol.xval;
-% thetai_inc(iter) = thetai_fixed_value;
-% thetai_step(iter) = theta_step;
-
 % adpative step in thetai
 [theta_step,obj_new,iter_theta_step] =...
 adaptive_step(optsol.fval,optsol.xval,prob_cas,p_val,thetai_fixed_value,PLE_threshold);
