1b1a67777be2e1298befd891157e9aa728f09610
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-16 16:08:08 -0400

    include type of change in thetai

--- KineticModel/estimation/ident/PLEiter.m
+++ KineticModel/estimation/ident/PLEiter.m
@@ -1,8 +1,8 @@
 function [optsol,thetai_fixed_value,theta_step,obj_new,iter_theta_step] =...
-        PLEiter(thetai_fixed_value,theta_step,xval,p_val,PLE_threshold,setup_opts)
+        PLEiter(thetai_fixed_value,theta_step,xval,p_val,PLE_threshold,setup_opts,type)
     
 % new theta
-thetai_fixed_value = thetai_fixed_value + theta_step;
+thetai_fixed_value = thetai_fixed_value + type*theta_step;
 
 % PLE through optimization for new thetai
 prob_cas = identopt_setup(setup_opts,thetai_fixed_value);
@@ -10,6 +10,6 @@ optsol = solve_nlsqopt(prob_cas,xval);
 
 % adpative step in thetai
 [theta_step,obj_new,iter_theta_step] =...
-adaptive_step(optsol.fval,optsol.xval,prob_cas,p_val,thetai_fixed_value,PLE_threshold);
+adaptive_step(optsol.fval,optsol.xval,prob_cas,p_val,thetai_fixed_value,PLE_threshold,type);
     
     
