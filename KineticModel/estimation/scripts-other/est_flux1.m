856a0ca96ba742528d3ea1c00477280fff62b387
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-20 16:48:03 -0400

    no major changes

--- KineticModel/estimation/scripts-other/est_flux1.m
+++ KineticModel/estimation/scripts-other/est_flux1.m
@@ -76,7 +76,7 @@ setup_opts.nle = allnle;
 [prob,optimdata] = setup_optim_prob(setup_opts,expdata);
 
 % initial values for consrained nl(or quadratic?) optimization
-x0 = getrandomivals(optimdata,.2,1000);
+x0 = getrandomivals(optimdata,.25,5000);
 solveropt = struct('solver','ipopt','multi',0);
 optsol = choose_nlconsopt(prob,x0,optimdata,solveropt);
 
