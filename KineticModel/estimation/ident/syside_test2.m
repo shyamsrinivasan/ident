dbe455dc6b40292476c718f60a5ebfaa8218ea34
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-20 23:20:11 -0400

    options for k1cat identifiability

--- KineticModel/estimation/ident/syside_test2.m
+++ KineticModel/estimation/ident/syside_test2.m
@@ -12,7 +12,7 @@ pt_sol_id = [1 2 3];
 [exp_sol,no_noise_sol] = dopert_nonoise_dyn(opts,xss,fss,odep_bkp,pt_sol_id);
 close all
 
-optim_opts = struct('pname','K1ac','nc',3,'nf',6,...
+optim_opts = struct('pname','k1cat','nc',3,'nf',6,...
                     'plim',[0.001 6],...
                     'casmodelfun',@kotteCASident,...
                     'integratorfun','RK4integrator_cas',...
@@ -23,7 +23,7 @@ optim_opts = struct('pname','K1ac','nc',3,'nf',6,...
                     'xexp',no_noise_sol(1).xdyn(:,1:100:2001));
 
 % set confidence interval threshold for PLE 
-alpha = .50; % alpha quantile for chi2 distribution
+alpha = .90; % alpha quantile for chi2 distribution
 dof = 1; % degrees of freedom
 % chi2 alpha quantile
 delta_alpha = chi2inv(alpha,dof);      
@@ -35,15 +35,21 @@ maxiter = 1000;
 
 % initial value for optimization
 scale = ones(12,1);
-scale(2) = 1e6;
-p0 = opts.odep(2:13)'./scale;
+
+% p0 for K1ac
+% scale(2) = 1e6;
+% p0 = opts.odep(2:13)'./scale;
+
+% p0 for k1cat
+scale(3) = 1e6;
+p0 = [opts.odep(1:10)';opts.odep(12:13)']./scale;
 
 % call PLE evaluation function
 [PLEvals] =...
 getPLE(thetai_fixed_value,theta_step,p0,opts.odep,delta_alpha,optim_opts,maxiter,2);
 
 % get confidence interval bounds from pl data
-sigma = pleCI_finite_sample(delta_alpha,PLEvals_pos.chiPLE,PLEvals_pos.thetai_inc)
+% sigma = pleCI_finite_sample(delta_alpha,PLEvals_pos.chiPLE,PLEvals_pos.thetai_inc);
 
 % figure
 % hold on
