4d991d7141b40fc83fa9f070739f81b11ae2c815
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-23 16:42:04 -0400

    optimize code for scatter plots

--- KineticModel/estimation/compare_vals.m
+++ KineticModel/estimation/compare_vals.m
@@ -1,159 +1,148 @@
-function allfh = compare_vals(est_sol,exp_sol,data,opts,test_data_id)
+function [allfh,barh] = compare_vals(est_sol,exp_sol,val_data,data,type)
+if nargin<5
+    type = 1;
+end
 if isempty(est_sol)
     fprintf('No optimal solution found\n');
     allfh = [];
+    barh = [];
     return
 end
 
-test_data_id = logical(test_data_id);
-all_labels = {'P1','P2','P3','WT'};
-
-% calculate new model fluxes
-% before perturbation - wt fluxes
-% assume wt (unperturbed) data is always included at the end
-wt_exp_xss = exp_sol(end).xss;
-wt_exp_fss = kotte_flux_noCAS(wt_exp_xss,data.odep);
-
-% after perturbation - perturb parameters to ascertain fluxes
-pt_datasize = length(find(test_data_id(1:end-1)));
-nf = size(wt_exp_fss,1);
-
-exp_xss = cat(2,exp_sol.xss);
-exp_fss = cat(2,exp_sol.fss);
-
-% collect all data to plot [p#1 p#2....  wt]  
-% estimated
-est_xss = est_sol.xss;
-est_xerr = est_sol.xerr;
-est_fss = est_sol.fss;
-est_ferr = est_sol.ferr;
-
-if ~test_data_id(end) % wt not included in estimation
-    rel_labels = all_labels(test_data_id);
+% get est data from fields in est_sol
+% optimal sol
+% if isfield(est_sol,'opt_xss')
+%     opt_xss = est_sol.opt_xss;
+% end
+% if isfield(est_sol,'opt_fss')
+%     opt_fss = est_sol.opt_fss;
+% end
+if isfield(est_sol,'opt_xss_avg')
+    opt_xss_avg = est_sol.opt_xss_avg;
 else
-    % experimental
-    exp_xss = exp_xss(:,test_data_id);
-    exp_fss = exp_fss(:,test_data_id);
-    % axis labels
-    rel_labels = [all_labels(test_data_id) all_labels(end)];
+    opt_xss_avg = [];
 end
-nplot = pt_datasize+1;
-% opt_xss(:,1) = [];
-% opt_fss(:,1) = [];
-
-% plot comparison
-% use cell arrays to store data for each subplot
-xss_plot = cell(data.nc,1);
-xss_error = cell(data.nc,1);
-fss_plot = cell(nf,1);
-fss_error = cell(nf,1);
-
-% collect all data
-for j = 1:data.nc
-    xss_plot{j} = [exp_xss(j,:)' est_xss(j,:)'];
-    xss_error{j} = [zeros(1,length(exp_xss(j,:)))' est_xerr(j,:)'];
+if isfield(est_sol,'opt_xss_err')
+    opt_xss_err = est_sol.opt_xss_err;
+else
+    opt_xss_err = [];
+end
+if isfield(est_sol,'opt_fss_avg')
+    opt_fss_avg = est_sol.opt_fss_avg;
+else
+    opt_fss_avg = [];
+end
+if isfield(est_sol,'opt_fss_err')
+    opt_fss_err = est_sol.opt_fss_err;
+else
+    opt_fss_err = [];
 end
 
-for k = 1:nf
-    fss_plot{k} = [exp_fss(k,:)' est_fss(k,:)'];
-    fss_error{k} = [zeros(1,length(exp_fss(k,:)))' est_ferr(k,:)'];
+% recalculated values
+% if isfield(est_sol,'calc_xss')
+%     calc_xss = est_sol.calc_xss;
+% end
+% if isfield(est_sol,'calc_fss')
+%     calc_fss = est_sol.calc_fss;
+% end
+if isfield(est_sol,'calc_xss_avg')
+    calc_xss_avg = est_sol.calc_xss_avg;
+end
+if isfield(est_sol,'calc_xss_err')
+    calc_xss_err = est_sol.calc_xss_err;
+end
+if isfield(est_sol,'calc_fss_avg')
+    calc_fss_avg = est_sol.calc_fss_avg;
+end
+if isfield(est_sol,'calc_fss_err')
+    calc_fss_err = est_sol.calc_fss_err;
 end
 
-% plot concentrations
-hfc = figure;
-ahc = zeros(data.nc,1);
-barc = struct();
-for j = 1:data.nc
-    ahc(j) = subplot(data.nc,1,j);
-    set(ahc(j),'NextPlot','add');
-    barc(j).h = bar(ahc(j),xss_plot{j});   
-    [~,ylbl] = getKotteaxislabels(2,2,[1,j]);
+% experimental data
+if isfield(exp_sol,'xss')
+    xss = cat(2,exp_sol.xss);
+end
+if isfield(exp_sol,'fss')
+    fss = cat(2,exp_sol.fss);
+end
+
+if type==1 % bar chart
+    % plot concentrations
+    xss_bar_data =...
+    barplotdata(data.nc,xss,[],opt_xss_avg,opt_xss_err,calc_xss_avg,calc_xss_err);
+    xss_plot = xss_bar_data.ss;
+    xss_error = xss_bar_data.err;
+    [hfc,barc] = plotbars(data.nc,xss_plot,xss_error);
+    
+    % plot fluxes
+    fss_bar_data =...
+    barplotdata(data.nflx,fss,[],...
+                [opt_fss_avg;zeros(data.nflx-1,length(opt_fss_avg))],...
+                [opt_fss_err;zeros(data.nflx-1,length(opt_fss_err))],...
+                calc_fss_avg,calc_fss_err);
+    fss_plot = fss_bar_data.ss;
+    fss_error = fss_bar_data.err;
+    [hfv,barv] = plotbars(data.nflx,fss_plot,fss_error,1);
+    
+elseif type==2 % scatter plot
+end
+    
+% ahc.XLabel.String = 'WT and Perturbations';
+legend('Noisy Data','Optimal Estimate','Model SS');
+allfh = [hfc;hfv];
+barh = {barc;barv};
+
+function [hf,barfs] = plotbars(nplot,ss_plot,error_plot,fc)
+if nargin<4
+    fc = 2;
+end
+
+hf = figure;
+barfs = struct();
+for j = 1:nplot
+    if fc==2
+        barfs(j).ah = subplot(nplot,1,j);        
+    elseif fc==1
+        barfs(j).ah = subplot(nplot/2,2,j);            
+    end
+    set(barfs(j).ah,'NextPlot','add');
+    barfs(j).h = bar(barfs(j).ah,ss_plot{j});   
+    [~,ylbl] = getKotteaxislabels(2,fc,[1,j]);
     if strcmpi(version('-release'),'2014a')
     elseif strcmpi(version('-release'),'2014b') ||...
            strcmpi(version('-release'),'2017a') ||...
            strcmpi(version('-release'),'2017b')
-        
-        ahc(j).YLabel.String = ylbl;    
-        ahc(j).XTickLabel = rel_labels;  
+
+        barfs(j).ah.YLabel.String = ylbl{1};    
+%         ah(j).XTickLabel = rel_labels;  
     end
 end
 
 % collect data for error bars
 % err_data = struct([]);
-for j = 1:data.nc
+for j = 1:nplot
     if strcmpi(version('-release'),'2014a')
-        xpos = zeros(size(xss_plot{j},1),length(barc(j).h));
-        ypos = zeros(size(xss_plot{j},1),length(barc(j).h));
-        for ih = 1:length(barc(j).h)
-            xpos_d = get(get(barc(j).h(ih),'children'),'xdata');
-            ypos_d = get(get(barc(j).h(ih),'children'),'ydata');
+        xpos = zeros(size(ss_plot{j},1),length(barfs(j).h));
+        ypos = zeros(size(ss_plot{j},1),length(barfs(j).h));
+        for ih = 1:length(barfs(j).h)
+            xpos_d = get(get(barfs(j).h(ih),'children'),'xdata');
+            ypos_d = get(get(barfs(j).h(ih),'children'),'ydata');
             xpos(:,ih) = ((xpos_d(2,:)+xpos_d(3,:))/2)';
             ypos(:,ih) = ypos_d(2,:)';
         end
     elseif strcmpi(version('-release'),'2014b') ||...
            strcmpi(version('-release'),'2017a') ||...
            strcmpi(version('-release'),'2017b')
-        xpos = (cat(1,barc(j).h.XData)+cat(1,barc(j).h.XOffset))';
-        ypos = xss_plot{j};
-            
+        xpos = (cat(1,barfs(j).h.XData)+cat(1,barfs(j).h.XOffset))';
+        ypos = ss_plot{j};
+
     end
-    barc(j).xpos = xpos;
-    barc(j).ypos = ypos;
+    barfs(j).xpos = xpos;
+    barfs(j).ypos = ypos;
 end
 
 % plot error bars
-for j = 1:data.nc
-    set(hfc,'CurrentAxes',ahc(j));
-    errorbar(barc(j).xpos,barc(j).ypos,xss_error{j},'LineStyle','none');
+for j = 1:nplot
+    set(hf,'CurrentAxes',barfs(j).ah);
+    errorbar(barfs(j).xpos,barfs(j).ypos,error_plot{j},'LineStyle','none');
 end
-
-% ahc.XLabel.String = 'WT and Perturbations';
-legend('Noisy Data','Model Estimate');
-
-% fluxes
-hfv = figure;
-ahf = zeros(nf,1);
-barf = struct();
-for k = 1:nf
-    ahf(k) = subplot(nf/2,2,k);    
-    set(ahf(k),'NextPlot','add');
-    barf(k).h = bar(ahf(k),fss_plot{k});
-    [~,ylbl] = getKotteaxislabels(2,1,[1,k]);
-    if strcmpi(version('-release'),'2014a')
-    elseif strcmpi(version('-release'),'2014b') ||...
-           strcmpi(version('-release'),'2017a') ||...
-           strcmpi(version('-release'),'2017b')        
-        ahf(k).YLabel.String = ylbl;  
-        ahf(k).XTickLabel = rel_labels;
-    end
-end
-% collect dsata for error bars
-for k = 1:nf
-    if strcmpi(version('-release'),'2014a')
-        xpos = zeros(size(fss_plot{k},1),length(barf(k).h));
-        ypos = zeros(size(fss_plot{k},1),length(barf(k).h));
-        for ih = 1:length(barf(j).h)
-            xpos_d = get(get(barf(k).h(ih),'children'),'xdata');
-            ypos_d = get(get(barf(k).h(ih),'children'),'ydata');
-            xpos(:,ih) = ((xpos_d(2,:)+xpos_d(3,:))/2)';
-            ypos(:,ih) = ypos_d(2,:)';
-        end
-    elseif strcmpi(version('-release'),'2014b') ||...
-           strcmpi(version('-release'),'2017a') ||...
-           strcmpi(version('-release'),'2017b')
-        xpos = (cat(1,barf(k).h.XData)+cat(1,barf(j).h.XOffset))';
-        ypos = fss_plot{j};
-            
-    end
-    barf(k).xpos = xpos;
-    barf(k).ypos = ypos;
-end
-
-% plot error bars
-for k = 1:nf
-    set(hfv,'CurrentAxes',ahf(k));
-    errorbar(barf(k).xpos,barf(k).ypos,fss_error{k},'LineStyle','none');
-end
-% ahf.XLabel.String = 'WT and Perturbations';
-legend('Noisy Data','Model Estimate');
-allfh = [hfc;hfv];
