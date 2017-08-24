a78d73ae4ceb10e0362972c090f1ab0421ce4d1d
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-24 13:47:22 -0400

    script generating experimental data

---
+++ KineticModel/estimation/noisy_model/gen_data.m
@@ -0,0 +1,19 @@
+% script to generate data (noisy and non-noisy) to be used for analysis 
+% in this git repo estimation and identifiability)
+% generate experimental data - get initial ss
+tspan = 0:0.1:100;
+[xdyn,fdyn,xss1,fss1,opts] = run_nonoise(tspan);
+
+% backup parameters and initial conditions
+ival_bkp = opts.x0;
+odep_bkp = opts.odep;
+
+% generate nsmp samples by adding random noise to ss values
+nsmp = 100;
+[noisy_xss,noisy_fss] = addnoise(repmat(xss1,1,nsmp),odep_bkp);
+
+% perturb system from noisy initial conditions
+pt_sol_id = [1 2 3];
+[exp_sol,noisy_sol] = dopert_noisy(opts,noisy_xss,noisy_fss,odep_bkp,pt_sol_id);
+close all
+
