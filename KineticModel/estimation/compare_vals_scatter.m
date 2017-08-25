43795b0e003c6877deee73935f5b744af35f1af7
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-25 11:36:20 -0400

    scatter plot for single experiment dataset

--- KineticModel/estimation/compare_vals_scatter.m
+++ KineticModel/estimation/compare_vals_scatter.m
@@ -1,87 +1,75 @@
 % scatter plot of experimental and estimated data
 % plot all data in proc_data struct
-function compare_vals_scatter(exp_sol,nexp,opt_xss,calc_xss,nval,nvar,npert,fc)
+function compare_vals_scatter(exp_sol,nexp,opt_xss,calc_xss,data,fc)
 if nargin<8
     fc = 2; % concentrations plots
 end
 
+npert = data.npert;
 % experimental data
-if fc==1
-    [~,exp_fss] = extract_expdata(exp_sol,1,nval,[],nvar,npert);
+if fc==2
+    nvar = data.nc;
+    nval = size(opt_xss,1)/nvar;
+    exp_xss = extract_expdata(exp_sol,1,[],[],nvar,npert);
     % optimal data curation into different conc/var
     xss = cell(nvar,1);
     for j = 1:nvar    
-        xss{j,1} = exp_fss(j:nvar:nvar*nval,:);
+        xss{j,1} = exp_xss(j,:);
         xss{j,2} = opt_xss(j:nvar:nvar*nval,:);
         xss{j,3} = calc_xss(j:nvar:nvar*nval,:);         
     end
-elseif fc==2
-    exp_xss = extract_expdata(exp_sol,1,nval,nvar,[],npert);
+elseif fc==1
+    nvar = data.nflx;
+    nval = size(opt_xss,1)/nvar;
+    [~,exp_fss] = extract_expdata(exp_sol,1,[],nvar,[],npert);
     xss = cell(nvar,1);
     for j = 1:nvar    
-        xss{j,1} = exp_xss(j:nvar:nvar*nval,:);
+        xss{j,1} = exp_fss(j:nvar:nvar*nval,:);
         xss{j,2} = opt_xss(j:nvar:nvar*nval,:);
         xss{j,3} = calc_xss(j:nvar:nvar*nval,:);        
     end
 end
 
 % plot scatter of xss{j,1} vs xss{j,2} and xss{j,3}
+plotdots(nvar,xss(:,1),xss(:,2),xss(:,3),fc);
+    
 
-
-
-
-
-
-test_data_id = logical(test_data_id);
-all_labels = {'P1','P2','P3','WT'};
-
-% calculate new model fluxes
-% before perturbation - wt fluxes
-% assume wt (unperturbed) data is always included at the end
-wt_exp_xss = exp_sol(end).xss;
-wt_exp_fss = kotte_flux_noCAS(wt_exp_xss,data.odep);
-
-% after perturbation - perturb parameters to ascertain fluxes
-pt_datasize = length(find(test_data_id(1:end-1)));
-nf = size(wt_exp_fss,1);
-
-
-
-% collect all data to plot [p#1 p#2....  wt]  
-% estimated
-est_xss = est_sol.xss;
-est_xerr = est_sol.xerr;
-est_fss = est_sol.fss;
-est_ferr = est_sol.ferr;
-
-if ~test_data_id(end) % wt not included in estimation
-    rel_labels = all_labels(test_data_id);
-else
-    % experimental
-    exp_xss = exp_xss(:,test_data_id);
-    exp_fss = exp_fss(:,test_data_id);
-    % axis labels
-    rel_labels = [all_labels(test_data_id) all_labels(end)];
-end
-nplot = pt_datasize+1;
-for j = 1:data.nc
-    xss_plot{j} = [exp_xss(j,:)' est_xss(j,:)'];
-    xss_error{j} = [zeros(1,length(exp_xss(j,:)))' est_xerr(j,:)'];
+function [hf,dotfs] = plotdots(nplot,ss_x,ss_y1,ss_y2,fc)
+if nargin<5
+    fc = 2;
 end
 
-for k = 1:nf
-    fss_plot{k} = [exp_fss(k,:)' est_fss(k,:)'];
-    fss_error{k} = [zeros(1,length(exp_fss(k,:)))' est_ferr(k,:)'];
+hf = figure;
+dotfs = struct();
+for j = 1:nplot
+    if fc==2
+        dotfs(j).ah = subplot(nplot,1,j);        
+    elseif fc==1
+        dotfs(j).ah = subplot(nplot/2,2,j);            
+    end
+    set(dotfs(j).ah,'NextPlot','add');
+%     line(dotfs(j).ah,ss_x{j},ss_x{j},'Color','k','LineWidth',2);
+    if ~isempty(ss_y1{j})        
+        dotfs(j).h = line(dotfs(j).ah,ss_x{j},ss_y1{j},...
+                          'LineStyle','none','Marker','.',...
+                          'MarkerSize',18);
+    end
+    if ~isempty(ss_y2{j})
+        dotfs(j).h = line(dotfs(j).ah,ss_x{j},ss_y2{j},...
+                          'LineStyle','none','Marker','.',...
+                          'MarkerSize',18,'Color','r');
+    end
+    [~,ylbl] = getKotteaxislabels(2,fc,[1,j]);
+    if strcmpi(version('-release'),'2014a')
+    elseif strcmpi(version('-release'),'2014b') ||...
+           strcmpi(version('-release'),'2017a') ||...
+           strcmpi(version('-release'),'2017b')
+
+        dotfs(j).ah.YLabel.String = ylbl{1};    
+%         ah(j).XTickLabel = rel_labels;  
+    end    
 end
 
-hfc = figure;
-for j = 1:data.nc
-    set(gca,'NextPlot','add');    
-    line(xss_plot{j}(:,1),xss_plot{j}(:,1),'Color','k','LineWidth',2)
-    line(xss_plot{j}(:,1),xss_plot{j}(:,2),'LineStyle','none',...
-                                            'Marker','.',...
-                                            'MarkerSize',16);
-end
-% overlay error bars
+
 
 
