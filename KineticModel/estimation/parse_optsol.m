0520e522773fe0d4142505023bf46cf7c5d9aaa8
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-28 12:43:14 -0400

    use parameter scaling if available

--- KineticModel/estimation/parse_optsol.m
+++ KineticModel/estimation/parse_optsol.m
@@ -3,6 +3,11 @@ function new_optsol = parse_optsol(optsol,data)
 
 nval = size(optsol,2);
 new_optsol = optsol;
+if isfield(data,'pscale')
+    pscale = data.pscale;
+else
+    pscale = [];
+end
 if nval>1
     xval = cat(2,optsol.xval);
     [xconc,xpar,xflx,xnoise] = parsesolvec(xval,data);
@@ -13,8 +18,12 @@ end
 % reshape and add to structure array
 for ival = 1:nval
     new_optsol(ival).xss = reshape(xconc(:,ival),[data.nc,data.npert]);
-    new_optsol(ival).xpar = xpar(:,ival);
-    new_optsol(ival).fss = xflx(:,ival)';
+    if ~isempty(pscale)
+        new_optsol(ival).xpar = xpar(:,ival)./pscale;
+    else
+        new_optsol(ival).xpar = xpar(:,ival);
+    end
+    new_optsol(ival).fss = reshape(xflx(:,ival),[data.nf,data.npert]);
     if ~isempty(xnoise)
         new_optsol(ival).eps_cf = xnoise(:,ival);
     end
