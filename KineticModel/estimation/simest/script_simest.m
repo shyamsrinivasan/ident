ec8ba17dc308f0f4061dedc014702c80e81d7af3
shyamsrinivasan <shyamsweth@gmail.com>
2017-08-28 18:06:53 -0400

    figures and figure saves+working version of weights

--- KineticModel/estimation/simest/script_simest.m
+++ KineticModel/estimation/simest/script_simest.m
@@ -28,7 +28,7 @@ p0_obj = odep_bkp(1:13)';
 scale = ones(np,1);
 scale(3) = 1e-6;
 
-weigths = [1;1;1;1];
+weigths = [1;1;1;100];
 p0_const = odep_bkp(14:end)';
 par_val = [p0_const;weigths];
 
@@ -55,6 +55,7 @@ lb(nvar-2+1:nvar-1) = 0;
 ub(nvar-2+1:nvar-1) = 1;
 lb(nvar-1+1:nvar) = 0;
 ub(nvar-1+1:nvar) = 1;
+
 % set bounds for cons
 lbg = ones(ncons,1);
 ubg = ones(ncons,1);
@@ -70,14 +71,15 @@ ubg = ones(ncons,1);
 % % nl noisy flux cons (<=)
 % lbg(nc*npert+nf*npert+2*nc*npert+1:nc*npert+nf*npert+2*nc*npert+2*nf*npert) = -Inf;
 % ubg(nc*npert+nf*npert+2*nc*npert+1:nc*npert+nf*npert+2*nc*npert+2*nf*npert) = 0;
+
 % set flux cons bounds type b
 lbg(1:nf*npert) = 0;
-ubg(nc*npert+1:nc*npert+nf*npert) = 0;
+ubg(1:nf*npert) = 0;
 % nl noisy conc cons (<=)
 lbg(nf*npert+1:nf*npert+2*nc*npert) = -Inf;
 ubg(nf*npert+1:nf*npert+2*nc*npert) = 0;
 % nl noisy flux cons (<=)
-lbg(nf*npert+2*nc*npert+1:nf*npert+2*nc*npert+2*nf*npert) = -Inf;
+lbg(nf*npert+2*nc*npert+1:nf*npert+2*nc*npert+2*nf*npert) = -1;
 ubg(nf*npert+2*nc*npert+1:nf*npert+2*nc*npert+2*nf*npert) = 0;
 
 estprob = struct('x',var,'f',obj,'g',cons,'p',par);
@@ -85,8 +87,190 @@ options.warn_initial_bounds = 1;
 options.ipopt.fixed_variable_treatment = 'make_constraint';
 solver = casadi.nlpsol('solver','ipopt',estprob,options);
 sol = solver('x0',x0,'p',par_val,'lbx',lb,'ubx',ub,'lbg',lbg,'ubg',ubg);
+optsol_sim.xval = full(sol.x);
+optsol_sim.fval = full(sol.f);
+optsol_sim.exitflag = 1;
 
-xopt = full(sol.x);
+%% data processing and figures
+data = struct('nc',nc,'nf',nf,'nflx',nf,'nvar',nvar,'npert',npert,...
+              'np',np,'type',2,'odep',odep_bkp,'p_id',1:13,...
+              'pscale',scale,'flxid',1);
+% exp_data = exp_sol{1};
+opts.tspan = 0:.1:600;
+[proc_data,exp_data] = recalcss(optsol_sim,noisy_sol{1},[],data,opts);
+
+%% figures
+exp_xss = cat(2,noisy_sol{1}.xss);
+pep = [proc_data.opt_xss(1,:)' proc_data.calc_xss(1,:)' exp_xss(1,:)'];
+fdp = [proc_data.opt_xss(2,:)' proc_data.calc_xss(2,:)' exp_xss(2,:)'];
+enz = [proc_data.opt_xss(3,:)' proc_data.calc_xss(3,:)' exp_xss(3,:)'];
+
+exp_fss = cat(2,noisy_sol{1}.fss);
+f1 = [proc_data.opt_fss(1,:)' proc_data.calc_fss(1,:)' exp_fss(1,:)'];
+f3 = [proc_data.opt_fss(3,:)' proc_data.calc_fss(3,:)' exp_fss(3,:)'];
+f4 = [proc_data.opt_fss(4,:)' proc_data.calc_fss(4,:)' exp_fss(4,:)'];
+f5 = [proc_data.opt_fss(5,:)' proc_data.calc_fss(5,:)' exp_fss(5,:)'];
+
+% bar plot
+hf1 = figure;
+bar(pep);
+ylabel('pep a.u.');
+legend('Optimal Value','Calculated Value','Noisy Data');
+hf2=figure;
+bar(fdp);
+ylabel('fdp a.u.');
+legend('Optimal Value','Calculated Value','Noisy Data');
+hf3=figure;
+bar(enz);
+ylabel('E a.u.');
+legend('Optimal Value','Calculated Value','Noisy Data');
+
+hf4 = figure;
+bar(f1);
+ylabel('v1 a.u.');
+legend('Optimal Value','Calculated Value','Noisy Data');
+hf5=figure;
+bar(f3);
+ylabel('v2 a.u.');
+legend('Optimal Value','Calculated Value','Noisy Data');
+hf6=figure;
+bar(f4);
+ylabel('v3 a.u.');
+legend('Optimal Value','Calculated Value','Noisy Data');
+bar(f5);
+ylabel('v4 a.u.');
+legend('Optimal Value','Calculated Value','Noisy Data');
+
+% scatter plots
+hf7 = figure;
+hold on
+plot(pep(:,end),pep(:,end),'LineWidth',1.5,'Color','k');
+plot(pep(:,end),pep(:,1),'LineStyle','none','Marker','.','MarkerSize',20,'Color','b');
+plot(pep(:,end),pep(:,2),'LineStyle','none','Marker','.','MarkerSize',12,'Color','r');
+xlabel('Experimental pep a.u.');
+ylabel('Optimal/ODE Sim pep a.u.');
+legend('Noisy Data Line','Optimal Value','Calculated Value');
+
+hf8 = figure;
+hold on
+plot(fdp(:,end),fdp(:,end),'LineWidth',1.5,'Color','k');
+plot(fdp(:,end),fdp(:,1),'LineStyle','none','Marker','.','MarkerSize',20,'Color','b');
+plot(fdp(:,end),fdp(:,2),'LineStyle','none','Marker','.','MarkerSize',12,'Color','r');
+xlabel('Experimental fdp a.u.');
+ylabel('Optimal/ODE Sim fdp a.u.');
+legend('Noisy Data Line','Optimal Value','Calculated Value');
+
+hf9=figure;
+hold on
+plot(enz(:,end),enz(:,end),'LineWidth',1.5,'Color','k');
+plot(enz(:,end),enz(:,1),'LineStyle','none','Marker','.','MarkerSize',20,'Color','b');
+plot(enz(:,end),enz(:,2),'LineStyle','none','Marker','.','MarkerSize',12,'Color','r');
+xlabel('Experimental E a.u.');
+ylabel('Optimal/ODE Sim E a.u.');
+legend('Noisy Data Line','Optimal Value','Calculated Value');
+
+hf10=figure;
+hold on
+plot(f1(:,end),f1(:,end),'LineWidth',1.5,'Color','k');
+plot(f1(:,end),f1(:,1),'LineStyle','none','Marker','.','MarkerSize',20,'Color','b');
+plot(f1(:,end),f1(:,2),'LineStyle','none','Marker','.','MarkerSize',12,'Color','r');
+xlabel('Experimental v1 a.u.');
+ylabel('Optimal/ODE Sim v1 a.u.');
+legend('Noisy Data Line','Optimal Value','Calculated Value');
+
+hf11=figure;
+hold on
+plot(f3(:,end),f3(:,end),'LineWidth',1.5,'Color','k');
+plot(f3(:,end),f3(:,1),'LineStyle','none','Marker','.','MarkerSize',20,'Color','b');
+plot(f3(:,end),f3(:,2),'LineStyle','none','Marker','.','MarkerSize',12,'Color','r');
+xlabel('Experimental v2 a.u.');
+ylabel('Optimal/ODE Sim v2 a.u.');
+legend('Noisy Data Line','Optimal Value','Calculated Value');
+
+hf12=figure;
+hold on
+plot(f4(:,end),f4(:,end),'LineWidth',1.5,'Color','k');
+plot(f4(:,end),f4(:,1),'LineStyle','none','Marker','.','MarkerSize',20,'Color','b');
+plot(f4(:,end),f4(:,2),'LineStyle','none','Marker','.','MarkerSize',12,'Color','r');
+xlabel('Experimental v3 a.u.');
+ylabel('Optimal/ODE Sim v3 a.u.');
+legend('Noisy Data Line','Optimal Value','Calculated Value');
+
+%% save figure files
+dir = 'C:\Users\shyam\Documents\Courses\CHE1125Project\Results\estimation\simest\typeb\';
+set(0,'CurrentFigure',hf1);
+fname = 'simest_noisy_pep_aug28';
+print([dir fname],'-depsc','-painters','-loose','-tiff','-r200');
+print([dir fname],'-dpng','-r200');
+close(hf1);
+set(0,'CurrentFigure',hf2);
+fname = 'simest_noisy_fdp_aug28';
+print([dir fname],'-depsc','-painters','-loose','-tiff','-r200');
+print([dir fname],'-dpng','-r200');
+close(hf2);
+set(0,'CurrentFigure',hf3);
+fname = 'simest_noisy_enz_aug28';
+print([dir fname],'-depsc','-painters','-loose','-tiff','-r200');
+print([dir fname],'-dpng','-r200');
+close(hf3);
+set(0,'CurrentFigure',hf4);
+fname = 'simest_noisy_v1_aug28';
+print([dir fname],'-depsc','-painters','-loose','-tiff','-r200');
+print([dir fname],'-dpng','-r200');
+close(hf4);
+set(0,'CurrentFigure',hf5);
+fname = 'simest_noisy_v2_aug28';
+print([dir fname],'-depsc','-painters','-loose','-tiff','-r200');
+print([dir fname],'-dpng','-r200');
+close(hf5);
+set(0,'CurrentFigure',hf6);
+fname = 'simest_noisy_v3_aug28';
+print([dir fname],'-depsc','-painters','-loose','-tiff','-r200');
+print([dir fname],'-dpng','-r200');
+close(hf6);
+set(0,'CurrentFigure',hf7);
+fname = 'simest_noisy_pep_scatter_aug28';
+print([dir fname],'-depsc','-painters','-loose','-tiff','-r200');
+print([dir fname],'-dpng','-r200');
+close(hf7);
+set(0,'CurrentFigure',hf8);
+fname = 'simest_noisy_fdp_scatter_aug28';
+print([dir fname],'-depsc','-painters','-loose','-tiff','-r200');
+print([dir fname],'-dpng','-r200');
+close(hf8);
+set(0,'CurrentFigure',hf9);
+fname = 'simest_noisy_enz_scatter_aug28';
+print([dir fname],'-depsc','-painters','-loose','-tiff','-r200');
+print([dir fname],'-dpng','-r200');
+close(hf9);
+set(0,'CurrentFigure',hf10);
+fname = 'simest_noisy_v1_scatter_aug28';
+print([dir fname],'-depsc','-painters','-loose','-tiff','-r200');
+print([dir fname],'-dpng','-r200');
+close(hf10);
+set(0,'CurrentFigure',hf11);
+fname = 'simest_noisy_v2_scatter_aug28';
+print([dir fname],'-depsc','-painters','-loose','-tiff','-r200');
+print([dir fname],'-dpng','-r200');
+close(hf11);
+set(0,'CurrentFigure',hf12);
+fname = 'simest_noisy_v3_scatter_aug28';
+print([dir fname],'-depsc','-painters','-loose','-tiff','-r200');
+print([dir fname],'-dpng','-r200');
+close(hf12);
+
+
+
+
+
+
+
+
+
+
+% hfcv = compare_vals(proc_data,noisy_sol{1},[],data,1);
+
+% xopt = full(sol.x);
 
 
 % x = casadi.SX.sym('x',1);
