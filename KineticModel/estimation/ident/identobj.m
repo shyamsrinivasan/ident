29577daeac15fd64a7dd0a92ae88c75dc10698c5
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-06 17:56:18 -0400

    arrange parameters for input to ode/nlae soln

--- KineticModel/estimation/ident/identobj.m
+++ KineticModel/estimation/ident/identobj.m
@@ -6,11 +6,11 @@ end
 if isfield(data,'ident_idx')
     idx = data.ident_idx;
 else
-    idx = 13;
+    idx = 14;
 end
-% if isfield(data,'odep')
-%     odep = data.odep;
-% end
+if isfield(data,'odep')
+    odep = data.odep;
+end
 if isfield(data,'tspan')
     tspan = data.tspan;
 end
@@ -19,6 +19,9 @@ if isfield(data,'nc')
 end
 if isfield(data,'npert')
     npert = data.npert;
+end
+if isfield(data,'nunpert')
+    nunpert = data.nunpert;
 end
 if isfield(data,'nlaefh')
     nlaefh = data.nlaefh;
@@ -30,12 +33,21 @@ if isfield(data,'odefh')
 else
     casfh = @kotteCAS_pert;
 end
-% p_useless = data.odep(14:16)';
-p_pert = reshape(data.p_pert,3*npert,1);
-p_pert(14-idx) = ident_c;
 
-ac = data.odep(end);
-p_all = [xvar(1:idx-1);ident_c;xvar(idx:end);ac;p_pert];
+p_unch = xvar(1:nunpert);
+if idx<nunpert
+    p_pert = xvar(nunpert+1:nunpert+3*npert);
+    p_all = [p_unch(1:idx-1);ident_c;p_unch(idx:end);odep(end);p_pert];
+elseif idx>nunpert
+    p_pert(end-(14-idx):-2:1) = ident_c;
+    p_all = [p_unch;ac;p_pert];
+end
+
+% p_pert = reshape(data.p_pert,3*npert,1);
+% p_pert(14-idx) = ident_c;
+
+% ac = data.odep(end);
+% p_all = [xvar(1:idx-1);ident_c;xvar(idx:end);ac;p_pert];
 % dynamic case
 options = struct('grid',tspan,'output_t0',1,'print_stats',1);
 ymodel = solve_model_ode(casfh,nc,npert,xinit,p_all,options);
