38cb25ea92b84f9c3501d66fc5231237be5a103f
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-06 17:16:45 -0400

    add extra field p_pert for perturbed parameter values

--- KineticModel/estimation/parseperturbations.m
+++ KineticModel/estimation/parseperturbations.m
@@ -4,6 +4,11 @@ if length(use_pert)<size(sol,2)
     newsol = sol(use_pert);
     exp_sol.xss = cat(2,newsol.xss);
     exp_sol.fss = cat(2,newsol.fss);
+    p_pert = cat(1,newsol.odep);
+    p_pert = p_pert(:,11:13);
+    exp_sol.p_pert = p_pert';
+    p_pert = mat2cell(p_pert,ones(1,size(p_pert,1)));
+    [newsol(:).p_pert] = p_pert{:};
     if isfield(newsol,'xdyn')
         if ismember(size(sol,2),use_pert)
             % for wt - use final ss value all along the time horizon
