a19d8ca270d977ab6011f5a3da952d217d9fec1c
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-07 10:09:24 -0400

    add p_pert and p_pert_logical

--- KineticModel/estimation/parseperturbations.m
+++ KineticModel/estimation/parseperturbations.m
@@ -1,21 +1,30 @@
 function [exp_sol,newsol] = parseperturbations(sol,use_pert)
 
-if length(use_pert)<size(sol,2)
+npert = size(sol,2);
+npert_used = length(use_pert);
+if npert_used<npert
     newsol = sol(use_pert);
     exp_sol.xss = cat(2,newsol.xss);
     exp_sol.fss = cat(2,newsol.fss);
     p_pert = cat(1,newsol.odep);
-    p_pert = p_pert(:,11:13);
+    p_wt = repmat(p_pert(end,11:13),npert_used,1);    
+    p_pert = p_pert(:,11:13);    
+    p_pert_logical = (p_pert~=p_wt);
+    % convert all wt parameters to 1
+    p_pert_logical(all(p_pert_logical(:,:)'==0)',:) = 1;
+    exp_sol.p_pert_logical = p_pert_logical';
     exp_sol.p_pert = p_pert';
-    p_pert = mat2cell(p_pert,ones(1,size(p_pert,1)));
+    p_pert = mat2cell(p_pert,ones(1,npert_used));
+    p_pert_logical = mat2cell(p_pert_logical,ones(1,npert_used));
     [newsol(:).p_pert] = p_pert{:};
+    [newsol(:).p_pert_logical] = p_pert_logical{:};
     if isfield(newsol,'xdyn')
-        if ismember(size(sol,2),use_pert)
+        if ismember(npert,use_pert)
             % for wt - use final ss value all along the time horizon
-            wt_xdyn = newsol(use_pert==size(sol,2)).xdyn;
+            wt_xdyn = newsol(use_pert==npert).xdyn;
             wt_xdyn_ss = wt_xdyn(:,end);
             t_freq = size(wt_xdyn,2);
-            newsol(use_pert==size(sol,2)).xdyn = repmat(wt_xdyn_ss,1,t_freq);
+            newsol(use_pert==npert).xdyn = repmat(wt_xdyn_ss,1,t_freq);
         end        
         exp_sol.xdyn = cat(1,newsol.xdyn);
     end
