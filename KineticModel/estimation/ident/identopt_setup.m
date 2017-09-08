f409eee48484cc59c29952e615e4964a334033d5
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-08 17:35:46 -0400

    get and use fluxes as var in optim prob

--- KineticModel/estimation/ident/identopt_setup.m
+++ KineticModel/estimation/ident/identopt_setup.m
@@ -16,6 +16,9 @@ end
 if isfield(data,'xinit')
     xinit = data.xinit;
 end
+if isfield(data,'yinit')
+    yinit = data.yinit;
+end    
 if isfield(data,'xexp')
     xexp = data.xexp;    
 end
@@ -47,16 +50,18 @@ intfun = str2func(intfun);
 [xdyn_fun,~,x,p,ident_c,p_useless,acetate] = intfun(casmodelf,data);
 
 % final symbolic expression to be used during optimization
-x_sym =...
+[x_sym,y_sym] =...
 xdyn_fun(xinit,repmat(p,1,npts),fixed_pvalue,repmat(data.odep(14:16)',1,npts),.1);
 
 % x_sym = x_sym; % [sum(x_sym(1:3,:));sum(x_sym(4:6,:));sum(x_sym(7:9,:))];
 % add initial value
 x_sym = [casadi.DM(xinit) x_sym];
+y_sym = [casadi.DM(yinit) y_sym];
 % y_sym = [casadi.DM([sum(xinit(1:3,:));sum(xinit(4:6,:));sum(xinit(7:9,:))]) y_sym];
 % choose only points present in experimental data
 % x_model_sym = x_sym(:,freq);
-y_model_sym = x_sym(1:2,freq);
+% y_model_sym = [x_sym(1:2,freq);y_sym([1 3 4 5],freq)];
+y_model_sym = y_sym([1 3 4 5],freq);
 % create nlsqopt objective function
 % x_error = (xexp-x_model_sym);
 y_error = (yexp-y_model_sym);
