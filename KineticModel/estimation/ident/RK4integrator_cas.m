e5ee3869f4b3eb77158456e83e14e61f881ba6a8
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-08 17:34:09 -0400

    include y = output var = flux as 2nd output arg in casadi fun

--- KineticModel/estimation/ident/RK4integrator_cas.m
+++ KineticModel/estimation/ident/RK4integrator_cas.m
@@ -13,7 +13,7 @@ if isfield(data,'ident_idx')
     ident_idx = data.ident_idx;
 end
 
-[ode,~,~,~,x,p_all,ident_c,p_useless,acetate] = casfh(ident_idx,data.nc,data.nf,data.npert);
+[ode,flux,~,~,x,p_all,ident_c,p_useless,acetate] = casfh(ident_idx,data.nc,data.nf,data.npert);
 % [ode,~,~,fx,x,p] = kotte_CAS();
 
 % RK4 integrator
@@ -23,22 +23,13 @@ k2 = ode(x+dt/2.0*k1,p_all,ident_c,p_useless,acetate);
 k3 = ode(x+dt/2.0*k2,p_all,ident_c,p_useless,acetate);
 k4 = ode(x+dt*k3,p_all,ident_c,p_useless,acetate);
 xfinal = x+dt/6.0*(k1+2*k2+2*k3+k4);
-yfinal = xfinal;
+yfinal = flux(xfinal,p_all,ident_c,p_useless,acetate);
 
 xstate_one_step =...
-casadi.Function('xstate_one_step',{x,p_all,ident_c,p_useless,acetate},{yfinal});
+casadi.Function('xstate_one_step',{x,p_all,ident_c,p_useless,acetate},{xfinal,yfinal});
 
-xstate = xstate_one_step(x,p_all,ident_c,p_useless,acetate);
+[xstate,ystate] = xstate_one_step(x,p_all,ident_c,p_useless,acetate);
 xstate_onepoint =...
-casadi.Function('xstate_onepoint',{x,p_all,ident_c,p_useless,acetate},{xstate});
+casadi.Function('xstate_onepoint',{x,p_all,ident_c,p_useless,acetate},{xstate,ystate});
 xstate_onepoint = xstate_onepoint.expand();
-
 xdyn_fun = xstate_onepoint.mapaccum('all_samples',npts);
-
-% yfinal = sum(x(1:3));sum(x(4:6));sum(x(7:9))
-
-% xdyn = all_samples(opts.x0,repmat(opts.odep(2:end)',1,3000));
-
-
-
-
