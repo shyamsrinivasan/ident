772b097db3a4aa394a91897cd3104d98a3e119cf
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-08 11:40:40 -0400

    bounds when perturbed parameters are optim var

--- KineticModel/estimation/ident/ident_bounds.m
+++ KineticModel/estimation/ident/ident_bounds.m
@@ -1,15 +1,19 @@
-function [lb,ub] = ident_bounds(np)
-
-lb = zeros(np,1);
-ub = zeros(np,1);
+function [lb,ub] = ident_bounds(np,lb,ub)
+if nargin<3
+    ub = zeros(np,1);
+end
+if nargin<2
+    lb = zeros(np,1);
+end
 
 % k1cat - PLE
 lb(:) = .01;
 lb(3) = 1;
 lb(10) = .05;
-lb(11) = .1;
-lb(12) = .1;
+% lb(11) = .1;
+% lb(12) = .1;
 
+ub(:) = 3;
 ub(1) = 2;
 ub(2) = 2;
 ub(3) = 5;
@@ -20,5 +24,5 @@ ub(7) = 4;
 ub(8) = 4;
 ub(9) = 1;
 ub(10) = 1;
-ub(11) = 2;
-ub(12) = 2;
+% ub(11) = 2;
+% ub(12) = 2;
