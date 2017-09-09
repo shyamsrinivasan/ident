17e3a492e9ea74b913b49e62fe0375d6c21263fd
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-09 10:28:51 -0400

    add exceptions when thresholds not given

--- KineticModel/estimation/ident/plotPLE.m
+++ KineticModel/estimation/ident/plotPLE.m
@@ -2,17 +2,31 @@
 % ci_df1 - point wise CI
 % ci_dfall - full dof CI
 function plotPLE(PLEval,ci_df1,ci_df_all)
+if nargin<3
+    ci_df_all = [];
+end
+if nargin<2
+    ci_df1 = [];
+end
 
 if isfield(PLEval,'thetai_inc')
     thetai = PLEval.thetai_inc;
 end
-if isfield(PLEeval,'chi2PLE')
-    chi2PLE = PLEval.chi2PLE;
+if isfield(PLEval,'chiPLE')
+    chi2PLE = PLEval.chiPLE;
 end
 
 npts = length(thetai);
 figure
 line(log10(thetai),chi2PLE,'LineStyle','-','LineWidth',1.5,'Color','k');
 hold on
-line(log10(thetai),repmat(ci_df1,1,npts),'LineStyle','-.','LineWidth',2,'Color','k');
-line(log10(thetai),repmat(ci_df_all,1,npts),'LineStyle','-.','LineWidth',2,'Color','k');
+if ~isempty(ci_df1)
+    line(log10(thetai),repmat(ci_df1,1,npts),'LineStyle','-.',...
+                                            'LineWidth',2,...
+                                            'Color','k');
+end
+if ~isempty(ci_df_all)
+    line(log10(thetai),repmat(ci_df_all,1,npts),'LineStyle','-.',...
+                                                'LineWidth',2,...
+                                                'Color','k');
+end
