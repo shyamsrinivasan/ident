b25b1e3e3a36a3d33f1eb6bc3140c09fb3008820
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-11 13:12:03 -0400

    fix transcriptional parameters as const

--- KineticModel/estimation/ident/kotteCASident.m
+++ KineticModel/estimation/ident/kotteCASident.m
@@ -1,13 +1,12 @@
-function [ode,flux,D2FX,oderhs,x,p_all,p_useless,acetate] =...
+function [ode,flux,D2FX,oderhs,x,p_all,p_other,acetate] =...
         kotteCASident(nc)
 
 x = casadi.SX.sym('x',nc,1);
-p_all = casadi.SX.sym('p_all',13,1);
-% ident_c = casadi.SX.sym('ident_c',1,1);
-p_useless = casadi.SX.sym('p_useless',3,1);
+p_all = casadi.SX.sym('p_all',9,1);
+p_other = casadi.SX.sum('p_other',4,1);
 acetate = casadi.SX.sym('acetate',1,1);
 
-p = [p_all;p_useless;acetate];
+p = [p_all;p_other;acetate];
 
 flux = cell(6,1);
 
@@ -17,15 +16,15 @@ K3fdp = p(2);
 L3fdp = p(3)*1e6;
 K3pep = p(4);
 K2pep = p(5);
-vemax = p(6);        % for bifurcation analysis: 0.7:0.1:1.3
-KeFDP = p(7);        % or 0.45
-ne = p(8);             % or 2
-d = p(9);
-V4max = p(10);
-k1cat = p(11);   
-V3max = p(12);    
-V2max = p(13);   
-ac = p(17);
+V4max = p(6);
+k1cat = p(7);   
+V3max = p(8);    
+V2max = p(9);
+vemax = p(10);        % for bifurcation analysis: 0.7:0.1:1.3
+KeFDP = p(11);        % or 0.45
+ne = p(12);             % or 2
+d = p(13);
+ac = p(14);
 
 flux{1} = k1cat.*x(3).*ac./(ac+K1ac);
 flux{2} = vemax.*(1-1./(1+(KeFDP./x(2)).^ne));
@@ -39,10 +38,10 @@ flux{6} = d.*x(3);
 oderhs = [flux{1} - flux{4} - flux{5};...
           flux{4} - flux{3};...
           flux{2} - flux{6}];
-ode = casadi.Function('ode',{x,p_all,p_useless,acetate},{oderhs}); 
+ode = casadi.Function('ode',{x,p_all,p_other,acetate},{oderhs}); 
 
 fluxeq = [flux{1};flux{2};flux{3};flux{4};flux{5};flux{6}];
-flux = casadi.Function('flux',{x,p_all,p_useless,acetate},{fluxeq});
+flux = casadi.Function('flux',{x,p_all,p_other,acetate},{fluxeq});
 D2FX = [];
 
 
