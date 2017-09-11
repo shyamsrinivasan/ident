ff6590083b63ebb09fbc5f6af3e9b4afd250a00d
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-11 13:14:49 -0400

    remove acetate from plist and change p to p_var

--- KineticModel/estimation/ident/mle/mle_setup.m
+++ KineticModel/estimation/ident/mle/mle_setup.m
@@ -32,7 +32,7 @@ data.npts = npts;
 
 if ~isfield(data,'ident_idx') && ~isempty(pname)    
     plist = {'K1ac','K3fdp','L3fdp','K3pep','K2pep',...
-            'V4max','k1cat','V3max','V2max','acetate'}; 
+            'V4max','k1cat','V3max','V2max'}; 
     ident_idx = find(strcmpi(plist,pname));    
     data.ident_idx = ident_idx;
 elseif ~isfield(data,'ident_idx') && isempty(pname)    
@@ -40,37 +40,37 @@ elseif ~isfield(data,'ident_idx') && isempty(pname)
 end   
 
 % create CAS function with custom integrator of nlsq opt
-[ode,flux,~,~,x,p,p_useless,acetate] = casfh(data.nc);
+[ode,flux,~,~,x,p_var,p_other,acetate] = casfh(data.nc);
 % [ode,~,~,fx,x,p] = kotte_CAS();
 
 % RK4 integrator
 dt = tspan(end)/npts;
-k1 = ode(x,p,p_useless,acetate);
-k2 = ode(x+dt/2.0*k1,p,p_useless,acetate);
-k3 = ode(x+dt/2.0*k2,p,p_useless,acetate);
-k4 = ode(x+dt*k3,p,p_useless,acetate);
+k1 = ode(x,p_var,p_other,acetate);
+k2 = ode(x+dt/2.0*k1,p_var,p_other,acetate);
+k3 = ode(x+dt/2.0*k2,p_var,p_other,acetate);
+k4 = ode(x+dt*k3,p_var,p_other,acetate);
 xfinal = x+dt/6.0*(k1+2*k2+2*k3+k4);
-yfinal = flux(xfinal,p,p_useless,acetate);
+yfinal = flux(xfinal,p_var,p_other,acetate);
 
 xstate_one_step =...
-casadi.Function('xstate_one_step',{x,p,p_useless,acetate},{xfinal,yfinal});
+casadi.Function('xstate_one_step',{x,p_var,p_other,acetate},{xfinal,yfinal});
 
-[xstate,ystate] = xstate_one_step(x,p,p_useless,acetate);
+[xstate,ystate] = xstate_one_step(x,p_var,p_other,acetate);
 xstate_onepoint =...
-casadi.Function('xstate_onepoint',{x,p,p_useless,acetate},{xstate,ystate});
+casadi.Function('xstate_onepoint',{x,p_var,p_other,acetate},{xstate,ystate});
 xstate_onepoint = xstate_onepoint.expand();
 xdyn_fun = xstate_onepoint.mapaccum('all_samples',npts);
 
 % intfun = str2func(intfun);
 % [xdyn_fun,~,x,p,ident_c,p_useless,acetate] = intfun(casmodelf,data);
 % p_all = [p(1:ident_idx-1);ident_c;p(ident_idx:end)]; % ;
-p_fixed = [p_useless;acetate];
+p_fixed = [p_other;acetate];
 
 % final symbolic expression to be used during optimization
 % [x_sym,y_sym] =...
 % xdyn_fun(xinit,repmat(p,1,npts),repmat(data.odep(14:16)',1,npts),.1);
 [x_sym,y_sym] =...
-xdyn_fun(xinit,repmat(p,1,npts),repmat(p_useless,1,npts),acetate);
+xdyn_fun(xinit,repmat(p_var,1,npts),repmat(p_other,1,npts),acetate);
 
 % add initial value
 x_sym = [casadi.DM(xinit) x_sym];
@@ -82,14 +82,14 @@ y_error = sum(y_error,2);
 obj = .5*dot(y_error,y_error);
 
 % define obj as function to be used outside casadi implementation of ipopt
-objfun = casadi.Function('objfun',{p,p_fixed,x},{obj});
+objfun = casadi.Function('objfun',{p_var,p_fixed,x},{obj});
 objfh = @(x)full(objfun(x,data.odep(14:17)',xinit));
 % input for jac fun is same as objfun (function whose jacobian is calculated)
 gradfun = jacobian(objfun); % this generates a function for jacobian
 gradfh = @(x)full(gradfun(x,data.odep(14:17)',xinit));
 
 % bounds for mle estimate of all 13 parameters for given input (acetate)
-[lb,ub] = ident_bounds_mle(length(p));
+[lb,ub] = ident_bounds_mle(length(p_var));
 hessfun = [];
 
 prob = struct('objfun',objfh,'grad',gradfh,'hess',hessfun,...
