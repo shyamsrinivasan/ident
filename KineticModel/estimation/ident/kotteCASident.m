df448aa2c07480f7110631795a61de03e4ffc1e9
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-12 11:12:32 -0400

    convert SX to MX symbolics

--- KineticModel/estimation/ident/kotteCASident.m
+++ KineticModel/estimation/ident/kotteCASident.m
@@ -1,10 +1,10 @@
 function [ode,flux,D2FX,oderhs,x,p_var,p_other,acetate] =...
         kotteCASident(nc)
 
-x = casadi.SX.sym('x',nc,1);
-p_var = casadi.SX.sym('p_all',9,1);
-p_other = casadi.SX.sym('p_other',4,1);
-acetate = casadi.SX.sym('acetate',1,1);
+x = casadi.MX.sym('x',nc,1);
+p_var = casadi.MX.sym('p_all',9,1);
+p_other = casadi.MX.sym('p_other',4,1);
+acetate = casadi.MX.sym('acetate',1,1);
 
 p_all = [p_var;p_other;acetate];
 
