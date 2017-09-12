eee0bc0108c24eb11d5d2f8118aaf563b6b7ff04
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-12 15:29:55 -0400

    get normally distributed random noise for yexpt

--- KineticModel/estimation/ident/mle/mle_script.m
+++ KineticModel/estimation/ident/mle/mle_script.m
@@ -1,5 +1,5 @@
 % get maximum likelihood estimate of all parameters for given experimental
-% data
+% data (no noise)
 %% load noise free data
 if ~exist('C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel')
     status = 2;
@@ -26,6 +26,9 @@ xinit = repmat(xss,npert,1);
 yinit = repmat(fss,npert,1);
 
 freq = [1:50:1500 1501:1500:3001];
+pd = makedist('Normal','mu',0,'sigma',.001);
+ynoise = random(pd,4,length(freq));
+
 mle_opts = struct('nc',3,'nf',6,'npert',npert,...                  
                   'casmodelfun',@kotteCASident,...
                   'odep',odep_bkp,...
@@ -34,7 +37,8 @@ mle_opts = struct('nc',3,'nf',6,'npert',npert,...
                   'xinit',xinit,...
                   'yinit',yinit,...
                   'xexp',exp_select_sol.xdyn(:,freq),...
-                  'yexp',exp_select_sol.fdyn([1 3 4 5],freq));
+                  'yexp',exp_select_sol.fdyn([1 3 4 5],freq),...
+                  'ynoise',ynoise);
 
 %% get MLE of all parameters
 % initial value for optimization
@@ -42,4 +46,5 @@ scale = ones(9,1);
 scale(3) = 1e6;
 p0 = [odep_bkp(1:5)';odep_bkp(10:13)']./scale;
 
-MLEvals = getMLE(mle_opts,p0,scale);                
+MLEvals = getMLE(mle_opts,p0,scale);  
+% MLE_no_noise = getMLE_CAS(mle_opts,p0,scale);
