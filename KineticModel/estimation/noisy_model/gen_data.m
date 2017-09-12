b5264313ffe84bfe63ef33b706c28cc87c0848a2
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-12 17:07:20 -0400

    generate noisy data

--- KineticModel/estimation/noisy_model/gen_data.m
+++ KineticModel/estimation/noisy_model/gen_data.m
@@ -2,7 +2,7 @@
 % in this git repo estimation and identifiability)
 % generate experimental data - get initial ss
 tspan = 0:0.1:100;
-[xdyn,fdyn,xss1,fss1,opts] = run_nonoise(tspan);
+[xdyn,fdyn,xss,fss,opts] = run_nonoise(tspan);
 
 % backup parameters and initial conditions
 ival_bkp = opts.x0;
@@ -10,10 +10,11 @@ odep_bkp = opts.odep;
 
 % generate nsmp samples by adding random noise to ss values
 nsmp = 100;
-[noisy_xss,noisy_fss] = addnoise(repmat(xss1,1,nsmp),odep_bkp);
+[noisy_xss,noisy_fss] = addnoise(repmat(xss,1,nsmp),odep_bkp);
 
 % perturb system from noisy initial conditions
-pt_sol_id = [1 2 3];
-[exp_sol,noisy_sol] = dopert_noisy(opts,noisy_xss,noisy_fss,odep_bkp,pt_sol_id);
+pt_sol_id = [1 2 3 4 5 6];
+[exp_sol,noisy_sol] = dopert_nonoise_dyn(opts,noisy_xss,noisy_fss,odep_bkp,pt_sol_id,xdyn,fdyn);
+% [exp_sol,noisy_sol] = dopert_noisy(opts,noisy_xss,noisy_fss,odep_bkp,pt_sol_id);
 close all
 
