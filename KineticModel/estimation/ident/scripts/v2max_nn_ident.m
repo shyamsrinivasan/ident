5346059b4f1d6521d4b5d542f682aa059aeb691c
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-13 12:33:27 -0400

    changes to support changes in parameter order

--- KineticModel/estimation/ident/scripts/v2max_nn_ident.m
+++ KineticModel/estimation/ident/scripts/v2max_nn_ident.m
@@ -10,17 +10,17 @@ else
 end
 
 if status == 1    
-    load('C:/Users/shyam/Documents/Courses/CHE1125Project/IntegratedModels/KineticModel/estimation/noiseless_model/pdata_nn_sep1');
+    load('C:/Users/shyam/Documents/Courses/CHE1125Project/IntegratedModels/KineticModel/estimation/ident/mle/mle_nn_sep13');
 elseif status == 2    
-    load('~/Documents/Courses/CHE1125Project/IntegratedModels/KineticModel/estimation/noiseless_model/pdata_nn_sep1');    
+    load('~/Documents/Courses/CHE1125Project/IntegratedModels/KineticModel/estimation/ident/mle/mle_nn_sep13');    
 end
 
 %% set PLE options
 % collect only needed perturbations for analysis
-avail_pert = size(no_noise_sol,2);
-use_pert = 1; % avail_pert;
-npert = length(use_pert);
-[exp_select_sol,no_noise_select_sol] = parseperturbations(no_noise_sol,use_pert);
+% avail_pert = size(no_noise_sol,2);
+% use_pert = 1; % avail_pert;
+% npert = length(use_pert);
+% [exp_select_sol,no_noise_select_sol] = parseperturbations(no_noise_sol,use_pert);
 
 % use wt as initial value for all perturbations
 xinit = repmat(xss,npert,1);
@@ -28,8 +28,8 @@ yinit = repmat(fss,npert,1);
 
 freq = [1:50:1500 1501:1500:3001];
 optim_opts = struct('pname','V2max','nc',3,'nf',6,'npert',npert,...                    
-                    'plim',[0.01 .3],...
-                    'minmax_step',[1e-6 .01],...
+                    'plim',[0.01 6],...
+                    'minmax_step',[1e-6 .5],...
                     'casmodelfun',@kotteCASident_pert,...
                     'integratorfun','RK4integrator_cas',...
                     'odep',odep_bkp,...
@@ -47,14 +47,14 @@ dof = 12; % degrees of freedom
 % chi2 alpha quantile
 delta_alpha_1 = chi2inv(alpha,1);      
 delta_alpha_all = chi2inv(alpha,dof);
-thetai_fixed_value = .1;
+thetai_fixed_value = MLE_no_noise.mle_pval(13);
 theta_step = 0;
 
 % loop all the abopve statements for complete identifiability algforithm
-maxiter = 500;
+maxiter = 1000;
 
 % initial value for optimization
-scale = ones(12,1);
+scale = ones(8,1);
 
 % p0 for K1ac
 % scale(2) = 1e6;
@@ -62,13 +62,19 @@ scale = ones(12,1);
 
 % p0 for V2max
 scale(3) = 1e6;
-p0 = [opts.odep(1:10)';opts.odep(11:12)']./scale;
+p0 = [opts.odep(1:5)';opts.odep(10:12)']./scale;
 
 %% call PLE evaluation function
-[PLEvals] =...
-getPLE(thetai_fixed_value,theta_step,p0,opts.odep,delta_alpha_1,optim_opts,maxiter,1);
+pos_neg = [1 3];
+nid = length(pos_neg);
+PLEvals = cell(nid,1);
+parfor id = 1:nid
+    PLEvals{id} =...
+    getPLE(thetai_fixed_value,theta_step,p0,opts.odep,...
+           delta_alpha_1,optim_opts,maxiter,pos_neg(id));
 
-plotPLE(PLEvals,delta_alpha_1,delta_alpha_all);
+%     plotPLE(PLEvals{id},delta_alpha_1,delta_alpha_all);                
+end
 %%    
 
 
