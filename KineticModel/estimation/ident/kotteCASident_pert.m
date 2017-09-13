e4a5f5836dc85743d22472e9750a25a1ee320123
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-13 11:28:37 -0400

    change parameter order

--- KineticModel/estimation/ident/kotteCASident_pert.m
+++ KineticModel/estimation/ident/kotteCASident_pert.m
@@ -1,13 +1,13 @@
-function [ode,flux,D2FX,oderhs,x,p_all,ident_c,p_useless,acetate] =...
+function [ode,flux,D2FX,oderhs,x,p_var,p_ident,p_fixed,acetate] =...
         kotteCASident_pert(idx,nc,nf,npert)
 
 x = casadi.SX.sym('x',nc*npert,1);
-p_all = casadi.SX.sym('p_all',12,1);
-ident_c = casadi.SX.sym('ident_c',1,1);
-p_useless = casadi.SX.sym('p_useless',3,1);
+p_var = casadi.SX.sym('p_var',8,1);
+p_ident = casadi.SX.sym('p_ident',1,1);
+p_fixed = casadi.SX.sym('p_fixed',4,1);
 acetate = casadi.SX.sym('acetate',1,1);
 % rearrange parameters
-p = [p_all(1:idx-1);ident_c;p_all(idx:end);p_useless;acetate];
+p = [p_var(1:idx-1);p_ident;p_var(idx:end);p_fixed;acetate];
 
 % parameters
 K1ac = p(1);    % or 0.02
@@ -15,15 +15,15 @@ K3fdp = p(2);
 L3fdp = p(3)*1e6;
 K3pep = p(4);
 K2pep = p(5);
-vemax = p(6);        % for bifurcation analysis: 0.7:0.1:1.3
-KeFDP = p(7);        % or 0.45
-ne = p(8);             % or 2
-d = p(9);
-V4max = p(10);
-k1cat = p(11);   
-V3max = p(12);    
-V2max = p(13);   
-ac = p(17);
+V4max = p(6);
+k1cat = p(7);   
+V3max = p(8);    
+V2max = p(9);
+vemax = p(10);        % for bifurcation analysis: 0.7:0.1:1.3
+KeFDP = p(11);        % or 0.45
+ne = p(12);             % or 2
+d = p(13); 
+ac = p(14);
 
 flux1 = k1cat.*x(3:nc:nc*npert).*ac./(ac+K1ac);
 flux2 = vemax.*(1-1./(1+(KeFDP./x(2:nc:nc*npert)).^ne));
@@ -45,8 +45,8 @@ for i = 1:npert
                      flux4(i) - flux3(i);...
                      flux2(i) - flux6(i)];
 end             
-ode = casadi.Function('ode',{x,p_all,ident_c,p_useless,acetate},{oderhs}); 
-flux = casadi.Function('flux',{x,p_all,ident_c,p_useless,acetate},...
+ode = casadi.Function('ode',{x,p_var,p_ident,p_fixed,acetate},{oderhs}); 
+flux = casadi.Function('flux',{x,p_var,p_ident,p_fixed,acetate},...
                                 {fluxeq});
 
 % dfx_sym = [];
