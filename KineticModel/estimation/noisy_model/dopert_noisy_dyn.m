20c1fb67c1ecf2675181f5c8908d093716249bd9
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-13 17:57:14 -0400

    supply added noise as output arg

--- KineticModel/estimation/noisy_model/dopert_noisy_dyn.m
+++ KineticModel/estimation/noisy_model/dopert_noisy_dyn.m
@@ -1,6 +1,12 @@
 function [exp_sol_start,noisy_sol_start] =...
         dopert_noisy_dyn(opts_temp,noisy_xss,noisy_fss,odep_bkp,...
-        pt_sol_id,noisy_xdyn,noisy_fdyn)
+        pt_sol_id,noisy_xdyn,noisy_fdyn,noise_ss,noise_dyn)
+if nargin<9
+    noise_dyn = [];
+end
+if nargin<8
+    noise_ss = [];
+end
 if nargin<7
     noisy_fdyn = [];
 end
@@ -45,6 +51,14 @@ if nstart>10
         else
             fdyn_temp = [];
         end
+        if ~isempty(noise_ss)
+            noise_xss_temp = noise_ss.x;
+            noise_fss_temp = noise_ss.f;
+        end
+        if ~isempty(noise_dyn)
+            noise_xdyn_temp = noise_dyn{ist}.x;
+            noise_fdyn_temp = noise_dyn{ist}.f;
+        end
 
         opts_temp = options(ist);
         sol = getperturbations(pt_val,@perturb_nonoise,opts_temp,[],1);
@@ -57,11 +71,15 @@ if nstart>10
         for j = 1:length(pt_sol_id)
             noisy_sol(j).xss = noisy_ss(j).xss;
             noisy_sol(j).fss = noisy_ss(j).fss;
+            noisy_sol(j).noise_xss = noisy_ss(j).ss_noise.x;
+            noisy_sol(j).noise_fss = noisy_ss(j).ss_noise.f;
             if isfield(noisy_ss,'xdyn')
-                noisy_sol(j).xdyn = noisy_ss(j).xdyn;                
+                noisy_sol(j).xdyn = noisy_ss(j).xdyn;  
+                noisy_sol(j).noise_xdyn = noisy_ss(j).xdyn_noise;
             end
             if isfield(noisy_ss,'fdyn')
                 noisy_sol(j).fdyn = noisy_ss(j).fdyn;
+                noisy_sol(j).noise_fdyn = noisy_ss(j).fdyn_noise;
             end
         end
         % include wt (unperturbed) data
@@ -73,6 +91,18 @@ if nstart>10
         if ~isempty(fdyn_temp)
             noisy_sol(j+1).fdyn = fdyn_temp;
         end
+        if ~isempty(noise_xss_temp)
+            noisy_sol(j+1).noise_xss = noise_xss_temp;
+        end
+        if ~isempty(noise_fss_temp)
+            noisy_sol(j+1).noise_fss = noise_fss_temp;
+        end
+        if ~isempty(noise_xdyn_temp)
+            noisy_sol(j+1).noise_xdyn = noise_xdyn_temp;
+        end
+        if ~isempty(noise_fdyn_temp)
+            noisy_sol(j+1).noise_fdyn = noise_fdyn_temp;
+        end
         noisy_sol(j+1).exp_pid = 0;
         noisy_sol(j+1).exp_pval = 0;
         noisy_sol(j+1).odep = odep_bkp;
@@ -80,8 +110,12 @@ if nstart>10
         % combine all perturbation flux data into a single vector
         exp_sol_start{ist}.xss = cat(2,noisy_sol.xss);
         exp_sol_start{ist}.fss = cat(2,noisy_sol.fss);
+        exp_sol_start{ist}.noise_xss = cat(2,noisy_sol.noise_xss);
+        exp_sol_start{ist}.noise_fss = cat(2,noisy_sol.noise_fss);
         exp_sol_start{ist}.xdyn = cat(1,noisy_sol.xdyn);
         exp_sol_start{ist}.fdyn = cat(1,noisy_sol.fdyn);
+        exp_sol_start{ist}.noise_xdyn = cat(1,noisy_sol.noise_xdyn);
+        exp_sol_start{ist}.noise_fdyn = cat(1,noisy_sol.noise_fdyn);
         exp_sol_start{ist}.exp_pid = cat(2,noisy_sol.exp_pid);
         exp_sol_start{ist}.exp_pval = cat(2,noisy_sol.exp_pval);
         exp_sol_start{ist}.odep = cat(1,noisy_sol.odep);
@@ -103,6 +137,14 @@ else
         else
             fdyn_temp = [];
         end
+        if ~isempty(noise_ss)
+            noise_xss_temp = noise_ss.x;
+            noise_fss_temp = noise_ss.f;
+        end
+        if ~isempty(noise_dyn)
+            noise_xdyn_temp = noise_dyn{ist}.x;
+            noise_fdyn_temp = noise_dyn{ist}.f;
+        end
 
         opts_temp = options(ist);
         sol = getperturbations(pt_val,@perturb_nonoise,opts_temp,[],1);
@@ -115,11 +157,15 @@ else
         for j = 1:length(pt_sol_id)
             noisy_sol(j).xss = noisy_ss(j).xss;
             noisy_sol(j).fss = noisy_ss(j).fss;
+            noisy_sol(j).noise_xss = noisy_ss(j).ss_noise.x;
+            noisy_sol(j).noise_fss = noisy_ss(j).ss_noise.f;
             if isfield(noisy_ss,'xdyn')
-                noisy_sol(j).xdyn = noisy_ss(j).xdyn;                
+                noisy_sol(j).xdyn = noisy_ss(j).xdyn;  
+                noisy_sol(j).noise_xdyn = noisy_ss(j).xdyn_noise;
             end
             if isfield(noisy_ss,'fdyn')
                 noisy_sol(j).fdyn = noisy_ss(j).fdyn;
+                noisy_sol(j).noise_fdyn = noisy_ss(j).fdyn_noise;
             end
         end
         % include wt (unperturbed) data
@@ -131,6 +177,18 @@ else
         if ~isempty(fdyn_temp)
             noisy_sol(j+1).fdyn = fdyn_temp;
         end
+        if ~isempty(noise_xss_temp)
+            noisy_sol(j+1).noise_xss = noise_xss_temp;
+        end
+        if ~isempty(noise_fss_temp)
+            noisy_sol(j+1).noise_fss = noise_fss_temp;
+        end
+        if ~isempty(noise_xdyn_temp)
+            noisy_sol(j+1).noise_xdyn = noise_xdyn_temp;
+        end
+        if ~isempty(noise_fdyn_temp)
+            noisy_sol(j+1).noise_fdyn = noise_fdyn_temp;
+        end
         noisy_sol(j+1).exp_pid = 0;
         noisy_sol(j+1).exp_pval = 0;
         noisy_sol(j+1).odep = odep_bkp;
@@ -138,8 +196,12 @@ else
         % combine all perturbation flux data into a single vector
         exp_sol_start{ist}.xss = cat(2,noisy_sol.xss);
         exp_sol_start{ist}.fss = cat(2,noisy_sol.fss);
+        exp_sol_start{ist}.noise_xss = cat(2,noisy_sol.noise_xss);
+        exp_sol_start{ist}.noise_fss = cat(2,noisy_sol.noise_fss);
         exp_sol_start{ist}.xdyn = cat(1,noisy_sol.xdyn);
         exp_sol_start{ist}.fdyn = cat(1,noisy_sol.fdyn);
+        exp_sol_start{ist}.noise_xdyn = cat(1,noisy_sol.noise_xdyn);
+        exp_sol_start{ist}.noise_fdyn = cat(1,noisy_sol.noise_fdyn);
         exp_sol_start{ist}.exp_pid = cat(2,noisy_sol.exp_pid);
         exp_sol_start{ist}.exp_pval = cat(2,noisy_sol.exp_pval);
         exp_sol_start{ist}.odep = cat(1,noisy_sol.odep);
