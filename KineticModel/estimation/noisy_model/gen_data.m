f05fb5dd2795baf1d3352ece999cb74f21c68207
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-12 20:50:19 -0400

    change ss time horizon

--- KineticModel/estimation/noisy_model/gen_data.m
+++ KineticModel/estimation/noisy_model/gen_data.m
@@ -1,7 +1,7 @@
 % script to generate data (noisy) to be used for analysis 
 % in this git repo estimation and identifiability)
 % generate experimental data - get initial ss
-tspan = 0:0.1:100;
+tspan = 0:0.1:300;
 [xdyn,fdyn,xss,fss,opts] = run_nonoise(tspan);
 
 % backup parameters and initial conditions
@@ -14,7 +14,6 @@ nsmp = 100;
 
 % perturb system from noisy initial conditions
 pt_sol_id = [1 2 3 4 5 6];
-[exp_sol,noisy_sol] = dopert_nonoise_dyn(opts,noisy_xss,noisy_fss,odep_bkp,pt_sol_id,xdyn,fdyn);
+[exp_sol,noisy_sol] = dopert_noisy_dyn(opts,noisy_xss,noisy_fss,odep_bkp,pt_sol_id,xdyn,fdyn);
 % [exp_sol,noisy_sol] = dopert_noisy(opts,noisy_xss,noisy_fss,odep_bkp,pt_sol_id);
 close all
-
