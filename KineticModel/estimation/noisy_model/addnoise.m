218d3e51a4dbbab8ab5f8a9df48ec14367722328
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-13 17:25:45 -0400

    obtain added noise as output args

--- KineticModel/estimation/noisy_model/addnoise.m
+++ KineticModel/estimation/noisy_model/addnoise.m
@@ -1,5 +1,5 @@
 % add noise to input data and calculate corresponding noisy flux
-function [noisy_xss,noisy_fss] = addnoise(x,flux)
+function [noisy_xss,noisy_fss,noise] = addnoise(x,flux)
 if nargin<2
     flux = [];
 end
@@ -10,9 +10,7 @@ end
 if ~isempty(flux)
     nsmp = size(flux,2);
 end
-% npar = size(p,1);
-% no_noise_flux = zeros(size(kotte_flux_noCAS(x,p(1,:)),1),nsmp);
-% for i = 1:npar
+
 if ~isempty(flux)
     no_noise_flux = flux; % kotte_flux_noCAS(x,p);    
 else
@@ -21,10 +19,10 @@ end
 nflx = size(no_noise_flux,1);
 
 if nsmp>1   
-    pd = makedist('Normal','mu',0,'sigma',.05);
+    pd = makedist('Normal','mu',0,'sigma',.01);
 %     pd = makedist('Uniform','lower',-.05,'upper',.05);    
 else
-    pd = makedist('Normal','mu',0,'sigma',.05);
+    pd = makedist('Normal','mu',0,'sigma',.01);
 %     pd = makedist('Uniform','lower',-.05,'upper',.05);    
 end
 met_noise = random(pd,nvar,nsmp);
@@ -40,15 +38,10 @@ if nargout>1 && ~isempty(no_noise_flux)
 else
     noisy_fss = [];
 end
-% noisy_fss = [];
 
-% nonlinear noise on flux from concentration
-% if npar>1
-%     noisy_fss = zeros(nflx,npar);
-%     for i = 1:npar
-%         noisy_fss(:,i) = kotte_flux_noCAS(noisy_xss(:,i),p(i,:));
-%     end
-% else
-%     noisy_fss = kotte_flux_noCAS(noisy_xss,p);
-% end
+if nargout>2
+    noise = struct();
+    noise.x = met_noise;
+    noise.f = flux_noise;
+end
 
