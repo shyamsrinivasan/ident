5d437bf7aa974f83d4f3b04fe09ff01582a27c5f
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-13 11:28:04 -0400

    change p order and add noise to obj

--- KineticModel/estimation/ident/identopt_setup.m
+++ KineticModel/estimation/ident/identopt_setup.m
@@ -27,6 +27,13 @@ if isfield(data,'yexp')
 else
     yexp = xexp;
 end
+if isfield(data,'ynoise')
+    % measurement noise/error => y ~ N(0,sigma^2);
+    ynoise = data.ynoise;
+else
+    ynoise = ones(size(yexp,1),size(yexp,2));
+%     y_noise(y_noise==1) = ;
+end
 if isfield(data,'tspan')
     tspan = data.tspan;
 end
@@ -37,8 +44,9 @@ npts = length(tspan)-1;
 data.npts = npts;
 
 if ~isfield(data,'ident_idx') && ~isempty(pname)    
-    plist = {'K1ac','K3fdp','L3fdp','K3pep','K2pep','vemax','KeFDP','ne',...
-            'd','V4max','k1cat','V3max','V2max','K1pep','K2fdp','rhoA','acetate'}; 
+    plist = {'K1ac','K3fdp','L3fdp','K3pep','K2pep',...
+             'V4max','k1cat','V3max','V2max',...
+             'vemax','KeFDP','ne','d','acetate'}; 
     ident_idx = find(strcmpi(plist,pname));    
     data.ident_idx = ident_idx;
 elseif ~isfield(data,'ident_idx') && isempty(pname)    
@@ -47,11 +55,13 @@ end
 
 % create CAS function with custom integrator of nlsq opt
 intfun = str2func(intfun);
-[xdyn_fun,~,x,p,ident_c,p_useless,acetate] = intfun(casmodelf,data);
+[xdyn_fun,~,x,p_var,p_ident,p_fixed,acetate] = intfun(casmodelf,data);
 
 % final symbolic expression to be used during optimization
 [x_sym,y_sym] =...
-xdyn_fun(xinit,repmat(p,1,npts),fixed_pvalue,repmat(data.odep(14:16)',1,npts),.1);
+xdyn_fun(xinit,repmat(p_var,1,npts),...
+               fixed_pvalue,repmat(data.odep(6:9)',1,npts),...
+               data.odep(17));
 
 % x_sym = x_sym; % [sum(x_sym(1:3,:));sum(x_sym(4:6,:));sum(x_sym(7:9,:))];
 % add initial value
@@ -64,14 +74,12 @@ y_sym = [casadi.DM(yinit) y_sym];
 y_model_sym = y_sym([1 3 4 5],freq);
 % create nlsqopt objective function
 % x_error = (xexp-x_model_sym);
-y_error = (yexp-y_model_sym);
+y_error = (yexp-y_model_sym)./ynoise;
 obj = .5*dot(y_error,y_error);
 
-% objfun = casadi.Function('objfun',{xinit,},{obj});
+objfun = casadi.Function('objfun',{p_var},{obj});
 
-[lb,ub] = ident_bounds(length(p));
-
-objfun = []; % casadi.Function('objfun',{x,p,ident_c,p_useless,acetate},{obj});
+[lb,ub] = ident_bounds(length(p_var));
 
 % jac = jacobian(obj,p);
 jacfun = []; % casadi.Function('jacfun',{x,p,ident_c,p_useless,acetate},{jac});
@@ -79,8 +87,8 @@ jacfun = []; % casadi.Function('jacfun',{x,p,ident_c,p_useless,acetate},{jac});
 % hess = hessian(obj,p);
 hessfun = []; % casadi.Function('hessfun',{x,p,ident_c,p_useless,acetate},{hess});
 
-prob_cas = struct('obj',obj,'x',x,'p',p,'ident_c',ident_c,....
-                'p_useless',p_useless,'acetate',acetate,'xdynfun',xdyn_fun,...
+prob_cas = struct('obj',obj,'objfun',objfun,'x',x,'p',p_var,'ident_c',p_ident,....
+                'p_useless',p_fixed,'acetate',acetate,'xdynfun',xdyn_fun,...
                 'objfun',objfun,'grad',jacfun,'hess',hessfun,...
                 'npts',npts,'xinit',xinit,'yinit',yinit,...
                 'xexp',xexp,'yexp',yexp,...
