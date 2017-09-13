afa52baf52eaa7ecadf43aaa11a19a38d70ac7e0
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-13 16:50:00 -0400

    correct noise addition to dynamic data

--- KineticModel/estimation/noisy_model/gen_data.m
+++ KineticModel/estimation/noisy_model/gen_data.m
@@ -9,11 +9,15 @@ ival_bkp = opts.x0;
 odep_bkp = opts.odep;
 
 % generate nsmp samples by adding random noise to ss values
-nsmp = 11;
-[noisy_xss,noisy_fss] = addnoise(repmat(xss,1,nsmp),odep_bkp);
+nsmp = 5;
+[noisy_xss,noisy_fss] = addnoise(repmat(xss,1,nsmp),repmat(fss,1,nsmp));
+[noisy_xdyn,noisy_fdyn] = addnoise_dyn(xdyn,fdyn,nsmp);
+
 
 % perturb system from noisy initial conditions
 pt_sol_id = [1 2 3 4 5 6];
-[exp_sol,noisy_sol] = dopert_noisy_dyn(opts,noisy_xss,noisy_fss,odep_bkp,pt_sol_id,xdyn,fdyn);
+[exp_sol,noisy_sol] =...
+dopert_noisy_dyn(opts,noisy_xss,noisy_fss,odep_bkp,pt_sol_id,...
+                noisy_xdyn,noisy_fdyn);
 % [exp_sol,noisy_sol] = dopert_noisy(opts,noisy_xss,noisy_fss,odep_bkp,pt_sol_id);
 close all
