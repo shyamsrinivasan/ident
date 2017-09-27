83dc152a6f82dda98e9b10d3db1151d819c04e06
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-27 16:45:33 -0400

    python kotte ode model w/ flux equations

---
+++ python2/ss-ident/kotte_model.py
@@ -0,0 +1,37 @@
+import numpy as np
+# import scipy.linalg
+
+# K1ac, K3fdp, L3fdp, K3pep, K2pep, vemax, Kefdp, ne, d, V4max, k1cat, V3max, V2max, ac
+def_par_val = np.array([.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, .1])
+
+
+def kotte_flux(y, p=def_par_val):
+    """function doc_string"""
+
+    K1ac, K3fdp, L3fdp, K3pep, K2pep, vemax, Kefdp, ne, d, V4max, k1cat, V3max, V2max, ac = p
+
+    flux_1 = k1cat*y[2]*ac/(ac+K1ac)
+    flux_2 = vemax*(1-1/(1+(Kefdp/y[1])**ne))
+    fdp_sat = 1 + y[1]/K3fdp
+    pep_sat = 1 + y[0]/K3pep
+    flux_3 = V3max*(fdp_sat-1)*(fdp_sat)**3/(fdp_sat**4+L3fdp*(pep_sat**(-4)))
+    flux_4 = V2max*y[0]/(y[0]+K2pep)
+    flux_5 = V4max*y[0]
+    flux_6 = d*y[2]
+    all_flux = np.hstack((flux_1,flux_2,flux_3,flux_4,flux_5,flux_6))
+
+    return all_flux
+
+
+def kotte_ode(t,y):
+
+    K1ac, K3fdp, L3fdp, K3pep, K2pep, vemax, Kefdp, ne, d, V4max, k1cat, V3max, V2max, ac = \
+        [.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, .1]
+    par_val = np.vstack((K1ac, K3fdp, L3fdp, K3pep, K2pep, vemax, Kefdp, ne, d, V4max, k1cat, V3max, V2max, ac))
+
+    flux = kotte_flux(y,par_val)
+    yd_pep = flux[0] - flux[3] - flux[4]
+    yd_fdp = flux[3] - flux[2]
+    yd_e = flux[1] - flux[5]
+
+    return np.hstack((yd_pep,yd_fdp,yd_e))
