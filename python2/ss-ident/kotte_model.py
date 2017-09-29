77d5803c614fac6fe487b4bb40874feac6e2b514
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-29 19:06:34 -0400

    add convenience kinetics for flux 3

--- python2/ss-ident/kotte_model.py
+++ python2/ss-ident/kotte_model.py
@@ -4,6 +4,37 @@ import numpy as np
 # K1ac, K3fdp, L3fdp, K3pep, K2pep, vemax, Kefdp, ne, d, V4max, k1cat, V3max, V2max, ac
 def_par_val = np.array([.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, .1])
 
+def kotte_ck_flux(y,p=def_par_val):
+    """calculate flux using convenience kinetics"""
+
+    K1ac, K3fdp, L3fdp, K3pep, K2pep, vemax, Kefdp, ne, d, V4max, k1cat, V3max, V2max, ac = p
+
+    flux_1 = k1cat * y[2] * ac / (ac + K1ac)
+    flux_2 = vemax * (1 - 1 / (1 + (Kefdp / y[1]) ** ne))
+    # convenience kinetics for flux 3
+    fdp_sat = y[1] / K3fdp
+    pep_sat = y[0] / K3pep
+    nr_3 = V3max*fdp_sat
+    dr_3 = 1 + fdp_sat
+    regulation_activate = 1/(1 + 1/pep_sat)
+    # regulation_inhibition = 1/(1 + pep_sat) # for future reference
+    flux_3 = regulation_activate * nr_3/dr_3
+    flux_4 = V2max * y[0] / (y[0] + K2pep)
+    flux_5 = V4max * y[0]
+    flux_6 = d * y[2]
+    all_flux = np.hstack((flux_1, flux_2, flux_3, flux_4, flux_5, flux_6))
+
+    return all_flux
+
+def kotte_ck_ode(t,y,par_val):
+    """ode calculation using convenience kinetics for flux 3"""
+
+    flux = kotte_ck_flux(y, par_val)
+    yd_pep = flux[0] - flux[3] - flux[4]
+    yd_fdp = flux[3] - flux[2]
+    yd_e = flux[1] - flux[5]
+
+    return np.hstack((yd_pep, yd_fdp, yd_e))
 
 def kotte_flux(y, p=def_par_val):
     """function doc_string"""
@@ -21,7 +52,6 @@ def kotte_flux(y, p=def_par_val):
     all_flux = np.hstack((flux_1,flux_2,flux_3,flux_4,flux_5,flux_6))
 
     return all_flux
-
 
 def kotte_ode(t,y,par_val):
 
