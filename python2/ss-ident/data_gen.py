a62243a1b9af99e272206ac7f8cc8c3b99954813
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-29 19:15:38 -0400

    include ck flux simulations

--- python2/ss-ident/data_gen.py
+++ python2/ss-ident/data_gen.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 # define rhs function - defined in kotte_model.py
 from kotte_model import kotte_ode
 from kotte_model import kotte_flux
+from kotte_model import kotte_ck_ode
+from kotte_model import kotte_ck_flux
 from simulate_ode import simulate_ode
 
 
@@ -25,16 +27,19 @@ if __name__=='__main__':
 
     y0 = np.array([5, 1, 1])
     cvode_options = ['Newton', 'Adams', 1e-6, 1e-6, 100]
-    ode_par_val = np.array([.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, 1])
+    ode_par_val = np.array([.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, .1])
     cvode_options.append(ode_par_val)
 
     time, y_dynamic = run_example(kotte_ode, y0, cvode_options, 100)[:2]
-
-    # get flux data for all dynamic concentrations in y_dynamic
-    # par_val = np.array([.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, .1])
-
-    # plot dynamic flux data
+    # calculate and plot dynamic flux data
     flux_function = lambda x: kotte_flux(x, ode_par_val)
     flux_dynamic = map(flux_function, y_dynamic)
     plt.plot(time, flux_dynamic, color="b")
+    plt.show()
+
+    time_ck, y_dynamic_ck = run_example(kotte_ck_ode, y0, cvode_options, 100)[:2]
+    # calculate and plot dynamic ck flux data
+    flux_function_ck = lambda x: kotte_ck_flux(x, ode_par_val)
+    flux_dynamic_ck = map(flux_function_ck, y_dynamic_ck)
+    plt.plot(time, flux_dynamic_ck, color="g")
     plt.show()
