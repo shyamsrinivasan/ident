26843c41efc1c318d14944ad47bdacacee47a075
shyamsrinivasan <shyamsweth@gmail.com>
2017-09-30 11:59:23 -0400

    fun file to plot dynamic profiles of ode sims

---
+++ python2/ss-ident/plot_dyn_profiles.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_dynamic_course(time, ydata, type=1):
+    if type==1: # concentrations
+        pass
+    elif type==2: # fluxes
+        pass
+
+    return None
+
