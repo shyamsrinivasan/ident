95d2fbf1ec43710f947aac79cca8e86bd121b1bc
shyamsrinivasan <shyamsweth@gmail.com>
2017-10-03 16:53:40 -0400

    use ss setup fun

--- KineticModel/estimation/ident/PLEiter.m
+++ KineticModel/estimation/ident/PLEiter.m
@@ -5,7 +5,8 @@ function [optsol,thetai_fixed_value,theta_step,obj_new,iter_theta_step] =...
 thetai_fixed_value = thetai_fixed_value + type*theta_step;
 
 % PLE through optimization for new thetai
-prob_cas = identopt_setup(setup_opts,thetai_fixed_value);
+% prob_cas = identopt_setup(setup_opts,thetai_fixed_value);
+prob_cas = setpleopt_ss(setup_opts,thetai_fixed_value);
 optsol = solve_nlsqopt(prob_cas,xval);
 
 % adpative step in thetai
