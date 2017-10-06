cc391e1f6fb72b79c0093b0aa61227b6577729cd
shyamsrinivasan <shyamsweth@gmail.com>
2017-10-06 11:18:17 -0400

    change parameter bounds

--- KineticModel/estimation/ident/mle/ident_bounds_mle.m
+++ KineticModel/estimation/ident/mle/ident_bounds_mle.m
@@ -9,19 +9,19 @@ end
 % k1cat - MLE holding transcription parameters and acetate constant 
 % {'vemax','KeFDP','ne','d','acetate'}
 lb(:) = .05;
-lb(3) = 1;
+lb(3) = 3.9;
 lb(6) = .1;
 lb(7) = .1;
 lb(8) = .1;
 lb(9) = .1;
 
 ub(:) = 3;
-ub(1) = 2;
-ub(2) = 2;
-ub(3) = 4.5;
-ub(4) = 2;
-ub(5) = 2;
-ub(6) = 1;
+ub(1) = .5;
+ub(2) = .5;
+ub(3) = 4.1;
+ub(4) = .5;
+ub(5) = .5;
+ub(6) = .5;
 ub(7:9) = 1.5;
 
 % ub(10) = 2;
