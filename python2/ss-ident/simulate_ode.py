098ddca72ba5cef3f95183baaa58a1da157bfa87
shyamsrinivasan <shyamsweth@gmail.com>
2017-10-12 17:30:03 -0400

    add fun to call simulate method

--- python2/ss-ident/simulate_ode.py
+++ python2/ss-ident/simulate_ode.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from assimulo.solvers import CVode
 from assimulo.problem import Explicit_Problem
 
@@ -23,3 +24,16 @@ def simulate_ode(fun, y_initial, tf, opts):
     time_course, y_result = solver.simulate(tf, time_points)
 
     return time_course, y_result, prob, solver
+
+def run_ode_sims(fun, y_initial, opts, t_final=500, args_1=True):
+    """run kotte model ode using cvode from assimulo"""
+
+    # def_par_val = np.array([.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, .1])
+    time_points, y_dynamic, prob, solver = simulate_ode(fun, y_initial, t_final, opts)
+    if args_1:
+        plt.plot(time_points, y_dynamic, color="r")
+        plt.xlabel('Time')
+        plt.ylabel('Dependent Variables')
+        plt.show()
+
+    return time_points, y_dynamic, prob, solver
