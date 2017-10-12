844ee772ba4e9f5bf5117f79784a781e63b63462
shyamsrinivasan <shyamsweth@gmail.com>
2017-10-12 17:30:37 -0400

    change into script - remove run fun

--- python2/ss-ident/data_gen.py
+++ python2/ss-ident/data_gen.py
@@ -1,23 +1,8 @@
 # generate experimental ss/dynamic data by solving model ode using assimulo for different input acetate concentrations
-import numpy as np
 import matplotlib.pyplot as plt
 # define rhs function - defined in kotte_model.py
 from kotte_model import *
-from simulate_ode import simulate_ode
-
-
-def run_example(fun, y_initial, opts, t_final=500, args_1=True):
-    """run kotte model ode using cvode from assimulo"""
-
-    # def_par_val = np.array([.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, .1])
-    time_points, y_dynamic, prob, solver = simulate_ode(fun, y_initial, t_final, opts)
-    if args_1:
-        plt.plot(time_points, y_dynamic, color="r")
-        plt.xlabel('Time')
-        plt.ylabel('Dependent Variables')
-        plt.show()
-
-    return time_points, y_dynamic, prob, solver
+from simulate_ode import run_ode_sims
 
 # main function
 if __name__=='__main__':
@@ -27,14 +12,14 @@ if __name__=='__main__':
     ode_par_val = np.array([.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, .1])
     cvode_options.append(ode_par_val)
 
-    time, y_dynamic = run_example(kotte_ode, y0, cvode_options, 100)[:2]
+    time, y_dynamic = run_ode_sims(kotte_ode, y0, cvode_options, 100)[:2]
     # calculate and plot dynamic flux data
     flux_function = lambda x: kotte_flux(x, ode_par_val)
     flux_dynamic = map(flux_function, y_dynamic)
     plt.plot(time, flux_dynamic, color="b")
     plt.show()
 
-    time_ck, y_dynamic_ck = run_example(kotte_ck_ode, y0, cvode_options, 100)[:2]
+    time_ck, y_dynamic_ck = run_ode_sims(kotte_ck_ode, y0, cvode_options, 100)[:2]
     # calculate and plot dynamic ck flux data
     flux_function_ck = lambda x: kotte_ck_flux(x, ode_par_val)
     flux_dynamic_ck = map(flux_function_ck, y_dynamic_ck)
