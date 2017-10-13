45bf42e0b6f5d37e6e53cebe322339fb2f337093
shyamsrinivasan <shyamsweth@gmail.com>
2017-10-13 18:01:04 -0400

    change variable names

--- python2/ss-ident/plot_profiles.py
+++ python2/ss-ident/plot_profiles.py
@@ -6,20 +6,20 @@ def plot_dynamic_course(time, concentration_data=np.array([]), flux_data=np.arra
     """use subplots from matplotlib sharing x per column and y per row"""
     if type==1: # concentrations
         if concentration_data.size:
-            plt.plot(time, ydatac)
+            plt.plot(time, concentration_data, color='r')
             plt.xlabel('Time')
             plt.ylabel('Concentrations')
     elif type==2: # fluxes
         if flux_data.size:
-            plt.plot(time, ydataf)
+            plt.plot(time, flux_data, color='g')
             plt.xlabel('Time')
             plt.ylabel('Fluxes')
     elif type==3: # both concentrations and fluxes in subplots
         f, axx = plt.subplots(2, sharex='col')
-        axx[0].plot(time, ydatac)
+        axx[0].plot(time, concentration_data)
         axx[0].set_title('Concentrations')
         if flux_data.size:
-            axx[1].plot(time, ydataf)
+            axx[1].plot(time, flux_data)
             axx[1].set_title('Fluxes')
     else:
         print('Incorrect plot type')
