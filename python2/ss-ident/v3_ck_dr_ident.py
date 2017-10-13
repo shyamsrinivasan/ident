5aa30b5a88ebe95bb9ece42424c1737f2972d2ff
shyamsrinivasan <shyamsweth@gmail.com>
2017-10-13 19:23:18 -0400

    use lambdify to generate expression

--- python2/ss-ident/v3_ck_dr_ident.py
+++ python2/ss-ident/v3_ck_dr_ident.py
@@ -1,5 +1,5 @@
-from sympy import *
 import numpy as np
+from sympy import *
 from generate_noisy_data import generate_noisy_data
 
 # generate noisy experimental data for testing identifiability
@@ -8,6 +8,7 @@ y0 = np.array([5, 1, 1])
     _, y_noisy_steady_state, flux_noisy_steady_state, _, _, y_steady_state, flux_steady_state = \
         generate_noisy_data(y0, 1)
 
+x11, x12, x13, x21, x22, x23, v31, v32, v33 = symbols('x11, x12, x13, x21, x22, x23, v31, v32, v33', positive=True)
 variables = [x11, x21, v31, x12, x22, v32, x13, x23, v33]
 
 # use denominator generated from mathematica to test identifiability for all fluxes
@@ -15,8 +16,9 @@ variables = [x11, x21, v31, x12, x22, v32, x13, x23, v33]
 # K3pep_sol_1
 V3max_sol_1 = -v32*v33*x11*x12*x21 + v32*v33*x11*x13*x21 + v31*v33*x11*x12*x22 - \
               v31*v33*x12*x13*x22 - v31*v32*x11*x13*x23 + v31*v32*x12*x13*x23
+v3max_fun_expression = lambdify(variables, V3max_sol_1, "numpy")
 
 # K3fdp_sol_2
 # K3pep_sol_2
-V3max_sol_2 = -v32*v33*x11*x12*x21 + v32*v33*x11*x13*x21 + v31*v33*x11*x12*x22 - \
-              v31*v33*x12*x13*x22 - v31*v32*x11*x13*x23 + v31*v32*x12*x13*x23
+# V3max_sol_2 = -v32*v33*x11*x12*x21 + v32*v33*x11*x13*x21 + v31*v33*x11*x12*x22 - \
+#               v31*v33*x12*x13*x22 - v31*v32*x11*x13*x23 + v31*v32*x12*x13*x23
