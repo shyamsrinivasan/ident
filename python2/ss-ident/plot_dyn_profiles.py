c963b0c3fe5356a58b2518254c0433b45debb995
shyamsrinivasan <shyamsweth@gmail.com>
2017-10-13 17:46:01 -0400

    add subplots for dynamic flux and conc data

--- python2/ss-ident/plot_dyn_profiles.py
+++ python2/ss-ident/plot_dyn_profiles.py
@@ -2,11 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_dynamic_course(time, ydata, type=1):
+def plot_dynamic_course(time, concentration_data=np.array([]), flux_data=np.array([]), type=1):
+    """use subplots from matplotlib sharing x per column and y per row"""
     if type==1: # concentrations
-        pass
+        if concentration_data.size:
+            plt.plot(time, ydatac)
+            plt.xlabel('Time')
+            plt.ylabel('Concentrations')
     elif type==2: # fluxes
-        pass
+        if flux_data.size:
+            plt.plot(time, ydataf)
+            plt.xlabel('Time')
+            plt.ylabel('Fluxes')
+    elif type==3: # both concentrations and fluxes in subplots
+        f, axx = plt.subplots(2, sharex='col')
+        axx[0].plot(time, ydatac)
+        axx[0].set_title('Concentrations')
+        if flux_data.size:
+            axx[1].plot(time, ydataf)
+            axx[1].set_title('Fluxes')
+    else:
+        print('Incorrect plot type')
 
+    plt.show()
     return None
 
