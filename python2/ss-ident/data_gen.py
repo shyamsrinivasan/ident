4bc72edbf964e97a41ac0e670a1ab1e3349f759d
shyamsrinivasan <shyamsweth@gmail.com>
2017-10-13 18:50:59 -0400

    subs code with fun call to generate_noisy_data

--- python2/ss-ident/data_gen.py
+++ python2/ss-ident/data_gen.py
@@ -1,50 +1,29 @@
 # generate experimental ss/dynamic data by solving model ode using assimulo for different input acetate concentrations
 import matplotlib.pyplot as plt
-# define rhs function - defined in kotte_model.py
-from kotte_model import *
-from simulate_ode import run_ode_sims
-from add_noise import add_noise_dynamic
+import numpy as np
+from generate_noisy_data import generate_noisy_data
 from plot_profiles import plot_dynamic_course
+
 
 # main function
 if __name__ == '__main__':
 
     y0 = np.array([5, 1, 1])
-    cvode_options = ['Newton', 'Adams', 1e-6, 1e-6, 100]
-    ode_par_val = np.array([.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, .1])
-    cvode_options.append(ode_par_val)
+    # generate data using MWC Kientics
+    time, y_noisy_steady_state, flux_noisy_steady_state, \
+    y_noisy_dynamic, flux_noisy_dynamic, \
+    y_steady_state, flux_steady_state, \
+    y_dynamic, flux_dynamic = generate_noisy_data(y0, 1)
 
-    time, y_dynamic = run_ode_sims(kotte_ode, y0, cvode_options, 100)[:2]
-    # calculate dynamic flux data
-    flux_dynamic = np.array(map(lambda x: kotte_flux(x, ode_par_val), y_dynamic))
-    # add noise to dynamic data
-    noisy_y_dynamic, noisy_flux_dynamic = add_noise_dynamic(y_dynamic, flux_dynamic)
-    # plot no noise data
-    plot_dynamic_course(time, y_dynamic, flux_dynamic, 3)
-    plot_dynamic_course(time, noisy_y_dynamic, noisy_flux_dynamic, 3)
+    # generate data using Convenience Kinetics
+    time_ck, y_ck_noisy_steady_state, flux_ck_noisy_steady_state, \
+    y_ck_noisy_dynamic, flux_ck_noisy_dynamic, \
+    y_ck_steady_state, flux__ck_steady_state, \
+    y_ck_dynamic, flux_ck_dynamic = generate_noisy_data(y0, 2)
 
-    # get ss info from dynamic data
-    y_steady_state = y_dynamic[-1, :]
-    flux_steady_state = flux_dynamic[-1, :]
-    # get noisy ss from dynamic data
-    y_noisy_steady_state = noisy_y_dynamic[-1, :]
-    flux_noisy_steady_state = noisy_flux_dynamic[-1, :]
-
-    time_ck, y_ck_dynamic = run_ode_sims(kotte_ck_ode, y0, cvode_options, 100)[:2]
-    # calculate dynamic flux
-    flux_ck_dynamic = np.array(map(lambda x: kotte_ck_flux(x, ode_par_val), y_ck_dynamic))
-    # add noise to dynamic data
-    noisy_y_ck_dynamic, noisy_flux_ck_dynamic = add_noise_dynamic(y_ck_dynamic, flux_ck_dynamic)
     # plot ck dynamic data
     plot_dynamic_course(time_ck, y_ck_dynamic, flux_ck_dynamic, 3)
-    plot_dynamic_course(time_ck, noisy_y_ck_dynamic, noisy_flux_ck_dynamic, 3)
-
-    # get ss info from dynamic data
-    y_ck_steady_state = y_ck_dynamic[-1, :]
-    flux_ck_steady_state = flux_ck_dynamic[-1, :]
-    # get noisy ss from dynamic data
-    y_ck_noisy_steady_state = noisy_y_ck_dynamic[-1, :]
-    flux_ck_noisy_steady_state = noisy_flux_ck_dynamic[-1, :]
+    plot_dynamic_course(time_ck, y_ck_noisy_dynamic, flux_ck_noisy_dynamic, 3)
 
     #
     f, axx = plt.subplots(2, 2, sharex='col', sharey='row')
