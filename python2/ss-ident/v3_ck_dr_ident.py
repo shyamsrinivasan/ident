16aeef02151214570f4947df469ab7f7e8bffbc0
shyamsrinivasan <shyamsweth@gmail.com>
2017-10-15 19:38:47 -0400

    add 2 more experiments - different acetate concentrations

--- python2/ss-ident/v3_ck_dr_ident.py
+++ python2/ss-ident/v3_ck_dr_ident.py
@@ -4,16 +4,33 @@ from generate_noisy_data import generate_noisy_data
 
 # generate noisy experimental data for testing identifiability
 y0 = np.array([5, 1, 1])
-all_options = []
+all_options_exp_1 = []
+all_options_exp_2 = []
+all_options_exp_3 = []
 cvode_options = ['Newton', 'Adams', 1e-6, 1e-6, 100]
-ode_par_val = np.array([.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, .1])
-all_options.append(cvode_options)
-all_options.append(ode_par_val)
 
+# experiment 1
+ode_par_val_experiment_1 = np.array([.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, .1])
+all_options_exp_1.append(cvode_options)
+all_options_exp_1.append(ode_par_val_experiment_1)
 # generate data using MWC Kinetics
-_, y_noisy_steady_state, flux_noisy_steady_state, _, _, y_steady_state, flux_steady_state, _, _ = \
-    generate_noisy_data(y0, all_options, 1)
+_, y_nss_exp1, flux_nss_exp1, _, _, _, _, _, _ = generate_noisy_data(y0, all_options_exp_1, 1)
 
+# experiment 2
+ode_par_val_experiment_2 = np.array([.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, .5])
+all_options_exp_2.append(cvode_options)
+all_options_exp_2.append(ode_par_val_experiment_2)
+# generate data using MWC Kinetics
+_, y_nss_exp2, flux_nss_exp2, _, _, _, _, _, _ = generate_noisy_data(y0, all_options_exp_2, 1)
+
+# experiment 3
+ode_par_val_experiment_3 = np.array([.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, 1])
+all_options_exp_3.append(cvode_options)
+all_options_exp_3.append(ode_par_val_experiment_3)
+# generate data using MWC Kinetics
+_, y_nss_exp3, flux_nss_exp3, _, _, _, _, _, _ = generate_noisy_data(y0, all_options_exp_3, 1)
+
+# symbolic expression for flux 3
 x11, x12, x13, x21, x22, x23, v31, v32, v33 = symbols('x11, x12, x13, x21, x22, x23, v31, v32, v33', positive=True)
 variables = [x11, x21, v31, x12, x22, v32, x13, x23, v33]
 
@@ -23,9 +40,9 @@ variables = [x11, x21, v31, x12, x22, v32, x13, x23, v33]
 V3max_sol_1 = -v32*v33*x11*x12*x21 + v32*v33*x11*x13*x21 + v31*v33*x11*x12*x22 - \
               v31*v33*x12*x13*x22 - v31*v32*x11*x13*x23 + v31*v32*x12*x13*x23
 v3max_fun_expression = lambdify([variables], V3max_sol_1, "numpy")
-experimental_data = np.hstack((y_noisy_steady_state[0:2], flux_noisy_steady_state[2],
-                               y_noisy_steady_state[0:2], flux_noisy_steady_state[2],
-                               y_noisy_steady_state[0:2], flux_noisy_steady_state[2]))
+experimental_data = np.hstack((y_nss_exp1[0:2], flux_nss_exp1[2],
+                               y_nss_exp2[0:2], flux_nss_exp2[2],
+                               y_nss_exp3[0:2], flux_nss_exp3[2]))
 print(experimental_data)
 print(v3max_fun_expression(experimental_data))
 
