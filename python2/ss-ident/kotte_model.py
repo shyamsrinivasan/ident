0dc2493a6a824319910b63795a03fd73b5f990d5
shyamsrinivasan <shyamsweth@gmail.com>
2017-10-25 01:56:07 -0400

    add numerator for all fluxes

--- python2/ss-ident/kotte_model.py
+++ python2/ss-ident/kotte_model.py
@@ -94,23 +94,45 @@ def flux_1_ident_expression(experimental_data):
         _, _, _, _, _, _ = define_sym_variables()
 
     # symbolic expression for flux v1 w/o enzyme concentration data
-    v1max_sol = -(ac2 * v11 - ac1 * v12)
-    k1ac_v1max_sol = -ac2 * v11 + ac1 * v12
-    v1max_fun_expression = lambdify([variables], v1max_sol, "numpy")
-    k1ac_v1max_fun_expression = lambdify([variables], k1ac_v1max_sol, "numpy")
-    v1max_no_enzyme_denominator_value = v1max_fun_expression(experimental_data)
-    k1ac_no_enzyme_denominator_value = k1ac_v1max_fun_expression(experimental_data)
+    v1max_numerator = ac1 * v11 * v12 - ac2 * v11 * v12
+    v1max_denominator = -(ac2 * v11 - ac1 * v12)
+    k1ac_numerator = ac1 * (ac2 * v11 - ac2 * v12)
+    k1ac_denominator = -ac2 * v11 + ac1 * v12
+    v1max_nr_expr = lambdify([variables], v1max_numerator, "numpy")
+    v1max_dr_expr = lambdify([variables], v1max_denominator, "numpy")
+    k1ac_nr_expr = lambdify([variables], k1ac_numerator, "numpy")
+    k1ac_dr_expr = lambdify([variables], k1ac_denominator, "numpy")
+    v1max_expression = lambdify([variables], v1max_numerator/v1max_denominator, "numpy")
+    k1ac_expression = lambdify([variables], k1ac_numerator/k1ac_denominator, "numpy")
+    v1max_no_enzyme_numerator_value = v1max_nr_expr(experimental_data)
+    v1max_no_enzyme_denominator_value = v1max_dr_expr(experimental_data)
+    v1max_no_enzyme_value = v1max_expression(experimental_data)
+    k1ac_no_enzyme_numerator_value = k1ac_nr_expr(experimental_data)
+    k1ac_no_enzyme_denominator_value = k1ac_dr_expr(experimental_data)
+    k1ac_no_enzyme_value = k1ac_expression(experimental_data)
 
     # symbolic expression for flux v1 w/ enzyme concentration data
-    k1cat_sol = -(ac1 * v12 * x31 - ac2 * v11 * x32)
-    k1ac_sol = ac1 * v12 * x31 - ac2 * v11 * x32
-    k1cat_fun_expression = lambdify([variables], k1cat_sol, "numpy")
-    k1ac_fun_expression = lambdify([variables], k1ac_sol, "numpy")
-    k1cat_enzyme_denominator_value = k1cat_fun_expression(experimental_data)
-    k1ac_enzyme_denominator_value = k1ac_fun_expression(experimental_data)
+    k1cat_nr = - ac1 * v11 * v12 + ac2 * v11 * v12
+    k1cat_dr = -(ac1 * v12 * x31 - ac2 * v11 * x32)
+    k1cat_nr_expr = lambdify([variables], k1cat_nr, "numpy")
+    k1cat_dr_expr = lambdify([variables], k1cat_dr, "numpy")
+    k1cat_expression = lambdify([variables], k1cat_nr / k1cat_dr, "numpy")
+    k1ac_nr = ac1 * (-ac2 * v12 * x31 + ac2 * v11 * x32)
+    k1ac_dr = ac1 * v12 * x31 - ac2 * v11 * x32
+    k1ac_nr_expr = lambdify([variables], k1ac_nr, "numpy")
+    k1ac_dr_expr = lambdify([variables], k1ac_dr, "numpy")
+    k1ac_expression = lambdify([variables], k1ac_nr/k1ac_dr, "numpy")
+    k1cat_enzyme_numerator_value = k1cat_nr_expr(experimental_data)
+    k1cat_enzyme_denominator_value = k1cat_dr_expr(experimental_data)
+    k1cat_enzyme_value = k1cat_expression(experimental_data)
+    k1ac_enzyme_numerator_value = k1ac_nr_expr(experimental_data)
+    k1ac_enzyme_denominator_value = k1ac_dr_expr(experimental_data)
+    k1ac_enzyme_value = k1ac_expression(experimental_data)
 
-    return [v1max_no_enzyme_denominator_value, k1ac_no_enzyme_denominator_value], \
-           [k1cat_enzyme_denominator_value, k1ac_enzyme_denominator_value]
+    return [v1max_no_enzyme_numerator_value, v1max_no_enzyme_denominator_value, v1max_no_enzyme_value], \
+           [k1ac_no_enzyme_numerator_value, k1ac_no_enzyme_denominator_value, k1ac_no_enzyme_value], \
+           [k1cat_enzyme_numerator_value, k1cat_enzyme_denominator_value, k1cat_enzyme_value], \
+           [k1ac_enzyme_numerator_value, k1ac_enzyme_denominator_value, k1ac_enzyme_value]
 
 
 def flux_2_ident_expression(experimental_data):
@@ -123,15 +145,26 @@ def flux_2_ident_expression(experimental_data):
 
     # symbolic expression for v2
     # V2max
-    v2max_sol = v22 * x21 - v21 * x22
+    v2max_numerator = -v21 * v22 * x21 + v21 * v22 * x22
+    v2max_denominator = -(v22 * x21 - v21 * x22)
     # K2pep
-    k2pep_sol = v22 * x21 - v21 * x22
-    v2max_fun_expression = lambdify([variables], v2max_sol, "numpy")
-    k2pep_fun_expression = lambdify([variables], k2pep_sol, "numpy")
-    v2max_denominator_value = v2max_fun_expression(experimental_data)
-    k2pep_denominator_value = k2pep_fun_expression(experimental_data)
+    k2pep_numerator = x21 * (v21 * x22 - v22 * x22)
+    k2pep_denominator = v22 * x21 - v21 * x22
+    v2max_nr_expr = lambdify([variables], v2max_numerator, "numpy")
+    v2max_dr_expr = lambdify([variables], v2max_denominator, "numpy")
+    v2max_expr = lambdify([variables], v2max_numerator/v2max_denominator, "numpy")
+    k2pep_nr_expr = lambdify([variables], k2pep_numerator, "numpy")
+    k2pep_dr_expr = lambdify([variables], k2pep_denominator, "numpy")
+    k2pep_expr = lambdify([variables], k2pep_numerator/k2pep_denominator, "numpy")
+    v2max_numerator_value = v2max_nr_expr(experimental_data)
+    v2max_denominator_value = v2max_dr_expr(experimental_data)
+    v2max_value = v2max_expr(experimental_data)
+    k2pep_numerator_value = k2pep_nr_expr(experimental_data)
+    k2pep_denominator_value = k2pep_dr_expr(experimental_data)
+    k2pep_value = k2pep_expr(experimental_data)
 
-    return [v2max_denominator_value, k2pep_denominator_value]
+    return [v2max_numerator_value, v2max_denominator_value, v2max_value], \
+           [k2pep_numerator_value, k2pep_denominator_value, k2pep_value]
 
 
 def flux_3_ident_expression(experimental_data):
@@ -144,6 +177,7 @@ def flux_3_ident_expression(experimental_data):
 
     # symbolic expression for v3
     # V3max
+    v3max_numerator = -v31*v32*v33*(x11*x12*x21 + x11*x13*x21 + x11*x12*x22 - x12*x13*x22 - x11*x13*x23 + x12*x13*x23) - (v31*v32*v33*x12*x21*(v33*x21*(-v31*x11*x12*x22 + v32*x11*x12*x22 + v32*x11*x13*x22 - v31*x12*x13*x22 - v32*x11*x12*x23 - v32*x11*x13*x23)  + v31*x23*(v32*x11*x13*x21 + v32*x12*x13*x21 + v33*x11*x12*x22 - v32*x11*x13*x22 - v32*x12*x13*x22 + v33*x12*x13*x22) - sqrt((v31*v33*x11*x12*x21*x22 - v32*v33*x11*x12*x21*x22 - v32*v33*x11*x13*x21*x22 +  v31*v33*x12*x13*x21*x22 + v32*v33*x11*x12*x21*x23 - v31*v32*x11*x13*x21*x23 + v32*v33*x11*x13*x21*x23 - v31*v32*x12*x13*x21*x23 - v31*v33*x11*x12*x22*x23 + v31*v32*x11*x13*x22*x23 + v31*v32*x12*x13*x22*x23 - v31*v33*x12*x13*x22*x23)**2 - 4*(-v32*v33*x11*x21*x22 + v31*v33*x12*x21*x22 + v32*v33*x11*x21*x23 - v31*v32*x13*x21*x23 - v31*v33*x12*x22*x23 + v31*v32*x13*x22*x23)*(v31*v33*x11*x12*x13*x21*x22 - v32*v33*x11*x12*x13*x21*x22 - v31*v32*x11*x12*x13*x21*x23 + v32*v33*x11*x12*x13*x21*x23 + v31*v32*x11*x12*x13*x22*x23 - v31*v33*x11*x12*x13*x22*x23))))/(2*(-v32*v33*x11*x21*x22 + v31*v33*x12*x21*x22 + v32*v33*x11*x21*x23 - v31*v32*x13*x21*x23 - v31*v33*x12*x22*x23 + v31*v32*x13*x22*x23)) + (v31*v32*v33*x13*x21*(-v31*v33*x11*x12*x21*x22 + v32*v33*x11*x12*x21*x22 + v32*v33*x11*x13*x21*x22 - v31*v33*x12*x13*x21*x22 - v32*v33*x11*x12*x21*x23 + v31*v32*x11*x13*x21*x23 - v32*v33*x11*x13*x21*x23 + v31*v32*x12*x13*x21*x23 + v31*v33*x11*x12*x22*x23 - v31*v32*x11*x13*x22*x23 - v31*v32*x12*x13*x22*x23 + v31*v33*x12*x13*x22*x23 - \[Sqrt]((v31*v33*x11*x12*x21*x22 - v32*v33*x11*x12*x21*x22 - v32*v33*x11*x13*x21*x22 + v31*v33*x12*x13*x21*x22 + v32*v33*x11*x12*x21*x23 - v31*v32*x11*x13*x21*x23 + v32*v33*x11*x13*x21*x23 - v31*v32*x12*x13*x21*x23 - v31*v33*x11*x12*x22*x23 +  v31*v32*x11*x13*x22*x23 + v31*v32*x12*x13*x22*x23 - v31*v33*x12*x13*x22*x23)**2 - 4*(-v32*v33*x11*x21*x22 + v31*v33*x12*x21*x22 +  v32*v33*x11*x21*x23 - v31*v32*x13*x21*x23 - v31*v33*x12*x22*x23 + v31*v32*x13*x22*x23)*(v31*v33*x11*x12*x13*x21*x22 - v32*v33*x11*x12*x13*x21*x22 - v31*v32*x11*x12*x13*x21*x23 + v32*v33*x11*x12*x13*x21*x23 + v31*v32*x11*x12*x13*x22*x23 - v31*v33*x11*x12*x13*x22*x23))))/(2*(-v32*v33*x11*x21*x22 + v31*v33*x12*x21*x22 + v32*v33*x11*x21*x23 - v31*v32*x13*x21*x23 - v31*v33*x12*x22*x23 + v31*v32*x13*x22*x23)) + (v31*v32*v33*x11*x22*(-v31*v33*x11*x12*x21*x22 + v32*v33*x11*x12*x21*x22 + v32*v33*x11*x13*x21*x22 - v31*v33*x12*x13*x21*x22 - v32*v33*x11*x12*x21*x23 + v31*v32*x11*x13*x21*x23 - v32*v33*x11*x13*x21*x23 + v31*v32*x12*x13*x21*x23 + v31*v33*x11*x12*x22*x23 - v31*v32*x11*x13*x22*x23 - v31*v32*x12*x13*x22*x23 + v31*v33*x12*x13*x22*x23 - \[Sqrt]((v31*v33*x11*x12*x21*x22 - v32*v33*x11*x12*x21*x22 - v32*v33*x11*x13*x21*x22 + v31*v33*x12*x13*x21*x22 + v32*v33*x11*x12*x21*x23 - v31*v32*x11*x13*x21*x23 + v32*v33*x11*x13*x21*x23 - v31*v32*x12*x13*x21*x23 - v31*v33*x11*x12*x22*x23 + v31*v32*x11*x13*x22*x23 + v31*v32*x12*x13*x22*x23 - v31*v33*x12*x13*x22*x23)**2 - 4*(-v32*v33*x11*x21*x22 + v31*v33*x12*x21*x22 + v32*v33*x11*x21*x23 - v31*v32*x13*x21*x23 - v31*v33*x12*x22*x23 + v31*v32*x13*x22*x23)*(v31*v33*x11*x12*x13*x21*x22 - v32*v33*x11*x12*x13*x21*x22 - v31*v32*x11*x12*x13*x21*x23 + v32*v33*x11*x12*x13*x21*x23 + v31*v32*x11*x12*x13*x22*x23 - v31*v33*x11*x12*x13*x22*x23))))/(2*(-v32*v33*x11*x21*x22 + v31*v33*x12*x21*x22 + v32*v33*x11*x21*x23 - v31*v32*x13*x21*x23 - v31*v33*x12*x22*x23 + v31*v32*x13*x22*x23)) - (v31*v32*v33*x13*x22*(-v31*v33*x11*x12*x21*x22 + v32*v33*x11*x21*x22*(x12 + x13) - v31*v33*x12*x13*x21*x22 - v32*v33*x11*x21*x23*(x12 + x13) + v31*v32*x13*x21*x23*(x11 + x12) + v31*v33*x12*x22*x23*(x11 + x13) - v31*v32*x13*x22*x23*(x11 + x12) - sqrt((v31*v33*x11*x12*x21*x22 - v32*v33*x11*x12*x21*x22 - v32*v33*x11*x13*x21*x22 + v31*v33*x12*x13*x21*x22 + v32*v33*x11*x12*x21*x23 - v31*v32*x11*x13*x21*x23 + v32*v33*x11*x13*x21*x23 - v31*v32*x12*x13*x21*x23 - v31*v33*x11*x12*x22*x23 + v31*v32*x11*x13*x22*x23 + v31*v32*x12*x13*x22*x23 - v31*v33*x12*x13*x22*x23)**2 - 4*(-v32*v33*x11*x21*x22 + v31*v33*x12*x21*x22 + v32*v33*x11*x21*x23 - v31*v32*x13*x21*x23 - v31*v33*x12*x22*x23 + v31*v32*x13*x22*x23)*(v31*v33*x11*x12*x13*x21*x22 - v32*v33*x11*x12*x13*x21*x22 - v31*v32*x11*x12*x13*x21*x23 + v32*v33*x11*x12*x13*x21*x23 + v31*v32*x11*x12*x13*x22*x23 - v31*v33*x11*x12*x13*x22*x23))))/(2*(-v32*v33*x11*x21*x22 + v31*v33*x12*x21*x22 + v32*v33*x11*x21*x23 - v31*v32*x13*x21*x23 - v31*v33*x12*x22*x23 + v31*v32*x13*x22*x23)) - (v31*v32*v33*x11*x23 (-v31*v33*x11*x12*x21 x22 + v32*v33*x11*x12*x21*x22 + v32*v33*x11*x13*x21*x22 - v31*v33*x12*x13*x21*x22 - v32*v33*x11*x12*x21*x23 + v31*v32*x11*x13*x21*x23 - v32*v33*x11*x13*x21*x23 + v31*v32*x12*x13*x21*x23 + v31*v33*x11*x12*x22*x23 - v31*v32*x11*x13*x22*x23 - v31*v32*x12*x13*x22*x23 + v31*v33*x12*x13*x22*x23 - sqrt((v31*v33*x11*x12*x21*x22 - v32*v33*x11**x21*x22(x12 - x13) + v31*v33*x12*x13*x21*x22 + v32*v33*x11*x21*x23(x12 + x13) - v31*v32*x13*x21*x23*(x11 + x12) - v31*v33*x12*x22*x23*(x11 + x13) + v31*v32*x13*x22*x23*(x11 + x12))**2 - 4*(-v32*v33*x11*x21*x22 + v31*v33*x12*x21*x22 + v32*v33*x11*x21*x23 - v31*v32*x13*x21*x23 -  v31*v33*x12*x22*x23 + v31*v32*x13*x22*x23) (v31*v33*x11*x12*x13*x21*x22 - v32*v33*x11*x12*x13*x21*x22 - v31*v32*x11*x12*x13*x21*x23 + v32*v33*x11*x12*x13*x21*x23 + v31*v32*x11*x12*x13*x22*x23 - v31*v33*x11*x12*x13*x22*x23))))/(2*(-v32*v33*x11*x21*x22 + v31*v33*x12*x21*x22 + v32*v33*x11*x21*x23 - v31*v32*x13*x21*x23 - v31*v33*x12*x22*x23 + v31*v32*x13*x22*x23)) + (v31*v32*v33*x12*x23*(-v31*v33*x11*x12*x21*x22 + v32*v33*x11*x12*x21*x22 + v32*v33*x11*x13*x21*x22 - v31*v33*x12*x13*x21*x22 - v32*v33*x11*x12*x21*x23 +  v31*v32*x11*x13*x21*x23 - v32*v33*x11*x13*x21*x23 + v31*v32*x12*x13*x21*x23 + v31*v33*x11*x12*x22*x23 - v31*v32*x11*x13*x22*x23 - v31*v32*x12*x13*x22*x23 + v31*v33*x12*x13*x22*x23 - sqrt((v31*v33*x11*x12*x21*x22 - v32*v33*x11*x12*x21*x22 - v32*v33*x11*x13*x21*x22 + v31*v33*x12*x13*x21*x22 + v32*v33*x11*x12*x21*x23 - v31*v32*x13*x21*x23*(x11 - x12) + v32*v33*x11*x13*x21*x23  - v31*v33*x11*x12*x22*x23 + v31*v32*x11*x13*x22*x23 + v31*v32*x12*x13*x22*x23 - v31*v33*x12*x13*x22*x23)**2 - 4*(-v32*v33*x11*x21*(x22 - x23) + v31*v33*x12*x22*(x21 - x23) - v31*v32*x13*x23*(x21 - x22))*(v31*v33*x11*x12*x13*x22*(x21 - x23) - v32*v33*x11*x12*x13*x21*(x22 - x23) - v31*v32*x11*x12*x13*x23(x21 - x22)))))/(2*(-v32*v33*x11*x21*(x22 - x23) + v31*v33*x12*x22*(x21 - x23) - v31*v32*x13*x23*(x21 - x22)))
     V3max_sol_1 = -v32 * v33 * x11 * x12 * x21 + v32 * v33 * x11 * x13 * x21 + v31 * v33 * x11 * x12 * x22 - \
                   v31 * v33 * x12 * x13 * x22 - v31 * v32 * x11 * x13 * x23 + v31 * v32 * x12 * x13 * x23
     # K3fdp
