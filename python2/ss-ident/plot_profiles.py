22bf893ad8e5eee2b472cc409b2918664722cd52
shyamsrinivasan <shyamsweth@gmail.com>
2017-10-28 18:38:33 -0400

    small formatting corrections

--- python2/ss-ident/plot_profiles.py
+++ python2/ss-ident/plot_profiles.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plt
 
 def plot_dynamic_course(time, concentration_data=np.array([]), flux_data=np.array([]), type=1):
     """use subplots from matplotlib sharing x per column and y per row"""
-    if type==1: # concentrations
+    if type == 1:  # concentrations
         if concentration_data.size:
             plt.plot(time, concentration_data, color='r')
             plt.xlabel('Time')
             plt.ylabel('Concentrations')
-    elif type==2: # fluxes
+    elif type == 2:  # fluxes
         if flux_data.size:
             plt.plot(time, flux_data, color='g')
             plt.xlabel('Time')
             plt.ylabel('Fluxes')
-    elif type==3: # both concentrations and fluxes in subplots
+    elif type == 3:  # both concentrations and fluxes in subplots
         f, axx = plt.subplots(2, sharex='col')
         axx[0].plot(time, concentration_data)
         axx[0].set_title('Concentrations')
