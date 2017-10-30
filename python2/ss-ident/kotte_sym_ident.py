0b26091128d957e45e591db912249e6fc8d5b499
shyamsrinivasan <shyamsweth@gmail.com>
2017-10-30 15:38:03 -0400

    add call to perturbation function

--- python2/ss-ident/kotte_sym_ident.py
+++ python2/ss-ident/kotte_sym_ident.py
@@ -1,5 +1,10 @@
+# clear workspace (removes all module names and objects)
+import sys
+sys.modules[__name__].__dict__.clear()
+
 import numpy as np
 from generate_noisy_data import generate_noisy_data
+from generate_noisy_data import run_noisy_parameter_perturbation
 from kotte_model import flux_1_ident_expression
 from kotte_model import flux_2_ident_expression
 from kotte_model import flux_3_ident_expression
@@ -9,7 +14,27 @@ y0 = np.array([5, 1, 1])
 all_options_exp_1 = []
 all_options_exp_2 = []
 all_options_exp_3 = []
-cvode_options = ['Newton', 'Adams', 1e-6, 1e-6, 100]
+# default parameter values
+cvode_options = ('Newton', 'Adams', 1e-6, 1e-6, 100)
+ode_paramater_values = np.array((.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, .1))
+
+# get initial noisy system steady state
+initial_options = []
+initial_options.append(cvode_options)
+initial_options.append(ode_paramater_values)
+noisy_initial_ss, _, _, _ = generate_noisy_data(y0, initial_options, 1)
+
+# all parameter perturbations
+parameter_perturbation = [(14, 4), (14, 9)]
+perturbation_options = {'ode_parameters':ode_paramater_values, 'cvode_options':cvode_options}
+noisy_ss, noisy_dynamic = run_noisy_parameter_perturbation(parameter_perturbation, noisy_initial_ss[0], perturbation_options)
+
+noisy_exp_xss = []
+noisy_exp_fss = []
+for index, ss_values in enumerate(noisy_ss):
+    noisy_exp_xss.append(ss_values[0])
+    noisy_exp_fss.append(ss_values[1])
+
 
 # experiment 1
 ode_par_val_experiment_1 = np.array([.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, .1])
