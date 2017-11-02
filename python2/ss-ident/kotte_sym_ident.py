664713e14543826705c296f34270e80fd46cf361
shyamsrinivasan <shyamsweth@gmail.com>
2017-11-01 22:59:19 -0400

    no major changes

--- python2/ss-ident/kotte_sym_ident.py
+++ python2/ss-ident/kotte_sym_ident.py
@@ -39,7 +39,8 @@ experimental_datasets = \
     arrange_experimental_data(noisy_exp_xss, noisy_exp_fss, perturbed_parameter_values, exp_flux_index)
 
 # identifiability for all kotte fluxes
-establish_kotte_flux_identifiability(experimental_datasets[100:103])
+flux1, flux2, flux3 = establish_kotte_flux_identifiability(experimental_datasets[0:100])
+print flux1, flux2, flux3
 
 # clear workspace (removes all module names and objects)
 # import sys
