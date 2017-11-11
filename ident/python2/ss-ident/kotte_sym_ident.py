6a5d3e839a01bd1d94077f955eeea562bd3b4a94
shyamsrinivasan <shyamsweth@gmail.com>
2017-11-11 18:15:50 -0500

    get all output args during ident fun call

--- ident/python2/ss-ident/kotte_sym_ident.py
+++ ident/python2/ss-ident/kotte_sym_ident.py
@@ -39,7 +39,7 @@ experimental_datasets = \
     arrange_experimental_data(noisy_exp_xss, noisy_exp_fss, perturbed_parameter_values, exp_flux_index)
 
 # identifiability for all kotte fluxes
-perturbation_list, _, _ = establish_kotte_flux_identifiability(experimental_datasets[0:50])
+parameter_list, perturbation_list, parameters_ident_each_perturbation = establish_kotte_flux_identifiability(experimental_datasets)
 print('Perturbation analysis for identifiability complete.\n')
 
 
