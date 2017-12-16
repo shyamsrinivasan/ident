eb0b5f00f3c1244a47aa796531b43cf19169a614
shyamsrinivasan <shyamsweth@gmail.com>
2017-12-16 12:19:18 -0500

    add no noise variants for perturbed parameter data generation

--- ident/python2/ss-ident/generate_expdata.py
+++ ident/python2/ss-ident/generate_expdata.py
@@ -17,25 +17,26 @@ def initialize_to_ss(y0, cvode_options, ode_parameter_values, noise=1):
         return initial_ss
 
 
-def perturb_parameters(initial_ss, parameter_perturbations, cvode_options, ode_parameter_values):
+def perturb_parameters(initial_ss, parameter_perturbations, cvode_options, ode_parameter_values, noise=1):
     """perform parameter perturbations from given initial ss"""
-    perturbation_options = {'ode_parameters': ode_parameter_values, 'cvode_options': cvode_options}
-    noisy_ss, noisy_dynamic, perturbation_details, _, dynamic_info = \
-        run_noisy_parameter_perturbation(parameter_perturbations, initial_ss["y"], perturbation_options)
-    return noisy_ss, perturbation_details
+
+    if noise:
+        perturbation_options = {'ode_parameters': ode_parameter_values, 'cvode_options': cvode_options}
+        noisy_ss, noisy_dynamic, perturbation_details, _, dynamic_info = \
+            run_noisy_parameter_perturbation(parameter_perturbations, initial_ss["y"], perturbation_options)
+        return noisy_ss, perturbation_details
+    else:
+        perturbation_options = {'ode_parameters': ode_parameter_values, 'cvode_options': cvode_options}
+        no_noise_ss, no_noise_dynamic, perturbation_details = \
+            run_noisy_parameter_perturbation(parameter_perturbations, initial_ss["y"], perturbation_options)
+        return no_noise_ss, perturbation_details
 
 
 def generate_expdata(y0, cvode_options, ode_parameter_values, noise=1):
     """generate noisy experimental data for kotte network to test identifiability"""
-    # default parameter values
-    # cvode_options = ('Newton', 'Adams', 1e-10, 1e-10, 200)
-    # ode_parameter_values = np.array([.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, .1])
 
-    if noise:
-        # get initial noisy system steady state
-        initial_ss = initialize_to_ss(y0, cvode_options, ode_parameter_values, 1)
-    else:
-        initial_ss = initialize_to_ss(y0, cvode_options, ode_parameter_values, 0)
+    # get initial noisy system steady state
+    initial_ss = initialize_to_ss(y0, cvode_options, ode_parameter_values, noise)
 
     # plot all dynamic courses
     # plot_multiple_dynamics(noisy_dynamic)
@@ -48,17 +49,18 @@ def generate_expdata(y0, cvode_options, ode_parameter_values, noise=1):
                               (11, .1), (11, .5), (11, 1), (11, -.1), (11, -.5),
                               (12, .1), (12, .5), (12, 1), (12, -.1), (12, -.5),
                               (13, .1), (13, .5), (13, 1), (13, -.1), (13, -.5)]
-    noisy_ss, perturbation_details = perturb_parameters(initial_ss, parameter_perturbation, cvode_options, ode_parameter_values)
+    perturbed_ss, perturbation_details = perturb_parameters(initial_ss, parameter_perturbation,
+                                                        cvode_options, ode_parameter_values, noise)
 
-    noisy_exp_xss = []
-    noisy_exp_fss = []
-    noisy_exp_ssid = []
-    for ss_values in noisy_ss:
-        noisy_exp_xss.append(ss_values["y"])
-        noisy_exp_fss.append(ss_values["flux"])
-        noisy_exp_ssid.append(ss_values["ssid"])
+    exp_xss = []
+    exp_fss = []
+    exp_ssid = []
+    for ss_values in perturbed_ss:
+        exp_xss.append(ss_values["y"])
+        exp_fss.append(ss_values["flux"])
+        exp_ssid.append(ss_values["ssid"])
 
-    return noisy_exp_xss, noisy_exp_fss, noisy_exp_ssid, perturbation_details
+    return exp_xss, exp_fss, exp_ssid, perturbation_details
 
 
 if __name__ == "__main__":
