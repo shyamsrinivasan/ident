795b9cd05db8ae27588cad97a79be97f68f7cd50
shyamsrinivasan <shyamsweth@gmail.com>
2017-12-16 12:30:50 -0500

    script to generate and identify with data w/o noise

---
+++ ident/python2/ss-ident/no_noise_script.py
@@ -0,0 +1,20 @@
+import numpy as np
+from generate_expdata import generate_expdata
+from kotte_model import establish_kotte_flux_identifiability
+from kotte_model import arrange_experimental_data
+from process_ident_data import process_info
+
+# generate noisy experimental data for testing identifiability
+y0 = np.array([5, 1, 1])
+# default parameter values
+cvode_options = ('Newton', 'Adams', 1e-10, 1e-10, 200)
+ode_parameter_values = np.array([.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, .1])
+
+# get experimental system steady state data without noise
+exp_xss, exp_fss, exp_ssid, perturbation_details = \
+    generate_expdata(y0, cvode_options, ode_parameter_values, noise=0)
+
+# arrange experimental data to form multiple data sets
+exp_flux_index = np.array([0, 3, 2, 4])
+# get combinations of experimental datasets
+experimental_datasets = arrange_experimental_data(exp_xss, exp_fss, perturbation_details, 3, exp_flux_index)
