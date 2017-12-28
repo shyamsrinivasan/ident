ddd90e5a3f614247b2707cd09c9a9cac04a9bbb8
shyamsrinivasan <shyamsrinivasan@users.noreply.github.com>
2017-12-28 02:30:12 -0500

    enable truncate fun to handle numbers written as exponents

--- ident/python2/ss-ident/kotte_model.py
+++ ident/python2/ss-ident/kotte_model.py
@@ -79,6 +79,8 @@ def truncate_values(f, n=3):
     """truncates floats to n specified values after the decimal"""
     if not np.isnan(f):
         s = '{}'.format(f)  # convert float to string
+        if 'e' in s or 'E' in s:
+            return float('{0:.{1}f}'.format(f, n))
         i, p, d = s.partition('.')
         return float('.'.join([i, (d+'0'*n)[:n]]))
     else:
