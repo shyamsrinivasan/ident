afd22ebacceb7223c65a4639eb239e4d6d1a6dfd
shyamsrinivasan <shyamsrinivasan@users.noreply.github.com>
2017-12-30 10:25:29 -0500

    remove funcs moved to simulate_data for creating data set combinations

--- ident/python2/ss-ident/kotte_model.py
+++ ident/python2/ss-ident/kotte_model.py
@@ -743,20 +743,6 @@ def establish_kotte_flux_identifiability(all_data, choose=()):
     return all_sample_ident_details
 
 
-def parameter_change(new_value, old_value):
-    return (new_value-old_value)/old_value
-
-
-def get_changed_parameters(original_parameters, changed_parameters, experiment_index, parameter_index):
-    parameter_array = np.zeros((1, 4))
-    parameter_array[0, 0] = experiment_index
-    parameter_array[0, 1] = parameter_index
-    parameter_array[0, 2] = changed_parameters[experiment_index, parameter_index]
-    parameter_array[0, 3] = parameter_change(changed_parameters[experiment_index, parameter_index],
-                                              original_parameters[parameter_index])
-    return parameter_array
-
-
 def select_experiment_for_dataset(experiment_id):
     return None
 
@@ -766,85 +752,4 @@ def loop_through_experiments(experiments_per_dataset, total_experiments):
 
         pass
     experimental_data = []
-    return experimental_data
-
-
-def arrange_experimental_data(xss, fss,
-                              perturbation_details,
-                              experiments_per_set, flux_id=np.array([0, 1, 2, 3, 4, 5]), choose=()):
-    """get combinations of datasets using xss, fss and parameters for kotte model.
-    see identifiability functions above for order of values in datasets"""
-    parameters = perturbation_details["values"]
-    experiment_indices = perturbation_details["indices"]
-    original = perturbation_details["original"]
-    ssid = perturbation_details["ssid"]
-
-    # get all experiment combinations based on experiments_per_set
-    number_of_experiments = len(xss)
-    data_combinations = []
-    for item in it.permutations(range(0, number_of_experiments), experiments_per_set):
-        data_combinations.append(item)
-
-    # choose only choose values of experimental data set combinations
-    if choose:
-        try:
-            data_combinations = data_combinations[:choose]
-            choose = range(0, choose)
-        except TypeError:
-            new_data_combinations = []
-            for indexes in choose:
-                new_data_combinations.append(data_combinations[indexes])
-            data_combinations = new_data_combinations[:]
-
-    data_combination_boolean = [[True if experiment_id in list_1 else False
-                                 for experiment_id in range(0, number_of_experiments)]
-                                for list_1 in data_combinations]
-    data_combination_boolean = np.array(data_combination_boolean)
-
-    # initialize vectors/arrays and other lists for arrangement
-    number_data_sets = len(data_combinations)
-    size_of_data_set = 8
-    dataset_value_array = np.zeros((number_data_sets, size_of_data_set * experiments_per_set))
-    all_parameter_change = np.zeros((number_data_sets, 4 * experiments_per_set))
-
-    # arrange and collect parameter/ss values
-    experiment_index_iter = []
-    all_parameter_ids = []
-    all_parameter_values = []
-    all_parameter_changes = []
-    all_ssid = []
-    for choice_index, data_index in zip(choose, enumerate(data_combinations)):
-        data = []
-        parameter_id = []
-        changes = []
-        experiment_index_iter.append(data_index[1])
-        parameter_value = []
-        parameter_change = []
-        ssid_changes = []
-        for iter, index in enumerate(data_index[1]):
-            data.append(np.hstack((parameters[index][-1],
-                                   xss[index],
-                                   fss[index][flux_id])))
-            changes.append(get_changed_parameters(original_parameters=original,
-                                                  changed_parameters=parameters,
-                                                  experiment_index=index,
-                                                  parameter_index=int(experiment_indices[index, 0])))
-            parameter_id.append(int(experiment_indices[index, 0]))
-            parameter_value.append(parameters[index, parameter_id[iter]])
-            parameter_change.append(changes[iter][0, 3])
-            ssid_changes.append(list(ssid[index, :]))
-
-        dataset_value_array[data_index[0], :] = np.hstack(data[:])
-        all_parameter_change[data_index[0], :] = np.hstack(changes[:])
-        all_parameter_ids.append(parameter_id)
-        all_parameter_values.append(parameter_value)
-        all_parameter_changes.append(parameter_change)
-        all_ssid.append(np.array(ssid_changes))
-
-    dataset_keys = ['values', 'details', 'boolean', 'parameter_ids',
-                    'parameter_values', 'parameter_changes', 'ssid', 'experiment_id',
-                    'dataset_id']
-    experimental_data = dict(zip(dataset_keys, [dataset_value_array, all_parameter_change, data_combination_boolean,
-                                                all_parameter_ids, all_parameter_values, all_parameter_changes,
-                                                all_ssid, experiment_index_iter, choose]))
     return experimental_data
