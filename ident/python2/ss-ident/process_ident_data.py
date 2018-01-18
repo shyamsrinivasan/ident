ac9eed985c0d82224b53084624c480dd45bff053
shyamsrinivasan <shyamsweth@gmail.com>
2018-01-18 17:14:18 -0500

    new definition to experiments in different positions for all identifying data sets for each parameter for each flux within each sample

--- ident/python2/ss-ident/process_ident_data.py
+++ ident/python2/ss-ident/process_ident_data.py
@@ -313,53 +313,49 @@ def dataset_with_experiment(data_exp, exp_id):
     return lst_data_id
 
 
-def experiments_in_ident_data(data_p_boolean, data_exp, exp_types, data_id):
-    """get all data sets that identify parameter j and get different types of experiments in these data sets"""
-    number_of_experiments_per_data = 3
-    number_exp_types = len(exp_types)
-    data_identifying_p = []
-    exp_data_parameter_info = []
-    for j_p, data_set_boolean in enumerate(data_p_boolean):
-        data_identifying_parameter_j = [j_data for j_data, value in enumerate(data_set_boolean) if value]
-        data_identifying_p.append(data_identifying_parameter_j)
-        chosen_data_experiments = [j_data_set for data_set_id, j_data_set in enumerate(data_exp["experiment_id"])
-                                   if data_set_id in data_identifying_parameter_j]
-        chosen_data_experiments = np.array(chosen_data_experiments)
-        # get number of experiments of each type at each position
-        exp_types_classification = []
-        for i_exp_type in exp_types:
-            try:
-                pos_based_lst = [[chosen_data_experiments[:, iexp] == np.array(j_exp_id) for j_exp_id in i_exp_type]
-                                 for iexp in range(0, number_of_experiments_per_data)]
-            except IndexError:
-                pos_based_lst = []
-            exp_types_classification.append(pos_based_lst)
-        # get experiment type numbers and percentages within data set at each position
-        total_identifying_experiments = chosen_data_experiments.shape[0]
-        counter_exp_type = 0
-        # print('Parameter {}'.format(j_p))
-        exp_type_all_pos_info = []
-        for j_exp_type, exp_id in zip(exp_types_classification, exp_types):
-            all_exp_types_info = []
-            for i_pos, i_exp_pos in enumerate(j_exp_type):
-                all_pos_info = []
-                # print('Experiment Position in Data sets {}'.format(i_pos))
-                for k_exp_at_i_exp_pos_in_j_exp_type, k_exp in zip(i_exp_pos, exp_id):
-                    total_number_of_k_exp_at_i_pos_in_j_exp_type = sum(k_exp_at_i_exp_pos_in_j_exp_type)
-                    percentage_of_k_exp_at_i_pos_in_j_exp_type = \
-                        float(sum(k_exp_at_i_exp_pos_in_j_exp_type))/float(total_identifying_experiments)
-                    info = {'type':counter_exp_type, 'id':k_exp,
-                            'occurrence':total_number_of_k_exp_at_i_pos_in_j_exp_type,
-                            'occurrence percentage':percentage_of_k_exp_at_i_pos_in_j_exp_type,
-                            'position':i_pos}
-                    all_pos_info.append(info)
-                    pass
-                # all_pos_info.append(all_pos_info)
-                all_exp_types_info.append(all_pos_info)
-            counter_exp_type += 1
-            exp_type_all_pos_info.append(all_exp_types_info)
-        exp_data_parameter_info.append(exp_type_all_pos_info)
-    return exp_data_parameter_info
+def experiments_in_ident_data(boolean_ident_data, experiment_data, experiment_type_index):
+    """get all data combinations identifying a given parameter within a given flux (passed as input)
+    and identify experiments used within the identifying data combination"""
+    number_of_data, number_of_parameter = boolean_ident_data.shape
+    boolean_ident_data = boolean_ident_data.transpose()
+    all_parameter_experiment_type_info = []
+    for j_parameter, j_parameter_info in enumerate(boolean_ident_data):
+        # get data combinations identifying parameter j
+        data_identifying_parameter_j = [j_data for j_data, bool_value in enumerate(j_parameter_info) if bool_value]
+
+        # get specific id of experiments in data combinations
+        experiments_in_identifying_data = [experiment_data["experiments"][j_data_id]
+                                           for j_data_id in data_identifying_parameter_j]
+        number_of_experiments_in_combination = len(experiment_data["experiments"][0])
+
+        # get unique experiments, experiment type and frequency in each position of combination
+        experiments_in_identifying_data = np.array(experiments_in_identifying_data)
+        all_position_experiment_info = []
+        for j_position_in_combination in range(0, number_of_experiments_in_combination):
+            experiment_type_boolean = [[True if exp_id in i_experiment_type else False
+                                        for exp_id in experiments_in_identifying_data[:, j_position_in_combination]]
+                                       for i_experiment_type in experiment_type_index]
+
+            # get experiment type frequency
+            experiment_type_frequency = [sum(i_experiment_type_boolean)
+                                         for i_experiment_type_boolean in experiment_type_boolean]
+            total_identifying_data = sum(experiment_type_frequency)
+            experiment_type_percentage = [float(i_frequency)*100/total_identifying_data
+                                          for i_frequency in experiment_type_frequency]
+
+            # get corresponding data combination ids
+            experiment_type_data_id = [[data_identifying_parameter_j[id]
+                                        for id, val in enumerate(i_experiment_type_boolean) if val]
+                                       for i_experiment_type_boolean in experiment_type_boolean]
+
+            all_position_experiment_info.append({"boolean": experiment_type_boolean,
+                                                 "frequency": experiment_type_frequency,
+                                                 "percentage": experiment_type_percentage,
+                                                 "data id": experiment_type_data_id,
+                                                 "position": j_position_in_combination})
+        all_parameter_experiment_type_info.append(all_position_experiment_info)
+
+    return all_parameter_experiment_type_info
 
 
 def experiments_per_sample_for_ident(all_sample_p_boolean, data_exp, exp_types, data_id):
