e6a7d3279abe5a5e7342eec2d1e7b10281fe6dde
shyamsrinivasan <shyamsweth@gmail.com>
2018-01-19 20:33:20 -0500

    add call_truncate_method from kotte_model

--- ident/python2/ss-ident/identifiability_analysis.py
+++ ident/python2/ss-ident/identifiability_analysis.py
@@ -11,3 +11,13 @@ def truncate_values(f, n=3):
         return float('.'.join([i, (d+'0'*n)[:n]]))
     else:
         return f
+
+
+def call_truncate_method(ident_value_list, parameter_count, expression_count=3):
+    """calculate truncate_values using map on list of values"""
+    flux_ident_value = np.zeros((parameter_count, expression_count))
+    for i, j in enumerate(ident_value_list):
+        trunc_value = map(truncate_values, j)
+        # trunc_value = map(float, trunc_value)
+        flux_ident_value[i, :] = np.array(trunc_value)
+    return flux_ident_value
