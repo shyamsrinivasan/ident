18f94beffd1c96338805ec76b82dc2297ee208a9
shyamsrinivasan <shyamsweth@gmail.com>
2018-01-26 19:21:31 -0500

    change flux choice term for testing

--- ident/python2/ss-ident/no_noise_script.py
+++ ident/python2/ss-ident/no_noise_script.py
@@ -29,7 +29,7 @@ choose_2 = range(0, 306) # choose numbr of experimental datasets to use of analy
 experimental_datasets_2_expts = \
     arrange_experimental_data(exp_xss, exp_fss, perturbation_details, 2, exp_flux_index, choose_2)
 ident_details_2 = flux_ident_2_data_combination(experimental_datasets_2_expts, choose=choose_2,
-                                                flux_ids=[1, 2], flux_choice=[0, 0])
+                                                flux_ids=[1, 2], flux_choice=[2, 0])
 print('Identifiability analysis for fluxes with 2 parameters complete.\n')
 # data processing
 experiment_type_indices = [[0], [1, 2], [3, 4, 5, 6, 7], [8, 9, 10, 11, 12], [13, 14, 15, 16, 17]]
