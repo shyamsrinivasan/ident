3ed4d746f0f43b7ea43f5eee5058d5d0d8e3c9b9
shyamsrinivasan <shyamsweth@gmail.com>
2018-01-26 19:19:42 -0500

    add  flux choice parameter

--- ident/python2/ss-ident/kotte_model.py
+++ ident/python2/ss-ident/kotte_model.py
@@ -988,20 +988,28 @@ def flux_3_ident_expression(experimental_data):
            [k3pep_nr_2_value, k3pep_dr_2_value, k3pep_2_value]
 
 
-def ident_parameter_name(parameter_id, flux_name=()):
+def ident_parameter_name(parameter_id, flux_name=(), flux_choice_id=0):
     parameter_list = ['V1max', 'K1ac (no enz)', 'k1cat', 'K1ac (enz)',
                       'V2max', 'K2pep',
                       'V3max (1)', 'K3fdp (1)', 'K3pep (1)',
                       'V3max (2)', 'K3fdp (2)', 'K3pep (2)']
-    flux_parameter_list = {"flux1":['V1max', 'K1ac (no enz)', 'k1cat', 'K1ac (enz)'],
-                           "flux2":['V2max', 'K2pep'],
-                           "flux3":['V3max (1)', 'K3fdp (1)', 'K3pep (1)',
-                                    'V3max (2)', 'K3fdp (2)', 'K3pep (2)']}
+    # flux_parameter_list = {"flux1":['V1max', 'K1ac (no enz)', 'k1cat', 'K1ac (enz)'],
+    #                        "flux2":['V2max', 'K2pep'],
+    #                        "flux3":['V3max (1)', 'K3fdp (1)', 'K3pep (1)',
+    #                                 'V3max (2)', 'K3fdp (2)', 'K3pep (2)']}
+    alter_list = {"flux1": [['V1max', 'K1ac (no enz)', 'k1cat', 'K1ac (enz)'],
+                            ['V1max', 'K1ac (no enz)'],
+                            ['k1cat', 'K1ac (enz)']],
+                  "flux2": [['V2max', 'K2pep']],
+                  "flux3": [['V3max (1)', 'K3fdp (1)', 'K3pep (1)', 'V3max (2)', 'K3fdp (2)', 'K3pep (2)'],
+                            ['V3max (1)', 'K3fdp (1)', 'K3pep (1)'],
+                            ['V3max (2)', 'K3fdp (2)', 'K3pep (2)']]}
     if flux_name:
         try:
-            parameter_name = [flux_parameter_list[name][id] for name, id in zip(flux_name, parameter_id)]
+            parameter_name = [alter_list[name][choice_id][id]
+                              for name, choice_id, id in zip(flux_name, flux_choice_id, parameter_id)]
         except TypeError:
-            parameter_name = flux_parameter_list[flux_name][parameter_id]
+            parameter_name = alter_list[flux_name][flux_choice_id][parameter_id]
     else:
         try:
             parameter_name = [parameter_list[id] for id in parameter_id]
