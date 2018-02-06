14488ac711b56f9ae173f977167e82dbae90ecf6
shyamsrinivasan <shyamsweth@gmail.com>
2018-02-06 14:05:49 -0500

    plot dynamic concentrations from all experiments in single figure

--- ident/python2/ss-ident/generate_noisy_data.py
+++ ident/python2/ss-ident/generate_noisy_data.py
@@ -4,6 +4,7 @@ from kotte_model import *
 from simulate_ode import run_ode_sims
 from add_noise import add_noise_dynamic
 from plot_profiles import plot_multiple_dynamics
+from plot_ident_results import plot_dynamic_sim_concentrations
 from copy import deepcopy
 
 
@@ -162,7 +163,8 @@ def run_no_noise_parameter_perturbation(parameter_perturbation, y0, other_option
 
     # plot all dynamic courses
     if plot_arg:
-        plot_multiple_dynamics(dynamic_info)
+        # plot_multiple_dynamics(dynamic_info)
+        plot_dynamic_sim_concentrations(dynamic_info, multiple=1)
         # plt.close("all")
         # plot_multiple_dynamics(dynamic_info)
         plt.close("all")
