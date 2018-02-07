e9bb7f1f5a3120ee287db7da0327e9986baed1ec
shyamsrinivasan <shyamsrinivasan@users.noreply.github.com>
2018-02-06 23:21:05 -0500

    no major changes

--- ident/python2/ss-ident/no_noise_script.py
+++ ident/python2/ss-ident/no_noise_script.py
@@ -31,8 +31,8 @@ ode_parameter_values = {"K1ac": np.array([.1]),
 
 # get experimental system steady state data without noise
 exp_xss, exp_fss, exp_ssid, perturbation_details = \
-    generate_expdata(y0, cvode_options, ode_parameter_values, noise=0, kinetics=1, dynamic_plot=1,
-                     perturbation_plot=1)
+    generate_expdata(y0, cvode_options, ode_parameter_values, noise=0, kinetics=1, dynamic_plot=0,
+                     perturbation_plot=0)
 
 # arrange experimental data to form multiple data sets
 exp_flux_index = np.array([0, 3, 2, 4])
