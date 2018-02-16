f76b169c65b29ee3891c08b4a651de3725ee3ffe
shyamsrinivasan <shyamsweth@gmail.com>
2018-02-16 17:03:51 -0500

    vectorize calcs

--- ident/numerical/kotteflux.m
+++ ident/numerical/kotteflux.m
@@ -15,18 +15,18 @@ function flux = kotteflux(y, p)
     V2max = p(13);
     ac = p(14);
 
-    flux_1 = k1cat * y(3) * ac / (ac + K1ac);
-    flux_2 = vemax * (1 - 1 / (1 + (Kefdp / y(2))^ne));
+    flux_1 = k1cat.*y(3, :).*ac./(ac + K1ac);
+    flux_2 = vemax.*(1 - 1./(1 + (Kefdp./y(2, :)).^ne));
     % convenience kinetics for flux 3
-    fdp_sat = 1 + y(2)./K3fdp;
-    pep_sat = 1 + y(1)./K3pep;
+    fdp_sat = 1 + y(2, :)./K3fdp;
+    pep_sat = 1 + y(1, :)./K3pep;
     % v3 with MWC kinetics
-    flux_3 = V3max.*(fdp_sat - 1).*(fdp_sat^3)./...
+    flux_3 = V3max.*(fdp_sat - 1).*(fdp_sat.^3)./...
             (fdp_sat.^4+L3fdp.*(pep_sat.^(-4)));
-    flux_4 = V2max * y(1) / (y(1) + K2pep);
-    flux_5 = V4max * y(1);
-    flux_6 = d * y(3);
-    flux = [flux_1, flux_2, flux_3, flux_4, flux_5, flux_6]';
+    flux_4 = V2max.*y(1, :)./(y(1, :) + K2pep);
+    flux_5 = V4max.*y(1, :);
+    flux_6 = d.*y(3, :);
+    flux = [flux_1;flux_2;flux_3;flux_4;flux_5;flux_6]';
 
     
     
