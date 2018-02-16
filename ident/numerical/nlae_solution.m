b739b47423a8cb30d38f1dfca3b34538976eb720
shyamsrinivasan <shyamsweth@gmail.com>
2018-02-15 21:28:37 -0500

    solve nlae system (provided as inoput fun handle) using fsolve

--- ident/numerical/nlae_solution.m
+++ ident/numerical/nlae_solution.m
@@ -1,2 +1,33 @@
-function solution = nlae_solution(initial_value)
-	% use fsolve to solve NLAE equation for parameters for flux v3
+function solution = nlae_solution(fun, p, initial_value, typical_vals)
+if nargin<4
+    typical_vals = [];
+end
+if nargin<3
+    initial_value = ones(3, 1);
+end
+if ~isempty(p)
+    nlaefun = @(x)fun(x, p);
+else
+    nlaefun = fun;
+end
+
+% set fsolve options
+options = optimoptions('fsolve','MaxIter',500,...
+                                'TolFun',1e-6,...
+                                'TolX',1e-6);
+if ~isempty(typical_vals)
+    options = optimoptions(options,'TypicalX',typical_vals);
+end
+% use fsolve to solve NLAE equation for parameters for flux v3
+[x, fval, exitflag] = fsolve(nlaefun, initial_value, options);
+
+if exitflag
+    solution.p = x;
+    solution.res = fval;
+else
+    solution.p = [];
+    solution.res = [];
+end
+solution.flag = exitflag;
+
+return
