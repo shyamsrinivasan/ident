2e235919e80330635bd1cec94ba5fceed30b2e05
shyamsrinivasan <shyamsweth@gmail.com>
2018-02-16 22:24:28 -0500

    use plotting fun to plot dynamic

--- ident/numerical/script.m
+++ ident/numerical/script.m
@@ -6,16 +6,14 @@ solver_options.abstol = 1e-8;
 fun_p = [.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, .1];
 solution = run_ode(@kotteode, solver_options, fun_p, @kotteflux);
 % plot results for verification
-subplot(211)
-plot(solution.t, solution.y)
-subplot(212)
-plot(solution.t, solution.flux)
+plot2d_dynamic(solution, 1, 1);
 
 % parameter perturbations
 perturbations = perturbations_info();
 solver_options.x0 = solution.yss;
 perturbed_solution = perturb_parameters(@kotteode, solver_options,...
                                         fun_p, perturbations, @kotteflux);
+% plot2d_dynamic(perturbed_solution, 1, 1);
                                     
 % arrange simulated data for parameter estimation
  
