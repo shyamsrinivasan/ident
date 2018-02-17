096535fb6478e238de6a4151d9f6e2e5ce776ccd
shyamsrinivasan <shyamsweth@gmail.com>
2018-02-16 21:39:16 -0500

    fun to plot dynamic concentrations and fluxes

--- ident/numerical/plot2d_dynamic.m
+++ ident/numerical/plot2d_dynamic.m
@@ -1,14 +1,29 @@
-function plot2d_dynamic(xdata,ydata,axish,hf)
-if nargin<4
-    hf = figure;
-end
+function plot2d_dynamic(solution, concentration, flux)
 if nargin<3
-    axish = 0;
+    flux=0;
 end
-
-if axish
-    axish.NextPlot = 'add';    
+if nargin<2
+    concentration=0;
 end
+hfig = figure;
+nplots = size(solution,2);
+ncolumns = 2;
+if nplots>1    
+    if rem(ndata,2)==0
+        nrows = ndata/2;
+    else
+        nrows = (ndata+1)/2;
+    end    
+else
+    nrows = 1;
+end
+haxis = zeros(nplots, 1);
 
+if concentration
+    plot_concentration(solution,hfig,haxis,nplots,nrows,ncolumns);
+end
+if flux
+    plot_flux(solution,hfig,haxis,nplots,nrows,ncolumns);
+end
 
     
