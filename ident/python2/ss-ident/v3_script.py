41af0a873eb0156abfc8b167031449e2f5ea7c87
shyamsrinivasan <shyamsweth@gmail.com>
2018-02-23 14:22:57 -0500

    use correct flux choice

--- ident/python2/ss-ident/v3_script.py
+++ ident/python2/ss-ident/v3_script.py
@@ -79,7 +79,7 @@ data_utility_plot(data_list_v3_root1)
 print('Practical Identifiability Analysis of v3 with 3 parameters: V3max, K3fdp and K3pep \n')
 # perform identifiability when v3 parameters are written for root (2)
 ident_details_v3_root2 = flux_ident_3_data_combination(experimental_datasets_3_expts, choose=combination_choice,
-                                                       flux_ids=[3], flux_choice=[1], ident_fun_choice=ident_fun_choice)
+                                                       flux_ids=[3], flux_choice=[2], ident_fun_choice=ident_fun_choice)
 print('Identifiability analysis for v3 with 3 parameters (V3max, K3fdp and K3pep) complete.\n')
 # data processing
 data_list_v3_root2, max_parameter_v3_root2, true_value_v3_root2, experiment_info_v3_root2, \
