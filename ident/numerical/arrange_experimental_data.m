59cc4bb742017ac589f67326f186b46efb97968b
shyamsrinivasan <shyamsweth@gmail.com>
2018-02-28 17:34:08 -0500

    get ss concentration and flux data for selected data combinations

--- ident/numerical/arrange_experimental_data.m
+++ ident/numerical/arrange_experimental_data.m
@@ -1,9 +1,10 @@
 % arrange experimental data (parameters) to perform numerical analysis of
 % identifiability for v3 in small kotte model
-function ident_data = arrange_experimental_data(perturbation_data,...
+function chosen_data = arrange_experimental_data(perturbation_data,...
                                                 nexpts,...
+                                                flux_id,...
                                                 select_exp_id)
-if nargin<3
+if nargin<4
     select_exp_id = [];
 end
 nperts = size(perturbation_data, 2);
@@ -19,9 +20,8 @@ else
 end
 
 % get data for chosen combos
-
+chosen_data = choose_experimental_data(chosen_combos,perturbation_data,nexpts,flux_id);
 return
-
 
 function selected_combos = select_combos(all_combos, nexpts, select_exp_id)
 % select only combos that have select_exp_id in them
@@ -38,5 +38,42 @@ end
 useful_combo_id = logical(sum(useful_combos, 2));
 selected_combos = all_combos(useful_combo_id, :);
 return
+
+function chosen_data = choose_experimental_data(chosen_combos,...
+                                                perturbation_data,...
+                                                nexpts,...
+                                                flux_id)
+% select data from experiments in chosen_combos for each combination in 
+% chosen_combos
+% 3 concentrations(pep, fdp, E) + 4 fluxes(v1,v2,v3,v4)+ 1
+% parameter(acetate)
+data_size = 8; 
+ncombos = size(chosen_combos,1);
+chosen_data = zeros(ncombos, data_size*nexpts);
+for icombo = 1:ncombos
+    chosen_data(icombo, :) =...
+        select_experimental_data(perturbation_data,...
+                                 chosen_combos(icombo, :),...
+                                 flux_id);    
+end
+return
+
+function dataset =...
+    select_experimental_data(perturbation_data, exp_id, flux_id)
+% get data for each experiment within each chosen combination passed as
+% input
+if nargin<3
+    flux_id = 0:length(perturbation_data(1).fss);
+end
+dataset = struct();
+nexpts = length(exp_id);
+for iexp = 1:nexpts
+    dataset(iexp).data = [perturbation_data(exp_id(iexp)).parameter(end);...
+                          perturbation_data(exp_id(iexp)).yss;...
+                          perturbation_data(exp_id(iexp)).fss(flux_id)];
+end
+dataset = cat(1, dataset.data);
+return
+
 
 
