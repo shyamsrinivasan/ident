ebca19f36c4ace505deb68f1f1ac3fefc13adff4
shyamsrinivasan <shyamsweth@gmail.com>
2018-02-28 20:10:02 -0500

    loop through every data combination and solve nlae for parameter values

---
+++ ident/numerical/do_numerical_ident.m
@@ -0,0 +1,30 @@
+function solution = do_numerical_ident(exp_data,ident_struct)
+ndata = size(exp_data,1);
+% get nlae solution for every data set in nadata
+% consider paralleling
+if isfield(ident_struct,'fun')
+    ident_fun = ident_struct.fun;
+end
+if isfield(ident_struct,'initial_value')
+    initial_val = ident_struct.initial_value;
+end
+if isfield(ident_struct,'typical_value')
+    typical_val = ident_struct.typical_value;
+end
+
+solution = struct([]);
+% assign structure fields in case of parallelization
+for idata = 1:ndata
+    solution(idata).p = [];
+    solution(idata).res = [];
+    solution(idata).flag = [];
+end
+for idata = 1:ndata
+    single_solution =...
+    nlae_solution(ident_fun,exp_data(idata, :),initial_val, typical_val);
+    solution(idata).p = single_solution.p;
+    solution(idata).res = single_solution.res;
+    solution(idata).flag = single_solution.flag;
+end
+
+return
