1ed9157b5a88f02e1a73bdc9a351553f91046cdd
shyamsrinivasan <shyamsweth@gmail.com>
2018-03-01 15:51:54 -0500

    specify ode and flux funs as single vars for future use

--- ident/numerical/script.m
+++ ident/numerical/script.m
@@ -1,18 +1,22 @@
+% specify ode fun and flux fun
+odefun = @kotteode;
+fluxfun = @kotteflux;
+
 % run ode to simulate data
 solver_options.tspan = 0:.1:250;
 solver_options.x0 = [5;1;1];
 solver_options.reltol = 1e-6;
 solver_options.abstol = 1e-8;
 fun_p = [.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, .1];
-solution = run_ode(@kotteode, solver_options, fun_p, @kotteflux);
+solution = run_ode(odefun, solver_options, fun_p, fluxfun);
 % plot results for verification
 plot2d_dynamic(solution, 1, 1);
 
 % parameter perturbations
 perturbations = perturbations_info();
 solver_options.x0 = solution.yss;
-perturbed_solution = perturb_parameters(@kotteode, solver_options,...
-                                        fun_p, perturbations, @kotteflux);
+perturbed_solution = perturb_parameters(odefun, solver_options,...
+                                        fun_p, perturbations, fluxfun);
 plot2d_dynamic(perturbed_solution, 1, 1);
                                     
 % arrange simulated data for parameter estimation
