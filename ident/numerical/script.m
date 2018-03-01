6d4cabfe08cf470c5ead785176fc048625ef8df6
shyamsrinivasan <shyamsweth@gmail.com>
2018-02-28 20:12:13 -0500

    use mwc kinetics to generate data and call fun to loop through all data for identification

--- ident/numerical/script.m
+++ ident/numerical/script.m
@@ -4,15 +4,15 @@ solver_options.x0 = [5;1;1];
 solver_options.reltol = 1e-6;
 solver_options.abstol = 1e-8;
 fun_p = [.1, .1, 4e6, .1, .3, 1.1, .45, 2, .25, .2, 1, 1, 1, .1];
-solution = run_ode(@kotteCKode, solver_options, fun_p, @kotteCKflux);
+solution = run_ode(@kotteode, solver_options, fun_p, @kotteflux);
 % plot results for verification
 plot2d_dynamic(solution, 1, 1);
 
 % parameter perturbations
 perturbations = perturbations_info();
 solver_options.x0 = solution.yss;
-perturbed_solution = perturb_parameters(@kotteCKode, solver_options,...
-                                        fun_p, perturbations, @kotteCKflux);
+perturbed_solution = perturb_parameters(@kotteode, solver_options,...
+                                        fun_p, perturbations, @kotteflux);
 plot2d_dynamic(perturbed_solution, 1, 1);
                                     
 % arrange simulated data for parameter estimation
@@ -25,12 +25,26 @@ experimental_data =...
                               nexpts,...
                               flux_id,...
                               selected_expts);
- 
+
+% run numerical parameter estimation (w/MWC model) on all combinations 
+% in experimental_data 
+ident.fun = @estimate_v3_parameter;
+ident.initial_value = [1;.1;.1];
+ident.typical_value = [1;1e-1;1e-1];
+solution = do_numerical_ident(experimental_data,ident);
+
 % use simulated data from 3 different perturbations to solve nlae for
 % parameters
-initial_p_val = [1;.1;.1];
-typical_p_val = [1;1e-1;1e-1];
-nlae_solution(@estimate_v3_CK_parameter, experimental_data(1, :), initial_p_val, typical_p_val);
+% initial_p_val = [1;.1;.1];
+% typical_p_val = [1;1e-1;1e-1];
+% nlae_solution(@estimate_v3_CK_parameter, experimental_data(1, :), initial_p_val, typical_p_val);
+
+% estimate parameters for MWC model
+% initial_p_val = [1;.1;.1];
+% typical_p_val = [1;1e-1;1e-1];
+% nlae_solution(@estimate_v3_parameter, experimental_data(1, :), initial_p_val, typical_p_val);
+
+
 
 
 
