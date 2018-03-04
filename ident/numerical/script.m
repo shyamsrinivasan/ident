4717adb28273748c22da8a3fff96bc7bcc99dcf2
shyamsrinivasan <shyamsweth@gmail.com>
2018-03-04 18:54:01 -0500

    select fewer experiments for build and test

--- ident/numerical/script.m
+++ ident/numerical/script.m
@@ -23,7 +23,7 @@ perturbed_solution = perturb_parameters(odefun, solver_options,...
 nexpts = 3;
 flux_id = [1, 4, 3, 5];
 % choose only experiments w/0 v3max perturbation
-selected_expts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 17, 18, 19, 20, 21];
+selected_expts = [1, 2]; %, 3, 4, 5, 6, 7, 8, 9, 10, 11, 17, 18, 19, 20, 21];
 experimental_data =...
     arrange_experimental_data(perturbed_solution,...
                               nexpts,...
@@ -38,6 +38,7 @@ ident.typical_value = [1;1e-1;1e-1];
 solution = do_numerical_ident(experimental_data,ident);
 
 % process data
+process_ident_info(solution);
 
 
 % use simulated data from 3 different perturbations to solve nlae for
