abb8c23905b59afe2f279ea8e29f0a132ab0af23
shyamsrinivasan <shyamsweth@gmail.com>
2018-03-06 21:20:21 -0500

    get info on parameter values and get box plots

--- ident/numerical/get_boxplots.m
+++ ident/numerical/get_boxplots.m
@@ -1,4 +1,19 @@
 function hf = get_boxplots(data)
+% get info on positive parameter values only for each parameter
+all_p_value = data.p;
+pos_p_value_id = data.pos_p_combos;
+np = size(all_p_value, 1);
+pos_p_value = cell(np, 1);
+pos_p_group = cell(np, 1);
+for ip = 1:np
+    parameter_name = sprintf('parameter %d',ip);
+    pos_p_value{ip, 1} = all_p_value(ip, logical(pos_p_value_id(ip, :)));
+    pos_p_group{ip, 1} =...
+        repmat({parameter_name}, 1, length(pos_p_value{ip, 1}));
+end
+all_data = [pos_p_value{:}]';
+all_group = [pos_p_group{:}]';
+
 hf = figure;
-boxplot(data);
+boxplot(all_data, all_group);
 return
