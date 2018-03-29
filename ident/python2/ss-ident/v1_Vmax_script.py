78e782b39f1e4248e6e2b4bda3e486ce72f945fe
shyamsrinivasan <shyamsweth@gmail.com>
2018-03-29 17:27:30 -0400

    fun to plot metabolite and flux distribution from experimental data

--- ident/python2/ss-ident/v1_Vmax_script.py
+++ ident/python2/ss-ident/v1_Vmax_script.py
@@ -9,6 +9,7 @@ from plot_ident_results import plot_parameter_values
 from plot_ident_results import parameter_identifibaility_plot
 # from plot_ident_results import parameter_experiment_info_plot
 from plot_ident_results import parameter_experiment_info_spider
+from plot_ident_results import plot_experiment_data_dist
 
 
 # generate noisy experimental data for testing identifiability
@@ -79,6 +80,11 @@ parameter_identifibaility_plot(max_parameter_v1_k1cat)
 parameter_experiment_info_spider(experiment_info_v1_k1cat)
 # plot true parameter values and determined parameter values
 plot_parameter_values(true_value_v1_k1cat)
+
+# plot distribution of experimental data (concentration and fluxes)
+plot_experiment_data_dist(exp_xss, exp_fss, experiment_choice=[0, 1, 2, 3, 4, 5,
+                                                               11, 12, 13, 14, 15,
+                                                               16, 17, 18, 19, 20])
 
 # plot utility of data sets (number of data sets identifying n, n-1, n-2, ...., 1, 0 parameters
 data_utility_plot(data_list_v1_k1cat)
