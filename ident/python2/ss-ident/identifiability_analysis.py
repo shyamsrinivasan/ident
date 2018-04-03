17e38693bcea78845cd9af7e786427d09b58699c
shyamsrinivasan <shyamsweth@gmail.com>
2018-04-03 10:55:13 -0400

    add exception when input f to truncate is a numpy array

--- ident/python2/ss-ident/identifiability_analysis.py
+++ ident/python2/ss-ident/identifiability_analysis.py
@@ -4,9 +4,14 @@ import numpy as np
 def truncate_values(f, n=3):
     """truncates floats to n specified values after the decimal"""
     if not np.isnan(f):
-        s = '{}'.format(f)  # convert float to string
-        if 'e' in s or 'E' in s:
-            return float('{0:.{1}f}'.format(f, n))
+        if type(f) is not np.ndarray:
+            s = '{}'.format(f)  # convert float to string
+            if 'e' in s or 'E' in s:
+                return float('{0:.{1}f}'.format(f, n))
+        else:
+            s = '{}'.format(f[0])  # convert np.ndarray to string
+            if 'e' in s or 'E' in s:
+                return float('{0:.{1}f}'.format(f[0], n))
         i, p, d = s.partition('.')
         return float('.'.join([i, (d+'0'*n)[:n]]))
     else:
