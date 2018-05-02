4ce63dfb68cfef8a12277909c4c3be4ee1731a42
shyamsrinivasan <shyamsweth@gmail.com>
2018-05-02 17:26:58 -0400

    remove second initial condition

--- ident/python2/ss-ident/v3_ck_numerical_ident.py
+++ ident/python2/ss-ident/v3_ck_numerical_ident.py
@@ -55,7 +55,7 @@ optim_options = {"solver": "ipopt",
 #                  "opts": {"qpsol": "qpoases"}}
 initial_value = [200, 100, 100, 0, 0, 0]
 # randomized_initial_values = generate_random_initial_conditions(initial_value, 10, negative=1)
-v3_all_x0_parameter_info = solve_multiple_initial_conditions(all_initial_conditions=[initial_value, [10, 1, 1, 0, 0, 0]],
+v3_all_x0_parameter_info = solve_multiple_initial_conditions(all_initial_conditions=[initial_value],
                                                              experimental_data=experimental_datasets_3_expts[0]["values"],
                                                              chosen_fun=0, optim_options=optim_options,
                                                              number_of_parameters=3, flux_id=3, flux_choice=[3])
