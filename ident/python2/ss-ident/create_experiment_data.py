46954ada334387928061916e7bd900dd69218b8e
shyamsrinivasan <shyamsrinivasan@users.noreply.github.com>
2018-05-12 00:41:29 -0400

    no major changes

--- ident/python2/ss-ident/create_experiment_data.py
+++ ident/python2/ss-ident/create_experiment_data.py
@@ -130,6 +130,6 @@ def retrieve_experimental_data(file_name, multi_index_lablel):
 
 
 if __name__ == "__main__":
-    file_name = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\ident\python2\ss-ident\experiments_noise_500_samples'
-    # experiment_info_df = create_experiment_data(file_name, noise=0, kinetics=2)
-    experiment_info_df = create_experiment_data(file_name, noise=1, kinetics=2, number_samples=500, noise_std=0.05)
+    file_name = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\ident\python2\ss-ident\experiments'
+    experiment_info_df = create_experiment_data(file_name, noise=0, kinetics=2)
+    # experiment_info_df = create_experiment_data(file_name, noise=1, kinetics=2, number_samples=500, noise_std=0.05)
