68768b8d5c79387af6135a85ebc617d7f9189d04
shyamsrinivasan <shyamsweth@gmail.com>
2018-05-24 15:21:32 -0400

    comment data generation portion of code

--- ident/python2/ss-ident/v1_kcat_mwc_script.py
+++ ident/python2/ss-ident/v1_kcat_mwc_script.py
@@ -9,8 +9,8 @@ from names_strings import true_parameter_values
 
 
 # create data for identifiability analysis
-from create_experiment_data import create_data_for_flux
-create_data_for_flux(flux_id='v1', noise=0, number_samples=1, kinetics=1)
+# from create_experiment_data import create_data_for_flux
+# create_data_for_flux(flux_id='v1', noise=0, number_samples=1, kinetics=1)
 
 # extract experimental data from file
 new_data_file_name = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels' \
@@ -24,11 +24,11 @@ arranged_data_df = retrieve_experimental_data_from_file(data_file_name=new_data_
 storage_file_name = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels' \
                      '\ident\python2\ss-ident\ident_v1_kcat_mwc'
 # choose identifiability functions to test
-ident_fun_choice = [0]
-# test identifiability and store data to file
-from kotte_model import flux_ident_2_data_combination
-flux_ident_2_data_combination(arranged_data_df, flux_ids=[1], flux_choice=[2],
-                              ident_fun_choice=ident_fun_choice, file_name=storage_file_name)
+# ident_fun_choice = [0]
+# # test identifiability and store data to file
+# from kotte_model import flux_ident_2_data_combination
+# flux_ident_2_data_combination(arranged_data_df, flux_ids=[1], flux_choice=[2],
+#                               ident_fun_choice=ident_fun_choice, file_name=storage_file_name)
 
 # retrieve identifiability data from file and process information
 ident_index_label = ['sample_name', 'data_set_id']
@@ -40,21 +40,21 @@ all_parameter_info = process_ident(ident_df, arranged_data_df)
 # get info from data sets that identify all 3 parameters
 validation_file_name = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\ident\python2' \
                        '\ss-ident\ident_validate_v1_mwc'
-from process_ident_data import get_parameter_value
-validation_info = get_parameter_value(all_parameter_info, ident_df)
+# from process_ident_data import get_parameter_value
+# validation_info = get_parameter_value(all_parameter_info, ident_df)
 # get default parameter values
 default_parameter_values = true_parameter_values()
 
 # initial value used to generate experimental data
-import numpy as np
-y0 = np.array([5, 1, 1])
-from validate_estimation import validate_model
-# integrator options
-cvode_options = {'iter': 'Newton', 'discr': 'Adams', 'atol': 1e-10, 'rtol': 1e-10, 'time_points': 200,
-                 'display_progress': False, 'verbosity': 50}
-validate_model(y0, cvode_options, default_parameter_values, validation_info,
-               save_file_name=validation_file_name,
-               ss=1, dyn=0, noise=0, kinetics=1)
+# import numpy as np
+# y0 = np.array([5, 1, 1])
+# from validate_estimation import validate_model
+# # integrator options
+# cvode_options = {'iter': 'Newton', 'discr': 'Adams', 'atol': 1e-10, 'rtol': 1e-10, 'time_points': 200,
+#                  'display_progress': False, 'verbosity': 50}
+# validate_model(y0, cvode_options, default_parameter_values, validation_info,
+#                save_file_name=validation_file_name,
+#                ss=1, dyn=0, noise=0, kinetics=1)
 
 # retrieve validation info from file
 validate_index_labels = ['estimate_id', 'sample_name', 'data_set_id', 'experiment_id']
