0e330155abdceddf4275f9cdbc2345afac56f22f
shyamsrinivasan <shyamsrinivasan@users.noreply.github.com>
2018-05-29 17:43:32 -0400

    change all file paths to relative os independent paths

--- ident/python2/ss-ident/v3_ck_numerical_ident.py
+++ ident/python2/ss-ident/v3_ck_numerical_ident.py
@@ -5,6 +5,7 @@ from names_strings import true_parameter_values
 from validate_estimation import process_validation_info
 from plot_ident_results import parameter_values_plot
 from plot_ident_results import validation_plot
+import os, os.path
 
 
 # create data for identifiability analysis
@@ -12,16 +13,14 @@ from plot_ident_results import validation_plot
 # create_data_for_flux(flux_id='v3', noise=0, number_samples=1)
 
 # extract experimental data from file
-new_data_file_name = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels' \
-                     '\ident\python2\ss-ident\exp_v3_3_experiments'
+new_data_file_name = os.path.join(os.getcwd(), 'exp\exp_v3_3_experiments')
 index_labels = ['sample_name', 'data_set_id', 'experiment_id']
 arranged_data_df = retrieve_experimental_data_from_file(data_file_name=new_data_file_name,
                                                         multi_index_label=index_labels)
 
 # perform identifiability when v3 parameters are written using convenience kinetics
 # get combination of 3 experiments and perform identifiability on all fluxes that require 3 data sets
-storage_file_name = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels' \
-                     '\ident\python2\ss-ident\ident_numerical_v3_l1_obj'
+storage_file_name = os.path.join(os.getcwd(), 'ident\ident_numerical_v3_l1_obj')
 # lexographic ordering of df indices
 all_exp_data = data_numerical_ident(arranged_data_df, 'sample_0')
 
@@ -64,8 +63,7 @@ validation_info = get_parameter_value(all_parameter_info, numerical_ident_df)
 # cvode_options = {'iter': 'Newton', 'discr': 'Adams', 'atol': 1e-10, 'rtol': 1e-10, 'time_points': 200,
 #                  'display_progress': False, 'verbosity': 50}
 # validate all parameter values
-validation_file_name = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\ident\python2' \
-                       '\ss-ident\ident_numerical_validate_v3_root_1'
+validation_file_name = os.path.join(os.getcwd(), 'validate\ident_numerical_validate_v3_l1_obj')
 # from validate_estimation import validate_model
 # validate_model(y0, cvode_options, default_parameter_values, validation_info,
 #                save_file_name=validation_file_name,
@@ -76,8 +74,7 @@ validate_index_labels = ['estimate_id', 'sample_name', 'data_set_id', 'experimen
 validate_df = retrieve_experimental_data_from_file(data_file_name=validation_file_name,
                                                    multi_index_label=validate_index_labels)
 # validation plot
-original_experiment_file = 'C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\ident\python2' \
-                            '\ss-ident\experiments'
+original_experiment_file = os.path.join(os.getcwd(), 'exp\experiments')
 exp_df = retrieve_experimental_data_from_file(data_file_name=original_experiment_file,
                                               multi_index_label=['sample_name', 'experiment_id'])
 
