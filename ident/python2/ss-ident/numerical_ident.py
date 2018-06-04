89ace2138fb43be71226316617a4a5d39267cbb4
shyamsrinivasan <shyamsrinivasan@users.noreply.github.com>
2018-06-04 15:19:38 -0400

    new option for mwc model with l1 and l2 objectives and change fun name for mwc estimation

--- ident/python2/ss-ident/numerical_ident.py
+++ ident/python2/ss-ident/numerical_ident.py
@@ -130,7 +130,7 @@ def v3_numerical_ck_all(chosen_data):
     return nlp
 
 
-def v3_numerical_problem(chosen_data):
+def v3_mwc_numerical_problem(chosen_data):
     """fun to calculate objectives and constraints for numerical identification of v3
         using MWC kinetics"""
 
@@ -166,6 +166,51 @@ def v3_numerical_problem(chosen_data):
     objective = error[0] ** 2
     for i_error_id in range(1, number_data):
         objective += error[i_error_id] ** 2
+
+    # get complete set of all optimization variables vertcat([parameters, error])
+    x = casadi.vertcat(*[v3max, k3fdp, k3pep, l3fdp, error])
+
+    nlp = {'x': x, 'f': objective, 'g': constraints}
+
+    return nlp
+
+
+def v3_mwc_numerical_problem_l1_obj(chosen_data):
+    """fun to calculate objectives and constraints for numerical identification of v3
+            using MWC kinetics"""
+
+    number_data = len(chosen_data)
+    # symbolic variables for use with casadi
+    v3max = casadi.SX.sym("v3max")
+    k3fdp = casadi.SX.sym("k3fdp")
+    k3pep = casadi.SX.sym("k3pep")
+    l3fdp = casadi.SX.sym("l3fdp")
+    error = casadi.SX.sym("error", number_data)
+
+    # fdp_sat = 1 + y[1] / K3fdp
+    # pep_sat = 1 + y[0] / K3pep
+    # flux_3 = V3max * (fdp_sat - 1) * (fdp_sat ** 3) / (fdp_sat ** 4 + L3fdp * (pep_sat ** (-4)))
+
+    all_constraints = []
+    for i_data_id, i_data in enumerate(chosen_data):
+        _, pep, fdp, _, _, _, v3, _, _, _ = list(i_data)
+
+        # flux equation for each experiment i_data in chosen_data
+        fdp_sat = 1 + fdp / k3fdp
+        pep_sat = 1 + pep / k3pep
+        nr_3 = v3max * (fdp_sat - 1) * (fdp_sat ** 3)
+        dr_3 = (fdp_sat ** 4 + l3fdp * 1e6 * pep_sat ** (-4))
+        flux_3 = nr_3 / dr_3
+        # formulate constraint for each experimental data set
+        all_constraints.append(flux_3 - v3 - error[i_data_id])
+
+    # create casadi array of constraints
+    constraints = casadi.vertcat(*all_constraints)
+
+    # create complete casadi objective fun
+    objective = error[0]
+    for i_error_id in range(1, number_data):
+        objective += error[i_error_id]
 
     # get complete set of all optimization variables vertcat([parameters, error])
     x = casadi.vertcat(*[v3max, k3fdp, k3pep, l3fdp, error])
@@ -301,11 +346,8 @@ def identify_all_data_sets(experimental_data, chosen_fun, x0, prob, optim_option
         error_names = ["eps_1", "eps_2", "eps_3"]
         cons_name = ["cons_1", "cons_2", "cons_3"]
     elif chosen_fun == 2:
-        ident_fun = v3_numerical_problem
-        arg = {"lbx": 8 * [0],
-               "ubx": [2, 1, 1, 5, .1, .1, .1, .1],
-               "lbg": 4 * [0],
-               "ubg": 4 * [0]}
+        ident_fun = v3_mwc_numerical_problem
+        arg = prob
         variable_name = ["V3max", "K3fdp", "K3pep", "L3fdp"]
         error_names = ["eps_1", "eps_2", "eps_3", "eps_4"]
         cons_name = ["cons_1", "cons_2", "cons_3", "cons_4"]
@@ -319,6 +361,12 @@ def identify_all_data_sets(experimental_data, chosen_fun, x0, prob, optim_option
         variable_name = ["V3max", "K3fdp", "K3pep"]
         error_names = ['eps_{}'.format(i) for i in range(0, number_data)]
         cons_name = ['cons_{}'.format(j) for j in range(0, number_data)]
+    elif chosen_fun == 4:
+        ident_fun = v3_mwc_numerical_problem_l1_obj
+        arg = prob
+        variable_name = ["V3max", "K3fdp", "K3pep", "L3fdp"]
+        error_names = ["eps_1", "eps_2", "eps_3", "eps_4"]
+        cons_name = ["cons_1", "cons_2", "cons_3", "cons_4"]
     else:
         ident_fun = []
         arg = {}
