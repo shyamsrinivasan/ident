32cf21b29cfc82338a60736a23c3e8f2f05951ba
shyamsrinivasan <shyamsrinivasan@users.noreply.github.com>
2018-06-08 21:04:59 -0400

    comment all data generation portions of scripts

--- ident/python2/ss-ident/v3_var_kfdp_ck_script.py
+++ ident/python2/ss-ident/v3_var_kfdp_ck_script.py
@@ -24,11 +24,11 @@ arranged_data_df = retrieve_experimental_data_from_file(data_file_name=new_data_
 # print('Practical Identifiability Analysis of v3 with 2 parameters: V3max and K3fdp \n')
 storage_file_name = os.path.join(os.getcwd(), 'ident/ident_v3_k3fdp')
 # choose identifiability functions to test
-ident_fun_choice = [3]
-# test identifiability and store data to file
-from kotte_model import flux_ident_2_data_combination
-flux_ident_2_data_combination(arranged_data_df, flux_ids=[6], flux_choice=[1],
-                              ident_fun_choice=ident_fun_choice, file_name=storage_file_name)
+# ident_fun_choice = [3]
+# # test identifiability and store data to file
+# from kotte_model import flux_ident_2_data_combination
+# flux_ident_2_data_combination(arranged_data_df, flux_ids=[6], flux_choice=[1],
+#                               ident_fun_choice=ident_fun_choice, file_name=storage_file_name)
 
 # retrieve identifiability data from file and process information
 ident_index_label = ['sample_name', 'data_set_id']
@@ -45,15 +45,15 @@ validation_info = get_parameter_value(all_parameter_info, ident_df)
 default_parameter_values = true_parameter_values()
 
 # initial value used to generate experimental data
-from validate_estimation import validate_model
-import numpy as np
-y0 = np.array([5, 1, 1])
-# integrator options
-cvode_options = {'iter': 'Newton', 'discr': 'Adams', 'atol': 1e-10, 'rtol': 1e-10, 'time_points': 500,
-                 'display_progress': False, 'verbosity': 50}
-validate_model(y0, cvode_options, default_parameter_values, validation_info,
-               save_file_name=validation_file_name,
-               ss=1, dyn=0, noise=0, kinetics=2)
+# from validate_estimation import validate_model
+# import numpy as np
+# y0 = np.array([5, 1, 1])
+# # integrator options
+# cvode_options = {'iter': 'Newton', 'discr': 'Adams', 'atol': 1e-10, 'rtol': 1e-10, 'time_points': 500,
+#                  'display_progress': False, 'verbosity': 50}
+# validate_model(y0, cvode_options, default_parameter_values, validation_info,
+#                save_file_name=validation_file_name,
+#                ss=1, dyn=0, noise=0, kinetics=2)
 
 # retrieve validation info from file
 validate_index_labels = ['estimate_id', 'sample_name', 'data_set_id', 'experiment_id']
