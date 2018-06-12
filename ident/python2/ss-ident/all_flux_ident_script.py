22f6c518af20f907b06ae4c8291541d3cde48acc
shyamsrinivasan <shyamsweth@gmail.com>
2018-06-12 17:59:48 -0400

    create new df for each parameter

--- ident/python2/ss-ident/all_flux_ident_script.py
+++ ident/python2/ss-ident/all_flux_ident_script.py
@@ -8,6 +8,11 @@ v1_file_name = os.path.join(os.getcwd(), 'ident/ident_v1_kcat')
 ident_index_label = ['sample_name', 'data_set_id']
 # retrieve identifiability info from file
 ident_df = retrieve_experimental_data_from_file(v1_file_name, ident_index_label)
+# original set of experiments
+original_experiment_file = os.path.join(os.getcwd(), 'exp/experiments')
+exp_df = retrieve_experimental_data_from_file(data_file_name=original_experiment_file,
+                                              multi_index_label=['sample_name', 'experiment_id'])
+all_possible_experiments = exp_df.index.levels[1].tolist()
 # all_parameter_info = process_ident(ident_df, arranged_data_df)
 idx = pd.IndexSlice
 
@@ -16,10 +21,24 @@ number_experiments = len(all_parameter_names)
 exp_column_ids = ['experiment_{}_id'.format(i_experiment) for i_experiment in range(0, number_experiments)]
 for i_parameter, i_parameter_name in enumerate(all_parameter_names):
     # get all data sets identifying each parameter
+    all_experiment_pos_info = []
     identifying_df = ident_df[(ident_df["parameter_name"] == i_parameter_name) & (ident_df["identified"])]
     for i_experiment, i_experiment_pos in enumerate(exp_column_ids):
         exp_frequency = identifying_df[i_experiment_pos].value_counts()
+        available_experiments = exp_frequency.index.tolist()
+        missing_experiments = [i_exp for i_exp in all_possible_experiments if i_exp not in available_experiments]
+        missing_experiment_value = [0] * len(missing_experiments)
+        missing_series = pd.Series(missing_experiment_value, index=missing_experiments)
+        new_experiment_frequency = exp_frequency.append(missing_series).rename(exp_frequency.name)
+        all_experiment_pos_info.append(new_experiment_frequency)
         pass
+    all_keys = [j_series.name for j_series in all_experiment_pos_info]
+    new_df = pd.concat(all_experiment_pos_info,
+                       keys=all_keys,
+                       names=['experiment_pos', 'experiment_id']).to_frame(name='frequency').reset_index()
+    complete_df = new_df.assign(parameter_name=[i_parameter_name] * len(new_df.index),
+                                flux_name=identifying_df["flux_name"].unique().tolist() * len(new_df.index))
+
     pass
 
 print('Run Complete\n')
