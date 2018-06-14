d76d4d3452f43366332b90b3cfedbd4a0d546c3e
shyamsrinivasan <shyamsrinivasan@users.noreply.github.com>
2018-06-14 13:14:56 -0400

    new fun to collect all ident experiment info

--- ident/python2/ss-ident/process_ident_data.py
+++ ident/python2/ss-ident/process_ident_data.py
@@ -1,6 +1,8 @@
 import numpy as np
 import itertools as it
 import pandas as pd
+from create_experiment_data import retrieve_experimental_data_from_file
+from collections import defaultdict
 import matplotlib.pyplot as plt
 from names_strings import true_parameter_values
 from names_strings import ident_parameter_name
@@ -433,3 +435,60 @@ def extract_parameter_values_numerical(all_parameter_info):
                              "parameter_value": extracted_parameter_values,
                              "data_id": all_parameter_info["data_id"]}]
     return extracted_parameters
+
+
+def get_all_ident_experiments(ident_file_name, exp_file_name, max_number_experiments):
+    """get occurrence of each experiment (not experiment type) in identifying data
+    for each parameter in a given data set"""
+    # set up list of experiment positions and experiment ids
+    all_exp_column_ids = ['experiment_{}_id'.format(i_experiment) for i_experiment in range(0, max_number_experiments)]
+    # original set of experiments
+    exp_df = retrieve_experimental_data_from_file(data_file_name=exp_file_name,
+                                                  multi_index_label=['sample_name', 'experiment_id'])
+    all_possible_experiments = exp_df.index.levels[1].tolist()
+
+    # retrieve ident file
+    ident_index_label = ['sample_name', 'data_set_id']
+    ident_df = retrieve_experimental_data_from_file(ident_file_name, ident_index_label)
+
+
+
+    all_parameter_names = ident_df["parameter_name"].unique().tolist()
+    number_experiments = len(all_parameter_names)
+    exp_column_ids = ['experiment_{}_id'.format(i_experiment) for i_experiment in range(0, number_experiments)]
+    all_parameter_info = []
+    all_parameter_flux = []
+    empty_dict = {}
+    for i_parameter, i_parameter_name in enumerate(all_parameter_names):
+        # get all data sets identifying each parameter
+        all_experiment_info = defaultdict(list)
+        identifying_df = ident_df[(ident_df["parameter_name"] == i_parameter_name) & (ident_df["identified"])]
+        for i_experiment, i_experiment_pos in enumerate(exp_column_ids):
+            try:
+                exp_frequency = identifying_df[i_experiment_pos].value_counts()
+            except KeyError:
+                exp_frequency = pd.Series([0] * len(all_possible_experiments), index=all_possible_experiments)
+            available_experiments = exp_frequency.index.tolist()
+            missing_experiments = [i_exp for i_exp in all_possible_experiments if i_exp not in available_experiments]
+            missing_experiment_value = [0] * len(missing_experiments)
+            missing_series = pd.Series(missing_experiment_value, index=missing_experiments)
+            new_experiment_frequency = exp_frequency.append(missing_series).rename(exp_frequency.name)
+            experiment_ids = new_experiment_frequency.index.tolist()
+            experiment_frequency = [int(i_value) for i_value in new_experiment_frequency.values.tolist()]
+            experiment_pos = [i_experiment_pos] * len(experiment_ids)
+            # flux_names = identifying_df["flux_name"].unique().tolist()
+            new_dict = dict(zip(['experiment_id', 'frequency', 'experiment_pos_id'],
+                                [experiment_ids, experiment_frequency, experiment_pos]))
+            for key, value in it.chain(empty_dict.items(), new_dict.items()):
+                for i_value in value:
+                    all_experiment_info[key].append(i_value)
+        all_parameter_info.append(all_experiment_info)
+        all_parameter_flux.append(identifying_df["flux_name"].unique()[0])
+        pass
+    all_frequency = [i_parameter["frequency"] for i_parameter in all_parameter_info]
+    col_ind_tuple = zip(all_parameter_info[0]['experiment_pos_id'], all_parameter_info[0]['experiment_id'])
+    col_ind = pd.MultiIndex.from_tuples(col_ind_tuple, names=['experiment_pos_id', 'experiment_id'])
+    flux_tuple = zip(all_parameter_flux, all_parameter_names)
+    row_ind = pd.MultiIndex.from_tuples(flux_tuple, names=['flux_name', 'parameter_name'])
+    df = pd.DataFrame(all_frequency, index=row_ind, columns=col_ind)
+    return None
