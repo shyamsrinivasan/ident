8aff74efaefb926568ae4f35619a70306a0c118e
shyamsrinivasan <shyamsrinivasan@users.noreply.github.com>
2018-06-14 21:46:49 -0400

    get dataframe as output from call to get expeirment info

--- ident/python2/ss-ident/all_flux_ident_script.py
+++ ident/python2/ss-ident/all_flux_ident_script.py
@@ -1,6 +1,7 @@
 # from create_experiment_data import retrieve_experimental_data_from_file
 from process_exp_details import exp_design_info
-from process_exp_details import get_original_experiments
+import seaborn as sns
+import pandas as pd
 import os.path
 
 
@@ -14,7 +15,13 @@ v3_file_name = os.path.join(os.getcwd(), 'ident/ident_v3_root_1')
 v5_file_name = os.path.join(os.getcwd(), 'ident/ident_v5_root_2')
 file_name_list = [v1_file_name, v2_file_name, v3_file_name, v5_file_name]
 
-exp_design_info(list_of_files=file_name_list, original_experiment_file=original_experiment_file,
-                max_number_experiments=3)
+write_to_file_name = os.path.join(os.getcwd(), 'ident/ident_experiments')
+
+df = exp_design_info(list_of_files=file_name_list, original_experiment_file=original_experiment_file,
+                     write_to_file_name=write_to_file_name, max_number_experiments=3)
+
+# idx = pd.IndexSlice
+# new_df = df.loc[idx[:, :], idx['experiment_0_id', :]]
+# iris = sns.load_dataset('iris')
 
 print('Run Complete\n')
