144cd56bcc73c0c9fad92a530651abe2da907064
shyamsrinivasan <shyamsrinivasan@users.noreply.github.com>
2018-06-29 17:16:42 -0400

    change fun call to setup_parallel_ode

--- ident/python2/ss-ident/mpi_ode_test.py
+++ ident/python2/ss-ident/mpi_ode_test.py
@@ -25,11 +25,11 @@ def rhs_fun(t, y, p):
 
 if __name__ == "__main__":
 
-    parameters = {'ode_opts': {'iter': 'Newton', 'discr': 'Adams', 'atol': 1e-10, 'rtol': 1e-10,
-                               'time_points': 200, 'display_progress': True, 'verbosity': 30},
-                  'ode_sys_opts': np.array([.5, .02, .4, .004])}
+    parameters = np.array([.5, .02, .4, .004])
+    ode_opts = {'iter': 'Newton', 'discr': 'Adams', 'atol': 1e-10, 'rtol': 1e-10,
+                'time_points': 200, 'display_progress': True, 'verbosity': 30}
     y0 = [np.array([1, .0001]), np.array([2, .0001]), np.array([3, .0001]), np.array([4, .0001]),
           np.array([5, .0001]), np.array([.0001, 1]), np.array([.0001, 2]), np.array([.0001, 3]),
           np.array([.0001, 4]), np.array([.0001, 5]), np.array([10, 1]), np.array([1, 10])]
     # import pdb; pdb.set_trace()
-    sim_result = setup_parallel_ode(ode_rhs_fun=rhs_fun, parameters=parameters, y0=y0, t_final=200)
+    sim_result = setup_parallel_ode(ode_rhs_fun=rhs_fun, parameters=parameters, y0=y0, t_final=200, ode_opts=ode_opts)
