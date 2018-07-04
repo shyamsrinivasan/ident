31225820984681011d49c7e612f030067a7c64c0
shyamsrinivasan <shyamsweth@gmail.com>
2018-07-04 17:25:50 -0400

    add bp

--- ident/python2/ss-ident/run_sims.py
+++ ident/python2/ss-ident/run_sims.py
@@ -63,6 +63,7 @@ class ModelSim(object):
         for i_parameter_change in changed_parameter:
             new_parameter = deepcopy(default_parameter)
             parameter_name = i_parameter_change.keys()[0]
+            import pdb; pdb.set_trace()
             parameter_change = np.array(i_parameter_change.values()[0])
             if parameter_name == 'wt':
                 new_parameter['ac'] = new_parameter['ac'] * (1 + parameter_change)
