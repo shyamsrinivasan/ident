e1a35092ce18e0c0a2ca3037a14307bdd4a5ed86
shyamsrinivasan <shyamsweth@gmail.com>
2018-07-06 16:46:52 -0400

    remove unused bps

--- ident/python2/ss-ident/parallel_ident.py
+++ ident/python2/ss-ident/parallel_ident.py
@@ -46,7 +46,7 @@ class ParallelIdent(object):
         idx = pd.IndexSlice
         all_df_indices = exp_df.index.unique().tolist()
         # create tuple of indices
-        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
         for j_index, sample_data_set_id in enumerate(all_df_indices):
             j_exp_data_set = exp_df.loc[idx[sample_data_set_id],
                                         ['acetate', 'pep', 'fdp', 'E', 'v1', 'v2', 'v3', 'v5']].values.tolist()
@@ -133,6 +133,4 @@ def setup_parallel_ident(ident_fun, flux_id, flux_choice, exp_data):
     else:  # Any slave
         MySlave().run()
 
-    # rearrange results based on order of parameters/initial values
-    import pdb;pdb.set_trace()
     return sim_result
