8a45050caa91f566bff3d75ea4600d2be93de8c5
shyamsrinivasan <shyamsrinivasan@users.noreply.github.com>
2018-07-11 18:22:57 -0400

    setup to run as a stand alone script for identifying any flux

--- ident/python2/ss-ident/parallel_ident.py
+++ ident/python2/ss-ident/parallel_ident.py
@@ -2,10 +2,14 @@ from mpi4py import MPI
 from mpi_master_slave import Master, Slave
 from mpi_master_slave import WorkQueue
 from identifiability_analysis import run_flux_ident
+from run_ident import ModelIdent
+from names_strings import true_parameter_values
 import pandas as pd
+import os.path
+import kotte_model
 
 
-class ParallelIdent(object):
+class ParallelProcess(object):
     """Class running multiple ode simulations by assigning jobs to different slaves
     until all the work is done"""
 
@@ -16,62 +20,67 @@ class ParallelIdent(object):
         self.work_queue = WorkQueue(self.master)
 
     def terminate_slaves(self):
-        """
+        """,
         Call this to make all slaves exit their run loop
         """
         self.master.terminate_slaves()
 
-    def __add_next_task(self, exp_data, ident_fun, flux_id, flux_choice, sample_id, data_set_id):
+    def __add_next_task(self, task, **kwargs):
         """
-        create tasks and add it to the work queue
-        Every task has specific arguments
+        create tasks and add it to the work queue. Every task has specific arguments
         """
-        # set ode rhs function, initial condition and parameters
-        data = {'ident_fun': ident_fun, 'flux_id': flux_id, 'flux_choice': flux_choice,
-                'sample_id': sample_id, 'data_set_id': data_set_id, 'exp_data': exp_data}
-        # add data to work queue
+        if task == 'ident':
+            data = {'ident_fun': kwargs['ident_fun'], 'flux_id': kwargs['flux_id'],
+                    'flux_choice': kwargs['flux_choice'], 'sample_id': kwargs['sample_id'],
+                    'data_set_id': kwargs['data_set_id'], 'exp_data': kwargs['exp_data'], 'task': task}
+
         self.work_queue.add_work(data)
 
-    def run_i_value(self, exp_df, ident_fun, flux_id, flux_choice):
-        """
-        This is the core where I keep starting slaves
-        as long as there is work to do
-        """
+    def run_all(self, task, **kwargs):
+        """parallel application core"""
 
-        # let's prepare our work queue. This can be built at initialization time
-        # but it can also be added later as more work become available
-        #
-        idx = pd.IndexSlice
-        all_df_indices = exp_df.index.unique().tolist()
-        # create tuple of indices
-        # import pdb; pdb.set_trace()
-        for j_index, sample_data_set_id in enumerate(all_df_indices):
-            j_exp_data_set = exp_df.loc[idx[sample_data_set_id],
-                                        ['acetate', 'pep', 'fdp', 'E', 'v1', 'v2', 'v3', 'v5']].values.tolist()
-            flat_data_list = [i_element for i_data in j_exp_data_set for i_element in i_data]
-            self.__add_next_task(exp_data=flat_data_list, ident_fun=ident_fun, flux_id=flux_id, flux_choice=flux_choice,
-                                 sample_id=sample_data_set_id[0], data_set_id=sample_data_set_id[1])
+        if task == 'ident':
+            original_df = kwargs['exp_df']
+
+            # remove experiment_id as index
+            reset_df = original_df.reset_index('experiment_id')
+            ident_fun = kwargs['ident_fun']
+            flux_id = kwargs['flux_id']
+            flux_choice = kwargs['flux_choice']
+
+            idx = pd.IndexSlice
+            all_df_indices = reset_df.index.unique().tolist()
+            # create tuple of indices
+            # import pdb; pdb.set_trace()
+            for j_index, sample_data_set_id in enumerate(all_df_indices):
+                j_exp_data_set = reset_df.loc[idx[sample_data_set_id],
+                                              ['acetate', 'pep', 'fdp', 'E', 'v1', 'v2', 'v3', 'v5']].values.tolist()
+                flat_data_list = [i_element for i_data in j_exp_data_set for i_element in i_data]
+                self.__add_next_task(task=task, **{'exp_data': flat_data_list, 'ident_fun': ident_fun,
+                                                   'flux_id': flux_id, 'flux_choice': flux_choice, 'sample_id':
+                                                       sample_data_set_id[0], 'data_set_id': sample_data_set_id[1]})
 
         # Keeep starting slaves as long as there is work to do
-        ident_results = []
+        results = []
         while not self.work_queue.done():
             # give more work to do to each idle slave (if any)
             self.work_queue.do_work()
+
             # reclaim returned data from completed slaves
             for slave_return_data in self.work_queue.get_completed_work():
-                complete, ident_info, slave_flux_id, slave_flux_choice, sample_id, data_set_id = slave_return_data
-                i_slave_result = {'done': complete, 'flux_id': flux_id, 'flux_choice': flux_choice, 'sample_id': sample_id,
-                                  'data_set_id': data_set_id, 'ident_info': ident_info}
-                ident_results.append(i_slave_result)
+                task, output = slave_return_data
+                if task == 'ident':
+                    ident_info, slave_flux_id, slave_flux_choice, sample_id, data_set_id = output
+                    i_slave_result = {'flux_id': slave_flux_id, 'flux_choice': slave_flux_choice, 'sample_id': sample_id,
+                                      'data_set_id': data_set_id, 'ident_info': ident_info}
+                    results.append(i_slave_result)
 
-                if complete:
-                    print('Master: slave finished its task returning: %s)' % str(data_set_id))
-            # sleep some time
-            # time.sleep(0.01)
-        return ident_results
+                    print('Master: slave finished task %s returning: %s)' % (task, str(data_set_id)))
+
+        return results
 
 
-class MySlave(Slave):
+class ProcessSlave(Slave):
     """
     A slave process extends Slave class, overrides the 'do_work' method
     and calls 'Slave.run'. The Master will do the rest
@@ -83,52 +92,133 @@ class MySlave(Slave):
     """
 
     def __init__(self):
-        super(MySlave, self).__init__()
+        super(ProcessSlave, self).__init__()
 
     def do_work(self, data):
         """do work method overrides Slave.do_work() and defines work
         to be done by every slave"""
-        # boolean variable set to determine whether work on slave was completed on not
-
-        # define explicit assimulo problem
-        ident_fun = data['ident_fun']
-        exp_data = data['exp_data']
-        flux_id = data['flux_id']
-        flux_choice = data['flux_choice']
-        sample_id = data['sample_id']
-        data_set_id = data['data_set_id']
-        try:
-            ident_info, _, _ = run_flux_ident(ident_fun, exp_data, flux_id, flux_choice)
-            complete = True
-        except TypeError:
-            ident_info = []
-            complete = False
-
         rank = MPI.COMM_WORLD.Get_rank()
         name = MPI.Get_processor_name()
 
-        print('  Slave %s rank %d executing task (sample: %s, data set: %s)' % (name, rank, sample_id, data_set_id))
+        print('  Slave %s rank %d executing task %s' % (name, rank, data['task']))
 
-        if complete:
-            return complete, ident_info, flux_id, flux_choice, sample_id, data_set_id
-        else:
-            return complete, [], [], [], [], []
+        if data['task'] == 'ident':
+            ident_fun = data['ident_fun']
+            exp_data = data['exp_data']
+            flux_id = data['flux_id']
+            flux_choice = data['flux_choice']
+            sample_id = data['sample_id']
+            data_set_id = data['data_set_id']
+
+            print('  Slave %s rank %d executing ident for sample: %s, data set: %s' %
+                  (name, rank, sample_id, data_set_id))
+
+            ident_info, _, _ = run_flux_ident(ident_fun, exp_data, flux_id, flux_choice)
+
+            return data['task'], (ident_info, flux_id, flux_choice, sample_id, data_set_id)
 
 
-def setup_parallel_ident(ident_fun, flux_id, flux_choice, exp_data, name, rank, size):
-    # name = MPI.Get_processor_name()
-    # rank = MPI.COMM_WORLD.Get_rank()
-    # size = MPI.COMM_WORLD.Get_size()
+def v1_ident():
+    name = MPI.Get_processor_name()
+    rank = MPI.COMM_WORLD.Get_rank()
+    size = MPI.COMM_WORLD.Get_size()
 
-    sim_result = {}
-    print('I am  %s rank %d (total %d)' % (name, rank, size))
-    if rank == 0:  # Master
-        ident_job = ParallelIdent(slaves=range(1, size))
-        # import pdb;pdb.set_trace()
-        sim_result = ident_job.run_i_value(exp_data, ident_fun=ident_fun, flux_id=flux_id, flux_choice=flux_choice)
+    if rank == 0:  # master
+        v1_obj = ModelIdent(ident_fun=kotte_model.flux_1_kcat_ident,
+                            arranged_data_file_name=os.path.join(os.getcwd(), 'exp/exp_v1_2_experiments'),
+                            ident_data_file_name=os.path.join(os.getcwd(), 'ident/ident_v1_kcat'),
+                            **{'original_exp_file': os.path.join(os.getcwd(), 'exp/experiments'),
+                               'flux_id': 1, 'flux_choice': 2,
+                               'values_figure': os.path.join(os.getcwd(), 'results/v1_kcat_parameter_values.eps'),
+                               'ident_figure': os.path.join(os.getcwd(), 'results/v1_kcat_ident.eps'),
+                               'exp_figure': os.path.join(os.getcwd(), 'results/v1_kcat_exp.eps'),
+                               'figure_format': 'eps'})
+        exp_df = v1_obj.retrieve_df_from_file()
 
-        ident_job.terminate_slaves()
-    else:  # Any slave
-        MySlave().run()
+        job = ParallelProcess(slaves=range(1, size))
 
-    return sim_result
+        ident_result = job.run_all(task='ident', **{'exp_df': exp_df, 'ident_fun': v1_obj.ident_fun,
+                                                    'flux_id': v1_obj.flux_id, 'flux_choice': v1_obj.flux_choice})
+
+        job.terminate_slaves()
+
+        # collect, arrange and collate data
+        ordered_info = v1_obj.order_ident_data(ident_result)
+        v1_obj.create_dict_for_df(ordered_info)
+
+        # write all ident data to file
+        v1_obj.write_ident_info_file()
+
+        # process ident info for further analysis
+        v1_obj.process_ident()
+
+        # extract parameter va;ues for model validation
+        v1_obj.get_parameter_value()
+
+        default_parameter_values = true_parameter_values()
+        v1_obj.parameter_values_plot(default_parameter_values, violin=True, box=False, bins=1)
+
+        v1_obj.identifiability_plot()
+
+        v1_obj.exp_info_plot()
+    else:
+        ProcessSlave().run()
+
+    return v1_obj
+
+
+def v2_ident():
+    name = MPI.Get_processor_name()
+    rank = MPI.COMM_WORLD.Get_rank()
+    size = MPI.COMM_WORLD.Get_size()
+
+    if rank == 0:  # master
+        v2_obj = ModelIdent(ident_fun=kotte_model.flux_2_ident_expression,
+                            arranged_data_file_name=os.path.join(os.getcwd(), 'exp/exp_v2_2_experiments'),
+                            ident_data_file_name=os.path.join(os.getcwd(), 'ident/ident_v2'),
+                            **{'original_exp_file': os.path.join(os.getcwd(), 'exp/experiments'),
+                               'flux_id': 1, 'flux_choice': 1,
+                               'values_figure': os.path.join(os.getcwd(), 'results/v2_parameter_values.eps'),
+                               'ident_figure': os.path.join(os.getcwd(), 'results/v2_ident.eps'),
+                               'exp_figure': os.path.join(os.getcwd(), 'results/v2_exp.eps'),
+                               'figure_format': 'eps'})
+        exp_df = v2_obj.retrieve_df_from_file()
+
+        job = ParallelProcess(slaves=range(1, size))
+
+        ident_result = job.run_all(task='ident', **{'exp_df': exp_df, 'ident_fun': v2_obj.ident_fun,
+                                                    'flux_id': v2_obj.flux_id, 'flux_choice': v2_obj.flux_choice})
+
+        job.terminate_slaves()
+
+        # collect, arrange and collate data
+        ordered_info = v2_obj.order_ident_data(ident_result)
+        v2_obj.create_dict_for_df(ordered_info)
+
+        # write all ident data to file
+        v2_obj.write_ident_info_file()
+
+        # process ident info for further analysis
+        v2_obj.process_ident()
+
+        # extract parameter va;ues for model validation
+        v2_obj.get_parameter_value()
+
+        default_parameter_values = true_parameter_values()
+        v2_obj.parameter_values_plot(default_parameter_values, violin=True, box=False, bins=1)
+
+        v2_obj.identifiability_plot()
+
+        v2_obj.exp_info_plot()
+    else:
+        ProcessSlave().run()
+
+    return v2_obj
+
+
+if __name__ == '__main__':
+
+    v1_object = v1_ident()
+    v2_object = v2_ident()
+    import pdb;pdb.set_trace()
+    print('Done\n')
