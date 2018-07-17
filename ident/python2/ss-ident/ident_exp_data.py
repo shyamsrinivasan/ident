4b98a1be74b6a651e0cee71d709b2da135b1405d
shyamsrinivasan <shyamsrinivasan@users.noreply.github.com>
2018-07-17 08:35:39 -0400

    make script compatabile with MPI runs

--- ident/python2/ss-ident/ident_exp_data.py
+++ ident/python2/ss-ident/ident_exp_data.py
@@ -1,44 +1,50 @@
 from create_experiment_data import create_experiment_data
 from create_experiment_data import create_data_for_flux
+from mpi4py import MPI
 import os.path
 
 
-file_name = os.path.join(os.getcwd(), 'exp/experiments')
-create_experiment_data(file_name, noise=0, kinetics=2)
+name = MPI.Get_processor_name()
+rank = MPI.COMM_WORLD.Get_rank()
+size = MPI.COMM_WORLD.Get_size()
+if rank == 0:
+    print('Master: I am %s rank %s' % (name, rank))
+    file_name = os.path.join(os.getcwd(), 'exp/experiments')
+    create_experiment_data(file_name, noise=0, kinetics=2)
 
-file_name = os.path.join(os.getcwd(), 'exp/experiments_mwc')
-create_experiment_data(file_name, noise=0, kinetics=1)
+    file_name = os.path.join(os.getcwd(), 'exp/experiments_mwc')
+    create_experiment_data(file_name, noise=0, kinetics=1)
 
-# file_name = os.path.join(os.getcwd(), 'exp/experiments_noise_5_samples')
-# experiment_info_df = create_experiment_data(file_name, noise=1, kinetics=2, number_samples=5, noise_std=0.05)
+    # file_name = os.path.join(os.getcwd(), 'exp/experiments_noise_5_samples')
+    # experiment_info_df = create_experiment_data(file_name, noise=1, kinetics=2, number_samples=5, noise_std=0.05)
 
-# file_name = os.path.join(os.getcwd(), 'exp/experiments_noise_500_samples')
-# experiment_info_df = create_experiment_data(file_name, noise=1, kinetics=2, number_samples=500, noise_std=0.05)
+    # file_name = os.path.join(os.getcwd(), 'exp/experiments_noise_500_samples')
+    # experiment_info_df = create_experiment_data(file_name, noise=1, kinetics=2, number_samples=500, noise_std=0.05)
 
-# create data for identifiability analysis
-# v1 - kcat and vmax with ck model
-create_data_for_flux(flux_id='v1', noise=0, number_samples=1)
+    # create data for identifiability analysis
+    # v1 - kcat and vmax with ck model
+    create_data_for_flux(flux_id='v1', noise=0, number_samples=1)
 
-# v1 - kcat and vmax with mwc model
-create_data_for_flux(flux_id='v1', noise=0, number_samples=1, kinetics=1)
+    # v1 - kcat and vmax with mwc model
+    create_data_for_flux(flux_id='v1', noise=0, number_samples=1, kinetics=1)
 
-# v2 - ck model
-create_data_for_flux(flux_id='v2', noise=0, number_samples=1)
+    # v2 - ck model
+    create_data_for_flux(flux_id='v2', noise=0, number_samples=1)
 
-# v2 - mwc model
-create_data_for_flux(flux_id='v2', noise=0, number_samples=1, kinetics=1)
+    # v2 - mwc model
+    create_data_for_flux(flux_id='v2', noise=0, number_samples=1, kinetics=1)
 
-# v3 - ck model
-create_data_for_flux(flux_id='v3', noise=0, number_samples=1)
+    # v3 - ck model
+    create_data_for_flux(flux_id='v3', noise=0, number_samples=1)
 
-# v3 - mwc model
-create_data_for_flux(flux_id='v3', noise=0, number_samples=1, kinetics=1)
+    # v3 - mwc model
+    create_data_for_flux(flux_id='v3', noise=0, number_samples=1, kinetics=1)
 
-# v3 - ck model with 2 experiments for kfdp and kpep
-create_data_for_flux(flux_id='v3b', noise=0, number_samples=1)
+    # v3 - ck model with 2 experiments for kfdp and kpep
+    create_data_for_flux(flux_id='v3b', noise=0, number_samples=1)
 
-# v5 - ck model
-create_data_for_flux(flux_id='v5', noise=0, number_samples=1)
+    # v5 - ck model
+    create_data_for_flux(flux_id='v5', noise=0, number_samples=1)
 
-# v5 - mwc model
-create_data_for_flux(flux_id='v5', noise=0, number_samples=1, kinetics=1)
+    # v5 - mwc model
+    create_data_for_flux(flux_id='v5', noise=0, number_samples=1, kinetics=1)
