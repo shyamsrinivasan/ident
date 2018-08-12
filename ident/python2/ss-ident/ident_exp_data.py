fe95c8ec25d8203a4c0a80e53b328463d51b0c88
shyamsrinivasan <shyamsrinivasan@users.noreply.github.com>
2018-08-12 19:39:29 -0400

    keep only v1 and v3 active

--- ident/python2/ss-ident/ident_exp_data.py
+++ ident/python2/ss-ident/ident_exp_data.py
@@ -6,25 +6,25 @@ from create_experiment_data import create_data_for_flux
 create_data_for_flux(flux_id='v1', noise=0, number_samples=1)
 
 # v1 - kcat and vmax with mwc model
-create_data_for_flux(flux_id='v1', noise=0, number_samples=1, kinetics=1)
+# create_data_for_flux(flux_id='v1', noise=0, number_samples=1, kinetics=1)
 
 # v2 - ck model
-create_data_for_flux(flux_id='v2', noise=0, number_samples=1)
+# create_data_for_flux(flux_id='v2', noise=0, number_samples=1)
 
 # v2 - mwc model
-create_data_for_flux(flux_id='v2', noise=0, number_samples=1, kinetics=1)
+# create_data_for_flux(flux_id='v2', noise=0, number_samples=1, kinetics=1)
 
 # v3 - ck model
 create_data_for_flux(flux_id='v3', noise=0, number_samples=1)
 
 # v3 - mwc model
-create_data_for_flux(flux_id='v3', noise=0, number_samples=1, kinetics=1)
+# create_data_for_flux(flux_id='v3', noise=0, number_samples=1, kinetics=1)
 
 # v3 - ck model with 2 experiments for kfdp and kpep
-create_data_for_flux(flux_id='v3b', noise=0, number_samples=1)
+# create_data_for_flux(flux_id='v3b', noise=0, number_samples=1)
 
 # v5 - ck model
-create_data_for_flux(flux_id='v5', noise=0, number_samples=1)
+# create_data_for_flux(flux_id='v5', noise=0, number_samples=1)
 
 # v5 - mwc model
-create_data_for_flux(flux_id='v5', noise=0, number_samples=1, kinetics=1)
+# create_data_for_flux(flux_id='v5', noise=0, number_samples=1, kinetics=1)
